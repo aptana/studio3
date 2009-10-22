934ea0c9aa6fe6cb13b036fb2b9c814fba8f48e7
sgtcoolguy <cwilliams@aptana.com>
2009-10-22 11:43:16 -0400

    Enabled creating new repo in sharing wizard

--- plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/sharing/ExistingOrNewPage.java
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/sharing/ExistingOrNewPage.java
@@ -12,7 +12,10 @@ import java.io.File;
 import java.net.URI;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.wizard.WizardPage;
@@ -30,12 +33,15 @@ import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
 
+import com.aptana.git.core.model.GitExecutable;
 import com.aptana.git.core.model.GitRepository;
+import com.aptana.git.ui.GitUIPlugin;
 
 /**
  * Wizard page for connecting projects to Git repositories.
  */
-class ExistingOrNewPage extends WizardPage {
+class ExistingOrNewPage extends WizardPage
+{
 
 	private final SharingWizard myWizard;
 	private Button button;
@@ -43,32 +49,35 @@ class ExistingOrNewPage extends WizardPage {
 	private Text repositoryToCreate;
 	private IPath minumumPath;
 
-	ExistingOrNewPage(SharingWizard w) {
+	ExistingOrNewPage(SharingWizard w)
+	{
 		super(ExistingOrNewPage.class.getName());
 		setTitle("Configure Git repository");
 		setDescription("Select git repository location");
-//		setImageDescriptor(UIIcons.WIZBAN_CONNECT_REPO);
+		// setImageDescriptor(UIIcons.WIZBAN_CONNECT_REPO);
 		this.myWizard = w;
 	}
 
-	public void createControl(Composite parent) {
+	public void createControl(Composite parent)
+	{
 		Group g = new Group(parent, SWT.NONE);
-		g.setLayout(new GridLayout(3,false));
+		g.setLayout(new GridLayout(3, false));
 		g.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
-		tree = new Tree(g, SWT.BORDER|SWT.MULTI);
+		tree = new Tree(g, SWT.BORDER | SWT.MULTI);
 		tree.setHeaderVisible(true);
 		tree.setLayout(new GridLayout());
-		tree.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).span(3,1).create());
-		TreeColumn c1 = new TreeColumn(tree,SWT.NONE);
+		tree.setLayoutData(GridDataFactory.fillDefaults().grab(true, true).span(3, 1).create());
+		TreeColumn c1 = new TreeColumn(tree, SWT.NONE);
 		c1.setText("Project");
 		c1.setWidth(100);
-		TreeColumn c2 = new TreeColumn(tree,SWT.NONE);
+		TreeColumn c2 = new TreeColumn(tree, SWT.NONE);
 		c2.setText("Path");
 		c2.setWidth(400);
-		TreeColumn c3 = new TreeColumn(tree,SWT.NONE);
+		TreeColumn c3 = new TreeColumn(tree, SWT.NONE);
 		c3.setText("Repository");
 		c3.setWidth(200);
-		for (IProject project : myWizard.projects) {
+		for (IProject project : myWizard.projects)
+		{
 			TreeItem treeItem = new TreeItem(tree, SWT.NONE);
 			treeItem.setData(project);
 			treeItem.setText(0, project.getName());
@@ -83,64 +92,71 @@ class ExistingOrNewPage extends WizardPage {
 		button = new Button(g, SWT.PUSH);
 		button.setLayoutData(GridDataFactory.fillDefaults().create());
 		button.setText("&Create...");
-		button.addSelectionListener(new SelectionListener() {
-			public void widgetSelected(SelectionEvent e) {
-				File gitDir = new File(repositoryToCreate.getText(),".git"); //$NON-NLS-1$
-				// FIXME Implement a way to generate a brand new git repo!
-//				try {
-//					Repository repository = new Repository(gitDir);
-//					repository.create();
-//					for (IProject project : getProjects()) {
-//						// If we don't refresh the project directories right
-//						// now we won't later know that a .git directory
-//						// exists within it and we won't mark the .git
-//						// directory as a team-private member. Failure
-//						// to do so might allow someone to delete
-//						// the .git directory without us stopping them.
-//						// (Half lie, we should optimize so we do not
-//						// refresh when the .git is not within the project)
-//						//
-//						if (!gitDir.toString().contains("..")) //$NON-NLS-1$
-//							project.refreshLocal(IResource.DEPTH_ONE,
-//									new NullProgressMonitor());
-//					}
-//				} catch (IOException e1) {
-//					MessageDialog.openError(getShell(), "ErrorFailedToCreateRepository", gitDir.toString() + ":\n" + e1.getMessage()); //$NON-NLS-1$
-//					Activator.logError("Failed to create repository at " + gitDir, e1); //$NON-NLS-1$
-//				} catch (CoreException e2) {
-//					Activator.logError("ErrorFailedToRefreshRepository" + gitDir, e2);
-//				}
-//				for (TreeItem ti : tree.getSelection()) {
-//					ti.setText(2, gitDir.toString());
-//				}
+		button.addSelectionListener(new SelectionListener()
+		{
+			public void widgetSelected(SelectionEvent e)
+			{
+				File gitDir = new File(repositoryToCreate.getText(), GitRepository.GIT_DIR); //$NON-NLS-1$
+				try
+				{
+					GitExecutable.instance().runInBackground(gitDir.getParentFile().getAbsolutePath(), "init");
+					for (IProject project : getProjects())
+					{
+						// If we don't refresh the project directories right
+						// now we won't later know that a .git directory
+						// exists within it and we won't mark the .git
+						// directory as a team-private member. Failure
+						// to do so might allow someone to delete
+						// the .git directory without us stopping them.
+						// (Half lie, we should optimize so we do not
+						// refresh when the .git is not within the project)
+						//
+						if (!gitDir.toString().contains("..")) //$NON-NLS-1$
+							project.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor());
+					}
+				}
+				catch (CoreException e2)
+				{
+					GitUIPlugin.logError("ErrorFailedToRefreshRepository" + gitDir, e2);
+				}
+				for (TreeItem ti : tree.getSelection())
+				{
+					ti.setText(2, gitDir.toString());
+				}
 				updateCreateOptions();
 				getContainer().updateButtons();
 			}
-			public void widgetDefaultSelected(SelectionEvent e) {
+
+			public void widgetDefaultSelected(SelectionEvent e)
+			{
 			}
 		});
 		repositoryToCreate = new Text(g, SWT.SINGLE | SWT.BORDER);
-		repositoryToCreate.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).span(1,1).create());
-		repositoryToCreate.addListener(SWT.Modify, new Listener() {
-			public void handleEvent(Event e) {
+		repositoryToCreate.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).span(1, 1).create());
+		repositoryToCreate.addListener(SWT.Modify, new Listener()
+		{
+			public void handleEvent(Event e)
+			{
 				if (e.text == null)
 					return;
 				IPath fromOSString = Path.fromOSString(e.text);
-				button.setEnabled(minumumPath
-						.matchingFirstSegments(fromOSString) == fromOSString
-						.segmentCount());
+				button.setEnabled(minumumPath.matchingFirstSegments(fromOSString) == fromOSString.segmentCount());
 			}
 		});
-		Text l = new Text(g,SWT.NONE);
+		Text l = new Text(g, SWT.NONE);
 		l.setEnabled(false);
 		l.setEditable(false);
-		l.setText(File.separatorChar + ".git"); //$NON-NLS-1$
+		l.setText(File.separatorChar + GitRepository.GIT_DIR);
 		l.setLayoutData(GridDataFactory.fillDefaults().create());
-		tree.addSelectionListener(new SelectionListener() {
-			public void widgetSelected(SelectionEvent e) {
+		tree.addSelectionListener(new SelectionListener()
+		{
+			public void widgetSelected(SelectionEvent e)
+			{
 				updateCreateOptions();
 			}
-			public void widgetDefaultSelected(SelectionEvent e) {
+
+			public void widgetDefaultSelected(SelectionEvent e)
+			{
 				// Empty
 			}
 		});
@@ -148,10 +164,12 @@ class ExistingOrNewPage extends WizardPage {
 		setControl(g);
 	}
 
-	private void updateCreateOptions() {
+	private void updateCreateOptions()
+	{
 		minumumPath = null;
 		IPath p = null;
-		for (TreeItem ti : tree.getSelection()) {
+		for (TreeItem ti : tree.getSelection())
+		{
 			String path = ti.getText(2);
 			if (!path.equals("")) { //$NON-NLS-1$
 				p = null;
@@ -161,15 +179,19 @@ class ExistingOrNewPage extends WizardPage {
 			IPath thisPath = Path.fromOSString(gitDirParentCandidate);
 			if (p == null)
 				p = thisPath;
-			else {
+			else
+			{
 				int n = p.matchingFirstSegments(thisPath);
 				p = p.removeLastSegments(p.segmentCount() - n);
 			}
 		}
 		minumumPath = p;
-		if (p != null) {
+		if (p != null)
+		{
 			repositoryToCreate.setText(p.toOSString());
-		} else {
+		}
+		else
+		{
 			repositoryToCreate.setText(""); //$NON-NLS-1$
 		}
 		button.setEnabled(p != null);
@@ -178,7 +200,8 @@ class ExistingOrNewPage extends WizardPage {
 	}
 
 	@Override
-	public boolean isPageComplete() {
+	public boolean isPageComplete()
+	{
 		if (tree.getItemCount() == 1)
 		{
 			TreeItem ti = tree.getItem(0);
@@ -202,7 +225,8 @@ class ExistingOrNewPage extends WizardPage {
 		return true;
 	}
 
-	public IProject[] getProjects() {
+	public IProject[] getProjects()
+	{
 		if (tree.getItemCount() == 1)
 		{
 			TreeItem ti = tree.getItem(0);
@@ -210,7 +234,7 @@ class ExistingOrNewPage extends WizardPage {
 		}
 		IProject[] ret = new IProject[tree.getSelection().length];
 		for (int i = 0; i < ret.length; ++i)
-			ret[i] = (IProject)tree.getSelection()[i].getData();
+			ret[i] = (IProject) tree.getSelection()[i].getData();
 		return ret;
 	}
 }
