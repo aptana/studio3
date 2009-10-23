486d2c28db26068b1dd4fe18dccd264aac44a40f
Kevin Lindsey <klindsey@aptana.com>
2009-10-23 18:16:00 -0500

    Move utilities into separate class to make it more obvious what needs to be moved once we've integrate with core

--- plugins/com.aptana.terminal/src/com/aptana/terminal/Activator.java
+++ plugins/com.aptana.terminal/src/com/aptana/terminal/Activator.java
@@ -5,15 +5,9 @@ import java.util.Map;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-import com.aptana.terminal.editor.TerminalEditorInput;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -97,59 +91,5 @@ public class Activator extends AbstractUIPlugin
 		}
 
 		return images.get(path);
-	}
-
-	// TODO: Use com.aptana.ide.core.ui.CoreUIUtils.openEditor once this has been merged to master
-	// See more comments in the method below
-	
-	/**
-	 * Opens a specific editor.
-	 * 
-	 * @param editorId
-	 *            the editor ID
-	 * @param activate
-	 *            true if the editor should be activated, false otherwise
-	 * @return
-	 */
-	public static IEditorPart openEditor(String editorId, boolean activate)
-	{
-		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		
-		if (window != null)
-		{
-			IWorkbenchPage page = window.getActivePage();
-			
-			try
-			{
-				// TODO: changed MATCH pattern from MATCH_ID to MATCH_INPUT, so we'll probably need our own version
-				// of this method
-				return page.openEditor(new TerminalEditorInput(), editorId, activate, IWorkbenchPage.MATCH_INPUT);
-			}
-			catch (PartInitException e)
-			{
-				e.printStackTrace();
-			}
-		}
-		
-		return null;
-	}
-	
-	public static String encodeString(String text)
-	{
-		StringBuffer buffer = new StringBuffer();
-		
-		for (char c : text.toCharArray())
-		{
-			if (0 <= c && c < 32 || 128 <= c)
-			{
-				buffer.append("\\x").append(Integer.toString(c, 16));
-			}
-			else
-			{
-				buffer.append(c);
-			}
-		}
-		
-		return buffer.toString();
 	}
 }
