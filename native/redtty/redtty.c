8bee4e7d3b67400b6d6edea0b78a0053bf8db7fe
Kevin Lindsey <klindsey@aptana.com>
2009-10-26 16:54:32 -0500

    Remove all test code.
    Add ability to recognize a window resize escape sequence and apply this to the ptty.

--- native/redtty/redtty.c
+++ native/redtty/redtty.c
@@ -2,11 +2,8 @@
 #include <stdlib.h> 
 #include <unistd.h> 
 #include <errno.h> 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <string.h>
+#include <sys/ioctl.h>
 
 #ifdef LINUX 
 #include <pty.h> 
@@ -17,6 +14,8 @@
 #include <util.h> 
 #endif
 
+#define SET_SIZE "\033[8;"
+
 int 
 main (void) 
 {
@@ -26,38 +25,9 @@ main (void)
 		
 	/* Descriptor set for select */
 	fd_set descriptor_set;
-
-#ifdef USE_CONTROL_SOCKET
-#define PORT		8182
-#define GETDIM		"GETDIM\n"
-	int sd;
-	struct sockaddr_in sa;
-	struct hostent *host;
-	
-	if ((host = gethostbyname("localhost")) == 0) {
-		perror("gethostbyname(localhost)");
-		exit(EXIT_FAILURE);
-	}
-	
-	memset(&sa, 0, sizeof(sa));
-	sa.sin_family = AF_INET;
-	sa.sin_addr.s_addr = ((struct in_addr *)(host->h_addr))->s_addr;
-	sa.sin_port = htons(PORT);
-	
-	if ((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket");
-		exit(EXIT_FAILURE);
-	}
-	
-	if (connect(sd, (struct sockaddr *)&sa, sizeof(sa)) == -1) {
-		perror("connect");
-		exit(EXIT_FAILURE);
-	}
-#endif
 	
 	int pty;
 	
-	//int stderr_fd = dup(STDERR_FILENO);
 	switch (forkpty(&pty,  /* pseudo-terminal master end descriptor */ 
 					 NULL,  /* This can be a char[] buffer used to get... */ 
 							/* ...the device name of the pseudo-terminal */ 
@@ -70,9 +40,6 @@ main (void)
 			exit (EXIT_FAILURE); 
 			
 		case 0: /* This is the child process */ 
-			//write(sd, buffer, sprintf(buffer, "%i\n", getpid()));
-			
-			//dup2(stderr_fd, 2);
 			execl("/bin/bash", "-bash", "-li", NULL); 
 			
 			perror("exec()"); /* Since exec* never return */ 
@@ -84,21 +51,12 @@ main (void)
 				FD_ZERO (&descriptor_set);
 				FD_SET (STDIN_FILENO, &descriptor_set); 
 				FD_SET (pty, &descriptor_set);
-#ifdef USE_CONTROL_SOCKET
-				FD_SET (sd, &descriptor_set); 
-#endif
 				
 				if (select (FD_SETSIZE, &descriptor_set, NULL, NULL, NULL) < 0) 
 				{ 
 					perror ("select()"); 
 					exit (EXIT_FAILURE); 
 				}
-				
-				// try forcing a resize
-				//struct winsize size;
-				//size.ws_row = 10;
-				//size.ws_col = 40;
-				//ioctl(pty, TIOCSWINSZ, &size);
 				
 				/* User typed something at STDIN */
 				if (FD_ISSET (STDIN_FILENO, &descriptor_set)) 
@@ -118,7 +76,27 @@ main (void)
 							break;
 							
 						default:
-							write(pty, &buffer, read_count);
+							buffer[read_count] = '\0';
+							
+							if (!strncmp(buffer, SET_SIZE, strlen(SET_SIZE)))
+							{
+								char *start = buffer + strlen(SET_SIZE);
+								char *semi = strstr(start, ";");
+								char *t = strstr(semi + 1, "t");
+								int height = strtol(start, &semi, 0);
+								int width = strtol(semi + 1, &t, 0);
+								
+								//fprintf(stderr, "%dx%d", width, height);
+								
+								struct winsize size;
+								size.ws_row = height;
+								size.ws_col = width;
+								ioctl(pty, TIOCSWINSZ, &size);
+							}
+							else
+							{
+								write(pty, &buffer, read_count);
+							}
 							break;
 					}
 				} 
@@ -145,33 +123,6 @@ main (void)
 							break;
 					}
 				}
-				
-#ifdef USE_CONTROL_SOCKET
-				/* Input from socket */
-				if (FD_ISSET (sd, &descriptor_set)) 
-				{
-					read_count = read(sd, &buffer, sizeof(buffer)-1);
-					switch (read_count)
-					{
-						case -1:
-							fprintf (stderr, "Socket disconnected.\n"); 
-							exit (EXIT_FAILURE); 
-							break;
-							
-						case 0:
-							fprintf (stderr, "Socket done\n"); 
-							exit (EXIT_SUCCESS);
-							break;
-							
-						default:
-							buffer[read_count] = '\0';
-							if (!strncmp(buffer, GETDIM, strlen(GETDIM))) {
-								write(sd, buffer, sprintf(buffer, "%i,%i\n", 10, 10));
-							}
-							break;
-					}
-				}
-#endif
 			}
     } 
 	
