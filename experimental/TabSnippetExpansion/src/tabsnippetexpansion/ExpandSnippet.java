e6f0e8522c4c0f2cb2c8da8932ed73dab26a8622
Kevin Lindsey <klindsey@aptana.com>
2009-11-03 12:09:21 -0600

    Applied standard formatting.

--- experimental/TabSnippetExpansion/src/tabsnippetexpansion/ExpandSnippet.java
+++ experimental/TabSnippetExpansion/src/tabsnippetexpansion/ExpandSnippet.java
@@ -1,4 +1,5 @@
 package tabsnippetexpansion;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -23,51 +24,66 @@ import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 import org.eclipse.ui.texteditor.ITextEditor;
 
+public class ExpandSnippet extends AbstractHandler
+{
 
-public class ExpandSnippet extends AbstractHandler {
-	
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
 		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
 		IWorkbenchPage page = activeWorkbenchWindow.getActivePage();
-		if (page == null) {
+		if (page == null)
+		{
 			return null;
 		}
 		IEditorPart editor = page.getActiveEditor();
-		if (editor == null) {
+		if (editor == null)
+		{
 			return null;
 		}
-		
+
 		IWorkbenchPartSite site = editor.getSite();
-		if (editor instanceof ITextEditor) {
+		if (editor instanceof ITextEditor)
+		{
 			ITextEditor abstractTextEditor = (ITextEditor) editor;
-			if (abstractTextEditor.isEditable()) {
-				final ITextOperationTarget textOperationTarget = (ITextOperationTarget) abstractTextEditor.getAdapter(ITextOperationTarget.class);
+			if (abstractTextEditor.isEditable())
+			{
+				final ITextOperationTarget textOperationTarget = (ITextOperationTarget) abstractTextEditor
+						.getAdapter(ITextOperationTarget.class);
 				Object adapter = (Control) abstractTextEditor.getAdapter(Control.class);
-				if (adapter instanceof Control) {
+				if (adapter instanceof Control)
+				{
 					Control control = (Control) adapter;
-					if (control instanceof StyledText) {
+					if (control instanceof StyledText)
+					{
 						StyledText styledText = (StyledText) control;
-						if (styledText.getSelectionCount() > 0) {
+						if (styledText.getSelectionCount() > 0)
+						{
 							return null;
 						}
-						
+
 						int caretOffset = styledText.getCaretOffset();
 						int lineAtOffset = styledText.getLineAtOffset(caretOffset);
 						int offsetAtLine = styledText.getOffsetAtLine(lineAtOffset);
-						if (offsetAtLine == caretOffset) {
+						if (offsetAtLine == caretOffset)
+						{
 							return null;
 						}
-						
-						if (Character.isJavaIdentifierStart(styledText.getLine(lineAtOffset).charAt(caretOffset - offsetAtLine - 1))) {
-							// Get the word at offset
-							if (textOperationTarget != null) {
-								Display display= null;
-								Shell shell= site.getShell();
-								if (shell != null && !shell.isDisposed())
-									display= shell.getDisplay();
 
-								BusyIndicator.showWhile(display, new Runnable() {
-									public void run() {
+						if (Character.isJavaIdentifierStart(styledText.getLine(lineAtOffset).charAt(
+								caretOffset - offsetAtLine - 1)))
+						{
+							// Get the word at offset
+							if (textOperationTarget != null)
+							{
+								Display display = null;
+								Shell shell = site.getShell();
+								if (shell != null && !shell.isDisposed())
+									display = shell.getDisplay();
+
+								BusyIndicator.showWhile(display, new Runnable()
+								{
+									public void run()
+									{
 										textOperationTarget.doOperation(ISourceViewer.CONTENTASSIST_PROPOSALS);
 									}
 								});
@@ -79,44 +95,55 @@ public class ExpandSnippet extends AbstractHandler {
 		}
 		return null;
 	}
-	
+
 	private boolean enabled = true;
-	
+
 	@Override
-	public void setEnabled(Object object) {
-		if (object instanceof IEvaluationContext) {
+	public void setEnabled(Object object)
+	{
+		if (object instanceof IEvaluationContext)
+		{
 			IEvaluationContext evaluationContext = (IEvaluationContext) object;
 			IEditorPart editor = (IEditorPart) evaluationContext.getVariable(ISources.ACTIVE_EDITOR_NAME);
-			if (editor instanceof AbstractTextEditor) {
+			if (editor instanceof AbstractTextEditor)
+			{
 				AbstractTextEditor abstractTextEditor = (AbstractTextEditor) editor;
-				if (!abstractTextEditor.isEditable()) {
+				if (!abstractTextEditor.isEditable())
+				{
 					enabled = false;
 					return;
 				}
 				Object adapter = (Control) abstractTextEditor.getAdapter(Control.class);
-				if (adapter instanceof Control) {
+				if (adapter instanceof Control)
+				{
 					Control control = (Control) adapter;
-					if (control instanceof StyledText) {
+					if (control instanceof StyledText)
+					{
 						StyledText styledText = (StyledText) control;
-						if (styledText.getSelectionCount() > 0) {
+						if (styledText.getSelectionCount() > 0)
+						{
 							enabled = false;
 							return;
 						}
 						int caretOffset = styledText.getCaretOffset();
 						int lineAtOffset = styledText.getLineAtOffset(caretOffset);
 						int offsetAtLine = styledText.getOffsetAtLine(lineAtOffset);
-						if (offsetAtLine == caretOffset) {
+						if (offsetAtLine == caretOffset)
+						{
 							enabled = false;
 							return;
 						}
 					}
 				}
-				if (editor instanceof ITextEditor) {
+				if (editor instanceof ITextEditor)
+				{
 					ITextEditor textEditor = (ITextEditor) editor;
-					IDocumentProvider provider= textEditor.getDocumentProvider();
-					if (provider != null) {
-						IDocument document= provider.getDocument(editor.getEditorInput());
-						if (document != null) {
+					IDocumentProvider provider = textEditor.getDocumentProvider();
+					if (provider != null)
+					{
+						IDocument document = provider.getDocument(editor.getEditorInput());
+						if (document != null)
+						{
 							enabled = (!LinkedModeModel.hasInstalledModel(document));
 							return;
 						}
@@ -125,16 +152,19 @@ public class ExpandSnippet extends AbstractHandler {
 			}
 		}
 	}
-	
-	public boolean isEnabled() {
+
+	public boolean isEnabled()
+	{
 		return enabled;
 	}
 
-	public boolean isHandled() {
+	public boolean isHandled()
+	{
 		return isEnabled();
 	}
 
-	public void removeHandlerListener(IHandlerListener handlerListener) {
+	public void removeHandlerListener(IHandlerListener handlerListener)
+	{
 	}
 
 }
