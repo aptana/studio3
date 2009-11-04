5906ddd8493f3ded073d3ce186dc3ff86f056f5a
Kevin Lindsey <klindsey@aptana.com>
2009-11-04 15:40:10 -0600

    Basic Ruby scripts to support bundles, snippets, and commands.

---
+++ plugins/com.aptana.scripting/bundles/radrails/command.rb
@@ -0,0 +1,81 @@
+require "java"
+
+module RadRails
+  
+  class Command
+    def initialize(name)
+      @jobj = com.aptana.scripting.model.Command.new($fullpath)
+      @jobj.display_name = name;
+    end
+    
+    def display_name
+      @jobj.display_name
+    end
+    
+    def display_name=(display_name)
+      @jobj.display_name = display_name
+    end
+    
+    def invoke
+      @jobj.invoke
+    end
+    
+    def invoke=(invoke)
+      @jobj.invoke = invoke
+    end
+    
+    def key_binding
+      @jobj.key_binding
+    end
+    
+    def key_binding=(key_binding)
+      as_strings = key_binding.map do |x|
+        x.to_s
+      end
+      @jobj.key_binding = as_strings.join(" ")
+    end
+    
+    def output
+      @jobj.output
+    end
+    
+    def output=(output)
+      @jobj.output = output.to_s
+    end
+    
+    def path
+      @jobj.path
+    end
+    
+    def scope
+      @jobj.scope
+    end
+    
+    def scope=(scope)
+      @jobj.scope = scope
+    end
+    
+    def to_s
+      <<-EOS
+      command(
+        path:   #{path}
+        name:   #{display_name}
+        invoke: #{invoke}
+        keys:   #{key_binding}
+        output: #{output}
+        scope:  #{scope}
+      )
+      EOS
+    end
+    
+    class << self
+      def define_command(name, &block)
+        command = Command.new(name)
+        block.call(command) if block_given?
+        
+        # add command to bundle
+      end
+    end
+  end
+  
+end
