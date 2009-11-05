f409792911c04348e150efeb5db6a1c072085ee2
Kevin Lindsey <klindsey@aptana.com>
2009-11-05 15:58:09 -0600

    Merge BundleLoader into BundleManager since these are really aspects of the same thing. Update references to old BundleLoader to point to BundleManager. Initial support for moving bundles. This does not include support for the underlying commands and snippets, however.

--- plugins/com.aptana.scripting/src/com/aptana/scripting/ResourceDeltaVisitor.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/ResourceDeltaVisitor.java
@@ -1,19 +1,23 @@
 package com.aptana.scripting;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
+
+import com.aptana.scripting.model.Bundle;
+import com.aptana.scripting.model.BundleManager;
 
 /**
  * ResourceDeltaVisitor
  */
 public class ResourceDeltaVisitor implements IResourceDeltaVisitor
 {
-	private static final Pattern FILE_PATTERN = Pattern.compile("/.+?/bundles/.+?/bundle.rb"); //$NON-NLS-1$
+	private static final Pattern BUNDLE_PATTERN = Pattern.compile("/.+?/bundles/.+?/bundle\\.rb$"); //$NON-NLS-1$
+	private static final Pattern FILE_PATTERN = Pattern.compile("/.+?/bundles/.+?/(?:commands|snippets)/[^/]+\\.rb$"); //$NON-NLS-1$
 
 	/*
 	 * (non-Javadoc)
@@ -21,63 +25,147 @@ public class ResourceDeltaVisitor implements IResourceDeltaVisitor
 	 */
 	public boolean visit(IResourceDelta delta) throws CoreException
 	{
-		String fullPath = delta.getFullPath().toString();
+		String fullPath = delta.getFullPath().toString().toLowerCase();
+		boolean visitChildren = true;
 		
-		System.out.println("visiting " + fullPath);
-		
-		Matcher matcher = FILE_PATTERN.matcher(fullPath.toLowerCase());
-		boolean result = true;
+		System.out.println("Visiting " + delta.getFullPath());
 
-		if (matcher.matches())
+		if (BUNDLE_PATTERN.matcher(fullPath).matches())
 		{
-			IFile file = (IFile) delta.getResource();
+			System.out.println("process bundle");
+			
+			visitChildren = this.processBundle(delta);
+			
+			BundleManager.getInstance().showBundles();
+		}
+		else if (FILE_PATTERN.matcher(fullPath).matches())
+		{
+			System.out.println("process command or snippet");
+			
+			visitChildren = this.processFile(delta);
+		}
 
-			if (file != null && file.getLocation() != null)
+		return visitChildren;
+	}
+
+	/**
+	 * processBundle
+	 * 
+	 * @param delta
+	 */
+	private boolean processBundle(IResourceDelta delta)
+	{
+		IFile file = (IFile) delta.getResource();
+		boolean visitChildren = true;
+
+		if (file != null && file.getLocation() != null)
+		{
+			IFolder bundleFolder = (IFolder) file.getParent();
+			String bundleFolderPath = bundleFolder.getLocation().toPortableString();
+
+			switch (delta.getKind())
 			{
-				//IFolder folder = (IFolder) file.getParent();
-				fullPath = file.getLocation().toPortableString();
-				System.out.println("processing " + fullPath);
+				case IResourceDelta.ADDED:
+					System.out.println("add bundle " + bundleFolderPath);
+					BundleManager.getInstance().processBundle(bundleFolder, false);
+					break;
 
-				switch (delta.getKind())
-				{
-					case IResourceDelta.ADDED:
-						// process new or changed script
-						System.out.println("added");
-						break;
+				case IResourceDelta.REMOVED:
+					System.out.println("remove bundle " + bundleFolderPath);
+					BundleManager.getInstance().removeBundle(bundleFolderPath);
+					break;
+
+				case IResourceDelta.CHANGED:
+					if ((delta.getFlags() & IResourceDelta.MOVED_FROM) != 0)
+					{
+						String oldPath = delta.getMovedFromPath().toPortableString();
 						
-					case IResourceDelta.REMOVED:
-						// process removed script
-						System.out.println("removed");
-						break;
+						BundleManager.getInstance().moveBundle(oldPath, bundleFolderPath);
 						
-					case IResourceDelta.CHANGED:
-						if ((delta.getFlags() & IResourceDelta.MOVED_FROM) != 0)
-						{
-							// remove script
-							// process new script
-							System.out.println("moved from");
-						}
-						if ((delta.getFlags() & IResourceDelta.MOVED_TO) != 0)
-						{
-							// remove script
-							// process new script
-							System.out.println("moved to");
-						}
-						if ((delta.getFlags() & IResourceDelta.REPLACED) != 0)
-						{
-							// process new script
-							System.out.println("replaced");
-						}
-						if ((delta.getFlags() & IResourceDelta.CONTENT) != 0)
-						{
-							// process new script
-							System.out.println("new content");
-						}
-						break;
-				}
+						System.out.println("Moved from " + oldPath + " to " + bundleFolderPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.MOVED_TO) != 0)
+					{
+						String newPath = delta.getMovedToPath().toString();
+						
+						BundleManager.getInstance().moveBundle(bundleFolderPath, newPath);
+						
+						System.out.println("Moved from " + bundleFolderPath + " to " + newPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.REPLACED) != 0)
+					{
+						System.out.println("Replacing " + bundleFolderPath);
+						BundleManager.getInstance().removeBundle(bundleFolderPath);
+						BundleManager.getInstance().processBundle(bundleFolder, false);
+					}
+					if ((delta.getFlags() & IResourceDelta.CONTENT) != 0)
+					{
+						System.out.println("Updating content " + bundleFolderPath);
+						BundleManager.getInstance().removeBundle(bundleFolderPath);
+						BundleManager.getInstance().processBundle(bundleFolder, false);
+					}
+					break;
 			}
 		}
 
-		return result;
+		return visitChildren;
+	}
+
+	/**
+	 * processFile
+	 * 
+	 * @param delta
+	 */
+	private boolean processFile(IResourceDelta delta)
+	{
+		IFile file = (IFile) delta.getResource();
+		boolean visitChildren = true;
+
+		if (file != null && file.getLocation() != null)
+		{
+			IFolder bundleFolder = (IFolder) file.getParent().getParent();
+			String bundleFolderPath = bundleFolder.getLocation().toPortableString();
+			String fullPath = file.getLocation().toPortableString();
+			Bundle bundle;
+
+			switch (delta.getKind())
+			{
+				case IResourceDelta.ADDED:
+					BundleManager.getInstance().processFile(file);
+					break;
+
+				case IResourceDelta.REMOVED:
+					// process removed script
+					System.out.println("removed " + fullPath);
+					break;
+
+				case IResourceDelta.CHANGED:
+					if ((delta.getFlags() & IResourceDelta.MOVED_FROM) != 0)
+					{
+						// remove script
+						// process new script
+						System.out.println("moved from " + fullPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.MOVED_TO) != 0)
+					{
+						// remove script
+						// process new script
+						System.out.println("moved to " + fullPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.REPLACED) != 0)
+					{
+						// process new script
+						System.out.println("replaced " + fullPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.CONTENT) != 0)
+					{
+						// process new script
+						System.out.println("new content " + fullPath);
+					}
+					break;
+			}
+		}
+
+		return visitChildren;
 	}
 }
--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleManager.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleManager.java
@@ -1,26 +1,35 @@
 package com.aptana.scripting.model;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.jruby.anno.JRubyMethod;
+
+import com.aptana.scripting.Activator;
+import com.aptana.scripting.ScriptingEngine;
 
 public class BundleManager
 {
+	private static final String BUNDLES_FOLDER_NAME = "bundles"; //$NON-NLS-1$
+	private static final String SNIPPETS_FOLDER_NAME = "snippets"; //$NON-NLS-1$
+	private static final String COMMANDS_FOLDER_NAME = "commands"; //$NON-NLS-1$
 	private static BundleManager INSTANCE;
-	
-	private List<Bundle> _bundles;
-	private Map<String,Bundle> _bundelsByPath;
-	
-	/**
-	 * BundleManager
-	 */
-	private BundleManager()
-	{
-	}
 	
 	/**
 	 * getInstance
@@ -36,6 +45,16 @@ public class BundleManager
 		}
 		
 		return INSTANCE;
+	}
+	private List<Bundle> _bundles;
+	
+	private Map<String,Bundle> _bundlesByPath;
+	
+	/**
+	 * BundleManager
+	 */
+	private BundleManager()
+	{
 	}
 	
 	/**
@@ -55,13 +74,51 @@ public class BundleManager
 			
 			this._bundles.add(bundle);
 			
-			if (this._bundelsByPath == null)
+			if (this._bundlesByPath == null)
 			{
-				this._bundelsByPath = new HashMap<String, Bundle>();
+				this._bundlesByPath = new HashMap<String, Bundle>();
 			}
 			
-			this._bundelsByPath.put(bundle.getPath(), bundle);
+			this._bundlesByPath.put(bundle.getPath(), bundle);
 		}
+	}
+	
+	/**
+	 * getBuildtinsLoadPath
+	 * @return
+	 */
+	private String getBuiltinsLoadPath()
+	{
+		URL url = FileLocator.find(Activator.getDefault().getBundle(), new Path(BUNDLES_FOLDER_NAME), null);
+		String result = null;
+
+		try
+		{
+			URL fileURL = FileLocator.toFileURL(url);
+			File file = new File(fileURL.toURI());
+			
+			result = file.getAbsolutePath();
+		}
+		catch (IOException e)
+		{
+			String message = MessageFormat.format(
+				"Error locating built-ins directory",
+				new Object[] { url.toString() }
+			);
+			
+			Activator.logError(message, e);
+		}
+		catch (URISyntaxException e)
+		{
+			String message = MessageFormat.format(
+				"Malformed built-ins directory URI",
+				new Object[] { url.toString() }
+			);
+			
+			Activator.logError(message, e);
+		}
+		
+		return result;
 	}
 	
 	/**
@@ -75,14 +132,14 @@ public class BundleManager
 	{
 		Bundle result = null;
 		
-		if (this._bundelsByPath != null)
+		if (this._bundlesByPath != null)
 		{
-			result = this._bundelsByPath.get(path);
+			result = this._bundlesByPath.get(path);
 		}
 		
 		return result;
 	}
-	
+
 	/**
 	 * getCommandsFromScope
 	 * 
@@ -102,7 +159,28 @@ public class BundleManager
 		
 		return result.toArray(new Command[result.size()]);
 	}
-
+	
+	/**
+	 * getLoadPaths
+	 * 
+	 * @param folder
+	 * @return
+	 */
+	private List<String> getLoadPaths(IResource resource)
+	{
+		IFolder folder = (resource instanceof IFolder) ? (IFolder) resource : (IFolder) resource.getParent();
+		List<String> loadPaths = new ArrayList<String>();
+		IFolder bundleFolder = (IFolder) folder.getParent();
+		IFolder bundlesFolder = (IFolder) bundleFolder.getParent();
+		
+		loadPaths.add(this.getBuiltinsLoadPath());
+		loadPaths.add(bundlesFolder.getLocation().toPortableString());
+		loadPaths.add(bundleFolder.getLocation().toPortableString());
+		loadPaths.add(".");
+		
+		return loadPaths;
+	}
+	
 	/**
 	 * getSnippetsFromScope
 	 * 
@@ -124,6 +202,166 @@ public class BundleManager
 	}
 	
 	/**
+	 * loadProjectBundles
+	 */
+	public void loadProjectBundles()
+	{
+		// possibly clear current scripts here
+
+		for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects())
+		{
+			this.processProject(project);
+		}
+	}
+	
+	/**
+	 * moveBundle
+	 * 
+	 * @param oldFolder
+	 * @param newFolder
+	 */
+	public void moveBundle(String oldFolder, String newFolder)
+	{
+		if (newFolder != null && newFolder.length() > 0)
+		{
+			Bundle bundle = this.getBundleFromPath(oldFolder);
+			
+			if (bundle != null)
+			{
+				// remove bundle path reference
+				this._bundlesByPath.remove(oldFolder);
+				
+				// update bundle path 
+				bundle.moveTo(newFolder);
+				
+				// add new path reference
+				this._bundlesByPath.put(newFolder, bundle);
+			}
+		}
+	}
+	
+	/**
+	 * processBundle
+	 * 
+	 * @param bundleRoot
+	 */
+	public void processBundle(IFolder bundleRoot)
+	{
+		this.processBundle(bundleRoot, true);
+	}
+	
+	/**
+	 * processBundle
+	 * 
+	 * @param bundleRoot
+	 * @param processChildren
+	 */
+	public void processBundle(IFolder bundleRoot, boolean processChildren)
+	{
+		IFile bundleFile = bundleRoot.getFile("bundle.rb");
+		String bundlePath = bundleRoot.getLocation().toPortableString();
+
+		if (bundleFile.exists())
+		{
+			String fullPath = bundleFile.getLocation().toPortableString();
+			List<String> loadPaths = new ArrayList<String>();
+			
+			loadPaths.add(this.getBuiltinsLoadPath());
+			loadPaths.add(".");
+			
+			ScriptingEngine.getInstance().runScript(fullPath, loadPaths);
+			
+			if (processChildren)
+			{
+				// process snippets and command folders
+				this.processFolder(bundleRoot.getFolder(SNIPPETS_FOLDER_NAME));
+				this.processFolder(bundleRoot.getFolder(COMMANDS_FOLDER_NAME));
+			}
+		}
+		else
+		{
+			System.out.println("No bundle.rb for " + bundlePath);
+		}
+	}
+
+	/**
+	 * processFile
+	 * 
+	 * @param file
+	 */
+	public void processFile(IFile file)
+	{
+		if (file != null)
+		{
+			List<String> loadPaths = getLoadPaths(file);
+			
+			if (file.getName().toLowerCase().endsWith(".rb"))
+			{
+				String fullPath = file.getLocation().toPortableString();
+				
+				ScriptingEngine.getInstance().runScript(fullPath, loadPaths);
+			}
+		}
+	}
+	
+	/**
+	 * processFolder
+	 * 
+	 * @param bundleRoot
+	 */
+	private void processFolder(IFolder folder)
+	{
+		if (folder != null)
+		{
+			List<String> loadPaths = getLoadPaths(folder);
+			
+			try
+			{
+				for (IResource resource : folder.members())
+				{
+					if (resource.getName().toLowerCase().endsWith(".rb"))
+					{
+						String fullPath = resource.getLocation().toPortableString();
+						
+						ScriptingEngine.getInstance().runScript(fullPath, loadPaths);
+					}
+				}
+			}
+			catch (CoreException e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * processProject
+	 * 
+	 * @param project
+	 */
+	private void processProject(IProject project)
+	{
+		IFolder bundlesFolder = project.getFolder(BUNDLES_FOLDER_NAME);
+
+		if (bundlesFolder != null)
+		{
+			try
+			{
+				for (IResource resource : bundlesFolder.members())
+				{
+					if (resource instanceof IFolder)
+					{
+						this.processBundle((IFolder) resource);
+					}
+				}
+			}
+			catch (CoreException e)
+			{
+			}
+		}
+	}
+	
+	/**
 	 * removeBundle
 	 * 
 	 * @param bundle
@@ -135,6 +373,35 @@ public class BundleManager
 			if (this._bundles != null)
 			{
 				this._bundles.remove(bundle);
+			}
+		}
+	}
+	
+	/**
+	 * removeBundle
+	 * 
+	 * @param bundleFolder
+	 */
+	public void removeBundle(String bundleFolder)
+	{
+		Bundle bundle = this.getBundleFromPath(bundleFolder);
+
+		if (bundle != null)
+		{
+			this.removeBundle(bundle);
+		}
+	}
+
+	/**
+	 * showBundles
+	 */
+	public void showBundles()
+	{
+		if (this._bundles != null)
+		{
+			for (Bundle bundle : this._bundles)
+			{
+				System.out.println(bundle);
 			}
 		}
 	}
