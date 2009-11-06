203f00e1176da0c32646458b57294c39511b43ec
Kevin Lindsey <klindsey@aptana.com>
2009-11-06 14:52:33 -0600

    Introduce notion of a match context. This is used to decompose a scope into its atomic parts. The context keeps track of the current position that has successfully matched so far, and provides utility methods to push and pop the current position.

---
+++ plugins/com.aptana.scripting/src/com/aptana/scope/MatchContext.java
@@ -0,0 +1,84 @@
+package com.aptana.scope;
+
+import java.util.Stack;
+import java.util.regex.Pattern;
+
+public class MatchContext
+{
+	private static final Pattern spaces = Pattern.compile("\\s+");
+	private String[] _steps;
+	private int _currentIndex;
+	private Stack<Integer> _savedPositions;
+	
+	/**
+	 * MatchContext
+	 * 
+	 * @param scope
+	 */
+	MatchContext(String scope)
+	{
+		this._steps = (scope != null) ? spaces.split(scope) : new String[0];
+		this._currentIndex = this._steps.length - 1;
+		this._savedPositions = new Stack<Integer>();
+	}
+	
+	/**
+	 * advance
+	 */
+	public void advance()
+	{
+		this._currentIndex--;
+	}
+	
+	/**
+	 * getCurrentStep
+	 * 
+	 * @return
+	 */
+	public String getCurrentStep()
+	{
+		String result = null;
+		
+		if (0 <= this._currentIndex && this._currentIndex < this._steps.length)
+		{
+			result = this._steps[this._currentIndex];
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * popCurrentStep
+	 * 
+	 * @param restore
+	 */
+	public void popCurrentStep(boolean restore)
+	{
+		if (this._savedPositions.size() > 0)
+		{
+			int value = this._savedPositions.pop();
+			
+			if (restore)
+			{
+				this._currentIndex = value;
+			}
+		}
+	}
+	
+	/**
+	 * pushCurrentStep
+	 */
+	public void pushCurrentStep()
+	{
+		this._savedPositions.push(this._currentIndex);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString()
+	{
+		return this.getCurrentStep().toString();
+	}
+}
