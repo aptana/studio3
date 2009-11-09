f3de1f5a35e5b1a996d50133c3209abb29c3c3be
Sandip Chitale <schitale@aptana.com>
2009-11-09 08:39:57 -0800

    Add information control creator.

--- plugins/com.aptana.editor.scripting/src/com/aptana/editor/scripting/actions/SnippetsContentAssistant.java
+++ plugins/com.aptana.editor.scripting/src/com/aptana/editor/scripting/actions/SnippetsContentAssistant.java
@@ -1,12 +1,23 @@
 package com.aptana.editor.scripting.actions;
 
+import org.eclipse.jface.text.AbstractReusableInformationControlCreator;
+import org.eclipse.jface.text.DefaultInformationControl;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.swt.widgets.Shell;
 
 public class SnippetsContentAssistant extends ContentAssistant {
 	
 	private IContentAssistProcessor contentAssistProcessor;
 	private ExpandSnippetAction expandSnippetAction;
+	
+	protected static class DefaultInformationControlCreator extends AbstractReusableInformationControlCreator {
+		public IInformationControl doCreateInformationControl(Shell shell) {
+			return new DefaultInformationControl(shell, true);
+		}
+	}
 
 	public SnippetsContentAssistant(ExpandSnippetAction expandSnippet) {
 		super();
@@ -14,7 +25,7 @@ public class SnippetsContentAssistant extends ContentAssistant {
 		enableAutoActivation(false);
 //		enableAutoInsert(true);
 		enablePrefixCompletion(true);
-		setInformationControlCreator(null);
+		setInformationControlCreator(new DefaultInformationControlCreator());
 	}
 	
 	@Override
@@ -25,5 +36,4 @@ public class SnippetsContentAssistant extends ContentAssistant {
 		}
 		return contentAssistProcessor;
 	}
-	
 }
