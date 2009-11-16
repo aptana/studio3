c1fa762935b02120f3afa5696d401e8d0483b694
sgtcoolguy <cwilliams@aptana.com>
2009-11-16 16:10:46 -0500

    validate after drag and drop, not just text entered in commit message text area

--- plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/actions/CommitDialog.java
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/actions/CommitDialog.java
@@ -272,6 +272,7 @@ public class CommitDialog extends StatusDialog
 					}
 					removeDraggedFilesFromSource(sourceDragTable, draggedFiles);
 					workaroundEmptyTableDropEffectBug(sourceDragTable);
+					validate();
 				}
 			}
 		});
