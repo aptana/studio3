56824945270be1e941ce6e97a4f52a06c67112db
sgtcoolguy <cwilliams@aptana.com>
2009-11-17 14:09:29 -0500

    extract binary file extensions array to a static constant. Add ".jar".

--- plugins/com.aptana.git.core/src/com/aptana/git/core/model/GitIndex.java
+++ plugins/com.aptana.git.core/src/com/aptana/git/core/model/GitIndex.java
@@ -28,6 +28,11 @@ import com.aptana.git.core.GitPlugin;
 public class GitIndex
 {
 
+	/**
+	 * File extensions we check against and use to assume if a file may be binary (to not show a diff/content in various UI views)
+	 */
+	private static final String[] BINARY_EXTENSIONS = new String[] { ".pdf", ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".o", ".class", ".zip", ".gz", ".tar", ".ico", ".so", ".jar" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$ //$NON-NLS-13$ //$NON-NLS-14$
+	
 	private GitRepository repository;
 	private boolean amend;
 	private String workingDirectory;
@@ -642,9 +647,8 @@ public class GitIndex
 			return false;
 		if (output.endsWith("binary: unspecified")) //$NON-NLS-1$
 		{
-			final String[] extensions = new String[] { ".pdf", ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".o", ".class", ".zip", ".gz", ".tar", ".ico", ".so" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$ //$NON-NLS-11$ //$NON-NLS-12$ //$NON-NLS-13$
 			// try common filename-extensions
-			for (String extension : extensions)
+			for (String extension : BINARY_EXTENSIONS)
 			{
 				if (file.getPath().endsWith(extension))
 					return true;
