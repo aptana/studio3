cd5e0fcd57dbc749e2d77941ef76989b43add5c2
Sandip Chitale <schitale@aptana.com>
2009-11-20 15:13:13 -0800

    Fix PLUGIN_ID.

--- tests/com.aptana.radrails.editor.html.tests/src/com/aptana/radrails/editor/html/tests/Activator.java
+++ tests/com.aptana.radrails.editor.html.tests/src/com/aptana/radrails/editor/html/tests/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "com.aptana.radrails.editor.ruby.tests";
+	public static final String PLUGIN_ID = "com.aptana.radrails.editor.html.tests";
 
 	// The shared instance
 	private static Activator plugin;
