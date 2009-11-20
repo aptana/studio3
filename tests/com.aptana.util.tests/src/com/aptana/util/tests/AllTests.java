cc16b8fa5348ba278d16022ca30936a35bda475a
sgtcoolguy <cwilliams@aptana.com>
2009-11-20 11:19:00 -0500

    add test suite for util test plugin

---
+++ tests/com.aptana.util.tests/src/com/aptana/util/tests/AllTests.java
@@ -0,0 +1,19 @@
+package com.aptana.util.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests
+{
+
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite("Test for com.aptana.util.tests");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(StringUtilTest.class);
+		suite.addTestSuite(IOUtilTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
