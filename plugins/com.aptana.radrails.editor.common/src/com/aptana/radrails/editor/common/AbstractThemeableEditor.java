b5b26c6902087da218300ab10ee38695820a7fd1
Sandip Chitale <schitale@aptana.com>
2009-11-24 16:45:11 -0800

    Use Aptana editor context to restrict the Show Find Bar command keybinding.

--- plugins/com.aptana.radrails.editor.common/src/com/aptana/radrails/editor/common/AbstractThemeableEditor.java
+++ plugins/com.aptana.radrails.editor.common/src/com/aptana/radrails/editor/common/AbstractThemeableEditor.java
@@ -1,9 +1,15 @@
 package com.aptana.radrails.editor.common;
 
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.internal.editors.text.EditorsPlugin;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
 import org.eclipse.ui.texteditor.ChainedPreferenceStore;
@@ -24,6 +30,26 @@ import com.aptana.radrails.editor.common.theme.ThemeUtil;
 @SuppressWarnings("restriction")
 public abstract class AbstractThemeableEditor extends AbstractDecoratedTextEditor
 {
+    // Adapter factory to adapt to IFindBarDecorated
+    private static IAdapterFactory factory = new IAdapterFactory() {
+        @SuppressWarnings("unchecked")
+		public Class[] getAdapterList() {
+            return new Class[] { AbstractThemeableEditor.class, IFindBarDecorated.class };
+        }
+
+        @SuppressWarnings("unchecked")
+		public Object getAdapter(Object adaptableObject, Class adapterType) {
+            if (adaptableObject instanceof AbstractThemeableEditor) {
+				AbstractThemeableEditor abstractThemeableEditor = (AbstractThemeableEditor) adaptableObject;
+				return abstractThemeableEditor.getAdapter(IFindBarDecorated.class);
+            }
+            return null;
+        }
+    };
+    
+    static {
+        Platform.getAdapterManager().registerAdapters(factory, AbstractThemeableEditor.class);
+    }
 
 	/**
 	 * AbstractThemeableEditor
@@ -33,12 +59,22 @@ public abstract class AbstractThemeableEditor extends AbstractDecoratedTextEdito
 		super();
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
-	public Object getAdapter(Class adapter) {
-		if (IFindBarDecorated.class == adapter) {
+	public Object getAdapter(Class required) {
+		if (IFindBarDecorated.class.equals(required)) {
 			return AbstractThemeableEditor.this.getFindBarDecorated();
 		}
-		return super.getAdapter(adapter);
+		return super.getAdapter(required);
+	}
+
+	@Override
+	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+		super.init(site, input);
+		IContextService contextService = (IContextService) site.getService(IContextService.class);
+		if (contextService != null) {
+			contextService.activateContext("com.aptana.editor.scope"); //$NON-NLS-1$
+		}
 	}
 	
     /* (non-Javadoc)
