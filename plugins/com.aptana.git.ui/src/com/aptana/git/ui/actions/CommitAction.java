91871bd635ae4e5b614be84d3b275e9b9228fbf7
sgtcoolguy <cwilliams@aptana.com>
2009-11-30 13:14:22 -0500

    Add opening error dialogs if we have no git repo

--- plugins/com.aptana.git.ui/src/com/aptana/git/ui/actions/CommitAction.java
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/actions/CommitAction.java
@@ -1,9 +1,11 @@
 package com.aptana.git.ui.actions;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 
 import com.aptana.git.core.model.GitRepository;
 import com.aptana.git.ui.internal.actions.CommitDialog;
+import com.aptana.git.ui.internal.actions.Messages;
 
 public class CommitAction extends GitAction
 {
@@ -12,6 +14,18 @@ public class CommitAction extends GitAction
 	public void run()
 	{
 		GitRepository theRepo = getSelectedRepository();
+		if (theRepo == null && getSelectedResources() != null && getSelectedResources().length == 0)
+		{
+			MessageDialog.openError(getShell(), Messages.CommitAction_NoRepo_Title,
+					Messages.CommitAction_NoRepo_Message);
+			return;
+		}
+		if (theRepo == null && getSelectedResources() != null && getSelectedResources().length != 1)
+		{
+			MessageDialog.openError(getShell(), Messages.CommitAction_MultipleRepos_Title,
+					Messages.CommitAction_MultipleRepos_Message);
+			return;
+		}
 		CommitDialog dialog = new CommitDialog(getTargetPart().getSite().getShell(), theRepo);
 		if (dialog.open() == Window.OK)
 		{
