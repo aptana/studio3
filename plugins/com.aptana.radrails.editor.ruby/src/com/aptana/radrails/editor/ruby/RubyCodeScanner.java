b348ce3a1875b341add59bdb7323c353cfae9cbc
sgtcoolguy <cwilliams@aptana.com>
2009-11-30 15:26:49 -0500

    add some hacks in to make certain module/class/method name tokens types match what Textmate would return

--- plugins/com.aptana.radrails.editor.ruby/src/com/aptana/radrails/editor/ruby/RubyCodeScanner.java
+++ plugins/com.aptana.radrails.editor.ruby/src/com/aptana/radrails/editor/ruby/RubyCodeScanner.java
@@ -12,6 +12,9 @@ public class RubyCodeScanner implements ITokenScanner
 {
 
 	private ITokenScanner fScanner;
+	private boolean nextIsMethodName;
+	private boolean nextIsModuleName;
+	private boolean nextIsClassName;
 
 	public RubyCodeScanner()
 	{
@@ -49,6 +52,11 @@ public class RubyCodeScanner implements ITokenScanner
 				case Tokens.k__FILE__:
 				case Tokens.k__LINE__:
 				case Tokens.kSELF:
+					if (nextIsClassName)
+					{
+						nextIsClassName = false;
+						return ThemeUtil.getToken("entity.name.type.class.ruby"); //$NON-NLS-1$
+					}
 					return ThemeUtil.getToken("variable.language.ruby"); //$NON-NLS-1$
 				case Tokens.kNIL:
 				case Tokens.kTRUE:
@@ -61,9 +69,14 @@ public class RubyCodeScanner implements ITokenScanner
 				case Tokens.kDO_BLOCK:
 					return ThemeUtil.getToken("keyword.control.start-block.ruby"); //$NON-NLS-1$
 				case Tokens.kCLASS:
+					nextIsClassName = true;
 					return ThemeUtil.getToken("keyword.control.class.ruby"); //$NON-NLS-1$
 				case Tokens.kMODULE:
+					nextIsModuleName = true;
 					return ThemeUtil.getToken("keyword.control.module.ruby"); //$NON-NLS-1$
+				case Tokens.kDEF:
+					nextIsMethodName = true;
+					return ThemeUtil.getToken("keyword.control.def.ruby"); //$NON-NLS-1$
 				default:
 					return ThemeUtil.getToken("keyword.control.ruby"); //$NON-NLS-1$
 			}
@@ -72,13 +85,13 @@ public class RubyCodeScanner implements ITokenScanner
 		{
 			case RubyTokenScanner.ASSIGNMENT:
 				return ThemeUtil.getToken("keyword.operator.assignment.ruby"); //$NON-NLS-1$
-			case Tokens.tCMP:		/* <=> */
-			case Tokens.tMATCH:     /* =~ */
-			case Tokens.tNMATCH:    /* !~ */
-			case Tokens.tEQ:            /* == */
-			case Tokens.tEQQ:           /* === */
-			case Tokens.tNEQ:           /* != */
-			case Tokens.tGEQ:           /* >= */
+			case Tokens.tCMP: /* <=> */
+			case Tokens.tMATCH: /* =~ */
+			case Tokens.tNMATCH: /* !~ */
+			case Tokens.tEQ: /* == */
+			case Tokens.tEQQ: /* === */
+			case Tokens.tNEQ: /* != */
+			case Tokens.tGEQ: /* >= */
 			case Tokens.tLEQ:
 			case Tokens.tLT:
 			case Tokens.tGT:
@@ -95,13 +108,23 @@ public class RubyCodeScanner implements ITokenScanner
 			case Tokens.tBANG:
 			case Tokens.tOROP:
 			case Tokens.tCARET:
+			case RubyTokenScanner.QUESTION:
 				return ThemeUtil.getToken("keyword.operator.logical.ruby"); //$NON-NLS-1$
+			case Tokens.tLSHFT:
+				if (nextIsClassName)
+				{
+					return ThemeUtil.getToken("entity.name.type.class.ruby"); //$NON-NLS-1$
+				}
+				return ThemeUtil.getToken("keyword.operator.assignment.augmented.ruby"); //$NON-NLS-1$
 			case Tokens.tOP_ASGN:
 				return ThemeUtil.getToken("keyword.operator.assignment.augmented.ruby"); //$NON-NLS-1$
 			case Tokens.tASSOC:
 				return ThemeUtil.getToken("punctuation.separator.key-value"); //$NON-NLS-1$
 			case RubyTokenScanner.CHARACTER:
 				return ThemeUtil.getToken("character.ruby"); //$NON-NLS-1$
+			case Tokens.tCOLON2:
+			case Tokens.tCOLON3:
+				return ThemeUtil.getToken("punctuation.separator.inheritance.ruby"); //$NON-NLS-1$
 			case Tokens.tFLOAT:
 			case Tokens.tINTEGER:
 				return ThemeUtil.getToken("constant.numeric.ruby"); //$NON-NLS-1$
@@ -114,9 +137,27 @@ public class RubyCodeScanner implements ITokenScanner
 			case Tokens.tCVAR:
 				return ThemeUtil.getToken("variable.other.readwrite.class.ruby"); //$NON-NLS-1$
 			case Tokens.tCONSTANT:
+				if (nextIsModuleName)
+				{
+					nextIsModuleName = false;
+					return ThemeUtil.getToken("entity.name.type.module.ruby"); //$NON-NLS-1$
+				}
+				if (nextIsClassName)
+				{
+					nextIsClassName = false;
+					return ThemeUtil.getToken("entity.name.type.class.ruby"); //$NON-NLS-1$
+				}
 				return ThemeUtil.getToken("variable.other.constant.ruby"); //$NON-NLS-1$
 			case Tokens.yyErrorCode:
 				return ThemeUtil.getToken("error.ruby"); //$NON-NLS-1$
+			case Tokens.tIDENTIFIER:
+			case Tokens.tFID:
+				if (nextIsMethodName)
+				{
+					nextIsMethodName = false;
+					return ThemeUtil.getToken("entity.name.function.ruby"); //$NON-NLS-1$
+				}
+				return ThemeUtil.getToken("entity.name.ruby"); //$NON-NLS-1$
 			default:
 				return ThemeUtil.getToken("default.ruby"); //$NON-NLS-1$
 		}
