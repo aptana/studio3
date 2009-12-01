cb59d4c716f737b21127c64db15a6ac560344417
Kevin Lindsey <klindsey@aptana.com>
2009-12-01 16:44:55 -0600

    Add ability to create sub-menus and to create menu seperators

--- plugins/com.aptana.scripting/bundles/radrails/menu.rb
+++ plugins/com.aptana.scripting/bundles/radrails/menu.rb
@@ -9,6 +9,15 @@ module RadRails
       @jobj.display_name = name;
     end
     
+    def add_menu(menu)
+      @jobj.add_menu menu.java_object
+    end
+    
+    def command(name)
+      child_menu = Menu.new(name)
+      add_menu(child_menu)
+    end
+    
     def command_name
       @jobj.command_name
     end
@@ -29,6 +38,14 @@ module RadRails
       @jobj
     end
     
+    def menu(name, &block)
+      child_menu = Menu.new(name)
+      
+      block.call(child_menu) if block_given?
+      
+      add_menu(child_menu)
+    end
+    
     def path
       @jobj.path
     end
@@ -41,16 +58,20 @@ module RadRails
       @jobj.scope = RadRails::ScopeSelector.new(scope).to_s
     end
     
+    def separator
+      menu("-")
+    end
+    
     class << self
       def define_menu(name, &block)
-        menu = Menu.new(name)
-        block.call(menu) if block_given?
+        new_menu = Menu.new(name)
+        block.call(new_menu) if block_given?
         
         # add command to bundle
-        bundle = BundleManager.bundle_from_path(menu.path)
+        bundle = BundleManager.bundle_from_path(new_menu.path)
         
         if bundle.nil? == false
-          bundle.add_menu(menu)
+          bundle.add_menu(new_menu)
         end
       end
     end
