7c2949d71059534c9f3461ab2c291793729b701d
sgtcoolguy <cwilliams@aptana.com>
2009-12-04 13:51:54 -0500

    Set JRuby Home so it will set up loadpaths for ruby lib properly

--- plugins/com.aptana.scripting/src/com/aptana/scripting/ScriptingEngine.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/ScriptingEngine.java
@@ -1,7 +1,10 @@
 package com.aptana.scripting;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.jruby.embed.LocalContextProvider;
 import org.jruby.embed.PathType;
 import org.jruby.embed.ScriptingContainer;
@@ -10,14 +13,14 @@ public class ScriptingEngine
 {
 	private static ScriptingEngine instance;
 	private static ScriptingContainer scriptingContainer;
-	
+
 	/**
 	 * ScriptingEngine
 	 */
 	private ScriptingEngine()
 	{
 	}
-	
+
 	/**
 	 * getInstance
 	 * 
@@ -29,10 +32,10 @@ public class ScriptingEngine
 		{
 			instance = new ScriptingEngine();
 		}
-		
+
 		return instance;
 	}
-	
+
 	/**
 	 * getScriptingContainer
 	 * 
@@ -43,11 +46,20 @@ public class ScriptingEngine
 		if (scriptingContainer == null)
 		{
 			scriptingContainer = new ScriptingContainer();
+			try
+			{
+				File pluginFile = FileLocator.getBundleFile(Activator.getDefault().getBundle());
+				scriptingContainer.getProvider().getRubyInstanceConfig().setJRubyHome(pluginFile.getAbsolutePath());
+			}
+			catch (IOException e)
+			{
+				Activator.logError(e.getMessage(), e);
+			}
 		}
-		
+
 		return scriptingContainer;
 	}
-	
+
 	/**
 	 * runScript
 	 * 
@@ -56,14 +68,14 @@ public class ScriptingEngine
 	public void runScript(String fullPath, List<String> loadPaths)
 	{
 		ScriptingContainer container = this.getScriptingContainer();
-		
+
 		if (loadPaths != null && loadPaths.size() > 0)
 		{
 			LocalContextProvider provider = container.getProvider();
-		
+
 			provider.setLoadPaths(loadPaths);
 		}
-		
+
 		// TODO: $0 should work, but until then, we'll use this hack so script
 		// can't get its full path
 		container.put("$fullpath", fullPath); //$NON-NLS-1$
