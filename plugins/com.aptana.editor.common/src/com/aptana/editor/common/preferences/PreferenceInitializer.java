6a6ca8d4d62d28e7302a056fd6d3c3eac7de9ef0
sgtcoolguy <cwilliams@aptana.com>
2009-12-04 15:23:10 -0500

    Add teh default pair matching for (), [], {}, '', "", `` and use teh current theme's caret color for the box when drawn

---
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/preferences/PreferenceInitializer.java
@@ -0,0 +1,29 @@
+package com.aptana.editor.common.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.osgi.service.prefs.BackingStoreException;
+
+import com.aptana.editor.common.CommonEditorPlugin;
+
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
+	@Override
+	public void initializeDefaultPreferences()
+	{
+		IEclipsePreferences prefs = new DefaultScope().getNode(CommonEditorPlugin.PLUGIN_ID);
+		try
+		{
+			prefs.putBoolean(IPreferenceConstants.ENABLE_CHARACTER_PAIR_COLORING, true);
+			prefs.put(IPreferenceConstants.CHARACTER_PAIR_COLOR, "128,128,128");
+			prefs.flush();
+		}
+		catch (BackingStoreException e)
+		{
+			CommonEditorPlugin.logError(e);
+		}
+
+	}
+
+}
