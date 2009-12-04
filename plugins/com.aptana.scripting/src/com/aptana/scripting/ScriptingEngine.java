9d26829085833940fad41202132cd2da4a7f829b
Kevin Lindsey <klindsey@aptana.com>
2009-12-04 11:32:44 -0600

    Move definition of built-ins load path to ScriptingEngine and make public so other code can use those results.
    Return the value generated when executing a script in case the calling code needs to use it.

--- plugins/com.aptana.scripting/src/com/aptana/scripting/ScriptingEngine.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/ScriptingEngine.java
@@ -1,13 +1,26 @@
 package com.aptana.scripting;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.MessageFormat;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.URIUtil;
 import org.jruby.embed.LocalContextProvider;
 import org.jruby.embed.PathType;
 import org.jruby.embed.ScriptingContainer;
 
+import com.aptana.scripting.model.Messages;
+
 public class ScriptingEngine
 {
+	private static final String BUILTIN_BUNDLES = "bundles"; //$NON-NLS-1$
+	
 	private static ScriptingEngine instance;
 	private static ScriptingContainer scriptingContainer;
 	
@@ -34,6 +47,46 @@ public class ScriptingEngine
 	}
 	
 	/**
+	 * getBuiltinsLoadPath
+	 * 
+	 * @return
+	 */
+	public static String getBuiltinsLoadPath()
+	{
+		URL url = FileLocator.find(Activator.getDefault().getBundle(), new Path(BUILTIN_BUNDLES), null);
+		String result = null;
+
+		try
+		{
+			URL fileURL = FileLocator.toFileURL(url);
+			URI fileURI = URIUtil.toURI(fileURL);	// Use Eclipse to get around Java 1.5 bug on Windows
+			File file = new File(fileURI);
+
+			result = file.getAbsolutePath();
+		}
+		catch (IOException e)
+		{
+			String message = MessageFormat.format(
+				Messages.BundleManager_Cannot_Locate_Built_Ins_Directory,
+				new Object[] { url.toString() }
+			);
+
+			Activator.logError(message, e);
+		}
+		catch (URISyntaxException e)
+		{
+			String message = MessageFormat.format(
+				Messages.BundleManager_Malformed_Built_Ins_URI,
+				new Object[] { url.toString() }
+			);
+
+			Activator.logError(message, e);
+		}
+
+		return result;
+	}
+	
+	/**
 	 * getScriptingContainer
 	 * 
 	 * @return
@@ -53,7 +106,7 @@ public class ScriptingEngine
 	 * 
 	 * @param fullPath
 	 */
-	public void runScript(String fullPath, List<String> loadPaths)
+	public Object runScript(String fullPath, List<String> loadPaths)
 	{
 		ScriptingContainer container = this.getScriptingContainer();
 		
@@ -65,8 +118,9 @@ public class ScriptingEngine
 		}
 		
 		// TODO: $0 should work, but until then, we'll use this hack so script
-		// can't get its full path
+		// can get its full path
 		container.put("$fullpath", fullPath); //$NON-NLS-1$
-		container.runScriptlet(PathType.ABSOLUTE, fullPath);
+		
+		return container.runScriptlet(PathType.ABSOLUTE, fullPath);
 	}
 }
