d9f0e179fddff2202ac681d1bb6d6c215282028a
sgtcoolguy <cwilliams@aptana.com>
2009-12-07 09:51:07 -0800

    Commit the beginnings of a markdown editor

---
+++ plugins/com.aptana.editor.markdown/src/com/aptana/editor/markdown/MarkdownSourceConfiguration.java
@@ -0,0 +1,168 @@
+/**
+ * This file Copyright (c) 2005-2009 Aptana, Inc. This program is
+ * dual-licensed under both the Aptana Public License and the GNU General
+ * Public license. You may elect to use one or the other of these licenses.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * AS-IS and WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, TITLE, or
+ * NONINFRINGEMENT. Redistribution, except as permitted by whichever of
+ * the GPL or APL you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or modify this
+ * program under the terms of the GNU General Public License,
+ * Version 3, as published by the Free Software Foundation.  You should
+ * have received a copy of the GNU General Public License, Version 3 along
+ * with this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * 
+ * Aptana provides a special exception to allow redistribution of this file
+ * with certain other free and open source software ("FOSS") code and certain additional terms
+ * pursuant to Section 7 of the GPL. You may view the exception and these
+ * terms on the web at http://www.aptana.com/legal/gpl/.
+ * 
+ * 2. For the Aptana Public License (APL), this program and the
+ * accompanying materials are made available under the terms of the APL
+ * v1.0 which accompanies this distribution, and is available at
+ * http://www.aptana.com/legal/apl/.
+ * 
+ * You may view the GPL, Aptana's exception and additional terms, and the
+ * APL in the file titled license.html at the root of the corresponding
+ * plugin containing this source file.
+ * 
+ * Any modifications to this file must keep this entire header intact.
+ */
+
+package com.aptana.editor.markdown;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.presentation.PresentationReconciler;
+import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
+import org.eclipse.jface.text.rules.ICharacterScanner;
+import org.eclipse.jface.text.rules.IPredicateRule;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.ITokenScanner;
+import org.eclipse.jface.text.rules.PatternRule;
+import org.eclipse.jface.text.rules.RuleBasedScanner;
+import org.eclipse.jface.text.rules.SingleLineRule;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.jface.text.source.ISourceViewer;
+
+import com.aptana.editor.common.IPartitioningConfiguration;
+import com.aptana.editor.common.ISourceViewerConfiguration;
+import com.aptana.editor.common.ISubPartitionScanner;
+import com.aptana.editor.common.SubPartitionScanner;
+import com.aptana.editor.common.theme.ThemeUtil;
+
+/**
+ * @author Max Stepanov
+ */
+public class MarkdownSourceConfiguration implements IPartitioningConfiguration, ISourceViewerConfiguration
+{
+
+	public final static String PREFIX = "__md_"; //$NON-NLS-1$
+	public final static String DEFAULT = "__md" + IDocument.DEFAULT_CONTENT_TYPE; //$NON-NLS-1$
+	public final static String HEADING = PREFIX + "heading"; //$NON-NLS-1$
+
+	public static final String[] CONTENT_TYPES = new String[] { DEFAULT, HEADING };
+
+	private IPredicateRule[] partitioningRules = new IPredicateRule[] { new SingleLineRule("#", "", new Token(HEADING)), //$NON-NLS-1$ //$NON-NLS-2$
+	};
+
+	private MarkdownScanner xmlScanner;
+	private RuleBasedScanner preProcessorScanner;
+
+	private static MarkdownSourceConfiguration instance;
+
+	public static MarkdownSourceConfiguration getDefault()
+	{
+		if (instance == null)
+		{
+			instance = new MarkdownSourceConfiguration();
+		}
+		return instance;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.IPartitioningConfiguration#getContentTypes()
+	 */
+	public String[] getContentTypes()
+	{
+		return CONTENT_TYPES;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.IPartitioningConfiguration#getPartitioningRules()
+	 */
+	public IPredicateRule[] getPartitioningRules()
+	{
+		return partitioningRules;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.IPartitioningConfiguration#createSubPartitionScanner()
+	 */
+	public ISubPartitionScanner createSubPartitionScanner()
+	{
+		return new SubPartitionScanner(partitioningRules, CONTENT_TYPES, new Token(DEFAULT));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.IPartitioningConfiguration#getDocumentDefaultContentType()
+	 */
+	public String getDocumentContentType(String contentType)
+	{
+		if (contentType.startsWith(PREFIX))
+		{
+			return IMarkdownConstants.CONTENT_TYPE_MARKDOWN;
+		}
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.ISourceViewerConfiguration#setupPresentationReconciler(org.eclipse.jface.text
+	 * .presentation.PresentationReconciler, org.eclipse.jface.text.source.ISourceViewer)
+	 */
+	public void setupPresentationReconciler(PresentationReconciler reconciler, ISourceViewer sourceViewer)
+	{
+		DefaultDamagerRepairer dr = new DefaultDamagerRepairer(getXMLScanner());
+		reconciler.setDamager(dr, IDocument.DEFAULT_CONTENT_TYPE);
+		reconciler.setRepairer(dr, IDocument.DEFAULT_CONTENT_TYPE);
+
+		reconciler.setDamager(dr, DEFAULT);
+		reconciler.setRepairer(dr, DEFAULT);
+
+		dr = new DefaultDamagerRepairer(getPreProcessorScanner());
+		reconciler.setDamager(dr, HEADING);
+		reconciler.setRepairer(dr, HEADING);
+	}
+
+	private ITokenScanner getPreProcessorScanner()
+	{
+		if (preProcessorScanner == null)
+		{
+			preProcessorScanner = new RuleBasedScanner();
+			IRule[] rules = new IRule[1];
+			rules[0] = new PatternRule("#", " ", ThemeUtil.getToken("punctuation.definition.heading.markdown"), (char) 0, true);
+			preProcessorScanner.setRules(rules);			
+			preProcessorScanner.setDefaultReturnToken(ThemeUtil.getToken("entity.name.section.markdown")); //$NON-NLS-1$
+		}
+		return preProcessorScanner;
+	}
+
+	protected ITokenScanner getXMLScanner()
+	{
+		if (xmlScanner == null)
+		{
+			xmlScanner = new MarkdownScanner();
+		}
+		return xmlScanner;
+	}
+
+}
