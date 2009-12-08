ce4a18614c23aae2e4c499852dfff0fa06cbffa8
sgtcoolguy <cwilliams@aptana.com>
2009-12-08 13:47:07 -0800

    Set up App Explorer font, default to using the Text Editor font

---
+++ plugins/com.aptana.explorer/src/com/aptana/explorer/AdapterFactory.java
@@ -0,0 +1,60 @@
+package com.aptana.explorer;
+
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.jface.resource.FontDescriptor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.ui.model.IWorkbenchAdapter2;
+
+public class AdapterFactory implements IAdapterFactory
+{
+
+	protected static final String APP_EXPLORER_FONT_NAME = "com.aptana.explorer.font"; //$NON-NLS-1$
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Object getAdapter(Object adaptableObject, Class adapterType)
+	{
+		if (adapterType.equals(IWorkbenchAdapter2.class))
+		{
+			return new IWorkbenchAdapter2()
+			{
+
+				@Override
+				public RGB getForeground(Object element)
+				{
+					return null;
+				}
+
+				@Override
+				public FontData getFont(Object element)
+				{
+					// FIXME We need to listen for changes to this font and force a redraw of the app explorer (and maybe any other views of resources)!
+					FontDescriptor fd = JFaceResources.getFontDescriptor(APP_EXPLORER_FONT_NAME);
+					if (fd == null)
+					{
+						fd = JFaceResources.getTextFontDescriptor();
+					}
+					return fd.getFontData()[0];
+				}
+
+				@Override
+				public RGB getBackground(Object element)
+				{
+					// TODO See if we can pick up the theme colors!
+					return null;
+				}
+			};
+		}
+		return null;
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Class[] getAdapterList()
+	{
+		return new Class[] { IWorkbenchAdapter2.class };
+	}
+
+}
