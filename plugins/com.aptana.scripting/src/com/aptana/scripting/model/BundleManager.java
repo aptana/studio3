b29557cb9d89ea7fc4c26201574edc320df0a382
Kevin Lindsey <klindsey@aptana.com>
2009-12-08 11:23:46 -0600

    Move resource change listener logic into BundleManager and remove duplicate listener in EarlyStartup. Remove obsolete classes.

--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleManager.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleManager.java
@@ -7,28 +7,30 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Platform;
 import org.jruby.anno.JRubyMethod;
 
-import com.aptana.scripting.ResourceChangeListener;
 import com.aptana.scripting.ScriptingEngine;
 
-public class BundleManager
+public class BundleManager implements IResourceChangeListener, IResourceDeltaVisitor
 {
 	static final Menu[] NO_MENUS = new Menu[0];
 	static final Snippet[] NO_SNIPPETS = new Snippet[0];
 	static final Command[] NO_COMMANDS = new Command[0];
-	
-	private static final IResourceChangeListener resourceListener = new ResourceChangeListener();
 	
 	private static final String USER_BUNDLE_DIRECTORY_GENERAL = "RadRails Bundles"; //$NON-NLS-1$
 	private static final String USER_BUNDLE_DIRECTORY_MACOSX = "/Documents/RadRails Bundles"; //$NON-NLS-1$
@@ -39,6 +41,12 @@ public class BundleManager
 	private static final String SNIPPETS_FOLDER_NAME = "snippets"; //$NON-NLS-1$
 	private static final String COMMANDS_FOLDER_NAME = "commands"; //$NON-NLS-1$
 	private static final String USER_HOME_PROPERTY = "user.home"; //$NON-NLS-1$
+	
+	private static final Pattern BUNDLE_PATTERN = Pattern.compile("/.+?/bundles/.+?/bundle\\.rb$"); //$NON-NLS-1$
+	private static final Pattern FILE_PATTERN = Pattern.compile("/.+?/bundles/.+?/(?:commands|snippets)/[^/]+\\.rb$"); //$NON-NLS-1$
+	private static final Pattern USER_BUNDLE_PATTERN;
+	private static final Pattern USER_FILE_PATTERN;
+	
 	private static BundleManager INSTANCE;
 
 	/**
@@ -57,16 +65,28 @@ public class BundleManager
 		return INSTANCE;
 	}
 	private List<Bundle> _bundles;
-	
+
 	private Map<String, Bundle> _bundlesByPath;
 
+	/**
+	 * static constructor
+	 */
+	static
+	{
+		String userBundlesRoot = BundleManager.getInstance().getUserBundlePath().toLowerCase();
+		
+		// TODO: make this work on win32
+		USER_BUNDLE_PATTERN = Pattern.compile(userBundlesRoot + "/.+?/bundle\\.rb$"); //$NON-NLS-1$
+		USER_FILE_PATTERN = Pattern.compile(userBundlesRoot + "/.+?/(?:commands|snippets)/[^/]+\\.rb$"); //$NON-NLS-1$
+	}
+	
 	/**
 	 * BundleManager
 	 */
 	private BundleManager()
 	{
 		// attach resource change listener so we can track changes to the workspace
-		ResourcesPlugin.getWorkspace().addResourceChangeListener(resourceListener, IResourceChangeEvent.POST_CHANGE);
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_CHANGE);
 	}
 
 	/**
@@ -500,6 +520,125 @@ public class BundleManager
 	}
 	
 	/**
+	 * processBundle
+	 * 
+	 * @param delta
+	 */
+	private boolean processBundle(IResourceDelta delta)
+	{
+		IResource file = delta.getResource();
+		boolean visitChildren = true;
+
+		if (file != null && file.getLocation() != null)
+		{
+			IResource bundleFolder = file.getParent();
+			String bundleFolderPath = bundleFolder.getLocation().toPortableString();
+
+			switch (delta.getKind())
+			{
+				case IResourceDelta.ADDED:
+					BundleManager.getInstance().processBundle(bundleFolder, false);
+					break;
+
+				case IResourceDelta.REMOVED:
+					BundleManager.getInstance().removeBundle(bundleFolderPath);
+					break;
+
+				case IResourceDelta.CHANGED:
+					if ((delta.getFlags() & IResourceDelta.MOVED_FROM) != 0)
+					{
+						String oldPath = delta.getMovedFromPath().toPortableString();
+						
+						BundleManager.getInstance().moveBundle(oldPath, bundleFolderPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.MOVED_TO) != 0)
+					{
+						String newPath = delta.getMovedToPath().toString();
+						
+						BundleManager.getInstance().moveBundle(bundleFolderPath, newPath);
+					}
+					if ((delta.getFlags() & IResourceDelta.REPLACED) != 0)
+					{
+						BundleManager.getInstance().removeBundle(bundleFolderPath);
+						BundleManager.getInstance().processBundle(bundleFolder, false);
+					}
+					if ((delta.getFlags() & IResourceDelta.CONTENT) != 0)
+					{
+						BundleManager.getInstance().removeBundle(bundleFolderPath);
+						BundleManager.getInstance().processBundle(bundleFolder, false);
+					}
+					break;
+			}
+		}
+
+		return visitChildren;
+	}
+
+	/**
+	 * processFile
+	 * 
+	 * @param delta
+	 */
+	private boolean processFile(IResourceDelta delta)
+	{
+		IResource file = delta.getResource();
+		boolean visitChildren = true;
+
+		if (file != null && file.getLocation() != null)
+		{
+			BundleManager manager = BundleManager.getInstance();
+			
+			switch (delta.getKind())
+			{
+				case IResourceDelta.ADDED:
+					BundleManager.getInstance().processSnippetOrCommand(file);
+					break;
+
+				case IResourceDelta.REMOVED:
+					BundleManager.getInstance().removeSnippetOrCommand(file);
+					break;
+
+				case IResourceDelta.CHANGED:
+					if ((delta.getFlags() & IResourceDelta.MOVED_FROM) != 0)
+					{
+						IPath movedFromPath = delta.getMovedFromPath();
+						IResource movedFrom = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(movedFromPath);
+						
+						if (movedFrom != null && movedFrom instanceof IFile)
+						{
+							manager.removeSnippetOrCommand(movedFrom);
+							manager.processSnippetOrCommand(file);
+						}
+					}
+					else if ((delta.getFlags() & IResourceDelta.MOVED_TO) != 0)
+					{
+						IPath movedToPath = delta.getMovedToPath();
+						IResource movedTo = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(movedToPath);
+						
+						if (movedTo != null && movedTo instanceof IFile)
+						{
+							manager.removeSnippetOrCommand(file);
+							manager.processSnippetOrCommand(movedTo);
+						}
+					}
+					else if ((delta.getFlags() & IResourceDelta.REPLACED) != 0)
+					{
+						manager.removeSnippetOrCommand(file);
+						manager.processSnippetOrCommand(file);
+					}
+					else if ((delta.getFlags() & IResourceDelta.CONTENT) != 0)
+					{
+						manager.removeSnippetOrCommand(file);
+						manager.processSnippetOrCommand(file);
+					}
+					break;
+			}
+		}
+
+		return visitChildren;
+	}
+
+	/**
 	 * processFolder
 	 * 
 	 * @param folder
@@ -587,7 +726,7 @@ public class BundleManager
 			}
 		}
 	}
-
+	
 	/**
 	 * removeBundle
 	 * 
@@ -602,7 +741,7 @@ public class BundleManager
 			this.removeBundle(bundle);
 		}
 	}
-
+	
 	/**
 	 * removeSnippetOrCommand
 	 * 
@@ -640,6 +779,22 @@ public class BundleManager
 		}
 	}
 	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.resources.IResourceChangeListener#resourceChanged(org.eclipse.core.resources.IResourceChangeEvent)
+	 */
+	public void resourceChanged(IResourceChangeEvent event)
+	{
+		try
+		{
+			event.getDelta().accept(this);
+		}
+		catch (CoreException e)
+		{
+			// log an error in the error log
+		}
+	}
+
 	/**
 	 * showBundles
 	 */
@@ -656,5 +811,27 @@ public class BundleManager
 		{
 			System.out.println(Messages.BundleManager_NO_BUNDLES);
 		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
+	 */
+	public boolean visit(IResourceDelta delta) throws CoreException
+	{
+		String fullProjectPath = delta.getFullPath().toString().toLowerCase();
+		String fullPath = delta.getResource().getLocation().toPortableString().toLowerCase();
+		boolean visitChildren = true;
+		
+		if (BUNDLE_PATTERN.matcher(fullProjectPath).matches() || USER_BUNDLE_PATTERN.matcher(fullPath).matches())
+		{
+			visitChildren = this.processBundle(delta);
+		}
+		else if (FILE_PATTERN.matcher(fullProjectPath).matches() || USER_FILE_PATTERN.matcher(fullPath).matches())
+		{
+			visitChildren = this.processFile(delta);
+		}
+
+		return visitChildren;
 	}
 }
