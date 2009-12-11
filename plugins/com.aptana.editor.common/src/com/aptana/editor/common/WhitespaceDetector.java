07420f563c0665eb9f5a49e597a5da84529ae39e
sgtcoolguy <cwilliams@aptana.com>
2009-12-11 11:35:33 -0800

    Just use Character.isWhitespace for determining whitespace.

--- plugins/com.aptana.editor.common/src/com/aptana/editor/common/WhitespaceDetector.java
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/WhitespaceDetector.java
@@ -39,18 +39,17 @@ import org.eclipse.jface.text.rules.IWhitespaceDetector;
 
 /**
  * @author Max Stepanov
- *
  */
-public class WhitespaceDetector implements IWhitespaceDetector {
+public class WhitespaceDetector implements IWhitespaceDetector
+{
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see org.eclipse.jface.text.rules.IWhitespaceDetector#isWhitespace(char)
 	 */
-	public boolean isWhitespace(char c) {
-		return c == ' '
-			|| c == '\t'
-			|| c == '\n'
-			|| c == '\r';
+	public boolean isWhitespace(char c)
+	{
+		return Character.isWhitespace(c);
 	}
 
 }
