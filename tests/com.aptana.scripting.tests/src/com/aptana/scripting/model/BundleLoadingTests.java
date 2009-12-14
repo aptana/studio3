b694420d06e4530851aafa4338cccbe2e807882f
Kevin Lindsey <klindsey@aptana.com>
2009-12-14 17:04:10 -0600

    Now clearing BundleManager before each test.
    Added sanity tests for bundle precedence.

--- tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingTests.java
+++ tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingTests.java
@@ -4,6 +4,16 @@ package com.aptana.scripting.model;
 public class BundleLoadingTests extends BundleTestBase
 {
 	/**
+	 * setUp
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		
+		BundleManager.getInstance().reset();
+	}
+
+	/**
 	 * testLoadLoneBundle
 	 */
 	public void testLoadLoneBundle()
@@ -52,5 +62,71 @@ public class BundleLoadingTests extends BundleTestBase
 		
 		assertNotNull(snippets);
 		assertEquals(1, snippets.length);
+	}
+	
+	/**
+	 * testUserOverridesApplication
+	 */
+	public void testUserOverridesApplication()
+	{
+		String bundleName = "bundleWithCommand";
+		
+		// confirm app bundle loaded properly
+		BundleEntry entry = this.loadBundleEntry(bundleName, BundleScope.APPLICATION);
+		CommandElement[] commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd", commands[0].getInvoke());
+		
+		// confirm user bundle overrides application
+		entry = this.loadBundleEntry(bundleName, BundleScope.USER);
+		commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd ..", commands[0].getInvoke());
+	}
+	
+	/**
+	 * testUserOverridesApplication
+	 */
+	public void testProjectOverridesApplication()
+	{
+		String bundleName = "bundleWithCommand";
+		
+		// confirm app bundle loaded properly
+		BundleEntry entry = this.loadBundleEntry(bundleName, BundleScope.APPLICATION);
+		CommandElement[] commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd", commands[0].getInvoke());
+		
+		// confirm project bundle overrides application
+		entry = this.loadBundleEntry(bundleName, BundleScope.PROJECT);
+		commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd /", commands[0].getInvoke());
+	}
+	
+	/**
+	 * testUserOverridesApplication
+	 */
+	public void testProjectOverridesUser()
+	{
+		String bundleName = "bundleWithCommand";
+		
+		// confirm app bundle loaded properly
+		BundleEntry entry = this.loadBundleEntry(bundleName, BundleScope.USER);
+		CommandElement[] commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd ..", commands[0].getInvoke());
+		
+		// confirm project bundle overrides application
+		entry = this.loadBundleEntry(bundleName, BundleScope.PROJECT);
+		commands = entry.getCommands();
+		assertNotNull(commands);
+		assertEquals(1, commands.length);
+		assertEquals("cd /", commands[0].getInvoke());
 	}
 }
