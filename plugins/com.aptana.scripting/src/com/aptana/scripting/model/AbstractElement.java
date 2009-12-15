dc287a65ffa62bda5b2e590fcbc7baaa925666a7
Kevin Lindsey <klindsey@aptana.com>
2009-12-15 16:54:49 -0600

    Track elements by the path of the script that created them. This is needed so we can remove any elements created in a file that has been deleted or modified.

--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/AbstractElement.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/AbstractElement.java
@@ -1,10 +1,91 @@
 package com.aptana.scripting.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public abstract class AbstractElement
 {
+	private static final Map<String, List<AbstractElement>> ELEMENTS_BY_PATH;
+	private static final AbstractElement[] NO_ELEMENTS = new AbstractElement[0];
+	
 	protected String _path;
 	protected String _displayName;
-
+	
+	/**
+	 * static constructor
+	 */
+	static
+	{
+		ELEMENTS_BY_PATH = new HashMap<String, List<AbstractElement>>();
+	}
+	
+	/**
+	 * unregisterElement
+	 * 
+	 * @param element
+	 */
+	public static void unregisterElement(AbstractElement element)
+	{
+		if (element != null)
+		{
+			String path = element.getPath();
+			List<AbstractElement> elements = ELEMENTS_BY_PATH.get(path);
+			
+			if (elements != null)
+			{
+				elements.remove(element);
+				
+				if (elements.size() == 0)
+				{
+					ELEMENTS_BY_PATH.remove(path);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * getRegisteredElements
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public static AbstractElement[] getRegisteredElements(String path)
+	{
+		AbstractElement[] result = NO_ELEMENTS;
+		List<AbstractElement> elements = ELEMENTS_BY_PATH.get(path);
+		
+		if (elements != null)
+		{
+			result = elements.toArray(new AbstractElement[elements.size()]);
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * registerElement
+	 * 
+	 * @param element
+	 */
+	public static void registerElement(AbstractElement element)
+	{
+		if (element != null)
+		{
+			String path = element.getPath();
+			List<AbstractElement> elements = ELEMENTS_BY_PATH.get(path);
+			
+			if (elements == null)
+			{
+				elements = new ArrayList<AbstractElement>();
+				ELEMENTS_BY_PATH.put(path, elements);
+			}
+			
+			elements.add(element);
+		}
+	}
+	
 	/**
 	 * ModelBase
 	 * 
@@ -12,9 +93,16 @@ public abstract class AbstractElement
 	 */
 	public AbstractElement(String path)
 	{
+		if (path == null || path.length() == 0)
+		{
+			throw new IllegalArgumentException("path must be defined");
+		}
+		
 		this._path = path;
+		
+		registerElement(this);
 	}
-
+	
 	/**
 	 * getDisplayName
 	 * 
