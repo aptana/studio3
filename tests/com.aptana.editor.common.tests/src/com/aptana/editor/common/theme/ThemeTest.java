62bf42f573b9f5e2f6138e05e6a5d13966f5f5cc
sgtcoolguy <cwilliams@aptana.com>
2009-12-22 18:29:39 -0500

    add more unit tests for Theme class

--- tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/theme/ThemeTest.java
+++ tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/theme/ThemeTest.java
@@ -4,13 +4,19 @@ import java.util.Properties;
 
 import junit.framework.TestCase;
 
+import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.swt.graphics.RGB;
 
 public class ThemeTest extends TestCase
 {
 
-	public void testBasics()
+	private Theme theme;
+	private ColorManager colormanager;
+
+	@Override
+	protected void setUp() throws Exception
 	{
+		super.setUp();
 		Properties props = new Properties();
 		props.put("background", "#ffffff");
 		props.put("foreground", "#ff0000");
@@ -20,14 +26,25 @@ public class ThemeTest extends TestCase
 		props.put("name", "chris");
 		props.put("constant", "#00ff00,#ff00ff,italic");
 		props.put("constant.language.js", "#000000,#ff0000,bold");
-		Theme theme = new Theme(new ColorManager(), props)
+		colormanager = new ColorManager();
+		theme = new Theme(colormanager, props)
 		{
 			@Override
 			protected void storeDefaults()
 			{
 				// do nothing
 			}
+
+			@Override
+			public void save()
+			{
+				// do nothing
+			}
 		};
+	}
+
+	public void testBasics()
+	{
 		assertEquals(new RGB(255, 255, 255), theme.getBackground());
 		assertEquals(new RGB(255, 0, 0), theme.getForeground());
 		assertEquals(new RGB(0, 255, 0), theme.getCaret());
@@ -41,12 +58,36 @@ public class ThemeTest extends TestCase
 
 		// Check "constant" token colors
 		assertEquals(new RGB(0, 255, 0), theme.getTextAttribute("constant").getForeground().getRGB());
+		assertEquals(new RGB(0, 255, 0), theme.getForegroundAsRGB("constant"));
 		assertEquals(new RGB(255, 0, 255), theme.getTextAttribute("constant").getBackground().getRGB());
+		assertEquals(new RGB(255, 0, 255), theme.getBackgroundAsRGB("constant"));
 		// "constant.langauge inherits "constant" token's colors
 		assertEquals(theme.getTextAttribute("constant"), theme.getTextAttribute("constant.language"));
-		// "constant.langauge.js" has overridden parents, so it has it's own colors
+		assertEquals(new RGB(0, 255, 0), theme.getForegroundAsRGB("constant.language"));
+		assertEquals(new RGB(255, 0, 255), theme.getBackgroundAsRGB("constant.language"));
+		// "constant.language.js" has overridden parents, so it has it's own colors
 		assertEquals(new RGB(0, 0, 0), theme.getTextAttribute("constant.language.js").getForeground().getRGB());
+		assertEquals(new RGB(0, 0, 0), theme.getForegroundAsRGB("constant.language.js"));
 		assertEquals(new RGB(255, 0, 0), theme.getTextAttribute("constant.language.js").getBackground().getRGB());
+		assertEquals(new RGB(255, 0, 0), theme.getBackgroundAsRGB("constant.language.js"));
 	}
-	// TODO Add tests for copy, delete, addNeDefaultToken, update methods
+
+	// TODO Add tests for copy, delete, addNewDefaultToken, update methods
+
+	public void testAddingTokens()
+	{
+		assertFalse(theme.hasEntry("chris"));
+		theme.addNewDefaultToken("chris");
+		assertTrue(theme.hasEntry("chris"));
+		assertEquals(new RGB(255, 0, 0), theme.getForegroundAsRGB("chris"));
+	}
+
+	public void testModifyingTokens()
+	{
+		TextAttribute at = new TextAttribute(colormanager.getColor(new RGB(128, 128, 128)), colormanager
+				.getColor(new RGB(64, 0, 64)), TextAttribute.UNDERLINE);
+		theme.update("constant", at);
+		assertEquals(new RGB(128, 128, 128), theme.getForegroundAsRGB("constant.language"));
+		assertEquals(new RGB(64, 0, 64), theme.getBackgroundAsRGB("constant.language"));
+	}
 }
