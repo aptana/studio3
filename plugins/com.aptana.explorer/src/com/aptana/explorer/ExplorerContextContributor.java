98744d4d2aff4ab4106c05dfbf30e8e9810bc909
Sandip Chitale <schitale@aptana.com>
2009-12-29 17:52:29 -0800

    Implent ContexContributor populating the Active Project related info.

---
+++ plugins/com.aptana.explorer/src/com/aptana/explorer/ExplorerContextContributor.java
@@ -0,0 +1,45 @@
+package com.aptana.explorer;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Platform;
+
+import com.aptana.scripting.model.CommandContext;
+import com.aptana.scripting.model.CommandElement;
+
+public class ExplorerContextContributor implements
+		com.aptana.scripting.model.ContextContributor {
+
+	public ExplorerContextContributor() {
+	}
+
+	@Override
+	public void modifyContext(CommandElement command, CommandContext context)
+	{
+		String activeProjectName = Platform.getPreferencesService().getString(ExplorerPlugin.PLUGIN_ID,
+				IPreferenceConstants.ACTIVE_PROJECT, null, null);
+		
+		IProject project = null;
+		if (activeProjectName != null)
+		{
+			project = ResourcesPlugin.getWorkspace().getRoot().getProject(activeProjectName);
+		}
+
+		if (project == null)
+		{
+			return;
+		}
+		
+		if (command.isShellCommand())
+		{
+			context.putEnv(CommandContext.ACTIVE_PROJECT_NAME, activeProjectName);
+			context.putEnv(CommandContext.ACTIVE_PROJECT_FOLDER, project.getLocation().toOSString());
+		}
+		else
+		{
+			context.put(CommandContext.ACTIVE_PROJECT_NAME, activeProjectName);
+			context.put(CommandContext.ACTIVE_PROJECT_FOLDER, project.getLocation().toOSString());
+		}
+	}
+
+}
