0f9ef8309e47763816926739bd9a7f1cb167eeba
sgtcoolguy <cwilliams@aptana.com>
2009-12-30 08:39:11 -0500

    make it easy to change teh property indent level, and default to no indent like Textmate

--- plugins/com.aptana.scripting/bundles/css/commands/format_css.rb
+++ plugins/com.aptana.scripting/bundles/css/commands/format_css.rb
@@ -7,8 +7,9 @@ RadRails::Command.define_command("Format CSS") do |cmd|
   cmd.scope = "source.css"
   cmd.invoke do |context|
     code = context.in.read
+    property_indent = "" # Set to multiple spaces or tabs if you want properties to be indented additionally
     # TODO Figure out initial indent and then use that in substitutions to properly indent
-    code.gsub!(/({|;)\s*([-\w]+:)\s*(?=\S)/im) {|match| "#{$1}\n  #{$2} " }
+    code.gsub!(/({|;)\s*([-\w]+:)\s*(?=\S)/im) {|match| "#{$1}\n#{property_indent}#{$2} " }
     code.gsub!(/\s*}[ \t]*\n?/im, "\n}\n") # TODO After close of rule, make sure that next selector starts at beginning of line/indent level
     code.gsub!(/\s*{[ \t]*/im, " {")
     code
