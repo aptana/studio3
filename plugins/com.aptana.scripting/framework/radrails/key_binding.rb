1fefae99732df212e3eafcfe1d40afcd74ac7492
Kevin Lindsey <klindsey@aptana.com>
2010-01-04 17:33:10 -0600

    Handle both cases where a key binding can be a single string value or an array of strings. This differs from how we're handling other similar cases in that we have two differently name methods instead of overloads.

--- plugins/com.aptana.scripting/framework/radrails/key_binding.rb
+++ plugins/com.aptana.scripting/framework/radrails/key_binding.rb
@@ -8,8 +8,13 @@ module RadRails
     end
 
     def []=(os, key_binding)
-      as_strings = key_binding.map {|x| x.to_s }
-      @jobj.set_key_binding(os.to_s, as_strings.join(" "))
+      if key_binding
+        if key_binding.kind_of?(Array)
+          @jobj.set_key_bindings(os.to_s, key_binding.to_java(:String))
+        else
+      	  @jobj.set_key_binding(os.to_s, key_binding.to_s)
+      	end
+      end
     end
   end
 
