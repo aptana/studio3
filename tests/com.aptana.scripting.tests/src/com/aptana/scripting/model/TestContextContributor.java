1227643d3c4372bad19458d7a23a99f12a6ed7b4
Kevin Lindsey <klindsey@aptana.com>
2010-01-05 17:00:03 -0600

    Create a context property of a Ruby object when the Ruby class exists.

--- tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/TestContextContributor.java
+++ tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/TestContextContributor.java
@@ -1,5 +1,9 @@
 package com.aptana.scripting.model;
 
+import org.jruby.runtime.builtin.IRubyObject;
+
+import com.aptana.scripting.ScriptUtils;
+
 public class TestContextContributor implements ContextContributor
 {
 	public static final String TEST_PROPERTY = "test_property";
@@ -19,7 +23,14 @@ public class TestContextContributor implements ContextContributor
 	public void modifyContext(CommandElement command, CommandContext context)
 	{
 		context.put(TEST_PROPERTY, new TestEnvironmentContributor());
-
-		// context.put("test2", ScriptUtils.instantiateClass("ContextContributor"));
+		
+		// We have to load a specific bundle in order for ContextContributor to exist, so
+		// we don't want to do anything when it doesn't exist.
+		IRubyObject value = ScriptUtils.instantiateClass("ContextContributor");
+		
+		if (value != null)
+		{
+			context.put("test2", value);
+		}
 	}
 }
