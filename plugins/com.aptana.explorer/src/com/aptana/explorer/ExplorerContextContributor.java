616df5d9fd3071312c526a286208594f014121ba
Kevin Lindsey <klindsey@aptana.com>
2010-01-06 17:31:00 -0600

    Remove now-obsolete constants from CommandContext.
    Add a Ruby-wrapped Project to the command context.

--- plugins/com.aptana.explorer/src/com/aptana/explorer/ExplorerContextContributor.java
+++ plugins/com.aptana.explorer/src/com/aptana/explorer/ExplorerContextContributor.java
@@ -3,35 +3,61 @@ package com.aptana.explorer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.preferences.IPreferencesService;
+import org.jruby.runtime.builtin.IRubyObject;
 
+import com.aptana.scripting.ScriptUtils;
 import com.aptana.scripting.model.CommandContext;
 import com.aptana.scripting.model.CommandElement;
+import com.aptana.scripting.model.ContextContributor;
 
-public class ExplorerContextContributor implements
-		com.aptana.scripting.model.ContextContributor {
+public class ExplorerContextContributor implements ContextContributor
+{
+	private static final String PROJECT_PROPERTY_NAME = "project";
+	private static final String PROJECT_RUBY_CLASS = "Project";
 
-	public ExplorerContextContributor() {
+	/**
+	 * ExplorerContextContributor
+	 */
+	public ExplorerContextContributor()
+	{
 	}
 
+	/**
+	 * getActiveProject
+	 * 
+	 * @return
+	 */
+	private IProject getActiveProject()
+	{
+		IPreferencesService preferencesService = Platform.getPreferencesService();
+		String activeProjectName = preferencesService.getString(ExplorerPlugin.PLUGIN_ID, IPreferenceConstants.ACTIVE_PROJECT, null, null);
+		IProject result = null;
+
+		if (activeProjectName != null)
+		{
+			result = ResourcesPlugin.getWorkspace().getRoot().getProject(activeProjectName);
+		}
+		
+		return result;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.scripting.model.ContextContributor#modifyContext(com.aptana.scripting.model.CommandElement,
+	 * com.aptana.scripting.model.CommandContext)
+	 */
 	@Override
 	public void modifyContext(CommandElement command, CommandContext context)
 	{
-		String activeProjectName = Platform.getPreferencesService().getString(ExplorerPlugin.PLUGIN_ID,
-				IPreferenceConstants.ACTIVE_PROJECT, null, null);
-		
-		IProject project = null;
-		if (activeProjectName != null)
-		{
-			project = ResourcesPlugin.getWorkspace().getRoot().getProject(activeProjectName);
-		}
+		IProject project = this.getActiveProject();
 
-		if (project == null)
+		if (project != null)
 		{
-			return;
+			IRubyObject[] args = new IRubyObject[] { ScriptUtils.javaToRuby(project) };
+			IRubyObject rubyInstance = ScriptUtils.instantiateClass(ScriptUtils.RADRAILS_MODULE, PROJECT_RUBY_CLASS, args);
+
+			context.put(PROJECT_PROPERTY_NAME, rubyInstance);
 		}
-		
-		context.put(CommandContext.ACTIVE_PROJECT_NAME, activeProjectName);
-		context.put(CommandContext.ACTIVE_PROJECT_FOLDER, project.getLocation().toOSString());
 	}
-
 }
