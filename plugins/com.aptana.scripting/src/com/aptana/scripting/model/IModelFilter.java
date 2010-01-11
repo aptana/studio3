00b726ed056b7720c04f223287fedd791c0367c0
Kevin Lindsey <klindsey@aptana.com>
2010-01-11 14:44:16 -0600

    Remove "exclude" method from IModelFilter interface

--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/IModelFilter.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/IModelFilter.java
@@ -3,14 +3,6 @@ package com.aptana.scripting.model;
 public interface IModelFilter
 {
 	/**
-	 * exclude
-	 * 
-	 * @param element
-	 * @return
-	 */
-	boolean exclude(AbstractElement element);
-	
-	/**
 	 * include
 	 * 
 	 * @param element
--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/TriggerOnlyFilter.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/TriggerOnlyFilter.java
@@ -4,15 +4,6 @@ public class TriggerOnlyFilter implements IModelFilter
 {
 	/*
 	 * (non-Javadoc)
-	 * @see com.aptana.scripting.model.IModelFilter#exclude(com.aptana.scripting.model.AbstractNode)
-	 */
-	public boolean exclude(AbstractElement element)
-	{
-		return (this.include(element) == false);
-	}
-
-	/*
-	 * (non-Javadoc)
 	 * @see com.aptana.scripting.model.IModelFilter#include(com.aptana.scripting.model.AbstractModel)
 	 */
 	public boolean include(AbstractElement element)
