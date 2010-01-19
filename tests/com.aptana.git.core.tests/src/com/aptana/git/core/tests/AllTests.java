e403a0434c442a23aacedb429b6b7c65c9d2e1ed
sgtcoolguy <cwilliams@aptana.com>
2010-01-19 10:49:34 -0500

    Hook up git core test suite to unit test build

---
+++ tests/com.aptana.git.core.tests/src/com/aptana/git/core/tests/AllTests.java
@@ -0,0 +1,18 @@
+package com.aptana.git.core.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests
+{
+
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
