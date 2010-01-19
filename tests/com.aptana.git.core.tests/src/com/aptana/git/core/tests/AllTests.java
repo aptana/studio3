d70ab188cadb6d6dfcb3fd1f23d967b584a2cb0b
sgtcoolguy <cwilliams@aptana.com>
2010-01-19 11:40:42 -0500

    hook GitExecutableTest to suite

--- tests/com.aptana.git.core.tests/src/com/aptana/git/core/tests/AllTests.java
+++ tests/com.aptana.git.core.tests/src/com/aptana/git/core/tests/AllTests.java
@@ -3,15 +3,17 @@ package com.aptana.git.core.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import com.aptana.git.core.model.GitExecutableTest;
+
 public class AllTests
 {
 
 	public static Test suite()
 	{
 		TestSuite suite = new TestSuite(AllTests.class.getName());
-		//$JUnit-BEGIN$
-
-		//$JUnit-END$
+		// $JUnit-BEGIN$
+		suite.addTestSuite(GitExecutableTest.class);
+		// $JUnit-END$
 		return suite;
 	}
 
