96e7d73e9866cd64a0a182aae6f4b7e78fa4710a
sgtcoolguy <cwilliams@aptana.com>
2010-01-25 14:51:34 -0500

    catch errors trying to apply theme to scripting console and log them, rather than failing by throwing uncaught exception

--- plugins/com.aptana.scripting.ui/src/com/aptana/scripting/ui/ScriptingConsole.java
+++ plugins/com.aptana.scripting.ui/src/com/aptana/scripting/ui/ScriptingConsole.java
@@ -30,12 +30,12 @@ import com.aptana.editor.common.theme.Theme;
 
 public class ScriptingConsole
 {
-	private static final String TEXTFONT_PROPERTY = "org.eclipse.jface.textfont";
-	private static final String CONSOLE_TRACE = "console.trace";
-	private static final String CONSOLE_WARNING = "console.warning";
-	private static final String CONSOLE_INFO = "console.info";
-	private static final String CONSOLE_ERROR = "console.error";
-	private static final String CONSOLE_OUTPUT = "console.output";
+	private static final String TEXTFONT_PROPERTY = "org.eclipse.jface.textfont"; //$NON-NLS-1$
+	private static final String CONSOLE_TRACE = "console.trace"; //$NON-NLS-1$
+	private static final String CONSOLE_WARNING = "console.warning"; //$NON-NLS-1$
+	private static final String CONSOLE_INFO = "console.info"; //$NON-NLS-1$
+	private static final String CONSOLE_ERROR = "console.error"; //$NON-NLS-1$
+	private static final String CONSOLE_OUTPUT = "console.output"; //$NON-NLS-1$
 	
 	private static final String CONSOLE_ICON_PATH = "icons/console.png"; //$NON-NLS-1$
 
@@ -90,35 +90,42 @@ public class ScriptingConsole
 	 */
 	private void applyTheme()
 	{
-		IWorkbench workbench = PlatformUI.getWorkbench();
-		final Display display = workbench.getDisplay();
-		
-		if (display == null)
+		try
 		{
-			throw new IllegalStateException(new SWTError(SWT.ERROR_THREAD_INVALID_ACCESS));
-		}
-		
-		display.syncExec(new Runnable()
-		{
-			public void run()
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			final Display display = workbench.getDisplay();
+			
+			if (display == null)
 			{
-				// set colors
-				CommonEditorPlugin plugin = CommonEditorPlugin.getDefault();
-				ColorManager colorManager = plugin.getColorManager();
-				Theme theme = plugin.getThemeManager().getCurrentTheme();
-				
-				// set background color
-				console.setBackground(colorManager.getColor(theme.getBackground()));
-				console.setFont(JFaceResources.getTextFont());
-				
-				// set stream colors
-				applyTheme(CONSOLE_OUTPUT, outputConsoleStream, display.getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
-				applyTheme(CONSOLE_ERROR, errorConsoleStream, display.getSystemColor(SWT.COLOR_DARK_RED));
-				applyTheme(CONSOLE_INFO, infoConsoleStream, display.getSystemColor(SWT.COLOR_DARK_BLUE));
-				applyTheme(CONSOLE_WARNING, warningConsoleStream, display.getSystemColor(SWT.COLOR_DARK_YELLOW));
-				applyTheme(CONSOLE_TRACE, traceConsoleStream, display.getSystemColor(SWT.COLOR_DARK_GREEN));
+				throw new IllegalStateException(new SWTError(SWT.ERROR_THREAD_INVALID_ACCESS));
 			}
-		});
+			
+			display.syncExec(new Runnable()
+			{
+				public void run()
+				{
+					// set colors
+					CommonEditorPlugin plugin = CommonEditorPlugin.getDefault();
+					ColorManager colorManager = plugin.getColorManager();
+					Theme theme = plugin.getThemeManager().getCurrentTheme();
+					
+					// set background color
+					console.setBackground(colorManager.getColor(theme.getBackground()));
+					console.setFont(JFaceResources.getTextFont());
+					
+					// set stream colors
+					applyTheme(CONSOLE_OUTPUT, outputConsoleStream, display.getSystemColor(SWT.COLOR_WIDGET_FOREGROUND));
+					applyTheme(CONSOLE_ERROR, errorConsoleStream, display.getSystemColor(SWT.COLOR_DARK_RED));
+					applyTheme(CONSOLE_INFO, infoConsoleStream, display.getSystemColor(SWT.COLOR_DARK_BLUE));
+					applyTheme(CONSOLE_WARNING, warningConsoleStream, display.getSystemColor(SWT.COLOR_DARK_YELLOW));
+					applyTheme(CONSOLE_TRACE, traceConsoleStream, display.getSystemColor(SWT.COLOR_DARK_GREEN));
+				}
+			});
+		}
+		catch (IllegalStateException e)
+		{
+			CommonEditorPlugin.logError(e);
+		}
 	}
 	
 	/**
