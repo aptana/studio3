556092a6374b498d4bba5397efa91a6b683a105e
sgtcoolguy <cwilliams@aptana.com>
2010-01-26 16:54:01 -0500

    make scope in all of text editor be "text"

--- plugins/com.aptana.editor.text/src/com/aptana/editor/text/TextSourceViewerConfiguration.java
+++ plugins/com.aptana.editor.text/src/com/aptana/editor/text/TextSourceViewerConfiguration.java
@@ -4,20 +4,31 @@ import org.eclipse.jface.preference.IPreferenceStore;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.common.CommonSourceViewerConfiguration;
+import com.aptana.editor.common.ICommonConstants;
+import com.aptana.editor.common.QualifiedContentType;
+import com.aptana.editor.common.tmp.ContentTypeTranslation;
 
 public class TextSourceViewerConfiguration extends CommonSourceViewerConfiguration
 {
+
+	static
+	{
+		ContentTypeTranslation.getDefault().addTranslation(
+				new QualifiedContentType(ICommonConstants.CONTENT_TYPE_UKNOWN), new QualifiedContentType("text")); //$NON-NLS-1$
+	}
 
 	public TextSourceViewerConfiguration(IPreferenceStore preferenceStore, AbstractThemeableEditor editor)
 	{
 		super(preferenceStore, editor);
 	}
 
-	@Override
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.ITopContentTypesProvider#getTopContentTypes()
+	 */
 	public String[][] getTopContentTypes()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return new String[][] { { ICommonConstants.CONTENT_TYPE_UKNOWN } };
 	}
 
 }
