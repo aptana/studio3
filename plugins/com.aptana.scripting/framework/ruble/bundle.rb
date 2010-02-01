f4b4937d65fcafcb541bf249b9a9762b57d6d1f7
sgtcoolguy <cwilliams@aptana.com>
2010-02-01 12:33:25 -0500

    fix typos, don't do underscore conversion twice

--- plugins/com.aptana.scripting/framework/ruble/bundle.rb
+++ plugins/com.aptana.scripting/framework/ruble/bundle.rb
@@ -107,23 +107,23 @@ module Ruble
     
     # This method does two things:
     # If the given file type is not associated with any editor, we associate it with our generic text editor
-    # We also register a top=level scope to report back for any file whose name matches the file type pattern.
+    # We also register a top-level scope to report back for any file whose name matches the file type pattern.
     # Understands * wildcard. Typical values for first arg: '*.yml', '*_spec.rb', 'Rakefile'
-    # scope is a string our a symbol. Underscores are converted to spaces (so 'source.yaml' and :source_yaml are equivalent)
+    # scope is a string or a symbol. Underscores are converted to spaces (so 'source.yaml' and :source_yaml are equivalent)
     def register_file_type(file_type, scope)
-      associate_scope(file_type, scope.to_s.gsub(/_/, '.'))
+      associate_scope(file_type, scope)
       associate_file_type(file_type)
     end
     
     # Given a filename pattern, we register files that match to our generic text editor (uses our theme and scripting)
-    # file_type must be either an exact filename to match, or a *.ext extension to associate with.
+    # file_type must be either an exact filename to match, or a *.ext extension to associate with. (i.e. 'Rakefile', '*.yml')
     def associate_file_type(file_type)
       @jobj.associateFileType(file_type.to_s)
     end
     
     # Registers a top-level scope to report back for any file whose name matches the passed in pattern.
     # Understands * wildcard. Typical values for first arg: '*.yml', '*_spec.rb', 'Rakefile'
-    # scope is a string our a symbol. Underscores are converted to spaces (so 'source.yaml' and :source_yaml are equivalent)
+    # scope is a string or a symbol. Underscores are converted to spaces (so 'source.yaml' and :source_yaml are equivalent)
     def associate_scope(file_type, scope)
       @jobj.associateScope(file_type, scope.to_s.gsub(/_/, '.'))
     end
