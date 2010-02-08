3c2adf0f26d6796bca87a6507d413f589c23ee11
sgtcoolguy <cwilliams@aptana.com>
2010-02-08 14:02:20 -0500

    add implementations for windows on other systems to grab the full user name

--- plugins/com.aptana.scripting/src/com/aptana/scripting/FullnameEnvContextContributor.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/FullnameEnvContextContributor.java
@@ -13,6 +13,7 @@ import com.aptana.scripting.model.CommandContext;
 import com.aptana.scripting.model.CommandElement;
 import com.aptana.scripting.model.ContextContributor;
 import com.aptana.scripting.model.EnvironmentContributor;
+import com.aptana.util.ProcessUtil;
 
 public class FullnameEnvContextContributor implements ContextContributor, EnvironmentContributor
 {
@@ -29,7 +30,6 @@ public class FullnameEnvContextContributor implements ContextContributor, Enviro
 	public Map<String, String> toEnvironment()
 	{
 		Map<String, String> map = new HashMap<String, String>();
-		// TODO Is there an equivalent for Linux/Windows?
 		// If we're on Mac, grab the full user name via applescript and stuff it in the TM_FULLNAME env var
 		if (Platform.getOS().equals(Platform.OS_MACOSX))
 		{
@@ -47,6 +47,30 @@ public class FullnameEnvContextContributor implements ContextContributor, Enviro
 			catch (ScriptException e)
 			{
 				Activator.logError(e.getMessage(), e);
+			}
+		}
+		// Seems like %USERNAME% typically holds the full name of the user now on Windows
+		else if (Platform.getOS().equals(Platform.OS_WIN32))
+		{
+			String username = System.getenv("USERNAME"); //$NON-NLS-1$
+			if (username != null && username.trim().length() > 0)
+			{
+				map.put(TM_FULLNAME, username);
+			}
+		}
+		// http://stackoverflow.com/questions/833227/whats-the-easiest-way-to-get-a-users-full-name-on-a-linux-posix-system
+		// else if (Platform.getOS().equals(Platform.OS_LINUX))
+		else
+		{
+			String username = System.getProperty("user.name"); //$NON-NLS-1$
+			if (username != null && username.trim().length() > 0)
+			{
+				String cmdLine = "getent passwd \"" + username + "\" | cut -d ':' -f 5 | cut -d ',' -f 1"; //$NON-NLS-1$ //$NON-NLS-2$
+				Map<Integer, String> result = ProcessUtil.runInBackground(cmdLine, null, new String[0]);
+				if (result != null && result.keySet().iterator().next() == 0)
+				{
+					map.put(TM_FULLNAME, result.values().iterator().next());
+				}
 			}
 		}
 
