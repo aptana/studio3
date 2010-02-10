7ef6e3a9de8de23962ef82796b3c78cccaa6c782
Kevin Lindsey <klindsey@aptana.com>
2010-02-10 09:45:13 -0600

    Initial implementation of negative lookahead for scope selectors. Added empty unit tests

---
+++ tests/com.aptana.scripting.tests/src/com/aptana/scope/NegativeLookaheadTests.java
@@ -0,0 +1,27 @@
+package com.aptana.scope;
+
+import junit.framework.TestCase;
+
+public class NegativeLookaheadTests extends TestCase
+{
+	/**
+	 * testLookaheadMatches
+	 */
+	public void testLookaheadMatches()
+	{
+	}
+	
+	/**
+	 * testLookaheadDoesNotMatch
+	 */
+	public void testLookaheadDoesNotMatch()
+	{
+	}
+	
+	/**
+	 * testLookaheadAgainstNothing
+	 */
+	public void testLookaheadAgainstNothing()
+	{
+	}
+}
