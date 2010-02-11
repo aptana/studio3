e4bab551c75332608810238154095783170c84b8
Kevin Lindsey <klindsey@aptana.com>
2010-02-11 11:59:55 -0600

    Somehow this didn't included during refactoring.

--- plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleElement.java
+++ plugins/com.aptana.scripting/src/com/aptana/scripting/model/BundleElement.java
@@ -71,7 +71,7 @@ public class BundleElement extends AbstractElement
 		}
 
 		// calculate the bundle scope
-		this._bundlePrecedence = BundleManager.getInstance().getBundlePrecedenceFromPath(path);
+		this._bundlePrecedence = BundleManager.getInstance().getBundlePrecedence(path);
 	}
 
 	/**
