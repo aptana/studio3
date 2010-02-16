c67f07d1fd0ba80daa0b5465d0f2d9dc7b91996d
Sandip Chitale <schitale@aptana.com>
2010-02-16 12:13:50 -0800

    Remove Send Feedback and Report Bug commands.

--- plugins/com.aptana.scripting/bundles/aptana.ruble/commands/aptana.rb
+++ plugins/com.aptana.scripting/bundles/aptana.ruble/commands/aptana.rb
@@ -6,19 +6,3 @@ command "Execute Line Inserting Results" do |cmd|
   cmd.input = [ :selection, :line ] 
   cmd.invoke { IO.popen(STDIN.read, 'r') {|io| io.read } }
 end
-
-command "Send Feedback..." do |cmd|
-  cmd.input = :none
-  cmd.output = :show_as_html
-  cmd.invoke do |context|
-    "<meta http-equiv='Refresh' content='0;URL=https://radrails.tenderapp.com/discussion/new'>"
-  end
-end
-
-command "Report Bug..." do |cmd|
-  cmd.input = :none
-  cmd.output = :show_as_html
-  cmd.invoke do |context|
-    "<meta http-equiv='Refresh' content='0;URL=https://aptana.lighthouseapp.com/projects/35266-radrails/tickets/new'>"
-  end
-end
