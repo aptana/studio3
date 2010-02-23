d1829d6719c8db11cae2f50e04854c03d753d01a
Kevin Lindsey <klindsey@aptana.com>
2010-02-22 18:50:34 -0600

    Initial attempt at adding support for copy/paste in WebTerm. Paste seems to be the only operation working at the moment.

--- plugins/com.aptana.terminal/src/com/aptana/terminal/TerminalBrowser.java
+++ plugins/com.aptana.terminal/src/com/aptana/terminal/TerminalBrowser.java
@@ -2,17 +2,28 @@ package com.aptana.terminal;
 
 import java.util.UUID;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
+import org.eclipse.jface.bindings.keys.KeyStroke;
+import org.eclipse.jface.bindings.keys.SWTKeySupport;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.util.PropertyChangeEvent;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -20,9 +31,11 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.keys.IBindingService;
 import org.eclipse.ui.part.WorkbenchPart;
+import org.eclipse.ui.progress.UIJob;
 
 import com.aptana.editor.common.CommonEditorPlugin;
 import com.aptana.editor.common.theme.IThemeManager;
+import com.aptana.terminal.server.ProcessWrapper;
 import com.aptana.terminal.server.TerminalServer;
 
 public class TerminalBrowser
@@ -30,12 +43,25 @@ public class TerminalBrowser
 	private static final String TERMINAL_URL = "http://{0}:{1}/webterm/"; //$NON-NLS-1$
 	private static final String TEXTFONT_PROPERTY = "org.eclipse.jface.textfont"; //$NON-NLS-1$
 
+	private static KeyStroke COPY_STROKE;
+	private static KeyStroke PASTE_STROKE;
+
 	private Browser _browser;
 	private WorkbenchPart _owningPart;
 	private String _id;
 	private String _startingDirectory;
 	private IPreferenceChangeListener _themeChangeListener;
 	private IPropertyChangeListener _fontChangeListener;
+	private KeyListener _keyListener;
+
+	static
+	{
+		boolean isMac = Platform.getOS().equals(Platform.OS_MACOSX);
+		int modifiers = SWT.SHIFT | ((isMac) ? SWT.COMMAND : SWT.CONTROL);
+
+		COPY_STROKE = KeyStroke.getInstance(modifiers, 'C');
+		PASTE_STROKE = KeyStroke.getInstance(modifiers, 'V');
+	}
 
 	/**
 	 * TerminalBrowser
@@ -58,6 +84,122 @@ public class TerminalBrowser
 		this._owningPart = owningPart;
 		this._startingDirectory = startingDirectory;
 		this._id = UUID.randomUUID().toString();
+	}
+
+	/**
+	 * addFocusListener
+	 */
+	private void addFocusListener()
+	{
+		// create focus listener so we can enable/disable key bindings
+		final IBindingService bindingService = (IBindingService) this._owningPart.getSite().getService(
+				IBindingService.class);
+
+		this._browser.addFocusListener(new FocusListener()
+		{
+			public void focusGained(FocusEvent e)
+			{
+				bindingService.setKeyFilterEnabled(false);
+			}
+
+			public void focusLost(FocusEvent e)
+			{
+				bindingService.setKeyFilterEnabled(true);
+			}
+		});
+	}
+
+	/**
+	 * addFontChangeListener
+	 */
+	private void addFontChangeListener()
+	{
+		// refresh CSS when theme's font changes
+		this._fontChangeListener = new IPropertyChangeListener()
+		{
+			@Override
+			public void propertyChange(PropertyChangeEvent event)
+			{
+				if (_browser != null && event.getProperty().equals(TEXTFONT_PROPERTY))
+				{
+					applyTheme();
+				}
+			}
+		};
+		JFaceResources.getFontRegistry().addListener(this._fontChangeListener);
+
+		// create our supporting process for access to the system's shell
+		TerminalServer.getInstance().createProcess(this._id, this.getStartingDirectory());
+
+		// load the terminal
+		String url = NLS.bind(TERMINAL_URL, new Object[] { TerminalServer.getInstance().getHost(),
+				TerminalServer.getInstance().getPort() })
+				+ "?id=" + this._id; //$NON-NLS-1$
+		this.setUrl(url);
+	}
+
+	/**
+	 * addKeyListener
+	 */
+	private void addKeyListener()
+	{
+		this._keyListener = new KeyListener()
+		{
+			public void keyPressed(KeyEvent e)
+			{
+				int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(e);
+				KeyStroke key = SWTKeySupport.convertAcceleratorToKeyStroke(accelerator);
+
+				if (key.isComplete())
+				{
+					if (COPY_STROKE.equals(key))
+					{
+						_browser.execute("copy()");
+					}
+					else if (PASTE_STROKE.equals(key))
+					{
+						paste();
+					}
+				}
+			}
+
+			public void keyReleased(KeyEvent e)
+			{
+			}
+		};
+
+		this._browser.addKeyListener(this._keyListener);
+	}
+
+	/**
+	 * addListeners
+	 */
+	private void addListeners()
+	{
+		this.addFocusListener();
+		this.addThemeListener();
+		this.addFontChangeListener();
+		this.addKeyListener();
+	}
+
+	/**
+	 * addThemeListener
+	 */
+	private void addThemeListener()
+	{
+		// Force refresh of CSS when theme changes
+		this._themeChangeListener = new IPreferenceChangeListener()
+		{
+			@Override
+			public void preferenceChange(PreferenceChangeEvent event)
+			{
+				if (_browser != null && event.getKey().equals(IThemeManager.THEME_CHANGED))
+				{
+					applyTheme();
+				}
+			}
+		};
+		new InstanceScope().getNode(CommonEditorPlugin.PLUGIN_ID).addPreferenceChangeListener(_themeChangeListener);
 	}
 
 	/**
@@ -95,59 +237,7 @@ public class TerminalBrowser
 		// create browser control
 		this._browser = new Browser(parent, SWT.NONE);
 
-		// create focus listener so we can enable/disable key bindings
-		final IBindingService bindingService = (IBindingService) this._owningPart.getSite().getService(
-				IBindingService.class);
-
-		this._browser.addFocusListener(new FocusListener()
-		{
-			public void focusGained(FocusEvent e)
-			{
-				bindingService.setKeyFilterEnabled(false);
-			}
-
-			public void focusLost(FocusEvent e)
-			{
-				bindingService.setKeyFilterEnabled(true);
-			}
-		});
-
-		// Force refresh of CSS when theme changes
-		this._themeChangeListener = new IPreferenceChangeListener()
-		{
-			@Override
-			public void preferenceChange(PreferenceChangeEvent event)
-			{
-				if (_browser != null && event.getKey().equals(IThemeManager.THEME_CHANGED))
-				{
-					applyTheme();
-				}
-			}
-		};
-		new InstanceScope().getNode(CommonEditorPlugin.PLUGIN_ID).addPreferenceChangeListener(_themeChangeListener);
-
-		// refresh CSS when theme's font changes
-		this._fontChangeListener = new IPropertyChangeListener()
-		{
-			@Override
-			public void propertyChange(PropertyChangeEvent event)
-			{
-				if (_browser != null && event.getProperty().equals(TEXTFONT_PROPERTY))
-				{
-					applyTheme();
-				}
-			}
-		};
-		JFaceResources.getFontRegistry().addListener(this._fontChangeListener);
-
-		// create our supporting process for access to the system's shell
-		TerminalServer.getInstance().createProcess(this._id, this.getStartingDirectory());
-
-		// load the terminal
-		String url = NLS.bind(TERMINAL_URL, new Object[] { TerminalServer.getInstance().getHost(),
-				TerminalServer.getInstance().getPort() })
-				+ "?id=" + this._id; //$NON-NLS-1$
-		this.setUrl(url);
+		this.addListeners();
 	}
 
 	/**
@@ -166,6 +256,12 @@ public class TerminalBrowser
 		{
 			JFaceResources.getFontRegistry().removeListener(this._fontChangeListener);
 			this._fontChangeListener = null;
+		}
+
+		if (this._keyListener != null)
+		{
+			this._browser.removeKeyListener(this._keyListener);
+			this._keyListener = null;
 		}
 
 		try
@@ -209,6 +305,37 @@ public class TerminalBrowser
 	private String getStartingDirectory()
 	{
 		return this._startingDirectory;
+	}
+
+	/**
+	 * paste
+	 */
+	public void paste()
+	{
+		UIJob job = new UIJob("Paste into Terminal")
+		{
+			public IStatus runInUIThread(IProgressMonitor monitor)
+			{
+				Display display = PlatformUI.getWorkbench().getDisplay();
+				Clipboard clipboard = new Clipboard(display);
+				String text = (String) clipboard.getContents(TextTransfer.getInstance());
+
+				if (text != null && text.length() > 0)
+				{
+					ProcessWrapper process = TerminalServer.getInstance().getProcess(_id);
+
+					if (process != null)
+					{
+						process.sendText(text);
+					}
+				}
+
+				return Status.OK_STATUS;
+			}
+		};
+		job.setSystem(true);
+		job.setPriority(Job.INTERACTIVE);
+		job.schedule();
 	}
 
 	/**
