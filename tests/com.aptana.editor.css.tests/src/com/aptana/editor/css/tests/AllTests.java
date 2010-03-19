eba43fb627059a87cdb1d81f6c2f47044b705268
sgtcoolguy <cwilliams@aptana.com>
2010-03-19 11:38:32 -0400

    Do not run performance "tests". They have no assertions and slow down the unit test build considerably. They need to be reworked with something like JUnitPerf before we should add them, and at that point they should probably be a separate special performance test build.

--- tests/com.aptana.editor.css.tests/src/com/aptana/editor/css/tests/AllTests.java
+++ tests/com.aptana.editor.css.tests/src/com/aptana/editor/css/tests/AllTests.java
@@ -3,9 +3,7 @@ package com.aptana.editor.css.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import com.aptana.editor.css.CSSCodeScannerPerformanceTest;
 import com.aptana.editor.css.CSSCodeScannerTest;
-import com.aptana.editor.css.CSSParserPerformanceTest;
 import com.aptana.editor.css.CSSSourcePartitionScannerTest;
 
 public class AllTests
@@ -17,8 +15,8 @@ public class AllTests
 		// $JUnit-BEGIN$
 		suite.addTestSuite(CSSSourcePartitionScannerTest.class);
 		suite.addTestSuite(CSSCodeScannerTest.class);
-		suite.addTestSuite(CSSCodeScannerPerformanceTest.class);
-		suite.addTestSuite(CSSParserPerformanceTest.class);
+//		suite.addTestSuite(CSSCodeScannerPerformanceTest.class);
+//		suite.addTestSuite(CSSParserPerformanceTest.class);
 		suite.addTest(com.aptana.editor.css.parsing.AllTests.suite());
 		suite.addTest(com.aptana.editor.css.outline.AllTests.suite());
 		// $JUnit-END$
