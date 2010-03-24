b70dc79d462caa759a351523880b25fc8ea5135e
mxia <mxia@aptana.com>
2010-03-24 14:20:19 -0700

    Added ERB edtior tests.

---
+++ tests/com.aptana.editor.erb.tests/src/com/aptana/editor/erb/html/AllTests.java
@@ -0,0 +1,18 @@
+package com.aptana.editor.erb.html;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests
+{
+
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite("Tests for com.aptana.editor.erb.html"); //$NON-NLS-1$
+		// $JUnit-BEGIN$
+		suite.addTestSuite(RHTMLContentDescriberTest.class);
+		suite.addTestSuite(RHTMLEditorTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+}
