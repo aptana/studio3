69dc7c9606f29653b794b72337c5383feaa546d3
Andrew Shebanow <ashebanow@gmail.com>
2010-03-29 16:06:46 -0700

    add debug url for testing new content, commented out

--- plugins/com.aptana.explorer/src/com/aptana/explorer/internal/ui/SingleProjectView.java
+++ plugins/com.aptana.explorer/src/com/aptana/explorer/internal/ui/SingleProjectView.java
@@ -175,6 +175,7 @@ public abstract class SingleProjectView extends CommonNavigator implements ISize
 	private Browser browser;
 	private IPreferenceChangeListener fThemeChangeListener;
 	private static final String BASE_MESSAGE_URL = "http://aptana.com/tools/content/"; //$NON-NLS-1$
+	// private static final String BASE_MESSAGE_URL = "http://localhost:3000/tools/content/"; //$NON-NLS-1$
 	private static final int MINIMUM_BROWSER_HEIGHT = 150;
 	private static final int MINIMUM_BROWSER_WIDTH = 310;
 	private static final String BROWSER_ID = "message.area.browser"; //$NON-NLS-1$
