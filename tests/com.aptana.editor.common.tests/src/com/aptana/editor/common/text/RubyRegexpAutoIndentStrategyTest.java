1c123e6dffd97fcd10a1086ad955a1a728db6bfb
sgtcoolguy <cwilliams@aptana.com>
2010-03-30 11:38:12 -0400

    fix how we do auto-dedent and update tests a little. We now behave more like Textmate where we check for and do dedent on non-newline characters.

--- tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/text/RubyRegexpAutoIndentStrategyTest.java
+++ tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/text/RubyRegexpAutoIndentStrategyTest.java
@@ -11,61 +11,105 @@ import org.jruby.RubyRegexp;
 public class RubyRegexpAutoIndentStrategyTest extends TestCase
 {
 
-	private int matchCount;
-
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-		matchCount = 0;
-	}
-
-	protected void tearDown() throws Exception
-	{
-		matchCount = 0;
-		super.tearDown();
-	}
-
 	public void testDoesntEraseRestofLineAfternewlineCharSentAndDedentMatches()
 	{
-		RubyRegexpAutoIndentStrategy strategy = new RubyRegexpAutoIndentStrategy("", null, null)
+		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
+		IDocument document = new Document("\n\t</script</head>");
+		DocumentCommand command = createTextCommand(10, ">");
+		strategy.customizeDocumentCommand(document, command);
+
+		// Looks wrong, but command still runs to insert the >
+		assertEquals("\n</script</head>", document.get());
+		assertTrue(command.doit);
+		assertEquals(">", command.text);
+	}
+	
+	public void testDedent()
+	{
+		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
+		IDocument document = new Document("if a.nil?\n  a=1\n  els");
+		DocumentCommand command = createTextCommand(21, "e");
+		strategy.customizeDocumentCommand(document, command);
+
+		assertEquals("if a.nil?\n  a=1\nels", document.get());
+		assertTrue(command.doit);
+		assertEquals("e", command.text);
+		assertEquals(19, command.offset);
+	}
+
+	public void testIndentAndPushTrailingContentAfterNewlineAndCursorForTagPair()
+	{
+		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
+		IDocument document = new Document("\t<div></div>");
+		DocumentCommand command = createNewlineCommand(6);
+		strategy.customizeDocumentCommand(document, command);
+		assertEquals("\n\t\t\n\t", command.text);
+		assertTrue(command.doit);
+		document = new Document("    <div></div>");
+		command = createNewlineCommand(9);
+		strategy.customizeDocumentCommand(document, command);
+		assertEquals("\n    \t\n    ", command.text);
+		assertTrue(command.doit);
+	}
+
+	public void testDoesNotIndentAndPushTrailingContentAfterNewlineAndCursorForTagTag()
+	{
+		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
+		IDocument document = new Document("\t<div><div>");
+		DocumentCommand command = createNewlineCommand(6);
+		strategy.customizeDocumentCommand(document, command);
+		assertEquals("\n\t\t", command.text);
+		assertTrue(command.doit);
+		document = new Document("    <div><div>");
+		command = createNewlineCommand(9);
+		strategy.customizeDocumentCommand(document, command);
+		assertEquals("\n    \t", command.text);
+		assertTrue(command.doit);
+	}
+
+	public void testRR3_129()
+	{
+		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy()
 		{
-			@Override
-			protected String findCorrectIndentString(IDocument d, int lineNumber, String currentLineIndent)
-					throws BadLocationException
-			{
-				return "";
-			}
-
-			@Override
-			protected RubyRegexp getIncreaseIndentRegexp(String scope)
-			{
-				return null;
-			}
-
-			@Override
-			protected RubyRegexp getDecreaseIndentRegexp(String scope)
-			{
-				return null;
-			}
-
-			@Override
-			protected String getScopeAtOffset(IDocument d, int offset) throws BadLocationException
-			{
-				return "";
-			}
+			private int count = 0;
 
 			@Override
 			protected boolean matchesRegexp(RubyRegexp regexp, String lineContent)
 			{
-				return matchCount++ == 1;
+				// match the second call, decrease regexp
+				return count++ == 1;
+			}
+
+			@Override
+			protected String findCorrectIndentString(IDocument d, int lineNumber, String currentLineIndent)
+					throws BadLocationException
+			{
+				return "\t\t";
 			}
 		};
-		IDocument document = new Document("\n\t</script></head>");
-		DocumentCommand command = createNewlineCommand(11);
+		IDocument document = new Document(
+				"\tvar advance = function()\n\t\t{\n\t\t\treturn word = that.getWord();\n\t\t};");
+		DocumentCommand command = createNewlineCommand(66);
 		strategy.customizeDocumentCommand(document, command);
-		// Looks wrong, but command still runs to insert the newline
-		assertEquals("\n</script></head>", document.get());
+		assertEquals("\n\t\t", command.text);
 		assertTrue(command.doit);
+	}
+
+	protected DocumentCommand createNewlineCommand(int offset)
+	{
+		return createTextCommand(offset, "\n");
+	}
+
+	protected DocumentCommand createTextCommand(int offset, String text)
+	{
+		DocumentCommand command = new DocumentCommand()
+		{
+		};
+		command.text = text;
+		command.offset = offset;
+		command.caretOffset = offset;
+		command.doit = true;
+		return command;
 	}
 
 	private static class AlwaysMatchRubyRegexpAutoIndentStrategy extends RubyRegexpAutoIndentStrategy
@@ -105,75 +149,6 @@ public class RubyRegexpAutoIndentStrategyTest extends TestCase
 		{
 			return true;
 		}
-	}
-
-	public void testIndentAndPushTrailingContentAfterNewlineAndCursorForTagPair()
-	{
-		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
-		IDocument document = new Document("\t<div></div>");
-		DocumentCommand command = createNewlineCommand(6);
-		strategy.customizeDocumentCommand(document, command);
-		assertEquals("\n\t\t\n\t", command.text);
-		assertTrue(command.doit);
-		document = new Document("    <div></div>");
-		command = createNewlineCommand(9);
-		strategy.customizeDocumentCommand(document, command);
-		assertEquals("\n    \t\n    ", command.text);
-		assertTrue(command.doit);
-	}
-
-	public void testDoesNotIndentAndPushTrailingContentAfterNewlineAndCursorForTagTag()
-	{
-		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy();
-		IDocument document = new Document("\t<div><div>");
-		DocumentCommand command = createNewlineCommand(6);
-		strategy.customizeDocumentCommand(document, command);
-		assertEquals("\n\t\t", command.text);
-		assertTrue(command.doit);
-		document = new Document("    <div><div>");
-		command = createNewlineCommand(9);
-		strategy.customizeDocumentCommand(document, command);
-		assertEquals("\n    \t", command.text);
-		assertTrue(command.doit);
-	}
-	
-	public void testRR3_129()
-	{
-		RubyRegexpAutoIndentStrategy strategy = new AlwaysMatchRubyRegexpAutoIndentStrategy()
-		{
-			private int count = 0;
-			
-			@Override
-			protected boolean matchesRegexp(RubyRegexp regexp, String lineContent)
-			{
-				// match the second call, decrease regexp
-				return count++ == 1;
-			}
-			
-			@Override
-			protected String findCorrectIndentString(IDocument d, int lineNumber, String currentLineIndent)
-					throws BadLocationException
-			{
-				return "\t\t";
-			}
-		};
-		IDocument document = new Document("\tvar advance = function()\n\t\t{\n\t\t\treturn word = that.getWord();\n\t\t};");
-		DocumentCommand command = createNewlineCommand(66);
-		strategy.customizeDocumentCommand(document, command);
-		assertEquals("\n\t\t", command.text);
-		assertTrue(command.doit);
-	}
-
-	protected DocumentCommand createNewlineCommand(int offset)
-	{
-		DocumentCommand command = new DocumentCommand()
-		{
-		};
-		command.text = "\n";
-		command.offset = offset;
-		command.caretOffset = offset;
-		command.doit = true;
-		return command;
 	}
 
 }
