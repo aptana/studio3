967f8c002994e45e358d1acc9b4a0867e82d8062
Max Stepanov <mstepanov@aptana.com>
2010-04-08 17:53:55 -0700

    remove unused imports

--- plugins/com.aptana.terminal/src/com/aptana/terminal/views/TerminalView.java
+++ plugins/com.aptana.terminal/src/com/aptana/terminal/views/TerminalView.java
@@ -42,9 +42,7 @@ import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.tm.internal.terminal.control.ITerminalListener;
 import org.eclipse.tm.internal.terminal.control.ITerminalViewControl;
-import org.eclipse.tm.internal.terminal.control.TerminalViewControlFactory;
 import org.eclipse.tm.internal.terminal.provisional.api.ITerminalConnector;
-import org.eclipse.tm.internal.terminal.provisional.api.ITerminalControl;
 import org.eclipse.tm.internal.terminal.provisional.api.TerminalConnectorExtension;
 import org.eclipse.tm.internal.terminal.provisional.api.TerminalState;
 import org.eclipse.ui.IMemento;
@@ -61,7 +59,6 @@ import com.aptana.terminal.Utils;
 import com.aptana.terminal.connector.LocalTerminalConnector;
 import com.aptana.terminal.editor.TerminalEditor;
 import com.aptana.terminal.internal.IProcessListener;
-import com.aptana.terminal.internal.emulator.ThemedStyleMap;
 import com.aptana.terminal.internal.emulator.VT100TerminalControl;
 import com.aptana.terminal.preferences.IPreferenceConstants;
 
