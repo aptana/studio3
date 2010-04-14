2d4ca8cca601a86c05840b9a1bfd2896cae620fe
Max Stepanov <mstepanov@aptana.com>
2010-04-14 14:23:44 -0700

    handle partially read ESC sequences

--- native/redtty/redtty.c
+++ native/redtty/redtty.c
@@ -3,6 +3,7 @@
 #include <unistd.h> 
 #include <errno.h> 
 #include <string.h>
+#include <ctype.h>
 #include <sys/ioctl.h>
 
 #ifdef LINUX 
@@ -14,13 +15,15 @@
 #include <util.h> 
 #endif
 
-#define SET_SIZE "\033[8;"
+#define ESC '\033'
+#define MAX_ESC_SEQUENCE_LENGTH	16
 
 int 
 main (void) 
 {
 	/* Read char */
 	char buffer[1024+1];
+	int buffer_index = 0;
 	int read_count;
 		
 	/* Descriptor set for select */
@@ -61,7 +64,7 @@ main (void)
 				/* User typed something at STDIN */
 				if (FD_ISSET (STDIN_FILENO, &descriptor_set)) 
 				{
-					read_count = read(STDIN_FILENO, &buffer, sizeof(buffer)-1);
+					read_count = read(STDIN_FILENO, &buffer[buffer_index], sizeof(buffer)-buffer_index-1);
 					
 					switch (read_count)
 					{
@@ -76,35 +79,66 @@ main (void)
 							break;
 							
 						default:
-							buffer[read_count] = '\0';
-							
-							if (!strncmp(buffer, SET_SIZE, strlen(SET_SIZE)))
-							{
-								char *start = buffer + strlen(SET_SIZE);
-								char *semi = strstr(start, ";");
-								char *t = (semi != NULL) ? strstr(semi + 1, "t") : NULL;
+							buffer_index += read_count;
+							buffer[buffer_index] = '\0';
+							do {
+								char *ch;
+								int i = 0;
 								
-								if (semi != NULL && t != NULL)
+								ch = strchr(buffer, ESC);
+								if (ch == NULL)
 								{
-									int height = strtol(start, &semi, 0);
-									int width = strtol(semi + 1, &t, 0);
-									
-									struct winsize size;
-									size.ws_row = height;
-									size.ws_col = width;
-									ioctl(pty, TIOCSWINSZ, &size);
-									
-									write(pty, t + 1, read_count - (t - buffer) - 1);
+									write(pty, &buffer, buffer_index);
+									buffer_index = 0;
+									break;
 								}
-								else
+
+								i = ch - buffer;
+								if (i > 0) {
+									write(pty, buffer, i);
+									buffer_index -= i;
+									memmove(buffer, ch, buffer_index);
+									
+								}
+								for( i = 1; (i < buffer_index) && (buffer[i] != ESC) && !isalpha(buffer[i]); ++i)
 								{
-									write(pty, &buffer, read_count);
+									/* noop */
 								}
-							}
-							else
-							{
-								write(pty, &buffer, read_count);
-							}
+								if (i < buffer_index)
+								{
+									int param[4];
+									switch (buffer[i])
+									{
+										case ESC:
+											write(pty, buffer, i);
+											--i;
+											break;
+										case 't':
+											if ((sscanf(&buffer[1], "[%d;%d;%dt", &param[0], &param[1], &param[2]) == 3) && (param[0] == 8))
+											{
+												struct winsize size;
+												size.ws_row = param[1];
+												size.ws_col = param[2];
+												ioctl(pty, TIOCSWINSZ, &size);
+												break;
+											}
+										default:
+											write(pty, buffer, i+1);
+											break;
+									}
+									buffer_index -= i + 1;
+									if (buffer_index > 0) {
+										memmove(buffer, &buffer[i+1], buffer_index);
+										continue;
+									}
+								} else {
+									if (buffer_index > MAX_ESC_SEQUENCE_LENGTH) {
+										write(pty, &buffer, buffer_index);
+										buffer_index = 0;
+									}
+								}
+
+							} while (0);
 							break;
 					}
 				} 
