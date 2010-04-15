6b689e5a5e8c031efdd1884965ce72b482098cbd
mxia <mxia@aptana.com>
2010-04-15 14:44:45 -0700

    Added com.aptana.ui plugin to the feature.

---
+++ plugins/com.aptana.ui/src/com/aptana/ui/internal/WebPerspectiveFactory.java
@@ -0,0 +1,13 @@
+package com.aptana.ui.internal;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class WebPerspectiveFactory implements IPerspectiveFactory
+{
+
+	@Override
+	public void createInitialLayout(IPageLayout layout)
+	{
+	}
+}
