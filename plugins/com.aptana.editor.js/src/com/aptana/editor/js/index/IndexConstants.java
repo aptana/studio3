239d363cbbe0fa890efc23f9912f459c8454e17e
Kevin Lindsey <klindsey@aptana.com>
2010-04-23 15:32:33 -0500

    Temporarily change delimiter to something that is visible.
    Added constant for property category.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/index/IndexConstants.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/index/IndexConstants.java
@@ -4,9 +4,10 @@ public interface IndexConstants
 {
 	// general constants
 	static final String METADATA = "js.metadata";
-	static final String DELIMITER = "\0";
+	static final String DELIMITER = "~";
 	
 	// index categories
 	static final String TYPE = "js.type";
 	static final String FUNCTION = "js.function";
+	static final String PROPERTY = "js.property";
 }
