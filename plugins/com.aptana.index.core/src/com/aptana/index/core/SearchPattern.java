e1fb065af2bbcbbd21de3d9e7906b71feea48977
Kevin Lindsey <klindsey@aptana.com>
2010-04-23 15:32:49 -0500

    Add bit flag for regex pattern matching

--- plugins/com.aptana.index.core/src/com/aptana/index/core/SearchPattern.java
+++ plugins/com.aptana.index.core/src/com/aptana/index/core/SearchPattern.java
@@ -6,5 +6,6 @@ public abstract class SearchPattern
 	public static final int PREFIX_MATCH = 0x0001;
 	public static final int PATTERN_MATCH = 0x0002;
 	public static final int CASE_SENSITIVE = 0x0008;
+	public static final int REGEX_MATCH = 0x0010;
 
 }
