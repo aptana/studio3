90692667567283411bd14839b2d770dc4aea332d
sgtcoolguy <cwilliams@aptana.com>
2010-04-27 11:13:36 -0400

    absolute minimum code ported from Studio to get expansion of external JS script tags.
    Still needs lots of loving: Should be a lazy tree so the resolution/downloading/parsing of the external files can be done asynch. Should use EFS to grab down the files to a local cache (which means we'd need an http/https IFileSystem). Needs to handle external stylesheets.

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/outline/HTMLOutlineContentProvider.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/outline/HTMLOutlineContentProvider.java
@@ -1,21 +1,32 @@
 package com.aptana.editor.html.outline;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 
 import com.aptana.editor.common.outline.CommonOutlineItem;
 import com.aptana.editor.common.outline.CompositeOutlineContentProvider;
 import com.aptana.editor.css.outline.CSSOutlineContentProvider;
 import com.aptana.editor.css.parsing.ICSSParserConstants;
+import com.aptana.editor.html.Activator;
 import com.aptana.editor.html.parsing.ast.HTMLElementNode;
 import com.aptana.editor.html.parsing.ast.HTMLSpecialNode;
 import com.aptana.editor.js.outline.JSOutlineContentProvider;
 import com.aptana.editor.js.parsing.IJSParserConstants;
+import com.aptana.editor.js.parsing.JSParser;
+import com.aptana.parsing.IParseState;
+import com.aptana.parsing.ParseState;
 import com.aptana.parsing.ast.IParseNode;
 import com.aptana.parsing.ast.ParseRootNode;
 
 public class HTMLOutlineContentProvider extends CompositeOutlineContentProvider
 {
+	
+	private Map<String, Object[]> cache = new HashMap<String, Object[]>();
 
 	public HTMLOutlineContentProvider()
 	{
@@ -35,9 +46,112 @@ public class HTMLOutlineContentProvider extends CompositeOutlineContentProvider
 		{
 			// HTMLSpecialNode always has the root node of the nested language as its child; we want to skip that and
 			// get the content below
-			return getChildren(((HTMLSpecialNode) parentElement).getChild(0));
+			HTMLSpecialNode item = (HTMLSpecialNode) parentElement;
+
+			// addition to allow expansion of script tags with src attribute;
+			if (item.getName().equals("script")) { //$NON-NLS-1$
+				String attribute = item.getAttributeValue("src"); //$NON-NLS-1$
+				if (attribute.length() > 0)
+				{
+					return getExternalScriptChildren(attribute,item);
+				}
+			}			
+			
+			return getChildren(item.getChild(0));
 		}
 		return super.getChildren(parentElement);
+	}
+	
+	private Object[] getExternalScriptChildren(final String attribute, HTMLSpecialNode parent)
+	{
+		Object[] cached = cache.get(attribute);
+		if (cached != null)
+		{
+			//we have a cached result
+			return cached;
+		}
+
+		if (resolver == null)
+		{
+			return EMPTY;
+		}
+		try
+		{
+			//registering listener
+//			IPropertyChangeListener propertyChangeListener = listeners.get(attribute);
+//			if (propertyChangeListener == null)
+//			{
+//				propertyChangeListener = new IPropertyChangeListener()
+//				{
+//
+//					public void propertyChange(PropertyChangeEvent event)
+//					{
+//						cache.remove(attribute);
+//					}
+//
+//				};
+//				resolver.addChangeListener(attribute, propertyChangeListener);
+//			}
+//			listeners.put(attribute, propertyChangeListener);
+			
+			//resolving source and editor input
+			String source = resolver.resolveSource(attribute);
+//			IEditorInput input = resolver.resolveEditorInput(attribute);
+			if (source == null)
+			{
+				return EMPTY;
+//				return new Object[] { new WarningItem(StringUtils.format(
+//						HTMLContentProviderMessages.HTMLContentProvider_NOT_RESOLVABLE, attribute)) };
+			}
+			JSParser ps;
+//			try
+//			{
+				//parsing
+				ps = new JSParser();
+				IParseState pState = new ParseState();
+				pState.setEditState(source, source, 0, 0);
+				IParseNode parse = ps.parse(pState);
+//				JSOutlineContentProvider provider = new JSOutlineContentProvider();
+				
+				Object[] elements = new Object[] { parse.getChild(0) };
+				//acquiring items
+//				Object[] elements = provider.getElements(parse.getChildren());
+//				for (int a = 0; a < elements.length; a++)
+//				{
+//					if (elements[a] instanceof JSOutlineItem)
+//					{
+//						JSOutlineItem item = (JSOutlineItem) elements[a];
+//						item.setResolveInformation(input);
+//						item.setParent(parent);
+//						parent.addChild(item);
+//					}
+//				}
+				
+				//caching result
+				cache.put(attribute, elements);
+				return elements;
+//			}
+//			catch (ParserInitializationException e)
+//			{
+//				IdeLog.logError(HTMLPlugin.getDefault(), e.getMessage());
+//				return NO_OBJECTS;
+//			}
+//			catch (LexerException e)
+//			{
+//				IdeLog.logError(HTMLPlugin.getDefault(), e.getMessage());
+//				return NO_OBJECTS;
+//			}
+
+		}
+		catch (Exception e)
+		{
+			Activator.getDefault().getLog().log(new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e));
+//			WarningItem warningItem = new WarningItem(e.getMessage());
+//			warningItem.setError(true);
+//			return new Object[] { warningItem };
+			return EMPTY;
+		}
+
 	}
 
 	@Override
