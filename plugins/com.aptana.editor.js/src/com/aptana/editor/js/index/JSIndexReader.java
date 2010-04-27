89e73838ac5d34bf5736543bd5d8b4c3acc0f672
Kevin Lindsey <klindsey@aptana.com>
2010-04-27 09:49:46 -0500

    Add ability to read the properties and functions of a type

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/index/JSIndexReader.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/index/JSIndexReader.java
@@ -2,9 +2,11 @@ package com.aptana.editor.js.index;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.aptana.editor.js.model.FunctionElement;
+import com.aptana.editor.js.model.ParameterElement;
 import com.aptana.editor.js.model.PropertyElement;
 import com.aptana.editor.js.model.TypeElement;
 import com.aptana.index.core.Index;
@@ -13,28 +15,171 @@ import com.aptana.index.core.SearchPattern;
 
 public class JSIndexReader
 {
+	/**
+	 * getDescription
+	 * 
+	 * @param index
+	 * @param descriptionKey
+	 * @return
+	 * @throws IOException
+	 */
 	public String getDescription(Index index, String descriptionKey) throws IOException
 	{
-		String result;
-		
+		String result = "";
+
 		// grab description
 		String descriptionPattern = descriptionKey + IndexConstants.DELIMITER;
 		List<QueryResult> descriptions = index.query(new String[] { IndexConstants.DESCRIPTION }, descriptionPattern, SearchPattern.PREFIX_MATCH);
-		
+
 		if (descriptions != null)
 		{
 			String descriptionValue = descriptions.get(0).getWord();
-			
+
 			result = descriptionValue.substring(descriptionValue.indexOf(IndexConstants.DELIMITER) + 1);
 		}
-		else
-		{
-			result = "";
-		}
-		
+
 		return result;
 	}
 	
+	/**
+	 * getFunctions
+	 * 
+	 * @param index
+	 * @param owningType
+	 * @return
+	 * @throws IOException
+	 */
+	public List<FunctionElement> getFunctions(Index index, String owningType) throws IOException
+	{
+		// read properties
+		List<QueryResult> functions = index.query(new String[] { IndexConstants.FUNCTION }, this.getMemberPattern(owningType), SearchPattern.REGEX_MATCH);
+		List<FunctionElement> result = new LinkedList<FunctionElement>();
+
+		if (functions != null)
+		{
+			for (QueryResult function : functions)
+			{
+				String[] columns = function.getWord().split(IndexConstants.DELIMITER);
+				String functionName = columns[0];
+				String descriptionKey = columns[2];
+				List<ParameterElement> parameters = this.getParameters(index, columns[3]);
+				
+				FunctionElement f = new FunctionElement();
+
+				f.setName(functionName);
+				f.setDescription(this.getDescription(index, descriptionKey));
+				
+				for (ParameterElement parameter : parameters)
+				{
+					f.addParameter(parameter);
+				}
+
+				result.add(f);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * getMemberPattern
+	 * 
+	 * @param typeName
+	 * @return
+	 */
+	private String getMemberPattern(String typeName)
+	{
+		return MessageFormat.format("^[^{0}]+{0}{1}(?:{0}|$)", new Object[] { IndexConstants.DELIMITER, typeName });
+	}
+
+	/**
+	 * getProperties
+	 * 
+	 * @param index
+	 * @param owningType
+	 * @return
+	 * @throws IOException
+	 */
+	public List<PropertyElement> getProperties(Index index, String owningType) throws IOException
+	{
+		// read properties
+		List<QueryResult> properties = index.query(new String[] { IndexConstants.PROPERTY }, this.getMemberPattern(owningType), SearchPattern.REGEX_MATCH);
+		List<PropertyElement> result = new LinkedList<PropertyElement>();
+
+		if (properties != null)
+		{
+			for (QueryResult property : properties)
+			{
+				String[] columns = property.getWord().split(IndexConstants.DELIMITER);
+				String propertyName = columns[0];
+				String descriptionKey = columns[2];
+				PropertyElement p = new PropertyElement();
+
+				p.setName(propertyName);
+				p.setDescription(this.getDescription(index, descriptionKey));
+
+				result.add(p);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * getParameters
+	 * 
+	 * @param index
+	 * @param parametersKey
+	 * @return
+	 * @throws IOException
+	 */
+	public List<ParameterElement> getParameters(Index index, String parametersKey) throws IOException
+	{
+		String descriptionPattern = parametersKey + IndexConstants.DELIMITER;
+		List<QueryResult> parameters = index.query(new String[] { IndexConstants.PARAMETERS }, descriptionPattern, SearchPattern.PREFIX_MATCH);
+		List<ParameterElement> result = new LinkedList<ParameterElement>();
+
+		if (parameters != null && parameters.size() > 0)
+		{
+			String parametersValue = parameters.get(0).getWord();
+			String[] parameterValues = parametersValue.split(IndexConstants.DELIMITER);
+
+			for (int i = 1; i < parameterValues.length; i++)
+			{
+				String parameterValue = parameterValues[i];
+				String[] columns = parameterValue.split(",");
+				ParameterElement parameter = new ParameterElement();
+
+				parameter.setName(columns[0]);
+				parameter.setUsage(columns[1]);
+
+				for (int j = 2; j < columns.length; j++)
+				{
+					parameter.addType(columns[j]);
+				}
+				
+				result.add(parameter);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * getTypeProperties
+	 * 
+	 * @return
+	 * @throws IOException 
+	 */
+	public List<PropertyElement> getTypeProperties(Index index, String typeName) throws IOException
+	{
+		List<PropertyElement> properties = this.getProperties(index, typeName);
+		
+		properties.addAll(this.getFunctions(index, typeName));
+		
+		return properties;
+	}
+
 	/**
 	 * readType
 	 * 
@@ -42,115 +187,47 @@ public class JSIndexReader
 	 * @param typeName
 	 * @return
 	 */
-	public TypeElement readType(Index index, String typeName)
+	public TypeElement loadType(Index index, String typeName)
 	{
 		TypeElement result = null;
-		
+
 		try
 		{
 			String pattern = typeName + IndexConstants.DELIMITER;
 			List<QueryResult> types = index.query(new String[] { IndexConstants.TYPE }, pattern, SearchPattern.PREFIX_MATCH);
-			
+
 			if (types != null && types.size() > 0)
 			{
 				String[] columns = types.get(0).getWord().split(IndexConstants.DELIMITER);
 				String retrievedName = columns[0];
 				String[] parentTypes = columns[1].split(",");
 				String descriptionKey = columns[2];
-				
+
+				// create type
 				result = new TypeElement();
+				
+				// set its name
 				result.setName(retrievedName);
+				
+				// add in the types it inherits
 				for (String parentType : parentTypes)
 				{
 					result.addParentType(parentType);
 				}
+				
+				// set the description
 				result.setDescription(this.getDescription(index, descriptionKey));
 
-				// create pattern for doing member queries
-				String memberPattern = MessageFormat.format(
-					"^[^{0}]+{0}{1}(?:{0}|$)",
-					new Object[] { IndexConstants.DELIMITER, typeName }
-				);
-				
-				// read properties
-				List<QueryResult> properties = index.query(new String[] { IndexConstants.PROPERTY }, memberPattern, SearchPattern.REGEX_MATCH);
-				
-				if (properties != null)
+				// add properties
+				for (PropertyElement property : this.getProperties(index, retrievedName))
 				{
-					for (QueryResult property : properties)
-					{
-						String[] propertyColumns = property.getWord().split(IndexConstants.DELIMITER);
-						String propertyName = propertyColumns[0];
-						
-						PropertyElement p = new PropertyElement();
-						p.setName(propertyName);
-						
-						result.addProperty(p);
-					}
+					result.addProperty(property);
 				}
-				
-				// read methods
-				List<QueryResult> methods = index.query(new String[] { IndexConstants.FUNCTION }, memberPattern, SearchPattern.REGEX_MATCH);
-				
-				if (methods != null)
+
+				// add functions
+				for (FunctionElement function: this.getFunctions(index, retrievedName))
 				{
-					for (QueryResult method : methods)
-					{
-						String[] methodColumns = method.getWord().split(IndexConstants.DELIMITER);
-						String methodName = methodColumns[0];
-						
-						descriptionKey = methodColumns[2]; 
-						
-						FunctionElement f = new FunctionElement();
-						f.setName(methodName);
-						f.setDescription(this.getDescription(index, descriptionKey));
-						
-						result.addProperty(f);
-					}
-				}
-				
-				// read description
-			}
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
-	/**
-	 * getFunction
-	 * 
-	 * @param index
-	 * @param owningType
-	 * @param name
-	 * @return
-	 */
-	public FunctionElement getFunction(Index index, String owningType, String name)
-	{
-		String pattern = MessageFormat.format("{1}{0}{2}", IndexConstants.DELIMITER, name, owningType);
-		FunctionElement result = null;
-		
-		try
-		{
-			List<QueryResult> functions = index.query(new String[] { IndexConstants.FUNCTION }, pattern, SearchPattern.PREFIX_MATCH);
-			
-			if (functions != null)
-			{
-				for (QueryResult function : functions)
-				{
-					String[] methodColumns = function.getWord().split(IndexConstants.DELIMITER);
-					String methodName = methodColumns[0];
-					String descriptionKey = methodColumns[2]; 
-					
-					result = new FunctionElement();
-					result.setName(methodName);
-					result.setDescription(this.getDescription(index, descriptionKey));
-					
-					// TODO: only handling the first function right now
-					break;
+					result.addProperty(function);
 				}
 			}
 		}
@@ -158,7 +235,7 @@ public class JSIndexReader
 		{
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
 }
