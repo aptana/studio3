41ba1377b0b6ac8a2ca1aad4d17e6d3995795eb4
Kevin Lindsey <klindsey@aptana.com>
2010-04-28 15:22:53 -0500

    Add CSS delimiter constant

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexConstants.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexConstants.java
@@ -2,8 +2,11 @@ package com.aptana.editor.css.contentassist.index;
 
 public interface CSSIndexConstants
 {
+	// general constants
+	static final String DELIMITER = "~";
 	static final String METADATA = "css.metadata";
 	
+	// index categories
 	static final String ELEMENT = "css.element"; //$NON-NLS-1$
 	static final String PROPERTY = "css.property"; //$NON-NLS-1$
 	static final String CLASS = "css.class"; //$NON-NLS-1$
