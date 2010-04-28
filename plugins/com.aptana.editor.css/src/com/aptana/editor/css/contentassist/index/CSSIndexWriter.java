7aaf39c60b3d8ab1e73ba93a4a49cdad6fa48246
Kevin Lindsey <klindsey@aptana.com>
2010-04-28 15:23:17 -0500

    Basic writing of CSS element metadata

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexWriter.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexWriter.java
@@ -2,7 +2,10 @@ package com.aptana.editor.css.contentassist.index;
 
 import java.io.InputStream;
 
+import com.aptana.editor.css.contentassist.model.ElementElement;
+import com.aptana.editor.css.contentassist.model.PropertyElement;
 import com.aptana.index.core.Index;
+import com.aptana.util.StringUtil;
 
 public class CSSIndexWriter
 {
@@ -14,6 +17,16 @@ public class CSSIndexWriter
 	public CSSIndexWriter()
 	{
 		this._reader = new CSSMetadataReader();
+	}
+	
+	/**
+	 * getDocumentPath
+	 * 
+	 * @return
+	 */
+	protected String getDocumentPath()
+	{
+		return CSSIndexConstants.METADATA;
 	}
 	
 	/**
@@ -29,11 +42,53 @@ public class CSSIndexWriter
 	}
 
 	/**
+	 * writeElement
+	 * 
+	 * @param index
+	 * @param element
+	 */
+	protected void writeElement(Index index, ElementElement element)
+	{
+		String[] columns = new String[] {
+			element.getName(),
+			element.getDisplayName(),
+			element.getDescription(),
+			element.getExample(),
+			element.getRemark()
+			// properties
+			// user agents
+		};
+		String key = StringUtil.join(CSSIndexConstants.DELIMITER, columns);
+		
+		index.addEntry(CSSIndexConstants.ELEMENT, key, this.getDocumentPath());
+	}
+	
+	/**
+	 * writeProperty
+	 * 
+	 * @param index
+	 * @param property
+	 */
+	protected void writeProperty(Index index, PropertyElement property)
+	{
+		
+	}
+	
+	/**
 	 * writeToIndex
 	 * 
 	 * @param index
 	 */
 	public void writeToIndex(Index index)
 	{
+		for (ElementElement element : this._reader.getElements())
+		{
+			this.writeElement(index, element);
+		}
+		
+		for (PropertyElement property : this._reader.getProperties())
+		{
+			this.writeProperty(index, property);
+		}
 	}
 }
