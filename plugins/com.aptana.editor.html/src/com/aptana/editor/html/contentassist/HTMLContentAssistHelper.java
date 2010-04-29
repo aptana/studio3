8154e5f2055c5cdffb3965ceb0fcddeec25dfd26
Kevin Lindsey <klindsey@aptana.com>
2010-04-29 15:19:51 -0500

    Create HTML content assist helper class for index queries

---
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/HTMLContentAssistHelper.java
@@ -0,0 +1,54 @@
+package com.aptana.editor.html.contentassist;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.aptana.editor.html.contentassist.index.HTMLIndexConstants;
+import com.aptana.editor.html.contentassist.index.HTMLIndexReader;
+import com.aptana.editor.html.contentassist.model.ElementElement;
+import com.aptana.index.core.Index;
+import com.aptana.index.core.IndexManager;
+
+public class HTMLContentAssistHelper
+{
+	private HTMLIndexReader _reader;
+	
+	/**
+	 * HTMLContentAssistHelper
+	 */
+	public HTMLContentAssistHelper()
+	{
+		this._reader = new HTMLIndexReader();
+	}
+	
+	/**
+	 * getElements
+	 * 
+	 * @return
+	 */
+	public List<ElementElement> getElements()
+	{
+		List<ElementElement> result = null;
+		
+		try
+		{
+			result = this._reader.getElements(this.getIndex());
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * getIndex
+	 * 
+	 * @return
+	 */
+	private Index getIndex()
+	{
+		return IndexManager.getInstance().getIndex(HTMLIndexConstants.METADATA);
+	}
+}
