52c50de2f15bdd74f16cfdb8d1de1cfb699c39c1
Kevin Lindsey <klindsey@aptana.com>
2010-04-29 15:18:17 -0500

    Add constants for delimiters and categories

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexConstants.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexConstants.java
@@ -2,7 +2,15 @@ package com.aptana.editor.html.contentassist.index;
 
 public interface HTMLIndexConstants
 {
+	// general constants
+	static final String DELIMITER = "~";
+	static final String SUB_DELIMITER = ",";
 	static final String METADATA = "html.metadata";
+	
+	// index categories
+	static final String ELEMENT = "html.element";
+	static final String ATTRIBUTE = "html.attribute";
+	static final String EVENT = "html.event";
 	
 	static final String RESOURCE_CSS = "html.resource.css"; //$NON-NLS-1$
 	static final String RESOURCE_JS = "html.resource.js"; //$NON-NLS-1$
