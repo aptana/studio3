21a103238df338a01b08e8a85889568485cfd712
Kevin Lindsey <klindsey@aptana.com>
2010-04-30 09:37:57 -0500

    Add hashCode and equals to UserAgentElement so we can use them correctly as Map keys

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/model/UserAgentElement.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/model/UserAgentElement.java
@@ -1,17 +1,47 @@
 package com.aptana.editor.css.contentassist.model;
 
+import com.aptana.util.StringUtil;
+
 public class UserAgentElement
 {
 	private String _description;
 	private String _os;
 	private String _platform;
 	private String _version;
+	private int _hash;
 
 	/**
 	 * UserAgentElement
 	 */
 	public UserAgentElement()
 	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		boolean result = false;
+		
+		if (this == obj)
+		{
+			result = true;
+		}
+		else if (obj instanceof UserAgentElement)
+		{
+			UserAgentElement that = (UserAgentElement) obj;
+			
+			result =
+				StringUtil.areEqual(this.getDescription(), ((UserAgentElement) obj).getDescription())
+			&&	StringUtil.areEqual(this.getOS(), that.getOS())
+			&&	StringUtil.areEqual(this.getPlatform(), that.getPlatform())
+			&&	StringUtil.areEqual(this.getVersion(), that.getVersion());
+		}
+		
+		return result;
 	}
 
 	/**
@@ -52,6 +82,38 @@ public class UserAgentElement
 		return this._version;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		int h = this._hash;
+		
+		if (h == 0)
+		{
+			String[] items = new String[] {
+				this._description,
+				this._os,
+				this._platform,
+				this._version
+			};
+			
+			for (String item : items)
+			{
+				if (item != null)
+				{
+					h = 31*h + item.hashCode();
+				}
+			}
+			
+			this._hash = h;
+		}
+		
+		return h;
+	}
+
 	/**
 	 * setDescription
 	 * 
@@ -60,6 +122,7 @@ public class UserAgentElement
 	public void setDescription(String description)
 	{
 		this._description = description;
+		this._hash = 0;
 	}
 
 	/**
@@ -70,6 +133,7 @@ public class UserAgentElement
 	public void setOS(String os)
 	{
 		this._os = os;
+		this._hash = 0;
 	}
 
 	/**
@@ -80,6 +144,7 @@ public class UserAgentElement
 	public void setPlatform(String platform)
 	{
 		this._platform = platform;
+		this._hash = 0;
 	}
 
 	/**
@@ -90,5 +155,6 @@ public class UserAgentElement
 	public void setVersion(String version)
 	{
 		this._version = version;
+		this._hash = 0;
 	}
 }
