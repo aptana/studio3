ac5be94f38cdd85c64e0c06c54df7e0fd1b891fc
sgtcoolguy <cwilliams@aptana.com>
2010-05-03 15:00:03 -0400

    minor cleanup

--- plugins/com.aptana.editor.common/src/com/aptana/editor/common/outline/URIResolver.java
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/outline/URIResolver.java
@@ -13,6 +13,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 
+import com.aptana.editor.common.CommonEditorPlugin;
 import com.aptana.util.IOUtil;
 
 /**
@@ -37,10 +38,16 @@ class URIResolver implements IPathResolver
 		URI uri = resolveURI(path);
 		sub.worked(5);
 		// get the filesystem that can handle the URI
-		IFileSystem fileSystem = EFS.getFileSystem(uri.getScheme());
-		IFileStore store = fileSystem.getStore(uri);
-		// grab down a local copy TODO What if file is already a local file?
-		File aFile = store.toLocalFile(EFS.CACHE, sub.newChild(90));
+		IFileStore store = getFileStore(uri);
+
+		int options = EFS.CACHE;
+		// If file is local no need to cache
+		if (store.getFileSystem().equals(EFS.getLocalFileSystem()))
+		{
+			options = EFS.NONE;
+		}
+		// grab down a local copy
+		File aFile = store.toLocalFile(options, sub.newChild(90));
 		if (aFile == null || !aFile.exists())
 		{
 			// Need to pass up correct original filename and says that's the one that doesn't exist
@@ -48,6 +55,14 @@ class URIResolver implements IPathResolver
 		}
 		// now read in the local copy
 		return IOUtil.read(new FileInputStream(aFile));
+	}
+
+	protected IFileStore getFileStore(URI uri) throws CoreException
+	{
+		IFileSystem fileSystem = EFS.getFileSystem(uri.getScheme());
+		if (fileSystem == null)
+			return EFS.getNullFileSystem().getStore(uri);
+		return fileSystem.getStore(uri);
 	}
 
 	public URI resolveURI(String path)
@@ -70,12 +85,10 @@ class URIResolver implements IPathResolver
 			uri = baseURI.resolve(path);
 			// TODO What if it fails here, then what do we do?
 		}
-		
-		
+
 		try
 		{
-			IFileSystem fileSystem = EFS.getFileSystem(uri.getScheme());
-			IFileStore store = fileSystem.getStore(uri);
+			IFileStore store = getFileStore(uri);
 			IFileInfo info = store.fetchInfo();
 			if (info.exists())
 			{
@@ -84,10 +97,9 @@ class URIResolver implements IPathResolver
 		}
 		catch (CoreException e)
 		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			CommonEditorPlugin.logError(e);
 		}
-		
+
 		return null;
 	}
 }
