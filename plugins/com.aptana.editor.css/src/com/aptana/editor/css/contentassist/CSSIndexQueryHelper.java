7d4086720c10852769b520e9dfb604cc6772ae8a
Kevin Lindsey <klindsey@aptana.com>
2010-05-03 10:31:04 -0500

    Add ability to return all HTML/CSS classes and all HTML/CSS ids.

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/CSSIndexQueryHelper.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/CSSIndexQueryHelper.java
@@ -1,7 +1,9 @@
 package com.aptana.editor.css.contentassist;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.aptana.editor.css.contentassist.index.CSSIndexConstants;
 import com.aptana.editor.css.contentassist.index.CSSIndexReader;
@@ -9,11 +11,13 @@ import com.aptana.editor.css.contentassist.model.ElementElement;
 import com.aptana.editor.css.contentassist.model.PropertyElement;
 import com.aptana.index.core.Index;
 import com.aptana.index.core.IndexManager;
+import com.aptana.index.core.QueryResult;
+import com.aptana.index.core.SearchPattern;
 
 public class CSSIndexQueryHelper
 {
 	private CSSIndexReader _reader;
-	
+
 	/**
 	 * CSSContentAssistHelper
 	 */
@@ -21,7 +25,17 @@ public class CSSIndexQueryHelper
 	{
 		this._reader = new CSSIndexReader();
 	}
-	
+
+	/**
+	 * getClasses
+	 * 
+	 * @return
+	 */
+	public Map<String, String> getClasses(Index index)
+	{
+		return this.getValues(index, CSSIndexConstants.CLASS);
+	}
+
 	/**
 	 * getElements
 	 * 
@@ -30,7 +44,7 @@ public class CSSIndexQueryHelper
 	public List<ElementElement> getElements()
 	{
 		List<ElementElement> result = null;
-		
+
 		try
 		{
 			result = this._reader.getElements(this.getIndex());
@@ -43,10 +57,21 @@ public class CSSIndexQueryHelper
 		{
 			t.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
+	/**
+	 * getIDs
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public Map<String, String> getIDs(Index index)
+	{
+		return this.getValues(index, CSSIndexConstants.IDENTIFIER);
+	}
+
 	/**
 	 * getIndex
 	 * 
@@ -65,7 +90,7 @@ public class CSSIndexQueryHelper
 	public List<PropertyElement> getProperties()
 	{
 		List<PropertyElement> result = null;
-		
+
 		try
 		{
 			result = this._reader.getProperties(this.getIndex());
@@ -78,7 +103,46 @@ public class CSSIndexQueryHelper
 		{
 			t.printStackTrace();
 		}
-		
+
+		return result;
+	}
+
+	/**
+	 * getValues
+	 * 
+	 * @return
+	 */
+	public Map<String, String> getValues(Index index, String category)
+	{
+		Map<String, String> result = null;
+
+		if (index != null)
+		{
+			String pattern = "*";
+
+			try
+			{
+				List<QueryResult> items = index.query(new String[] { category }, pattern, SearchPattern.PATTERN_MATCH);
+
+				if (items != null && items.size() > 0)
+				{
+					result = new HashMap<String, String>();
+
+					for (QueryResult item : items)
+					{
+						String[] paths = item.getDocuments();
+						String path = (paths != null && paths.length > 0) ? paths[0] : "";
+
+						result.put(item.getWord(), path);
+					}
+				}
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+
 		return result;
 	}
 }
