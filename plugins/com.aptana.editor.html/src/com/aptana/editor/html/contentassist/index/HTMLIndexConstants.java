79b4a54b9ad4cbddfe3672154a612a8877b7e2b3
Kevin Lindsey <klindsey@aptana.com>
2010-05-04 16:42:20 -0500

    Add user agent and "core" constants

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexConstants.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexConstants.java
@@ -6,11 +6,13 @@ public interface HTMLIndexConstants
 	static final String DELIMITER = "~";
 	static final String SUB_DELIMITER = ",";
 	static final String METADATA = "html.metadata";
+	static final String CORE = "HTML Core";
 	
 	// index categories
 	static final String ELEMENT = "html.element";
 	static final String ATTRIBUTE = "html.attribute";
 	static final String EVENT = "html.event";
+	static final String USER_AGENT = "html.user_agent";
 	
 	static final String RESOURCE_CSS = "html.resource.css"; //$NON-NLS-1$
 	static final String RESOURCE_JS = "html.resource.js"; //$NON-NLS-1$
