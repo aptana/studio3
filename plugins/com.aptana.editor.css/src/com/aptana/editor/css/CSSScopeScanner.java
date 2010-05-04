af3a06c17ddd32a1a40817f1d692a3b64e5445fd
Kevin Lindsey <klindsey@aptana.com>
2010-05-04 09:23:23 -0500

    minor formatting

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/CSSScopeScanner.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/CSSScopeScanner.java
@@ -9,21 +9,24 @@ import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.MultiLineRule;
 import org.eclipse.jface.text.rules.SingleLineRule;
 import org.eclipse.jface.text.rules.Token;
-import com.aptana.editor.css.CSSSourceConfiguration.WordPredicateRule;
 
+import com.aptana.editor.css.CSSSourceConfiguration.WordPredicateRule;
 
 public class CSSScopeScanner extends CSSCodeScanner
 {
+	/**
+	 * CSSScopeScanner
+	 */
 	public CSSScopeScanner()
 	{
 		List<IRule> rules = new ArrayList<IRule>();
-		
+
 		// Add the rules for block comments, single and double quoted strings
 		rules.add(new SingleLineRule("\"", "\"", createToken("string.quoted.double.css"), '\\')); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		rules.add(new SingleLineRule("\'", "\'", createToken("string.quoted.single.css"), '\\')); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 		rules.add(new WordPredicateRule(createToken("comment.block.css"))); //$NON-NLS-1$
 		rules.add(new MultiLineRule("/*", "*/", createToken("comment.block.css"), (char) 0, true)); //$NON-NLS-1$ //$NON-NLS-2$//$NON-NLS-3$
-		
+
 		if (fRules != null)
 		{
 			// Add the rules created by the super class
@@ -32,12 +35,15 @@ public class CSSScopeScanner extends CSSCodeScanner
 
 		setRules(rules.toArray(new IRule[rules.size()]));
 	}
-	
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.css.CSSCodeScanner#createToken(java.lang.String)
+	 */
 	@Override
 	protected IToken createToken(String string)
 	{
 		// Simply use the token type string that is passed in as the data
 		return new Token(string);
 	}
-
 }
