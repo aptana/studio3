d854e4ff6910d12fc21e8d7c5bb8dcef3d084a89
Kevin Lindsey <klindsey@aptana.com>
2010-05-04 10:46:35 -0500

    Implement listing of all properties in a CSS index
    Support reading of values when reading PropertyElements
    
    Minor refactoring of common code for creating new PropertyElements.

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexReader.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/index/CSSIndexReader.java
@@ -7,6 +7,7 @@ import java.util.List;
 import com.aptana.editor.css.contentassist.model.ElementElement;
 import com.aptana.editor.css.contentassist.model.PropertyElement;
 import com.aptana.editor.css.contentassist.model.UserAgentElement;
+import com.aptana.editor.css.contentassist.model.ValueElement;
 import com.aptana.index.core.Index;
 import com.aptana.index.core.QueryResult;
 import com.aptana.index.core.SearchPattern;
@@ -19,7 +20,48 @@ public class CSSIndexReader
 	public CSSIndexReader()
 	{
 	}
-	
+
+	/**
+	 * createPropertyFromKey
+	 * 
+	 * @param index
+	 * @param key
+	 * @return
+	 * @throws IOException
+	 */
+	private PropertyElement createPropertyFromKey(Index index, String key) throws IOException
+	{
+		String columns[] = key.split(CSSIndexConstants.DELIMITER);
+		int column = 0;
+		PropertyElement property = new PropertyElement();
+
+		property.setName(columns[column++]);
+		property.setAllowMultipleValues(Boolean.valueOf(columns[column++]));
+		property.setType(columns[column++]);
+		// TODO: specifications
+
+		for (String userAgentKey : columns[column++].split(CSSIndexConstants.SUB_DELIMITER))
+		{
+			// get user agent and add to element
+			property.addUserAgent(this.getUserAgent(index, userAgentKey));
+		}
+
+		property.setDescription(columns[column++]);
+		property.setExample(columns[column++]);
+		property.setHint(columns[column++]);
+		property.setRemark(columns[column++]);
+
+		if (column < columns.length)
+		{
+			for (String valueKey : columns[column++].split(CSSIndexConstants.SUB_DELIMITER))
+			{
+				property.addValue(this.getValue(index, valueKey));
+			}
+		}
+
+		return property;
+	}
+
 	/**
 	 * getElements
 	 * 
@@ -29,7 +71,8 @@ public class CSSIndexReader
 	 */
 	public List<ElementElement> getElements(Index index) throws IOException
 	{
-		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.ELEMENT }, "*", SearchPattern.PATTERN_MATCH);
+		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.ELEMENT }, "*",
+				SearchPattern.PATTERN_MATCH);
 		List<ElementElement> result = new LinkedList<ElementElement>();
 
 		if (items != null)
@@ -72,59 +115,58 @@ public class CSSIndexReader
 	 * 
 	 * @param index
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public List<PropertyElement> getProperties(Index index) throws IOException
 	{
-		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.PROPERTY }, "*", SearchPattern.PATTERN_MATCH);
+		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.PROPERTY }, "*",
+				SearchPattern.PATTERN_MATCH);
 		List<PropertyElement> result = new LinkedList<PropertyElement>();
-		
+
 		if (items != null)
 		{
 			for (QueryResult queryResult : items)
 			{
 				String key = queryResult.getWord();
-				String columns[] = key.split(CSSIndexConstants.DELIMITER);
-				int column = 0;
-				PropertyElement property = new PropertyElement();
-				
-				property.setName(columns[column++]);
-				property.setAllowMultipleValues(Boolean.valueOf(columns[column++]));
-				property.setType(columns[column++]);
-				// TODO: specifications
-				
-				for (String userAgentKey : columns[column++].split(CSSIndexConstants.SUB_DELIMITER))
-				{
-					// get user agent and add to element
-					property.addUserAgent(this.getUserAgent(index, userAgentKey));
-				}
-				
-				property.setDescription(columns[column++]);
-				property.setExample(columns[column++]);
-				property.setHint(columns[column++]);
-				property.setRemark(columns[column++]);
-				// TODO: values
-				
+				PropertyElement property = this.createPropertyFromKey(index, key);
+
 				result.add(property);
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * getProperties
 	 * 
 	 * @param index
 	 * @param names
 	 * @return
+	 * @throws IOException
 	 */
-	public List<PropertyElement> getProperties(Index index, String[] names)
+	public List<PropertyElement> getProperties(Index index, String... names) throws IOException
 	{
 		List<PropertyElement> result = new LinkedList<PropertyElement>();
-		
-		// TODO
-		
+
+		for (String name : names)
+		{
+			String searchKey = name + CSSIndexConstants.DELIMITER;
+			List<QueryResult> items = index.query(new String[] { CSSIndexConstants.PROPERTY }, searchKey,
+					SearchPattern.PREFIX_MATCH);
+
+			if (items != null)
+			{
+				for (QueryResult item : items)
+				{
+					String key = item.getWord();
+					PropertyElement property = this.createPropertyFromKey(index, key);
+
+					result.add(property);
+				}
+			}
+		}
+
 		return result;
 	}
 
@@ -133,25 +175,26 @@ public class CSSIndexReader
 	 * 
 	 * @param userAgentKey
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	protected UserAgentElement getUserAgent(Index index, String userAgentKey) throws IOException
 	{
 		String searchKey = userAgentKey + CSSIndexConstants.DELIMITER;
-		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.USER_AGENT }, searchKey, SearchPattern.PREFIX_MATCH);
+		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.USER_AGENT }, searchKey,
+				SearchPattern.PREFIX_MATCH);
 		UserAgentElement result = null;
-		
+
 		if (items != null && items.size() > 0)
 		{
 			String key = items.get(0).getWord();
 			String[] columns = key.split(CSSIndexConstants.DELIMITER);
 			int column = 1; // skip index
-			
+
 			result = new UserAgentElement();
 			result.setDescription(columns[column++]);
 			result.setOS(columns[column++]);
 			result.setPlatform(columns[column++]);
-			
+
 			// NOTE: split does not return a final empty element if the string being split
 			// ends with the delimiter.
 			if (column < columns.length)
@@ -159,7 +202,48 @@ public class CSSIndexReader
 				result.setVersion(columns[column++]);
 			}
 		}
-		
+
+		return result;
+	}
+
+	/**
+	 * getValue
+	 * 
+	 * @param index
+	 * @param valueKey
+	 * @return
+	 * @throws IOException
+	 */
+	private ValueElement getValue(Index index, String valueKey) throws IOException
+	{
+		String searchKey = valueKey + CSSIndexConstants.DELIMITER;
+		List<QueryResult> items = index.query(new String[] { CSSIndexConstants.VALUE }, searchKey, SearchPattern.PREFIX_MATCH);
+		ValueElement result = null;
+
+		if (items != null && items.size() > 0)
+		{
+			String key = items.get(0).getWord();
+			String[] columns = key.split(CSSIndexConstants.DELIMITER);
+			int column = 1; // skip index
+
+			result = new ValueElement();
+			result.setName(columns[column++]);
+
+			result.setDescription(columns[column++]);
+
+			// NOTE: split does not return an empty element if the string being
+			// split ends with the delimiter pattern. So, we have to make sure
+			// we actually have a column for user agents before using it.
+			if (column < columns.length)
+			{
+				for (String userAgentKey : columns[column++].split(CSSIndexConstants.SUB_DELIMITER))
+				{
+					// get user agent and add to element
+					result.addUserAgent(this.getUserAgent(index, userAgentKey));
+				}
+			}
+		}
+
 		return result;
 	}
 }
