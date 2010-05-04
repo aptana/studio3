2eacc3d3972b8d4c5747da1f4294e9dbfdd7e41e
Kevin Lindsey <klindsey@aptana.com>
2010-05-04 16:42:07 -0500

    Add support for reading user agents
    Refactor code to create new element from key for better code re-use.

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexReader.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/index/HTMLIndexReader.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.aptana.editor.css.contentassist.index.CSSIndexConstants;
 import com.aptana.editor.html.contentassist.model.ElementElement;
+import com.aptana.editor.html.contentassist.model.UserAgentElement;
 import com.aptana.index.core.Index;
 import com.aptana.index.core.QueryResult;
 import com.aptana.index.core.SearchPattern;
@@ -12,55 +14,136 @@ import com.aptana.index.core.SearchPattern;
 public class HTMLIndexReader
 {
 	/**
+	 * createElement
+	 * 
+	 * @param key
+	 * @return
+	 * @throws IOException 
+	 */
+	private ElementElement createElementFromKey(Index index, String key) throws IOException
+	{
+		String[] columns = key.split(HTMLIndexConstants.DELIMITER);
+		ElementElement element = new ElementElement();
+		int column = 0;
+
+		element.setName(columns[column++]);
+		element.setDisplayName(columns[column++]);
+		element.setRelatedClass(columns[column++]);
+
+		for (String attribute : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
+		{
+			element.addAttribute(attribute);
+		}
+
+		for (String userAgentKey : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
+		{
+			element.addUserAgent(this.getUserAgent(index, userAgentKey));
+		}
+
+		element.setDeprecated(columns[column++]);
+		element.setDescription(columns[column++]);
+
+		for (String event : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
+		{
+			element.addEvent(event);
+		}
+
+		element.setExample(columns[column++]);
+
+		for (String reference : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
+		{
+			element.addReference(reference);
+		}
+
+		element.setRemark(columns[column++]);
+
+		return element;
+	}
+
+	/**
+	 * getElement
+	 * 
+	 * @param index
+	 * @param name
+	 * @return
+	 * @throws IOException
+	 */
+	public ElementElement getElement(Index index, String name) throws IOException
+	{
+		String searchKey = name + CSSIndexConstants.DELIMITER;
+		List<QueryResult> items = index.query(new String[] { HTMLIndexConstants.ELEMENT }, searchKey, SearchPattern.PREFIX_MATCH);
+		ElementElement result = null;
+
+		if (items != null)
+		{
+			for (QueryResult item : items)
+			{
+				String key = item.getWord();
+
+				result = this.createElementFromKey(index, key);
+
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	/**
 	 * getElements
 	 * 
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public List<ElementElement> getElements(Index index) throws IOException
 	{
 		List<QueryResult> items = index.query(new String[] { HTMLIndexConstants.ELEMENT }, "*", SearchPattern.PATTERN_MATCH);
 		List<ElementElement> result = new LinkedList<ElementElement>();
-		
+
 		if (items != null)
 		{
-			for (QueryResult queryResult : items)
+			for (QueryResult item : items)
 			{
-				String key = queryResult.getWord();
-				String[] columns = key.split(HTMLIndexConstants.DELIMITER);
-				ElementElement element = new ElementElement();
-				int column = 0;
-				
-				element.setName(columns[column++]);
-				element.setDisplayName(columns[column++]);
-				element.setRelatedClass(columns[column++]);
-				
-				for (String attribute : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
-				{
-					element.addAttribute(attribute);
-				}
-				
-				element.setDeprecated(columns[column++]);
-				element.setDescription(columns[column++]);
-				
-				for (String event : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
-				{
-					element.addEvent(event);
-				}
-				
-				element.setExample(columns[column++]);
-				
-				for (String reference : columns[column++].split(HTMLIndexConstants.SUB_DELIMITER))
-				{
-					element.addReference(reference);
-				}
-				
-				element.setRemark(columns[column++]);
+				String key = item.getWord();
+				ElementElement element = this.createElementFromKey(index, key);
 
 				result.add(element);
 			}
 		}
-		
+
+		return result;
+	}
+
+	/**
+	 * getUserAgent
+	 * 
+	 * @param userAgentKey
+	 * @return
+	 * @throws IOException
+	 */
+	protected UserAgentElement getUserAgent(Index index, String userAgentKey) throws IOException
+	{
+		String searchKey = userAgentKey + HTMLIndexConstants.DELIMITER;
+		List<QueryResult> items = index.query(new String[] { HTMLIndexConstants.USER_AGENT }, searchKey, SearchPattern.PREFIX_MATCH);
+		UserAgentElement result = null;
+
+		if (items != null && items.size() > 0)
+		{
+			String key = items.get(0).getWord();
+			String[] columns = key.split(HTMLIndexConstants.DELIMITER);
+			int column = 1; // skip index
+
+			result = new UserAgentElement();
+			result.setPlatform(columns[column++]);
+
+			// NOTE: split does not return a final empty element if the string being split
+			// ends with the delimiter.
+			if (column < columns.length)
+			{
+				result.setVersion(columns[column++]);
+			}
+		}
+
 		return result;
 	}
 }
