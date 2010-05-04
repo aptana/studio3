d4ee1eeef387b0b394b5ccb51f5b706116d1869d
Kevin Lindsey <klindsey@aptana.com>
2010-05-04 16:44:02 -0500

    Override the createToken methods to return the correct token type

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/HTMLScopeScanner.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/HTMLScopeScanner.java
@@ -5,24 +5,53 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
+
+import com.aptana.editor.html.parsing.lexer.HTMLTokenType;
 
 public class HTMLScopeScanner extends HTMLTagScanner
 {
+	/**
+	 * HTMLScopeScanner
+	 */
 	public HTMLScopeScanner()
 	{
 		List<IRule> rules = new ArrayList<IRule>();
-		
+
 		// add custom rules with higher precedence here
-		
+
 		// Add the rules created by the super class first so they have higher
 		// precedence
 		if (fRules != null)
 		{
 			rules.addAll(Arrays.asList(fRules));
 		}
-		
+
 		// add custom rules with lower precedence here
-		
+
 		setRules(rules.toArray(new IRule[rules.size()]));
+	}
+
+	/**
+	 * createToken
+	 * 
+	 * @param type
+	 * @return
+	 */
+	protected IToken createToken(HTMLTokenType type)
+	{
+		return this.createToken(type.getName());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.css.CSSCodeScanner#createToken(java.lang.String)
+	 */
+	@Override
+	protected IToken createToken(String string)
+	{
+		// Simply use the token type string that is passed in as the data
+		return new Token(string);
 	}
 }
