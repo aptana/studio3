1cf3f1bbe5d05c67f0948382d9fc8a0484e6f340
Kevin Lindsey <klindsey@aptana.com>
2010-05-06 12:20:50 -0500

    No long load metadata on plugin startup.
    Sort members and format

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/Activator.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/Activator.java
@@ -1,141 +1,91 @@
 package com.aptana.editor.html;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-import com.aptana.editor.html.contentassist.index.HTMLIndexConstants;
-import com.aptana.editor.html.contentassist.index.HTMLIndexWriter;
-import com.aptana.index.core.Index;
-import com.aptana.index.core.IndexManager;
-
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
-
-	// The plug-in ID
+public class Activator extends AbstractUIPlugin
+{
 	public static final String PLUGIN_ID = "com.aptana.editor.html"; //$NON-NLS-1$
-
-	// The shared instance
 	private static Activator plugin;
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault()
+	{
+		return plugin;
+	}
+
+	/**
+	 * getImage
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public static Image getImage(String path)
+	{
+		ImageRegistry registry = plugin.getImageRegistry();
+		Image image = registry.get(path);
+		
+		if (image == null)
+		{
+			ImageDescriptor id = getImageDescriptor(path);
+			
+			if (id != null)
+			{
+				registry.put(path, id);
+				image = registry.get(path);
+			}
+		}
+		
+		return image;
+	}
+
+	/**
+	 * getImageDescriptor
+	 * 
+	 * @param path
+	 * @return
+	 */
+	public static ImageDescriptor getImageDescriptor(String path)
+	{
+		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public Activator()
+	{
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception
+	{
 		super.start(context);
+		
 		plugin = this;
-		
-		HTMLIndexWriter indexer = new HTMLIndexWriter();
-		
-		this.loadMetadata(indexer, "/metadata/html_metadata.xml");
-		
-		IndexManager manager = IndexManager.getInstance();
-		Index index = manager.getIndex(HTMLIndexConstants.METADATA);
-		
-		indexer.writeToIndex(index);
 	}
 
-	/**
-	 * loadMetadata
-	 * 
-	 * @param indexer
-	 * @param resources
-	 */
-	private void loadMetadata(HTMLIndexWriter indexer, String ... resources)
-	{
-		for (String resource : resources)
-		{
-			URL url = FileLocator.find(this.getBundle(), new Path(resource), null);
-			
-			if (url != null)
-			{
-				InputStream stream = null;
-				
-				try
-				{
-					stream = url.openStream();
-					
-					indexer.loadXML(stream);
-				}
-				catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-				catch (Throwable t)
-				{
-					t.printStackTrace();
-				}
-				finally
-				{
-					if (stream != null)
-					{
-						try
-						{
-							stream.close();
-						}
-						catch (IOException e)
-						{
-						}
-					}
-				}
-			}
-		}
-	}
-	
 	/*
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
+	public void stop(BundleContext context) throws Exception
+	{
 		plugin = null;
+		
 		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
-
-	public static Image getImage(String path)
-	{
-		ImageRegistry registry = plugin.getImageRegistry();
-		Image image = registry.get(path);
-		if (image == null)
-		{
-			ImageDescriptor id = getImageDescriptor(path);
-			if (id == null)
-			{
-				return null;
-			}
-			registry.put(path, id);
-			image = registry.get(path);
-		}
-		return image;
-	}
-
-	public static ImageDescriptor getImageDescriptor(String path)
-	{
-		return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
 }
