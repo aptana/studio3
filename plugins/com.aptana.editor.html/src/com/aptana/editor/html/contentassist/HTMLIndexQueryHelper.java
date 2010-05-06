d53c13f81f926aff24497b248c5a17f80ce598a8
Kevin Lindsey <klindsey@aptana.com>
2010-05-06 12:22:10 -0500

    Switch over to the delayed load and caching model that we're now using in CSS.
    Add methods to get classes and ids from the current project index.

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/HTMLIndexQueryHelper.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/contentassist/HTMLIndexQueryHelper.java
@@ -1,24 +1,42 @@
 package com.aptana.editor.html.contentassist;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
-import com.aptana.editor.html.contentassist.index.HTMLIndexConstants;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+
+import com.aptana.editor.css.contentassist.index.CSSIndexConstants;
+import com.aptana.editor.html.Activator;
 import com.aptana.editor.html.contentassist.index.HTMLIndexReader;
+import com.aptana.editor.html.contentassist.index.HTMLMetadataReader;
 import com.aptana.editor.html.contentassist.model.ElementElement;
+import com.aptana.editor.html.contentassist.model.EntityElement;
 import com.aptana.index.core.Index;
-import com.aptana.index.core.IndexManager;
 
 public class HTMLIndexQueryHelper
 {
 	private HTMLIndexReader _reader;
+	private HTMLMetadataReader _metadata;
 	
 	/**
 	 * HTMLContentAssistHelper
 	 */
 	public HTMLIndexQueryHelper()
 	{
-		this._reader = new HTMLIndexReader();
+	}
+	
+	/**
+	 * getClasses
+	 * 
+	 * @return
+	 */
+	public Map<String, String> getClasses(Index index)
+	{
+		return this.getReader().getValues(index, CSSIndexConstants.CLASS);
 	}
 	
 	/**
@@ -31,12 +49,17 @@ public class HTMLIndexQueryHelper
 	{
 		ElementElement result = null;
 		
-		try
+		if (name != null && name.length() > 0)
 		{
-			result = this._reader.getElement(this.getIndex(), name);
-		}
-		catch (IOException e)
-		{
+			// TODO: optimize with a name->element hash
+			for (ElementElement element : this.getElements())
+			{
+				if (name.equals(element.getName()))
+				{
+					result = element;
+					break;
+				}
+			}
 		}
 		
 		return result;
@@ -49,27 +72,116 @@ public class HTMLIndexQueryHelper
 	 */
 	public List<ElementElement> getElements()
 	{
-		List<ElementElement> result = null;
-		
-		try
-		{
-			result = this._reader.getElements(this.getIndex());
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return result;
+		return this.getMetadata().getElements();
+	}
+	
+//	/**
+//	 * getIndex
+//	 * 
+//	 * @return
+//	 */
+//	private Index getIndex()
+//	{
+//		return IndexManager.getInstance().getIndex(HTMLIndexConstants.METADATA);
+//	}
+	
+	/**
+	 * getIDs
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public Map<String, String> getIDs(Index index)
+	{
+		return this.getReader().getValues(index, CSSIndexConstants.IDENTIFIER);
 	}
 	
 	/**
-	 * getIndex
+	 * getEntities
 	 * 
 	 * @return
 	 */
-	private Index getIndex()
+	public List<EntityElement> getEntities()
 	{
-		return IndexManager.getInstance().getIndex(HTMLIndexConstants.METADATA);
+		return this.getMetadata().getEntities();
+	}
+	
+	/**
+	 * getMetadata
+	 * 
+	 * @return
+	 */
+	private HTMLMetadataReader getMetadata()
+	{
+		if (this._metadata == null)
+		{
+			this._metadata = new HTMLMetadataReader();
+			String[] resources = this.getMetadataResources();
+			
+			for (String resource : resources)
+			{
+				URL url = FileLocator.find(Activator.getDefault().getBundle(), new Path(resource), null);
+
+				if (url != null)
+				{
+					InputStream stream = null;
+
+					try
+					{
+						stream = url.openStream();
+
+						this._metadata.loadXML(stream);
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+					catch (Throwable t)
+					{
+						t.printStackTrace();
+					}
+					finally
+					{
+						if (stream != null)
+						{
+							try
+							{
+								stream.close();
+							}
+							catch (IOException e)
+							{
+							}
+						}
+					}
+				}
+			}
+		}
+		
+		return this._metadata;
+	}
+	
+	/**
+	 * getMetadataResources
+	 * 
+	 * @return
+	 */
+	protected String[] getMetadataResources()
+	{
+		return new String[] { "/metadata/html_metadata.xml" };
+	}
+	
+	/**
+	 * getReader
+	 * 
+	 * @return
+	 */
+	protected HTMLIndexReader getReader()
+	{
+		if (this._reader == null)
+		{
+			this._reader = new HTMLIndexReader();
+		}
+		
+		return this._reader;
 	}
 }
