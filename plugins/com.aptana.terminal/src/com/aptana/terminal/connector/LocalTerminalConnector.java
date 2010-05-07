4db390dc69529533885d1a70f8454178f5f12345
Max Stepanov <mstepanov@aptana.com>
2010-05-07 13:54:18 -0700

    pre-handle terminal closing to show confirmation dialog

--- plugins/com.aptana.terminal/src/com/aptana/terminal/connector/LocalTerminalConnector.java
+++ plugins/com.aptana.terminal/src/com/aptana/terminal/connector/LocalTerminalConnector.java
@@ -37,6 +37,8 @@ package com.aptana.terminal.connector;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -237,6 +239,12 @@ public class LocalTerminalConnector extends TerminalConnectorImpl implements IPr
 			}
 		}
 		return null;
+	}
+
+	public List<String> getRunningProcesses() {
+		List<String> processes = new ArrayList<String>();
+		processes.add("bash");
+		return processes;
 	}
 
 }
