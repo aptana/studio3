b02873314c3f4c3d399ef47ae8fe745f3aca3f1b
Kevin Lindsey <klindsey@aptana.com>
2010-05-07 17:57:48 -0500

    Remove unused (commented out) code

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/CSSSourceViewerConfiguration.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/CSSSourceViewerConfiguration.java
@@ -93,7 +93,6 @@ public class CSSSourceViewerConfiguration extends CommonSourceViewerConfiguratio
 	@Override
 	protected IContentAssistProcessor getContentAssistProcessor(ISourceViewer sourceViewer, String contentType)
 	{
-		//return new CSSIndexContentAssistProcessor(getAbstractThemeableEditor());
 		return new CSSContentAssistProcessor(getAbstractThemeableEditor());
 	}
 }
