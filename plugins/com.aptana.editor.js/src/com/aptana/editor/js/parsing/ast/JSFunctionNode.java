92d3781c51da25da0dd81478c84646e36bc293cb
Kevin Lindsey <klindsey@aptana.com>
2010-05-14 09:09:51 -0500

    Return the function's name as an attribute

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSFunctionNode.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSFunctionNode.java
@@ -1,24 +1,68 @@
 package com.aptana.editor.js.parsing.ast;
 
+import com.aptana.parsing.ast.IParseNodeAttribute;
+import com.aptana.parsing.ast.ParseBaseNode;
+import com.aptana.parsing.ast.ParseNodeAttribute;
+
 public class JSFunctionNode extends JSNode
 {
-
+	/**
+	 * JSFunctionNode
+	 * 
+	 * @param children
+	 * @param start
+	 * @param end
+	 */
 	public JSFunctionNode(JSNode[] children, int start, int end)
 	{
 		super(JSNodeTypes.FUNCTION, children, start, end);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.parsing.ast.ParseBaseNode#getAttributes()
+	 */
+	public IParseNodeAttribute[] getAttributes()
+	{
+		String name = getName();
+		
+		if (name != null && name.length() > 0)
+		{
+			// TODO: possibly cache this
+			return new IParseNodeAttribute[] {
+				new ParseNodeAttribute(this, "name", name)
+			};
+		}
+		else
+		{
+			return ParseBaseNode.NO_ATTRIBUTES;
+		}
+	}
+	
+	/**
+	 * getName
+	 * 
+	 * @return
+	 */
 	public String getName()
 	{
 		return getChild(0).getText();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.parsing.ast.ParseBaseNode#getText()
+	 */
 	@Override
 	public String getText()
 	{
 		return getName();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.js.parsing.ast.JSNode#toString()
+	 */
 	@Override
 	public String toString()
 	{
