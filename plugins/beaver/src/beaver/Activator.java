e151b92fac7a4521f328ca8562c37aa57f0daad6
Kevin Lindsey <klindsey@aptana.com>
2010-05-18 10:20:48 -0500

    I18N and remove UI dependency

--- plugins/beaver/src/beaver/Activator.java
+++ plugins/beaver/src/beaver/Activator.java
@@ -1,12 +1,12 @@
 package beaver;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "beaver";
