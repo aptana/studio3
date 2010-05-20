f13058e9935b4a3ce836a0f85ea507be0ad57adc
sgtcoolguy <cwilliams@aptana.com>
2010-05-20 15:41:47 -0700

    Add plugin for running tests in parallel, make ftp/ftps/sftp tests run in parallel

--- tests/com.aptana.filesystem.ftp.tests/src/com/aptana/filesystem/ftp/tests/AllTests.java
+++ tests/com.aptana.filesystem.ftp.tests/src/com/aptana/filesystem/ftp/tests/AllTests.java
@@ -3,12 +3,14 @@ package com.aptana.filesystem.ftp.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.kohsuke.junit.ParallelTestSuite;
+
 public class AllTests
 {
 
 	public static Test suite()
 	{
-		TestSuite suite = new TestSuite(AllTests.class.getName());
+		TestSuite suite = new ParallelTestSuite(AllTests.class.getName(), 2);
 		// $JUnit-BEGIN$
 		suite.addTestSuite(FTPConnectionTest.class);
 		suite.addTestSuite(FTPConnectionWithBasePathTest.class);
