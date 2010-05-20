5ee9a708a8384e0ed977e3fb95e1e300ecdb0af5
sgtcoolguy <cwilliams@aptana.com>
2010-05-20 09:46:54 -0700

    officially move ftp/sftp tests to ui suite. re-order UI suite to be alphabetical just so I can easily match against the workspace listing and see if any are missing

--- tests/com.aptana.red.core.tests.all/src/com/aptana/red/core/tests/all/UITests.java
+++ tests/com.aptana.red.core.tests.all/src/com/aptana/red/core/tests/all/UITests.java
@@ -14,20 +14,20 @@ public class UITests
 	{
 		TestSuite suite = new TestSuite(UITests.class.getName());
 		// $JUnit-BEGIN$
-		// FIXME Why do the FTP tests need to be run on UI thread on unit test build machine?
-		suite.addTest(com.aptana.filesystem.ftp.tests.AllTests.suite());
-		suite.addTest(com.aptana.filesystem.secureftp.tests.AllTests.suite());
-		suite.addTest(com.aptana.git.ui.AllTests.suite());
 		suite.addTest(com.aptana.editor.common.tests.AllTests.suite());
-		// suite.addTest(com.aptana.editor.text.tests.AllTests.suite()); // TODO Add Tests for editor.text
-		suite.addTest(com.aptana.editor.xml.tests.AllTests.suite());
 		suite.addTest(com.aptana.editor.css.tests.AllTests.suite());
+		suite.addTest(com.aptana.editor.erb.tests.AllTests.suite());
 		suite.addTest(com.aptana.editor.js.tests.AllTests.suite());
 		suite.addTest(com.aptana.editor.html.tests.AllTests.suite());
 		suite.addTest(com.aptana.editor.ruby.tests.AllTests.suite());
 		suite.addTest(com.aptana.editor.sass.tests.AllTests.suite());
-		suite.addTest(com.aptana.editor.erb.tests.AllTests.suite());
+		// suite.addTest(com.aptana.editor.text.tests.AllTests.suite()); // TODO Add Tests for editor.text
+		suite.addTest(com.aptana.editor.xml.tests.AllTests.suite());
 		// suite.addTest(com.aptana.explorer.tests.AllTests.suite()); // TODO Add Tests for explorer
+		// FIXME Why do the FTP tests need to be run on UI thread on unit test build machine?
+		suite.addTest(com.aptana.filesystem.ftp.tests.AllTests.suite());
+		suite.addTest(com.aptana.filesystem.secureftp.tests.AllTests.suite());
+		suite.addTest(com.aptana.git.ui.AllTests.suite());
 		// suite.addTest(com.aptana.scripting.ui.tests.AllTests.suite()); // TODO Add Tests for scripting.ui
 		// $JUnit-END$
 		
