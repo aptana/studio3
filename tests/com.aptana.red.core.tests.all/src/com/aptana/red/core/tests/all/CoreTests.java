2e4c3d2aa0f70d9f2f7f31c8703392d9cbd3fe2c
Max Stepanov <mstepanov@aptana.com>
2010-05-19 17:08:02 -0700

    disable FTP tests

--- tests/com.aptana.red.core.tests.all/src/com/aptana/red/core/tests/all/CoreTests.java
+++ tests/com.aptana.red.core.tests.all/src/com/aptana/red/core/tests/all/CoreTests.java
@@ -13,8 +13,10 @@ public class CoreTests
 		suite.addTest(com.aptana.git.core.tests.AllTests.suite());
 		suite.addTest(com.aptana.util.tests.AllTests.suite());
 		suite.addTest(com.aptana.scripting.tests.AllTests.suite());
+		/*
 		suite.addTest(com.aptana.filesystem.ftp.tests.AllTests.suite());
 		suite.addTest(com.aptana.filesystem.secureftp.tests.AllTests.suite());
+		*/
 		// $JUnit-END$
 		return suite;
 	}
