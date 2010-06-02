029698866f18272a255e51ff3371998017933161
Andrew Shebanow <ashebanow@gmail.com>
2010-06-02 15:55:02 -0700

    update copyright dates
    Conflicts:
    
    	plugins/com.aptana.editor.diff/src/com/aptana/editor/diff/DiffDocumentProvider.java
    	plugins/com.aptana.editor.diff/src/com/aptana/editor/diff/DiffSourceConfiguration.java
    	plugins/com.aptana.editor.diff/src/com/aptana/editor/diff/DiffSourcePartitionScanner.java
    	plugins/com.aptana.editor.diff/src/com/aptana/editor/diff/DiffSourceViewerConfiguration.java
    	plugins/com.aptana.editor.diff/src/com/aptana/editor/diff/IDiffConstants.java
    	plugins/com.aptana.syncing.core/src/com/aptana/ide/syncing/core/old/handlers/SyncEventHandlerAdapterWithProgressMonitor.java
    	plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/actions/SynchronizeFilesAction.java
    	plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/old/actions/SiteConnectionSynchronizeAction.java
    	plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/old/editors/FileCompareEditorInput.java
    	plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/old/editors/FileNode.java
    	plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/old/views/Messages.java

--- plugins/com.aptana.core.epl/src/com/aptana/core/epl/CoreEPLPlugin.java
+++ plugins/com.aptana.core.epl/src/com/aptana/core/epl/CoreEPLPlugin.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2005-2009 Aptana, Inc.
+ * Copyright (c) 2005-2010 Aptana, Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
