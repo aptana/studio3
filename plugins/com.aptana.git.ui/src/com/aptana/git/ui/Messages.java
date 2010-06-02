c7841c9a0cf75724aaee37050405f93116627377
Andrew Shebanow <ashebanow@gmail.com>
2010-06-02 12:53:43 -0700

    Fix issue 475: Warning text needs editing Fix issue 474: Incorrect title in msysgit warning dialog on Vista
    Mac's dialog has been internationalized, and the readability of the text has been
    improved.
    
    Signed-off-by: Andrew Shebanow <ashebanow@gmail.com>

--- plugins/com.aptana.git.ui/src/com/aptana/git/ui/Messages.java
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/Messages.java
@@ -6,6 +6,17 @@ public class Messages extends NLS
 {
 	private static final String BUNDLE_NAME = "com.aptana.git.ui.messages"; //$NON-NLS-1$
 	public static String DiffFormatter_NoContent;
+	public static String GitUIPlugin_0;
+	public static String GitUIPlugin_1;
+	public static String GitUIPlugin_10;
+	public static String GitUIPlugin_2;
+	public static String GitUIPlugin_3;
+	public static String GitUIPlugin_4;
+	public static String GitUIPlugin_5;
+	public static String GitUIPlugin_6;
+	public static String GitUIPlugin_7;
+	public static String GitUIPlugin_8;
+	public static String GitUIPlugin_9;
 	static
 	{
 		// initialize resource bundle
