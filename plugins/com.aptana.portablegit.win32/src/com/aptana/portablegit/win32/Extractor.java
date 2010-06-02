39280ca6a0d24c65afce60d9a6a9eb921bbc7295
Max Stepanov <mstepanov@aptana.com>
2010-06-02 13:17:26 -0700

    fix warning

--- plugins/com.aptana.portablegit.win32/src/com/aptana/portablegit/win32/Extractor.java
+++ plugins/com.aptana.portablegit.win32/src/com/aptana/portablegit/win32/Extractor.java
@@ -39,7 +39,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.eclipse.core.runtime.FileLocator;
