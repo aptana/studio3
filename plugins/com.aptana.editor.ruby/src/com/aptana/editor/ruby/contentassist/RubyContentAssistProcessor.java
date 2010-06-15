eec1307700fb8b67e830dd02e74ef3f316ba7913
sgtcoolguy <cwilliams@aptana.com>
2010-06-15 12:56:10 -0400

    Add images to proposals, clean up code

--- plugins/com.aptana.editor.ruby/src/com/aptana/editor/ruby/contentassist/RubyContentAssistProcessor.java
+++ plugins/com.aptana.editor.ruby/src/com/aptana/editor/ruby/contentassist/RubyContentAssistProcessor.java
@@ -10,7 +10,6 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
-import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.swt.graphics.Image;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
@@ -24,6 +23,25 @@ import com.aptana.index.core.SearchPattern;
 
 public class RubyContentAssistProcessor extends CommonContentAssistProcessor
 {
+	/**
+	 * Prefixes to cheat and easily tell type of variable
+	 */
+	private static final String INSTANCE_VAR_NAME_PREFIX = "@"; //$NON-NLS-1$
+	private static final String CLASS_VAR_NAME_PREFIX = "@@"; //$NON-NLS-1$
+	private static final String GLOBAL_NAME_PREFIX = "$"; //$NON-NLS-1$
+
+	/**
+	 * Images for various proposals for Ruby.
+	 */
+	private static final String CONSTANT_IMAGE = "icons/constant_obj.gif"; //$NON-NLS-1$
+	private static final String CLASS_IMAGE = "icons/class_obj.png"; //$NON-NLS-1$
+	private static final String MODULE_IMAGE = "icons/module_obj.png"; //$NON-NLS-1$
+	private static final String METHOD_IMAGE = "icons/method_public_obj.png"; //$NON-NLS-1$
+	private static final String INSTANCE_VAR_IMAGE = "icons/instance_var_obj.gif"; //$NON-NLS-1$
+	private static final String CLASS_VAR_IMAGE = "icons/class_var_obj.gif"; //$NON-NLS-1$
+	private static final String GLOBAL_IMAGE = "icons/global_obj.png"; //$NON-NLS-1$
+	private static final String LOCAL_VAR_IMAGE = "icons/local_var_obj.gif"; //$NON-NLS-1$
+
 	public RubyContentAssistProcessor(AbstractThemeableEditor editor)
 	{
 		super(editor);
@@ -46,26 +64,7 @@ public class RubyContentAssistProcessor extends CommonContentAssistProcessor
 			{
 				for (QueryResult result : results)
 				{
-					String name = result.getWord();
-					// FIXME We need to "decode" the word since it's the raw key, which has identifier plus a bunch of
-					// other info
-					int firstSeparator = name.indexOf(IRubyIndexConstants.SEPARATOR);
-					if (firstSeparator != -1)
-					{
-						name = name.substring(0, firstSeparator);
-					}
-					CommonCompletionProposal proposal = createProposal(offset, prefix, name);
-					StringBuilder builder = new StringBuilder();
-					for (String doc : result.getDocuments())
-					{
-						builder.append(doc).append(", "); //$NON-NLS-1$
-					}
-					if (builder.length() > 0)
-					{
-						builder.delete(builder.length() - 2, builder.length());
-					}
-					proposal.setFileLocation(builder.toString());
-					// add it to the list
+					CommonCompletionProposal proposal = createProposal(offset, prefix, result);
 					proposals.add(proposal);
 				}
 			}
@@ -89,17 +88,85 @@ public class RubyContentAssistProcessor extends CommonContentAssistProcessor
 		return proposals.toArray(new ICompletionProposal[proposals.size()]);
 	}
 
-	protected CommonCompletionProposal createProposal(int offset, String prefix, String value)
+	protected CommonCompletionProposal createProposal(int offset, String prefix, QueryResult result)
 	{
+		String value = result.getWord();
+		// We need to "decode" the word since it's the raw key, which has identifier plus a bunch of
+		// other info
+		int firstSeparator = value.indexOf(IRubyIndexConstants.SEPARATOR);
+		if (firstSeparator != -1)
+		{
+			value = value.substring(0, firstSeparator);
+		}
+
 		String description = ""; //$NON-NLS-1$
 		int replaceLength = prefix.length();
 		int length = value.length();
 		String displayName = value;
-		Image image = null;
-		IContextInformation contextInfo = null;
-		// build proposal
-		return new CommonCompletionProposal(value, offset - replaceLength, replaceLength, length, image, displayName,
-				contextInfo, description);
+
+		CommonCompletionProposal proposal = new CommonCompletionProposal(value, offset - replaceLength, replaceLength,
+				length, getImage(result), displayName, null, description);
+		proposal.setFileLocation(getLocations(result));
+
+		return proposal;
+	}
+
+	protected String getLocations(QueryResult result)
+	{
+		StringBuilder builder = new StringBuilder();
+		for (String doc : result.getDocuments())
+		{
+			builder.append(doc).append(", "); //$NON-NLS-1$
+		}
+		if (builder.length() > 0)
+		{
+			builder.delete(builder.length() - 2, builder.length());
+		}
+		return builder.toString();
+	}
+
+	protected Image getImage(QueryResult result)
+	{
+		Image image = Activator.getImage(LOCAL_VAR_IMAGE);
+		if (result.getWord().startsWith(GLOBAL_NAME_PREFIX))
+		{
+			image = Activator.getImage(GLOBAL_IMAGE);
+		}
+		// Must check class var before instance because of prefix!
+		else if (result.getWord().startsWith(CLASS_VAR_NAME_PREFIX))
+		{
+			image = Activator.getImage(CLASS_VAR_IMAGE);
+		}
+		else if (result.getWord().startsWith(INSTANCE_VAR_NAME_PREFIX))
+		{
+			image = Activator.getImage(INSTANCE_VAR_IMAGE);
+		}
+		else if (result.getWord().indexOf(IRubyIndexConstants.SEPARATOR) != -1)
+		{
+			// Must be a class/module/method
+			String[] parts = result.getWord().split("" + IRubyIndexConstants.SEPARATOR); //$NON-NLS-1$
+			if (parts.length == 2)
+			{
+				image = Activator.getImage(METHOD_IMAGE);
+			}
+			else
+			{
+				String isModule = parts[2];
+				if (isModule.equals("" + IRubyIndexConstants.MODULE_SUFFIX)) //$NON-NLS-1$
+				{
+					image = Activator.getImage(MODULE_IMAGE);
+				}
+				else
+				{
+					image = Activator.getImage(CLASS_IMAGE);
+				}
+			}
+		}
+		else if (Character.isUpperCase(result.getWord().charAt(0)))
+		{
+			image = Activator.getImage(CONSTANT_IMAGE);
+		}
+		return image;
 	}
 
 	protected String getPrefix(ITextViewer viewer, int offset) throws BadLocationException
