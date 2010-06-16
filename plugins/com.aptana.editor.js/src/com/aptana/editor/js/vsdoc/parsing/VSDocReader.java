e14ead502bb185b40b549cc0a244ef3b40191b1f
Kevin Lindsey <klindsey@aptana.com>
2010-06-16 14:21:23 -0500

    Implement some of the new vsdoc elements/attributes, specfically the ones that easily map to sdoc tags.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/vsdoc/parsing/VSDocReader.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/vsdoc/parsing/VSDocReader.java
@@ -22,11 +22,12 @@ public class VSDocReader extends MetadataReader
 
 	private String _summary;
 	private List<Tag> _tags;
+	private String _exceptionType;
 	private Parameter _currentParameter;
 	private String _currentType;
 
 	private DocumentationBlock _block;
-	
+
 	/**
 	 * process docs element
 	 * 
@@ -39,7 +40,22 @@ public class VSDocReader extends MetadataReader
 	{
 		this._tags = new ArrayList<Tag>();
 	}
-	
+
+	/**
+	 * process exception element
+	 * 
+	 * @param ns
+	 * @param name
+	 * @param qname
+	 * @param attributes
+	 */
+	public void enterException(String ns, String name, String qname, Attributes attributes)
+	{
+		this._exceptionType = attributes.getValue("cref");
+
+		this.startTextBuffer();
+	}
+
 	/**
 	 * process param element
 	 * 
@@ -54,7 +70,7 @@ public class VSDocReader extends MetadataReader
 		boolean optional = Boolean.parseBoolean(attributes.getValue("optional"));
 		boolean parameterArray = Boolean.parseBoolean(attributes.getValue("parameterArray"));
 		Usage usage;
-		
+
 		if (optional)
 		{
 			if (parameterArray)
@@ -77,15 +93,30 @@ public class VSDocReader extends MetadataReader
 				usage = Usage.REQUIRED;
 			}
 		}
-		
+
 		this._currentParameter = new Parameter(parameterName);
 		this._currentParameter.setUsage(usage);
-		
+
 		this._currentType = attributes.getValue("type");
-		
+
 		this.startTextBuffer();
 	}
 
+	/**
+	 * process see element
+	 * 
+	 * @param ns
+	 * @param name
+	 * @param qname
+	 * @param attributes
+	 */
+	public void enterSee(String ns, String name, String qname, Attributes attributes)
+	{
+		String type = attributes.getValue("cref");
+
+		this._tags.add(new Tag(TagType.SEE, type));
+	}
+	
 	/**
 	 * process returns element
 	 * 
@@ -97,7 +128,7 @@ public class VSDocReader extends MetadataReader
 	public void enterReturns(String ns, String name, String qname, Attributes attributes)
 	{
 		this._currentType = attributes.getValue("type");
-		
+
 		this.startTextBuffer();
 	}
 
@@ -111,10 +142,44 @@ public class VSDocReader extends MetadataReader
 	public void exitDocs(String ns, String name, String qname)
 	{
 		this._block = new DocumentationBlock(this._summary, this._tags);
-		
+
 		// clean up
 		this._summary = null;
 		this._tags = null;
+	}
+
+	/**
+	 * Exit example element
+	 * 
+	 * @param ns
+	 * @param name
+	 * @param qname
+	 */
+	public void exitExample(String ns, String name, String qname)
+	{
+		String text = this.getText();
+
+		this._tags.add(new Tag(TagType.EXAMPLE, text));
+	}
+
+	/**
+	 * Exit exception element
+	 * 
+	 * @param ns
+	 * @param name
+	 * @param qname
+	 */
+	public void exitException(String ns, String name, String qname)
+	{
+		String text = this.getText();
+
+		List<Type> types = new ArrayList<Type>();
+		types.add(new Type(this._exceptionType != null ? this._exceptionType : "Object"));
+
+		this._tags.add(new TagWithTypes(TagType.EXCEPTION, types, text));
+
+		// clean up
+		this._exceptionType = null;
 	}
 
 	/**
@@ -127,12 +192,12 @@ public class VSDocReader extends MetadataReader
 	public void exitParam(String ns, String name, String qname)
 	{
 		String text = this.getText();
-		
+
 		List<Type> types = new ArrayList<Type>();
 		types.add(new Type(this._currentType != null ? this._currentType : "Object"));
-		
+
 		this._tags.add(new ParamTag(this._currentParameter, types, text));
-		
+
 		// reset
 		this._currentParameter = null;
 		this._currentType = null;
@@ -149,10 +214,10 @@ public class VSDocReader extends MetadataReader
 	public void exitPrivate(String ns, String name, String qname)
 	{
 		String text = this.getText();
-		
+
 		this._tags.add(new Tag(TagType.PRIVATE, text));
 	}
-	
+
 	/**
 	 * Exit returns element
 	 * 
@@ -163,12 +228,12 @@ public class VSDocReader extends MetadataReader
 	public void exitReturns(String ns, String name, String qname)
 	{
 		String text = this.getText();
-		
+
 		List<Type> types = new ArrayList<Type>();
 		types.add(new Type(this._currentType != null ? this._currentType : "Object"));
-		
+
 		this._tags.add(new TagWithTypes(TagType.RETURN, types, text));
-		
+
 		// reset
 		this._currentType = null;
 	}
@@ -194,7 +259,7 @@ public class VSDocReader extends MetadataReader
 	{
 		return this._block;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.aptana.editor.common.contentassist.MetadataReader#getSchemaStream()
@@ -205,7 +270,8 @@ public class VSDocReader extends MetadataReader
 		return this.getClass().getResourceAsStream(METADATA_SCHEMA_XML);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see com.aptana.editor.common.contentassist.MetadataReader#getText()
 	 */
 	@Override
