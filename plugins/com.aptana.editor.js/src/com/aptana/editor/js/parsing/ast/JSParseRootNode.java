09351bc3fad073a6e22169daa7e948713316c911
Kevin Lindsey <klindsey@aptana.com>
2010-06-16 10:04:07 -0500

    Remove all code to store, retrieve, and process documentation blocks now that we're associating documentation blocks with thier neighbor nodes in JSParser.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSParseRootNode.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSParseRootNode.java
@@ -1,20 +1,14 @@
 package com.aptana.editor.js.parsing.ast;
 
-import java.util.List;
-
 import beaver.Symbol;
 
 import com.aptana.editor.js.parsing.IJSParserConstants;
-import com.aptana.editor.js.sdoc.model.DocumentationBlock;
 import com.aptana.parsing.Scope;
-import com.aptana.parsing.ast.IParseNode;
 import com.aptana.parsing.ast.ParseRootNode;
 
 public class JSParseRootNode extends ParseRootNode
 {
 	private Scope<JSNode> _globalScope;
-	private List<DocumentationBlock> _preBlocks;
-	private List<DocumentationBlock> _postBlocks;
 
 	/**
 	 * JSParseRootNode
@@ -37,158 +31,6 @@ public class JSParseRootNode extends ParseRootNode
 	}
 
 	/**
-	 * findDocumentationBlock
-	 * 
-	 * @param offset
-	 * @return
-	 */
-	public DocumentationBlock findDocumentationBlock(IParseNode node)
-	{
-		DocumentationBlock block = null;
-		
-		if (node instanceof JSNode)
-		{
-			JSNode jsNode = (JSNode) node;
-			IParseNode statement = jsNode.getContainingStatementNode();
-			
-			block = this.findPreDocumentationBlock(statement.getStartingOffset());
-	
-			if (block != null)
-			{
-				IParseNode parent = statement.getParent();
-				
-				if (parent != null && parent.getFirstChild() != statement)
-				{
-					IParseNode previousNode = statement.getPreviousNode();
-					
-					if (previousNode instanceof JSNode)
-					{
-						IParseNode previousStatement = ((JSNode) previousNode).getContainingStatementNode();
-						
-						if (previousStatement != null && previousStatement.getEndingOffset() > block.getEnd())
-						{
-							block = null;
-						}
-					}
-				}
-			}
-			
-			if (block == null && node instanceof JSFunctionNode)
-			{
-				IParseNode body = ((JSFunctionNode) node).getBody();
-				
-				block = this.findPostDocumentationBlock(body.getStartingOffset());
-				
-				if (block != null)
-				{
-					if (body.contains(block.getStart()) && body.contains(block.getEnd()))
-					{
-						if (body.hasChildren())
-						{
-							if (body.getFirstChild().getEndingOffset() < block.getStart())
-							{
-								block = null;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return block;
-	}
-
-	/**
-	 * Finds the comment block ending or just before the given offset
-	 * 
-	 * @param offset
-	 * @return
-	 */
-	public DocumentationBlock findPostDocumentationBlock(int offset)
-	{
-		DocumentationBlock result = null;
-
-		if (this._postBlocks != null)
-		{
-			int index = this.getDocumentationBlockIndex(this._postBlocks, offset);
-
-			if (index < 0)
-			{
-				index = -index - 1;
-			}
-
-			if (index < this._postBlocks.size())
-			{
-				result = this._postBlocks.get(index);
-			}
-		}
-
-		return result;
-	}
-
-	/**
-	 * Finds the comment block ending or just before the given offset
-	 * 
-	 * @param offset
-	 * @return
-	 */
-	public DocumentationBlock findPreDocumentationBlock(int offset)
-	{
-		DocumentationBlock result = null;
-
-		if (this._preBlocks != null)
-		{
-			int index = this.getDocumentationBlockIndex(this._preBlocks, offset);
-
-			if (index < 0)
-			{
-				index = -index - 1 - 1;
-			}
-
-			if (0 <= index && index < this._preBlocks.size())
-			{
-				result = this._preBlocks.get(index);
-			}
-		}
-
-		return result;
-	}
-
-	/**
-	 * getDocumentationBlockIndex
-	 * 
-	 * @param list
-	 * @param offset
-	 * @return
-	 */
-	protected int getDocumentationBlockIndex(List<DocumentationBlock> list, int offset)
-	{
-		int low = 0;
-		int high = list.size() - 1;
-
-		while (low <= high)
-		{
-			int mid = (low + high) >>> 1;
-			DocumentationBlock candidate = list.get(mid);
-
-			if (offset < candidate.getStart())
-			{
-				high = mid - 1;
-			}
-			else if (candidate.getEnd() < offset)
-			{
-				low = mid + 1;
-			}
-			else
-			{
-				return mid;
-			}
-		}
-
-		return -(low + 1);
-	}
-
-	/**
 	 * getGlobalScope
 	 * 
 	 * @return
@@ -199,26 +41,6 @@ public class JSParseRootNode extends ParseRootNode
 	}
 
 	/**
-	 * getPostDocumentationBlocks
-	 * 
-	 * @return
-	 */
-	public List<DocumentationBlock> getPostDocumentationBlocks()
-	{
-		return this._postBlocks;
-	}
-
-	/**
-	 * getPreDocumentationBlocks
-	 * 
-	 * @return
-	 */
-	public List<DocumentationBlock> getPreDocumentationBlocks()
-	{
-		return this._preBlocks;
-	}
-
-	/**
 	 * setGlobalScope
 	 * 
 	 * @param globalScope
@@ -226,25 +48,5 @@ public class JSParseRootNode extends ParseRootNode
 	public void setGlobalScope(Scope<JSNode> globalScope)
 	{
 		this._globalScope = globalScope;
-	}
-
-	/**
-	 * setPostDocumentationBlocks
-	 * 
-	 * @param blocks
-	 */
-	public void setPostDocumentationBlocks(List<DocumentationBlock> blocks)
-	{
-		this._postBlocks = blocks;
-	}
-
-	/**
-	 * setPreDocumentationBlocks
-	 * 
-	 * @param blocks
-	 */
-	public void setPreDocumentationBlocks(List<DocumentationBlock> blocks)
-	{
-		this._preBlocks = blocks;
 	}
 }
