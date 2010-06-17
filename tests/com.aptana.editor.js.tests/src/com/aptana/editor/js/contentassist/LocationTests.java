009b81b7fcabb265b434305d3b0b65473fc3a365
Kevin Lindsey <klindsey@aptana.com>
2010-06-17 09:53:37 -0500

    Update JS CA location type unit tests to test all offsets within the source being tested.

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/LocationTests.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/LocationTests.java
@@ -1,5 +1,7 @@
 package com.aptana.editor.js.contentassist;
 
+import java.text.MessageFormat;
+
 import org.eclipse.jface.text.BadLocationException;
 
 import com.aptana.editor.common.contentassist.LexemeProvider;
@@ -9,24 +11,49 @@ import com.aptana.editor.js.tests.EditorBasedTests;
 public class LocationTests extends EditorBasedTests
 {
 	/**
+	 * testLocations
+	 * 
+	 * @param resource
+	 * @param ranges
+	 */
+	protected void testLocations(String resource, LocationTypeRange... ranges)
+	{
+		// setup everything for the test
+		TestContext context = this.getTestContext(resource);
+		
+		for (LocationTypeRange range : ranges)
+		{
+			for (int offset = range.startingOffset; offset <= range.endingOffset; offset++)
+			{
+				LexemeProvider<JSTokenType> lexemeProvider = context.processor.createLexemeProvider(context.document, offset);
+				LocationType location = context.processor.getLocation(lexemeProvider, offset);
+				String message = MessageFormat.format(
+					"Expected {0} at location {1} of ''{2}'': character = ''{3}''",
+					range.location.toString(),
+					Integer.toString(offset),
+					context.source,
+					(offset < context.source.length()) ? context.source.charAt(offset) : '\0'
+				);
+				assertEquals(message, range.location, location);
+			}
+		}
+	}
+	
+	/**
 	 * testEmptyArgs
 	 * 
 	 * @throws BadLocationException 
 	 */
 	public void testEmptyArgs()
 	{
-		// setup everything for the test
-		TestContext context = this.getTestContext("locations/global_in_empty_args.js");
-		
-		// find the offset to test within the source
-		int offset = context.source.lastIndexOf(")");
-		assertTrue(offset != -1);
-		
-		// lex around the offset
-		LexemeProvider<JSTokenType> lexemeProvider = context.processor.createLexemeProvider(context.document, offset);
-
-		LocationType location = context.processor.getLocation(lexemeProvider, offset);
-		assertEquals(LocationType.IN_GLOBAL, location);
+		this.testLocations(
+			"locations/global_in_empty_args.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.NONE, 1, 15),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 16, 20),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 21, 23),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 24, 25)
+		);
 	}
 	
 	/**
@@ -34,18 +61,15 @@ public class LocationTests extends EditorBasedTests
 	 */
 	public void testInArgs()
 	{
-		// setup everything for the test
-		TestContext context = this.getTestContext("locations/global_in_arg.js");
-		
-		// find the offset to test within the source
-		int offset = context.source.lastIndexOf(",");
-		assertTrue(offset != -1);
-		offset++;
-		
-		// lex around the offset
-		LexemeProvider<JSTokenType> lexemeProvider = context.processor.createLexemeProvider(context.document, offset);
-
-		LocationType location = context.processor.getLocation(lexemeProvider, offset);
-		assertEquals(LocationType.IN_GLOBAL, location);
+		this.testLocations(
+			"locations/global_in_arg.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.NONE, 1, 15),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 16, 20),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 21, 23),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 24),
+			new LocationTypeRange(LocationType.NONE, 25, 31),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 32, 33)
+		);
 	}
 }
