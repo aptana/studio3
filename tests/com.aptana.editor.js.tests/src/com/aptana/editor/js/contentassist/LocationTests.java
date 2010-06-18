3087c09afe455854e3753faa8c1d4cec17b73ee4
Kevin Lindsey <klindsey@aptana.com>
2010-06-18 12:10:16 -0500

    Add in skeleton tests for new location test files

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/LocationTests.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/LocationTests.java
@@ -37,20 +37,6 @@ public class LocationTests extends EditorBasedTests
 		}
 	}
 	
-	public void testForIn()
-	{
-		this.testLocations(
-			"ranges/forInStatement.js"
-		);
-	}
-	
-	public void testForStatement()
-	{
-		this.testLocations(
-			"ranges/forStatement.js"
-		);
-	}
-	
 	/**
 	 * testInvokeWithoutParams
 	 */
@@ -80,56 +66,6 @@ public class LocationTests extends EditorBasedTests
 			new LocationTypeRange(LocationType.IN_GLOBAL, 24),
 			new LocationTypeRange(LocationType.NONE, 25, 31),
 			new LocationTypeRange(LocationType.IN_GLOBAL, 32, 33)
-		);
-	}
-	
-	/**
-	 * testFunctionWithArgs
-	 */
-	public void testFunctionWithArgs()
-	{
-		this.testLocations(
-			"ranges/functionWithArgs.js"
-		);
-	}
-	
-	/**
-	 * testFunctionWithoutArgs
-	 */
-	public void testFunctionWithoutArgs()
-	{
-		this.testLocations(
-			"ranges/functionWithoutArgs.js"
-		);
-	}
-	
-	/**
-	 * testIfStatement
-	 */
-	public void testIfStatement()
-	{
-		this.testLocations(
-			"ranges/functionWithoutArgs.js"
-		);
-	}
-	
-	/**
-	 * testInvokeWithoutParams2
-	 */
-	public void testInvokeWithoutParams2()
-	{
-		this.testLocations(
-			"ranges/invokeWithoutParams.js"
-		);
-	}
-	
-	/**
-	 * testInvokeWithParams
-	 */
-	public void testInvokeWithParams()
-	{
-		this.testLocations(
-			"ranges/invokeWithParams.js"
 		);
 	}
 	
@@ -192,17 +128,267 @@ public class LocationTests extends EditorBasedTests
 			new LocationTypeRange(LocationType.NONE, 5),
 			new LocationTypeRange(LocationType.IN_GLOBAL, 6, 7),
 			new LocationTypeRange(LocationType.NONE, 8, 9),
-			new LocationTypeRange(LocationType.IN_GLOBAL, 10)
+			new LocationTypeRange(LocationType.IN_GLOBAL, 10, 11)
 		);
 	}
 	
 	/**
-	 * testWhileStatement
+	 * testGetSimpleProperty1
 	 */
-	public void testWhileStatement()
+	public void testGetSimpleProperty1()
 	{
 		this.testLocations(
-			"ranges/whileStatement.js"
+			"locations/simpleGetProperty1.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 1, 3),
+			new LocationTypeRange(LocationType.IN_PROPERTY_NAME, 4, 7),
+			new LocationTypeRange(LocationType.NONE, 8),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 9),
+			new LocationTypeRange(LocationType.NONE, 10, 11),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 12)
+		);
+	}
+	
+	/**
+	 * testSimpleGetProperty2
+	 */
+	public void testSimpleGetProperty2()
+	{
+		this.testLocations(
+			"locations/simpleGetProperty2.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 1, 3),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 4),
+			new LocationTypeRange(LocationType.IN_PROPERTY_NAME, 5, 9),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 10),
+			new LocationTypeRange(LocationType.NONE, 11),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 12, 13),
+			new LocationTypeRange(LocationType.NONE, 14, 15),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 16, 17)
+		);
+	}
+	
+	/**
+	 * testSimpleGetElement1
+	 */
+	public void testSimpleGetElement1()
+	{
+		this.testLocations(
+			"locations/simpleGetElement1.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 1, 3),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 4),
+			new LocationTypeRange(LocationType.NONE, 5, 6),				// should 7 be NONE?
+			new LocationTypeRange(LocationType.IN_GLOBAL, 7, 8),
+			new LocationTypeRange(LocationType.NONE, 9, 10),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 11)
+		);
+	}
+	
+	/**
+	 * testSimpleGetElement2
+	 */
+	public void testSimpleGetElement2()
+	{
+		this.testLocations(
+			"locations/simpleGetElement2.js",
+			new LocationTypeRange(LocationType.IN_GLOBAL, 0),
+			new LocationTypeRange(LocationType.IN_VARIABLE_NAME, 1, 3),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 4, 6),
+			new LocationTypeRange(LocationType.NONE, 7, 10),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 11, 13),
+			new LocationTypeRange(LocationType.NONE, 14, 15),
+			new LocationTypeRange(LocationType.IN_GLOBAL, 16, 17)
+		);
+	}
+	
+	/**
+	 * testTryCatchFinally
+	 */
+	public void testTryCatchFinally()
+	{
+		this.testLocations(
+			"locations/tryCatchFinally.js"
+		);
+	}
+	
+	/**
+	 * testConditional
+	 */
+	public void testConditional()
+	{
+		this.testLocations(
+			"locations/conditional.js"
+		);
+	}
+	
+	/**
+	 * testNew
+	 */
+	public void testNew()
+	{
+		this.testLocations(
+			"locations/new.js"
+		);
+	}
+	
+	/**
+	 * testVar1
+	 */
+	public void testVar1()
+	{
+		this.testLocations(
+			"locations/var1.js"
+		);
+	}
+	
+	/**
+	 * testVar2
+	 */
+	public void testVar2()
+	{
+		this.testLocations(
+			"locations/var2.js"
+		);
+	}
+	
+	/**
+	 * testVar3
+	 */
+	public void testVar3()
+	{
+		this.testLocations(
+			"locations/var3.js"
+		);
+	}
+	
+	/**
+	 * testDo
+	 */
+	public void testDo()
+	{
+		this.testLocations(
+			"locations/do.js"
+		);
+	}
+	
+	/**
+	 * testForIn
+	 */
+	public void testForIn()
+	{
+		this.testLocations(
+			"locations/forIn.js"
+		);
+	}
+	
+	/**
+	 * testFor
+	 */
+	public void testFor()
+	{
+		this.testLocations(
+			"locations/for.js"
+		);
+	}
+	
+	/**
+	 * testIf
+	 */
+	public void testIf()
+	{
+		this.testLocations(
+			"locations/if.js"
+		);
+	}
+	
+	/**
+	 * testLabelledFor
+	 */
+	public void testLabelledFor()
+	{
+		this.testLocations(
+			"locations/labelledFor.js"
+		);
+	}
+	
+	/**
+	 * testArrayLiteral
+	 */
+	public void testArrayLiteral()
+	{
+		this.testLocations(
+			"locations/arrayLiteral.js"
+		);
+	}
+	
+	/**
+	 * testObjectLiteral
+	 */
+	public void testObjectLiteral()
+	{
+		this.testLocations(
+			"locations/objectLiteral.js"
+		);
+	}
+	
+	/**
+	 * testSwitch
+	 */
+	public void testSwitch()
+	{
+		this.testLocations(
+			"locations/switch.js"
+		);
+	}
+	
+	/**
+	 * testGroup
+	 */
+	public void testGroup()
+	{
+		this.testLocations(
+			"locations/group.js"
+		);
+	}
+	
+	/**
+	 * testFunctionWithReturn
+	 */
+	public void testFunctionWithReturn()
+	{
+		this.testLocations(
+			"locations/functionWithReturn.js"
+		);
+	}
+	
+	/**
+	 * testFunctionWithThrow
+	 */
+	public void testFunctionWithThrow()
+	{
+		this.testLocations(
+			"locations/functionWithThrow.js"
+		);
+	}
+	
+	/**
+	 * testWhile
+	 */
+	public void testWhile()
+	{
+		this.testLocations(
+			"locations/while.js"
+		);
+	}
+	
+	/**
+	 * testWith
+	 */
+	public void testWith()
+	{
+		this.testLocations(
+			"locations/with.js"
 		);
 	}
 }
