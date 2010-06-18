d5ee854a453317c5ced481d36a2f79320d33eebd
Kevin Lindsey <klindsey@aptana.com>
2010-06-18 12:09:53 -0500

    Include the DOT symbol so we can correctly determine location type for properties

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSGetPropertyOperatorNode.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/parsing/ast/JSGetPropertyOperatorNode.java
@@ -1,6 +1,10 @@
 package com.aptana.editor.js.parsing.ast;
 
+import com.aptana.editor.js.contentassist.LocationType;
 import com.aptana.parsing.ast.IParseNode;
+
+import beaver.Symbol;
+
 
 public class JSGetPropertyOperatorNode extends JSBinaryOperatorNode
 {
@@ -10,12 +14,38 @@ public class JSGetPropertyOperatorNode extends JSBinaryOperatorNode
 	 * @param left
 	 * @param right
 	 */
-	public JSGetPropertyOperatorNode(JSNode left, JSNode right)
+	public JSGetPropertyOperatorNode(JSNode left, Symbol operator, JSNode right)
 	{
-		super(left, right);
-		setType(JSNodeTypes.GET_PROPERTY);
+		super(left, operator, right);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.js.parsing.ast.JSNode#getLocationType(int)
+	 */
+	@Override
+	LocationType getLocationType(int offset)
+	{
+		LocationType result = LocationType.UNKNOWN;
+
+		if (this.contains(offset))
+		{
+			IParseNode lhs = this.getLeftHandSide();
+			Symbol operator = this.getOperator();
+			
+			if (lhs.contains(offset) || lhs.getEndingOffset() <= offset && offset < operator.getStart())
+			{
+				result = ((JSNode) lhs).getLocationType(offset);
+			}
+			else
+			{
+				result = LocationType.IN_PROPERTY_NAME;
+			}
+		}
+
+		return result;
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see com.aptana.editor.js.parsing.ast.JSBinaryOperatorNode#toString()
@@ -24,8 +54,11 @@ public class JSGetPropertyOperatorNode extends JSBinaryOperatorNode
 	public String toString()
 	{
 		StringBuilder text = new StringBuilder();
-		IParseNode[] children = getChildren();
-		text.append(children[0]).append(".").append(children[1]); //$NON-NLS-1$
+		
+		text.append(this.getLeftHandSide());
+		text.append("."); //$NON-NLS-1$
+		text.append(this.getRightHandSide());
+		
 		this.appendSemicolon(text);
 
 		return text.toString();
