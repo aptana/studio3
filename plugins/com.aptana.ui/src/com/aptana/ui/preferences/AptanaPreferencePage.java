59ba51891f67ff211c799c9edf9eb7c35f5f77ba
mxia <mxia@aptana.com>
2010-06-23 17:50:28 -0700

    Implemented #621: Added links to sub-preferences in the top level Aptana preferences page.

--- plugins/com.aptana.ui/src/com/aptana/ui/preferences/AptanaPreferencePage.java
+++ plugins/com.aptana.ui/src/com/aptana/ui/preferences/AptanaPreferencePage.java
@@ -1,28 +1,13 @@
 package com.aptana.ui.preferences;
 
-import org.eclipse.jface.preference.PreferencePage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPreferencePage;
-
-public class AptanaPreferencePage extends PreferencePage implements IWorkbenchPreferencePage
+public class AptanaPreferencePage extends GenericRootPreferencePage
 {
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
-	 */
-	protected Control createContents(Composite parent)
-	{
-		return new Composite(parent, SWT.NONE);
-	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
-	 */
-	public void init(IWorkbench workbench)
+	protected static String PAGE_ID = "com.aptana.ui.AptanaPreferencePage"; //$NON-NLS-1$
+
+	@Override
+	protected String getPageId()
 	{
+		return PAGE_ID;
 	}
 }
