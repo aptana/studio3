089b3245aedfc6492097d254400ae5216065f8eb
mxia <mxia@aptana.com>
2010-06-29 18:02:16 -0700

    Fixed Studio 3 #666: remember the setting for publishing the Heroku application immediately.

--- plugins/com.aptana.deploy/src/com/aptana/deploy/preferences/IPreferenceConstants.java
+++ plugins/com.aptana.deploy/src/com/aptana/deploy/preferences/IPreferenceConstants.java
@@ -3,4 +3,5 @@ package com.aptana.deploy.preferences;
 public interface IPreferenceConstants
 {
 	public static final String AUTO_SYNC = "AutoSyncChangesWithRemote"; //$NON-NLS-1$
+	public static final String HEROKU_AUTO_PUBLISH = "HerokuAutoPublish"; //$NON-NLS-1$
 }
