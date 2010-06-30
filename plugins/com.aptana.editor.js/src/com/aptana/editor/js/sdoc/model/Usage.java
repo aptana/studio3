91da71321245cf2ef3ac8222b049423b1651402d
Kevin Lindsey <klindsey@aptana.com>
2010-06-30 16:36:33 -0500

    Add ability to go to and from Usage enums and strings

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/sdoc/model/Usage.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/sdoc/model/Usage.java
@@ -1,9 +1,63 @@
 package com.aptana.editor.js.sdoc.model;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Usage
 {
-	REQUIRED,
-	OPTIONAL,
-	ZERO_OR_MORE,
-	ONE_OR_MORE;
+	UNKNOWN("unknown"),
+	REQUIRED("required"),
+	OPTIONAL("optional"),
+	ZERO_OR_MORE("zero-or-more"),
+	ONE_OR_MORE("one-or-more");
+	
+	private static final Map<String, Usage> NAME_MAP;
+	private String _name;
+	
+	/**
+	 * static initializer
+	 */
+	static
+	{
+		NAME_MAP = new HashMap<String, Usage>();
+		
+		for (Usage usage : EnumSet.allOf(Usage.class))
+		{
+			NAME_MAP.put(usage.getName(), usage);
+		}
+	}
+	
+	/**
+	 * Usage
+	 * 
+	 * @param name
+	 */
+	private Usage(String name)
+	{
+		this._name = name;
+	}
+	
+	/**
+	 * fromName
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static Usage fromName(String name)
+	{
+		Usage result = NAME_MAP.get(name);
+		
+		return (result != null) ? result : UNKNOWN;
+	}
+	
+	/**
+	 * getName
+	 * 
+	 * @return
+	 */
+	public String getName()
+	{
+		return this._name;
+	}
 }
