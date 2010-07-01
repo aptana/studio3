fab6749d15868ab307924c061c028585be14f4b7
sgtcoolguy <cwilliams@aptana.com>
2010-07-01 10:53:17 -0400

    Just set "Aptana Studio" theme as default

--- plugins/com.aptana.theme/src/com/aptana/theme/internal/preferences/ThemerPreferenceInitializer.java
+++ plugins/com.aptana.theme/src/com/aptana/theme/internal/preferences/ThemerPreferenceInitializer.java
@@ -5,7 +5,6 @@ import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.osgi.service.prefs.BackingStoreException;
 
-import com.aptana.core.util.EclipseUtil;
 import com.aptana.theme.ThemePlugin;
 import com.aptana.theme.internal.ThemeManager;
 
@@ -21,16 +20,7 @@ public class ThemerPreferenceInitializer extends AbstractPreferenceInitializer
 	public void initializeDefaultPreferences()
 	{
 		IEclipsePreferences node = new DefaultScope().getNode(ThemePlugin.PLUGIN_ID);
-		if (EclipseUtil.isStandalone())
-		{
-			// When they're using our standalone, use Twilight copy.
-			node.put(ThemeManager.ACTIVE_THEME, "Aptana Studio"); //$NON-NLS-1$
-		}
-		else
-		{
-			// When we're installed as a plugin use our old coloring. "Eclipse" theme is too boring.
-			node.put(ThemeManager.ACTIVE_THEME, "Aptana Studio 2.x"); //$NON-NLS-1$
-		}
+		node.put(ThemeManager.ACTIVE_THEME, "Aptana Studio"); //$NON-NLS-1$
 		try
 		{
 			node.flush();
