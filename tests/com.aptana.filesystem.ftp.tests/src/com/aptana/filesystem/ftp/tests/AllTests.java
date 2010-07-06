a02fe418728359e92b5696dc5ec55845fce8e8f1
Max Stepanov <mstepanov@aptana.com>
2010-07-06 15:37:18 -0700

    Revert "Change back to using the parallel test suite."
    This reverts commit 34b9c2b889a75e8e13124d73edc757cc2b4fc7cb.

--- tests/com.aptana.filesystem.ftp.tests/src/com/aptana/filesystem/ftp/tests/AllTests.java
+++ tests/com.aptana.filesystem.ftp.tests/src/com/aptana/filesystem/ftp/tests/AllTests.java
@@ -3,8 +3,6 @@ package com.aptana.filesystem.ftp.tests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.kohsuke.junit.ParallelTestSuite;
-
 public class AllTests
 {
 
@@ -19,7 +17,7 @@ public class AllTests
 		// }
 		// else
 		// {
-		suite = new ParallelTestSuite(AllTests.class.getName(), 2);
+		suite = new TestSuite(AllTests.class.getName());
 		// }
 		// $JUnit-BEGIN$
 		suite.addTestSuite(FTPConnectionTest.class);
