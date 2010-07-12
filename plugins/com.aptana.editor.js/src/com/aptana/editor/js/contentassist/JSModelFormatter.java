ad74359a56ea47400611105d1dddaf6a01c52ac4
Kevin Lindsey <klindsey@aptana.com>
2010-07-12 15:02:21 -0500

    Add formatting for documents so they show only the last segment of the URI. No longer performing any special formatting for properties names versus function names. Member sorting and formatting.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSModelFormatter.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSModelFormatter.java
@@ -17,13 +17,13 @@ public class JSModelFormatter
 	private static void addReturnTypes(StringBuilder buffer, ReturnTypeElement[] returnTypes, String defaultType)
 	{
 		boolean first;
-		
+
 		if (returnTypes != null && returnTypes.length > 0)
 		{
 			buffer.append(" : "); //$NON-NLS-1$
-			
+
 			first = true;
-			
+
 			for (ReturnTypeElement returnType : returnTypes)
 			{
 				if (first == false)
@@ -34,9 +34,9 @@ public class JSModelFormatter
 				{
 					first = false;
 				}
-				
+
 				String type = getTypeDisplayName(returnType.getType());
-				
+
 				buffer.append(type);
 			}
 		}
@@ -45,7 +45,7 @@ public class JSModelFormatter
 			buffer.append(" : ").append(defaultType); //$NON-NLS-1$
 		}
 	}
-	
+
 	/**
 	 * formatFunction
 	 * 
@@ -55,13 +55,13 @@ public class JSModelFormatter
 	public static String getDescription(FunctionElement function)
 	{
 		StringBuilder buffer = new StringBuilder();
-		
+
 		// title
 		buffer.append("<b>").append(function.getName()).append("</b>").append("("); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		
+
 		// append parameters
 		boolean first = true;
-		
+
 		for (ParameterElement parameter : function.getParameters())
 		{
 			if (first == false)
@@ -72,43 +72,43 @@ public class JSModelFormatter
 			{
 				first = false;
 			}
-			
+
 			String usage = parameter.getUsage();
 			boolean isOptional = ("zero-or-more".equals(usage) || "optional".equals(usage)); //$NON-NLS-1$ //$NON-NLS-2$
 			boolean isRepeating = ("zero-or-more".equals(usage) || "one-or-more".equals(usage)); //$NON-NLS-1$ //$NON-NLS-2$
-			
+
 			if (isOptional)
 			{
 				buffer.append("["); //$NON-NLS-1$
 			}
-			
+
 			buffer.append("<b>").append(parameter.getName()).append("</b>"); //$NON-NLS-1$ //$NON-NLS-2$
-			
+
 			if (isRepeating)
 			{
 				buffer.append("+"); //$NON-NLS-1$
 			}
-			
+
 			buffer.append(" : ").append(StringUtil.join("|", parameter.getTypes())); //$NON-NLS-1$ //$NON-NLS-2$
-			
+
 			if (isOptional)
 			{
 				buffer.append("]"); //$NON-NLS-1$
 			}
 		}
-		
+
 		buffer.append(")"); //$NON-NLS-1$
-		
+
 		// return type
 		addReturnTypes(buffer, function.getReturnTypes(), "void"); //$NON-NLS-1$
-		
+
 		// description
 		buffer.append("<br><br>"); //$NON-NLS-1$
 		buffer.append(function.getDescription());
-		
+
 		return buffer.toString();
 	}
-	
+
 	/**
 	 * formatProperty
 	 * 
@@ -121,22 +121,22 @@ public class JSModelFormatter
 		{
 			return getDescription((FunctionElement) property);
 		}
-		
+
 		StringBuilder buffer = new StringBuilder();
-		
+
 		// title
 		buffer.append("<b>").append(property.getName()).append("</b>"); //$NON-NLS-1$ //$NON-NLS-2$
-		
+
 		// type
 		addReturnTypes(buffer, property.getTypes(), "undefined"); //$NON-NLS-1$
-		
+
 		// description
 		buffer.append("<br><br>"); //$NON-NLS-1$
 		buffer.append(property.getDescription());
-		
+
 		return buffer.toString();
 	}
-	
+
 	/**
 	 * getDescription
 	 * 
@@ -147,14 +147,41 @@ public class JSModelFormatter
 	public static String getDescription(String header, List<String> items)
 	{
 		StringBuilder buffer = new StringBuilder();
-		
+
 		// emit header
 		buffer.append("<b>").append(header).append("</b><br><br>"); //$NON-NLS-1$ //$NON-NLS-2$
-		
+
 		// emit list
 		buffer.append(StringUtil.join("<br>", items)); //$NON-NLS-1$
-		
+
 		return buffer.toString();
+	}
+
+	/**
+	 * getDocumentDisplayName
+	 * 
+	 * @param document
+	 * @return
+	 */
+	public static String getDocumentDisplayName(String document)
+	{
+		String result = null;
+
+		if (document != null)
+		{
+			int index = document.lastIndexOf('/');
+
+			if (index != -1)
+			{
+				result = document.substring(index + 1);
+			}
+			else
+			{
+				result = document;
+			}
+		}
+
+		return result;
 	}
 
 	/**
@@ -166,7 +193,7 @@ public class JSModelFormatter
 	public static String getTypeDisplayName(String type)
 	{
 		String result = null;
-		
+
 		if (type != null)
 		{
 			if (type.startsWith(GENERIC_CLASS_OPEN) && type.endsWith(GENERIC_CLASS_CLOSE))
@@ -182,39 +209,10 @@ public class JSModelFormatter
 				result = type;
 			}
 		}
-		
+
 		return result;
 	}
-	
-	/**
-	 * getName
-	 * 
-	 * @param function
-	 * @return
-	 */
-	public static String getName(FunctionElement function)
-	{
-		return function.getName() + "()"; //$NON-NLS-1$
-	}
-	
-	/**
-	 * getName
-	 * 
-	 * @param property
-	 * @return
-	 */
-	public static String getName(PropertyElement property)
-	{
-		if (property instanceof FunctionElement)
-		{
-			return getName((FunctionElement) property);
-		}
-		else
-		{
-			return property.getName();
-		}
-	}
-	
+
 	private JSModelFormatter()
 	{
 	}
