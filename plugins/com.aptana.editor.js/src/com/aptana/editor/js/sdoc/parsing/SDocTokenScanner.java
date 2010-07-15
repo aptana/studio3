52749ef9f9fa042958941ba6b7859159f17707e6
Kevin Lindsey <klindsey@aptana.com>
2010-07-15 12:08:17 -0500

    Use an annotation to suppress nls warnings instead of tagging each string individually, per Chris' suggestion

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/sdoc/parsing/SDocTokenScanner.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/sdoc/parsing/SDocTokenScanner.java
@@ -84,12 +84,13 @@ public class SDocTokenScanner extends RuleBasedScanner
 	/**
 	 * SDocTokenScanner
 	 */
+	@SuppressWarnings("nls")
 	public SDocTokenScanner()
 	{
 		List<IRule> rules = new LinkedList<IRule>();
 
-		rules.add(new RegexpRule("[ \\t]+", getToken(SDocTokenType.WHITESPACE), true)); //$NON-NLS-1$
-		rules.add(new RegexpRule("^[ \\t]*\\*(?!/)[ \\t]*", getToken(SDocTokenType.WHITESPACE), true)); //$NON-NLS-1$
+		rules.add(new RegexpRule("[ \\t]+", getToken(SDocTokenType.WHITESPACE), true));
+		rules.add(new RegexpRule("^[ \\t]*\\*(?!/)[ \\t]*", getToken(SDocTokenType.WHITESPACE), true));
 		rules.add(new SingleCharacterRule('\r', getToken(SDocTokenType.WHITESPACE)));
 		rules.add(new SingleCharacterRule('\n', getToken(SDocTokenType.WHITESPACE)));
 
@@ -98,34 +99,34 @@ public class SDocTokenScanner extends RuleBasedScanner
 		rules.add(new SingleCharacterRule(']', getToken(SDocTokenType.RBRACKET)));
 
 		WordRule operatorRules = new WordRule(new OperatorDetector(), getToken(SDocTokenType.UNKNOWN));
-		operatorRules.addWord("/**", getToken(SDocTokenType.START_DOCUMENTATION)); //$NON-NLS-1$
-		operatorRules.addWord("*/", getToken(SDocTokenType.END_DOCUMENTATION)); //$NON-NLS-1$
+		operatorRules.addWord("/**", getToken(SDocTokenType.START_DOCUMENTATION));
+		operatorRules.addWord("*/", getToken(SDocTokenType.END_DOCUMENTATION));
 		rules.add(operatorRules);
 
-		rules.add(new PatternRule("{", "}", getToken(SDocTokenType.TYPES), '\0', false)); //$NON-NLS-1$ //$NON-NLS-2$
+		rules.add(new PatternRule("{", "}", getToken(SDocTokenType.TYPES), '\0', false));
 
 		WordRule tagRules = new WordRule(new TagDetector(), getToken(SDocTokenType.UNKNOWN));
-		tagRules.addWord("@advanced", getToken(SDocTokenType.ADVANCED)); //$NON-NLS-1$
-		tagRules.addWord("@alias", getToken(SDocTokenType.ALIAS)); //$NON-NLS-1$
-		tagRules.addWord("@author", getToken(SDocTokenType.AUTHOR)); //$NON-NLS-1$
-		tagRules.addWord("@classDescription", getToken(SDocTokenType.CLASS_DESCRIPTION)); //$NON-NLS-1$
-		tagRules.addWord("@constructor", getToken(SDocTokenType.CONSTRUCTOR)); //$NON-NLS-1$
-		tagRules.addWord("@example", getToken(SDocTokenType.EXAMPLE)); //$NON-NLS-1$
-		tagRules.addWord("@exception", getToken(SDocTokenType.EXCEPTION)); //$NON-NLS-1$
-		tagRules.addWord("@extends", getToken(SDocTokenType.EXTENDS)); //$NON-NLS-1$
-		tagRules.addWord("@internal", getToken(SDocTokenType.INTERNAL)); //$NON-NLS-1$
-		tagRules.addWord("@method", getToken(SDocTokenType.METHOD)); //$NON-NLS-1$
-		tagRules.addWord("@namespace", getToken(SDocTokenType.NAMESPACE)); //$NON-NLS-1$
-		tagRules.addWord("@overview", getToken(SDocTokenType.OVERVIEW)); //$NON-NLS-1$
-		tagRules.addWord("@param", getToken(SDocTokenType.PARAM)); //$NON-NLS-1$
-		tagRules.addWord("@private", getToken(SDocTokenType.PRIVATE)); //$NON-NLS-1$
-		tagRules.addWord("@property", getToken(SDocTokenType.PROPERTY)); //$NON-NLS-1$
-		tagRules.addWord("@return", getToken(SDocTokenType.RETURN)); //$NON-NLS-1$
-		tagRules.addWord("@see", getToken(SDocTokenType.SEE)); //$NON-NLS-1$
-		tagRules.addWord("@type", getToken(SDocTokenType.TYPE)); //$NON-NLS-1$
+		tagRules.addWord("@advanced", getToken(SDocTokenType.ADVANCED));
+		tagRules.addWord("@alias", getToken(SDocTokenType.ALIAS));
+		tagRules.addWord("@author", getToken(SDocTokenType.AUTHOR));
+		tagRules.addWord("@classDescription", getToken(SDocTokenType.CLASS_DESCRIPTION));
+		tagRules.addWord("@constructor", getToken(SDocTokenType.CONSTRUCTOR));
+		tagRules.addWord("@example", getToken(SDocTokenType.EXAMPLE));
+		tagRules.addWord("@exception", getToken(SDocTokenType.EXCEPTION));
+		tagRules.addWord("@extends", getToken(SDocTokenType.EXTENDS));
+		tagRules.addWord("@internal", getToken(SDocTokenType.INTERNAL));
+		tagRules.addWord("@method", getToken(SDocTokenType.METHOD));
+		tagRules.addWord("@namespace", getToken(SDocTokenType.NAMESPACE));
+		tagRules.addWord("@overview", getToken(SDocTokenType.OVERVIEW));
+		tagRules.addWord("@param", getToken(SDocTokenType.PARAM));
+		tagRules.addWord("@private", getToken(SDocTokenType.PRIVATE));
+		tagRules.addWord("@property", getToken(SDocTokenType.PROPERTY));
+		tagRules.addWord("@return", getToken(SDocTokenType.RETURN));
+		tagRules.addWord("@see", getToken(SDocTokenType.SEE));
+		tagRules.addWord("@type", getToken(SDocTokenType.TYPE));
 		rules.add(tagRules);
 
-		rules.add(new RegexpRule("[^ \\t{\\[\\]#]+", getToken(SDocTokenType.TEXT), true)); //$NON-NLS-1$
+		rules.add(new RegexpRule("[^ \\t{\\[\\]#]+", getToken(SDocTokenType.TEXT), true));
 
 		this.setDefaultReturnToken(getToken(SDocTokenType.ERROR));
 		this.setRules(rules.toArray(new IRule[rules.size()]));
