3be3ac8abf7381b931b092d0967d95ce7bb1ef93
Kevin Lindsey <klindsey@aptana.com>
2010-07-15 16:38:05 -0500

    Add a preference to keep track of the current index format version for JS

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/preferences/PreferenceInitializer.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/preferences/PreferenceInitializer.java
@@ -40,16 +40,23 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 import com.aptana.editor.js.Activator;
 
-public class PreferenceInitializer extends AbstractPreferenceInitializer {
+public class PreferenceInitializer extends AbstractPreferenceInitializer
+{
+	public static final boolean DEFAULT_COMMENT_INDENT_USE_STAR = true;
+	public static final boolean DEFAULT_AUTO_INDENT_ON_RETURN = true;
 
-    public static final boolean DEFAULT_COMMENT_INDENT_USE_STAR = true;
-    public static final boolean DEFAULT_AUTO_INDENT_ON_RETURN = true;
-
-    @Override
-    public void initializeDefaultPreferences() {
-        IEclipsePreferences prefs = (new DefaultScope()).getNode(Activator.PLUGIN_ID);
-        prefs.putBoolean(IPreferenceConstants.COMMENT_INDENT_USE_STAR, DEFAULT_COMMENT_INDENT_USE_STAR);
-        prefs.putBoolean(IPreferenceConstants.AUTO_INDENT_ON_CARRIAGE_RETURN, DEFAULT_AUTO_INDENT_ON_RETURN);
-        prefs.putBoolean(com.aptana.editor.common.preferences.IPreferenceConstants.LINK_OUTLINE_WITH_EDITOR, true);
-    }
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	@Override
+	public void initializeDefaultPreferences()
+	{
+		IEclipsePreferences prefs = (new DefaultScope()).getNode(Activator.PLUGIN_ID);
+		
+		prefs.putBoolean(IPreferenceConstants.COMMENT_INDENT_USE_STAR, DEFAULT_COMMENT_INDENT_USE_STAR);
+		prefs.putBoolean(IPreferenceConstants.AUTO_INDENT_ON_CARRIAGE_RETURN, DEFAULT_AUTO_INDENT_ON_RETURN);
+		prefs.putBoolean(com.aptana.editor.common.preferences.IPreferenceConstants.LINK_OUTLINE_WITH_EDITOR, true);
+		prefs.putDouble(IPreferenceConstants.JS_INDEX_VERSION, 0);
+	}
 }
