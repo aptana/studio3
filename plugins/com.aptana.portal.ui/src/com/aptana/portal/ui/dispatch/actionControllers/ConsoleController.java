73ad3e7414a9d5a84366b00223ce9e7dff4dc623
Shalom Gibly <sgibly@aptana.com>
2010-07-19 14:48:00 -0700

    Add a ConsoleController with registrations for console.log, console.error and console.debug

---
+++ plugins/com.aptana.portal.ui/src/com/aptana/portal/ui/dispatch/actionControllers/ConsoleController.java
@@ -0,0 +1,124 @@
+package com.aptana.portal.ui.dispatch.actionControllers;
+
+import java.util.Set;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+import org.mortbay.util.ajax.JSON;
+
+import com.aptana.configurations.processor.ConfigurationStatus;
+import com.aptana.portal.ui.PortalUIPlugin;
+import com.aptana.portal.ui.dispatch.IBrowserNotificationConstants;
+
+/**
+ * A controller the tunnels console.log calls to the Studio's console.<br>
+ * Note that regular log messages will only work when Platform.inDevelopmentMode() is true. While error logging will be
+ * active regardless of the development mode.
+ * 
+ * @author shalom
+ */
+public class ConsoleController extends AbstractActionController
+{
+
+	private static final String BROWSER_CONSOLE_NAME = "Browser Console";
+
+	public ConsoleController()
+	{
+	}
+
+	@Override
+	public void configurationStateChanged(ConfigurationStatus status, Set<String> attributesChanged)
+	{
+	}
+
+	// ############## Actions ###############
+
+	@ControllerAction
+	public Object log(Object arguments)
+	{
+		if (Platform.inDevelopmentMode())
+		{
+			writeToConsole(arguments, false);
+		}
+		return IBrowserNotificationConstants.JSON_OK;
+	}
+
+	@ControllerAction
+	public Object debug(Object arguments)
+	{
+		return log(arguments);
+	}
+
+	@ControllerAction
+	public Object error(Object arguments)
+	{
+		writeToConsole(arguments, true);
+		return IBrowserNotificationConstants.JSON_OK;
+	}
+
+	private void writeToConsole(Object arguments, boolean isError)
+	{
+		MessageConsole console = findOrCreateConsole();
+		revealConsole(console);
+
+		MessageConsoleStream out = console.newMessageStream();
+		if (isError)
+		{
+			out.setColor(Display.getDefault().getSystemColor(SWT.COLOR_RED));
+		}
+		else
+		{
+			out.setColor(Display.getDefault().getSystemColor(SWT.COLOR_BLUE));
+		}
+		out.println(JSON.toString(arguments));
+	}
+
+	private void revealConsole(MessageConsole console)
+	{
+		IWorkbenchWindow workbenchWindow = PortalUIPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow();
+		if (workbenchWindow != null)
+		{
+			IWorkbenchPage activePage = workbenchWindow.getActivePage();
+			String id = IConsoleConstants.ID_CONSOLE_VIEW;
+			IConsoleView view;
+			try
+			{
+				view = (IConsoleView) activePage.showView(id);
+				view.display(console);
+			}
+			catch (PartInitException e)
+			{
+				PortalUIPlugin.logError(e);
+			}
+		}
+
+	}
+
+	private MessageConsole findOrCreateConsole()
+	{
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		for (IConsole console : conMan.getConsoles())
+		{
+			if (BROWSER_CONSOLE_NAME.equals(console.getName()))
+			{
+				return (MessageConsole) console;
+			}
+		}
+		// no console found, so create a new one
+		MessageConsole myConsole = new MessageConsole(BROWSER_CONSOLE_NAME, null);
+		conMan.addConsoles(new IConsole[] { myConsole });
+		return myConsole;
+	}
+}
