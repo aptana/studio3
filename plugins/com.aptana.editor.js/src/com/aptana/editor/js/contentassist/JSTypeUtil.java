60caf1c8548f5ec3fe7d0e46c46885fe71deecde
Kevin Lindsey <klindsey@aptana.com>
2010-07-23 12:14:15 -0500

    Rename JSContentAssistUtil to JSTypeUtil now that other type-related methods are being added here.

---
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSTypeUtil.java
@@ -0,0 +1,186 @@
+package com.aptana.editor.js.contentassist;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.aptana.editor.js.JSTypeConstants;
+import com.aptana.editor.js.contentassist.model.FunctionElement;
+import com.aptana.editor.js.contentassist.model.ParameterElement;
+import com.aptana.editor.js.contentassist.model.PropertyElement;
+import com.aptana.editor.js.contentassist.model.ReturnTypeElement;
+import com.aptana.editor.js.sdoc.model.DocumentationBlock;
+import com.aptana.editor.js.sdoc.model.ExampleTag;
+import com.aptana.editor.js.sdoc.model.ParamTag;
+import com.aptana.editor.js.sdoc.model.ReturnTag;
+import com.aptana.editor.js.sdoc.model.Tag;
+import com.aptana.editor.js.sdoc.model.TagType;
+import com.aptana.editor.js.sdoc.model.Type;
+import com.aptana.editor.js.sdoc.model.TypeTag;
+
+public class JSTypeUtil
+{
+	/**
+	 * applyDocumentation
+	 * 
+	 * @param function
+	 * @param block
+	 */
+	public static void applyDocumentation(FunctionElement function, DocumentationBlock block)
+	{
+		if (block != null)
+		{
+			// apply description
+			function.setDescription(block.getText());
+
+			// apply parameters
+			for (Tag tag : block.getTags(TagType.PARAM))
+			{
+				ParamTag paramTag = (ParamTag) tag;
+				ParameterElement parameter = new ParameterElement();
+
+				parameter.setName(paramTag.getName());
+				parameter.setDescription(paramTag.getText());
+				parameter.setUsage(paramTag.getUsage().getName());
+
+				for (Type type : paramTag.getTypes())
+				{
+					parameter.addType(type.toSource());
+				}
+
+				function.addParameter(parameter);
+			}
+
+			// apply return types
+			for (Tag tag : block.getTags(TagType.RETURN))
+			{
+				ReturnTag returnTag = (ReturnTag) tag;
+
+				for (Type type : returnTag.getTypes())
+				{
+					ReturnTypeElement returnType = new ReturnTypeElement();
+
+					returnType.setType(type.toSource());
+					returnType.setDescription(returnTag.getText());
+
+					function.addReturnType(returnType);
+				}
+			}
+
+			// apply examples
+			for (Tag tag : block.getTags(TagType.EXAMPLE))
+			{
+				ExampleTag exampleTag = (ExampleTag) tag;
+
+				function.addExample(exampleTag.getText());
+			}
+		}
+	}
+
+	/**
+	 * applyDocumentation
+	 * 
+	 * @param property
+	 * @param block
+	 */
+	public static void applyDocumentation(PropertyElement property, DocumentationBlock block)
+	{
+		if (property instanceof FunctionElement)
+		{
+			applyDocumentation((FunctionElement) property, block);
+		}
+		else
+		{
+			if (block != null)
+			{
+				// apply description
+				property.setDescription(block.getText());
+
+				// apply types
+				for (Tag tag : block.getTags(TagType.TYPE))
+				{
+					TypeTag typeTag = (TypeTag) tag;
+
+					for (Type type : typeTag.getTypes())
+					{
+						ReturnTypeElement returnType = new ReturnTypeElement();
+
+						returnType.setType(type.toSource());
+						returnType.setDescription(typeTag.getText());
+
+						property.addType(returnType);
+					}
+				}
+
+				// apply examples
+				for (Tag tag : block.getTags(TagType.EXAMPLE))
+				{
+					ExampleTag exampleTag = (ExampleTag) tag;
+
+					property.addExample(exampleTag.getText());
+				}
+			}
+		}
+	}
+
+	/**
+	 * getUniqueTypeName
+	 * 
+	 * @return
+	 */
+	public static String getUniqueTypeName()
+	{
+		UUID uuid = UUID.randomUUID();
+
+		return MessageFormat.format("{0}{1}", JSTypeConstants.DYNAMIC_CLASS_PREFIX, uuid); //$NON-NLS-1$
+	}
+
+	/**
+	 * propertyToDocumentation
+	 * 
+	 * @param function
+	 * @return
+	 */
+	public static DocumentationBlock propertyToDocumentation(FunctionElement function)
+	{
+		return null;
+	}
+
+	/**
+	 * propertyToDocumentation
+	 * 
+	 * @param property
+	 * @return
+	 */
+	public static DocumentationBlock propertyToDocumentation(PropertyElement property)
+	{
+		DocumentationBlock block;
+
+		if (property instanceof FunctionElement)
+		{
+			block = propertyToDocumentation((FunctionElement) property);
+		}
+		else
+		{
+			String description = property.getDescription();
+			List<Tag> tags = new ArrayList<Tag>();
+
+			for (ReturnTypeElement returnType : property.getTypes())
+			{
+				//Type t = new Type(returnType.getType());
+			}
+
+			block = new DocumentationBlock(description, tags);
+		}
+
+		return block;
+	}
+
+	/**
+	 * JSContentAssistUtil
+	 */
+	private JSTypeUtil()
+	{
+	}
+}
