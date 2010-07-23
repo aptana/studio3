80aeec6dc8e24014f79b566e88a6fcd49f069399
Shalom Gibly <sgibly@aptana.com>
2010-07-23 10:59:40 -0700

    Support providing the toolbox URL in a Java argument -DtoolboxURL when the Platform is in development mode

--- plugins/com.aptana.portal.ui/src/com/aptana/portal/ui/internal/Portal.java
+++ plugins/com.aptana.portal.ui/src/com/aptana/portal/ui/internal/Portal.java
@@ -45,8 +45,9 @@ public class Portal
 	//	public static final String BASE_URL_PREFIX = "http://toolbox.aptana.com/toolbox"; //$NON-NLS-1$
 	//	private static final String BASE_REMOTE_URL = "http://toolbox.aptana.com/toolbox"; //$NON-NLS-1$
 	// for debugging, do NOT check in with these uncommented:
-	public static final String BASE_URL_PREFIX = "http://localhost:3000/toolbox"; //$NON-NLS-1$
-	private static final String BASE_REMOTE_URL = "http://localhost:3000/toolbox"; //$NON-NLS-1$
+	public static final String BASE_URL_PREFIX = Platform.inDevelopmentMode() ? System.getProperty(
+			"toolboxURL", "http://localhost:3000/toolbox") : "http://localhost:3000/toolbox"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private static final String BASE_REMOTE_URL = BASE_URL_PREFIX; //$NON-NLS-1$
 	private static final String BASE_LOCAL_URL = "/content/index.html"; //$NON-NLS-1$
 
 	private static final String EXPLORER_PLUGIN_ID = "com.aptana.explorer"; //$NON-NLS-1$
