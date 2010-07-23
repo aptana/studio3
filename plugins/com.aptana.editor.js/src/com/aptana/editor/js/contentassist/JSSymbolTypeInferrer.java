5b67622921dc813aa6f50ff30ed4f914e00b84ee
Kevin Lindsey <klindsey@aptana.com>
2010-07-23 12:18:47 -0500

    Cache types in JSObject. This prevents infinite recursion when inferring types from some identifiers. Start honoring documentation again. Minor name changes

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSSymbolTypeInferrer.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSSymbolTypeInferrer.java
@@ -13,22 +13,29 @@ import com.aptana.editor.js.contentassist.model.ContentSelector;
 import com.aptana.editor.js.contentassist.model.PropertyElement;
 import com.aptana.editor.js.contentassist.model.TypeElement;
 import com.aptana.editor.js.parsing.ast.JSNode;
+import com.aptana.editor.js.sdoc.model.DocumentationBlock;
 import com.aptana.index.core.Index;
 
 public class JSSymbolTypeInferrer
 {
 	private static final EnumSet<ContentSelector> MEMBER_CONTENT = EnumSet.of(ContentSelector.NAME, ContentSelector.TYPES, ContentSelector.RETURN_TYPES);
+	private static List<TypeElement> _generatedTypes;
 
 	private Index _index;
-	private JSScope _globals;
-	private List<TypeElement> _generatedTypes;
+	private JSScope _activeScope;
 
-	public JSSymbolTypeInferrer(Index index, JSScope globals)
+	/**
+	 * JSSymbolTypeInferrer
+	 * 
+	 * @param index
+	 * @param activeScope
+	 */
+	public JSSymbolTypeInferrer(Index index, JSScope activeScope)
 	{
 		this._index = index;
-		this._globals = globals;
+		this._activeScope = activeScope;
 	}
-	
+
 	/**
 	 * generateType
 	 * 
@@ -38,17 +45,43 @@ public class JSSymbolTypeInferrer
 	{
 		// create new type and give it a unique name
 		TypeElement result = new TypeElement();
-		result.setName(JSContentAssistUtil.getUniqueTypeName());
+		result.setName(JSTypeUtil.getUniqueTypeName());
 
 		// save type for future reference
-		if (this._generatedTypes == null)
+		if (_generatedTypes == null)
 		{
-			this._generatedTypes = new ArrayList<TypeElement>();
+			_generatedTypes = new ArrayList<TypeElement>();
 		}
 
-		this._generatedTypes.add(result);
+		_generatedTypes.add(result);
 
 		return result;
+	}
+
+	/**
+	 * getAdditionalProperties
+	 * 
+	 * @param activeObject
+	 * @param types
+	 * @return
+	 */
+	private List<String> getAdditionalProperties(JSObject activeObject, List<String> types)
+	{
+		Map<String, PropertyElement> propertyMap = this.getTypePropertyMap(types);
+		List<String> additionalProperties = new ArrayList<String>();
+
+		// create a list of properties that are not in the ancestor chain of the
+		// types passed into this method
+		for (String name : activeObject.getPropertyNames())
+		{
+			// TODO: Treat as new property if names match but not their types?
+			if (propertyMap.containsKey(name) == false)
+			{
+				additionalProperties.add(name);
+			}
+		}
+		
+		return additionalProperties;
 	}
 
 	/**
@@ -58,16 +91,16 @@ public class JSSymbolTypeInferrer
 	 */
 	public List<TypeElement> getGeneratedTypes()
 	{
-		List<TypeElement> result = this._generatedTypes;
-		
+		List<TypeElement> result = _generatedTypes;
+
 		if (result == null)
 		{
 			result = Collections.emptyList();
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * getScopeProperties
 	 * 
@@ -76,12 +109,113 @@ public class JSSymbolTypeInferrer
 	public List<PropertyElement> getScopeProperties()
 	{
 		List<PropertyElement> result = new ArrayList<PropertyElement>();
-		
-		for (String symbol : this._globals.getLocalSymbolNames())
+
+		for (String symbol : this._activeScope.getLocalSymbolNames())
 		{
 			PropertyElement p = this.getSymbolPropertyElement(symbol);
 
 			result.add(p);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getSymbolPropertyElement
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public PropertyElement getSymbolPropertyElement(JSObject activeObject, String symbol)
+	{
+		List<String> types = new ArrayList<String>();
+
+		// create resulting property element
+		PropertyElement result = new PropertyElement();
+		result.setName(symbol);
+
+		// try to grab property from active object
+		JSObject property = activeObject.getProperty(symbol);
+
+		// if no property, then try a scope-based lookup
+		if (property == null)
+		{
+			property = this._activeScope.getSymbol(symbol);
+		}
+
+		if (property != null)
+		{
+			if (property.hasTypes())
+			{
+				types = property.getTypes();
+			}
+			else
+			{
+				// infer types
+				for (JSNode value : property.getValues())
+				{
+					DocumentationBlock docs = value.getDocumentation();
+	
+					if (docs != null)
+					{
+						JSTypeUtil.applyDocumentation(result, docs);
+					}
+					else
+					{
+						JSTypeInferrer inferrer = new JSTypeInferrer(this._activeScope);
+	
+						inferrer.visit(value);
+	
+						types.addAll(inferrer.getTypes());
+					}
+				}
+	
+				// process additional properties, possibly creating a new type in the
+				// process
+				if (property.hasProperties())
+				{
+					List<String> additionalProperties = getAdditionalProperties(property, types);
+	
+					if (additionalProperties.isEmpty() == false)
+					{
+						// create new type
+						TypeElement subType = this.generateType();
+	
+						// make the current list of types parent types of this type
+						for (String superType : types)
+						{
+							subType.addParentType(superType);
+						}
+	
+						// reset list to contain only this newly generated type
+						types.clear();
+						types.add(subType.getName());
+						
+						property.addType(subType.getName());
+	
+						// infer types of the
+						for (String pname : additionalProperties)
+						{
+							PropertyElement p = this.getSymbolPropertyElement(property, pname);
+	
+							subType.addProperty(p);
+						}
+					}
+				}
+				else
+				{
+					for (String typeName : types)
+					{
+						property.addType(typeName);
+					}
+				}
+			}
+		}
+
+		// add types to property
+		for (String typeName : types)
+		{
+			result.addType(typeName);
 		}
 
 		return result;
@@ -95,48 +229,7 @@ public class JSSymbolTypeInferrer
 	 */
 	public PropertyElement getSymbolPropertyElement(String symbol)
 	{
-		return this.getSymbolPropertyElement(this._globals.getObject(), symbol);
-	}
-	
-	/**
-	 * getSymbolPropertyElement
-	 * 
-	 * @param name
-	 * @return
-	 */
-	public PropertyElement getSymbolPropertyElement(JSObject activeObject, String symbol)
-	{
-		// create resulting property element
-		PropertyElement result = new PropertyElement();
-		result.setName(symbol);
-
-		JSObject property = activeObject.getProperty(symbol);
-		List<String> types = new ArrayList<String>();
-
-		// infer types
-		for (JSNode value : property.getValues())
-		{
-			JSTypeInferrer inferrer = new JSTypeInferrer(this._globals);
-
-			inferrer.visit(value);
-
-			types.addAll(inferrer.getTypes());
-		}
-
-		// process additional properties, possibly creating a new type in the
-		// process
-		if (property.hasProperties())
-		{
-			this.processSubProperties(property, types);
-		}
-
-		// add types to property
-		for (String typeName : types)
-		{
-			result.addType(typeName);
-		}
-
-		return result;
+		return this.getSymbolPropertyElement(this._activeScope.getObject(), symbol);
 	}
 
 	/**
@@ -168,54 +261,5 @@ public class JSSymbolTypeInferrer
 		}
 
 		return propertyMap;
-	}
-
-	/**
-	 * processSubProperties
-	 * 
-	 * @param activeObject
-	 * @param types
-	 */
-	private void processSubProperties(JSObject activeObject, List<String> types)
-	{
-		Map<String, PropertyElement> propertyMap = this.getTypePropertyMap(types);
-		List<String> additionalProperties = new ArrayList<String>();
-
-		// create a list of properties that are not in the ancestor chain of the
-		// types passed into this method
-		for (String name : activeObject.getPropertyNames())
-		{
-			// TODO: Treat as new property if types don't match?
-			if (propertyMap.containsKey(name) == false)
-			{
-				additionalProperties.add(name);
-			}
-		}
-
-		// if we have new properties, create a new sub-type and add the new
-		// properties to it
-		if (additionalProperties.isEmpty() == false)
-		{
-			// create new type
-			TypeElement subType = this.generateType();
-
-			// make the current list of types parent types of this type
-			for (String superType : types)
-			{
-				subType.addParentType(superType);
-			}
-
-			// reset the type list to this newly generated type
-			types.clear();
-			types.add(subType.getName());
-
-			// infer types of the
-			for (String pname : additionalProperties)
-			{
-				PropertyElement p = this.getSymbolPropertyElement(activeObject, pname);
-
-				subType.addProperty(p);
-			}
-		}
 	}
 }
