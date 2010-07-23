d5514d2f49d841fb6c00043b2d7b6cbf6653083d
Kevin Lindsey <klindsey@aptana.com>
2010-07-22 20:25:14 -0500

    Start work on processing the global scope via JSScope and JSObjects. Keep track of generated types and write those to the specified index. Replace refererences to Scope<T> with JSScope. Relace references to JSTypeWalker with JSTypeInferrer.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/index/JSFileIndexingParticipant.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/index/JSFileIndexingParticipant.java
@@ -3,7 +3,11 @@ package com.aptana.editor.js.contentassist.index;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.core.filesystem.EFS;
@@ -16,8 +20,13 @@ import org.jaxen.XPath;
 
 import com.aptana.core.util.IOUtil;
 import com.aptana.editor.js.Activator;
+import com.aptana.editor.js.contentassist.JSContentAssistUtil;
+import com.aptana.editor.js.contentassist.JSIndexQueryHelper;
+import com.aptana.editor.js.contentassist.JSObject;
+import com.aptana.editor.js.contentassist.JSScope;
 import com.aptana.editor.js.contentassist.JSSymbolCollector;
-import com.aptana.editor.js.contentassist.JSTypeWalker;
+import com.aptana.editor.js.contentassist.JSTypeInferrer;
+import com.aptana.editor.js.contentassist.model.ContentSelector;
 import com.aptana.editor.js.contentassist.model.FunctionElement;
 import com.aptana.editor.js.contentassist.model.PropertyElement;
 import com.aptana.editor.js.contentassist.model.ReturnTypeElement;
@@ -32,13 +41,15 @@ import com.aptana.parsing.IParser;
 import com.aptana.parsing.IParserPool;
 import com.aptana.parsing.ParseState;
 import com.aptana.parsing.ParserPoolFactory;
-import com.aptana.parsing.Scope;
 import com.aptana.parsing.ast.IParseNode;
 import com.aptana.parsing.xpath.ParseNodeXPath;
 
 public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 {
+	private static final EnumSet<ContentSelector> MEMBER_CONTENT = EnumSet.of(ContentSelector.NAME, ContentSelector.TYPES, ContentSelector.RETURN_TYPES);
+
 	private JSIndexWriter _indexWriter;
+	private List<TypeElement> _generatedTypes;
 
 	/**
 	 * JSFileIndexingParticipant
@@ -49,14 +60,36 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 	}
 
 	/**
+	 * generateType
+	 * 
+	 * @return
+	 */
+	private TypeElement generateType()
+	{
+		// create new type and give it a unique name
+		TypeElement result = new TypeElement();
+		result.setName(JSContentAssistUtil.getUniqueTypeName());
+		
+		// save type for future reference
+		if (this._generatedTypes == null)
+		{
+			this._generatedTypes = new ArrayList<TypeElement>();
+		}
+		
+		this._generatedTypes.add(result);
+
+		return result;
+	}
+
+	/**
 	 * getGlobals
 	 * 
 	 * @param root
 	 * @return
 	 */
-	protected Scope<JSNode> getGlobals(IParseNode root)
+	protected JSScope getGlobals(IParseNode root)
 	{
-		Scope<JSNode> result = null;
+		JSScope result = null;
 
 		if (root instanceof JSParseRootNode)
 		{
@@ -68,6 +101,102 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 		}
 
 		return result;
+	}
+
+	/**
+	 * getScopeProperties
+	 * 
+	 * @param index
+	 * @param globals
+	 * @return
+	 */
+	private List<PropertyElement> getScopeProperties(Index index, JSScope globals)
+	{
+		List<PropertyElement> result = new ArrayList<PropertyElement>();
+
+		for (String symbol : globals.getLocalSymbolNames())
+		{
+			PropertyElement p = this.getSymbolPropertyElement(index, globals, globals.getObject(), symbol);
+
+			result.add(p);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getSymbolPropertyElement
+	 * 
+	 * @param index
+	 * @param globals
+	 * @param name
+	 * @return
+	 */
+	private PropertyElement getSymbolPropertyElement(Index index, JSScope globals, JSObject activeObject, String symbol)
+	{
+		// create resulting property element
+		PropertyElement result = new PropertyElement();
+		result.setName(symbol);
+
+		JSObject property = activeObject.getProperty(symbol);
+		List<String> types = new ArrayList<String>();
+
+		// infer types
+		for (JSNode value : property.getValues())
+		{
+			JSTypeInferrer inferrer = new JSTypeInferrer(globals);
+
+			inferrer.visit(value);
+
+			types.addAll(inferrer.getTypes());
+		}
+
+		// if property has child properties, determine if they already exist in
+		// one of the types (or their ancestors)
+		if (property.hasProperties())
+		{
+			processSubProperties(index, globals, property, types);
+		}
+
+		// add types to property
+		for (String typeName : types)
+		{
+			result.addType(typeName);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getTypePropertyMap
+	 * 
+	 * @param index
+	 * @param type
+	 * @return
+	 */
+	private Map<String, PropertyElement> getTypePropertyMap(Index index, List<String> types)
+	{
+		JSIndexQueryHelper helper = new JSIndexQueryHelper();
+
+		// create a set from the specified types and their ancestors
+		Set<String> ancestors = new HashSet<String>();
+
+		for (String type : types)
+		{
+			ancestors.add(type);
+			ancestors.addAll(helper.getTypeAncestorNames(index, type));
+		}
+
+		List<String> typesAndAncestors = new ArrayList<String>(ancestors);
+		List<PropertyElement> typeMembers = helper.getTypeMembers(index, typesAndAncestors, MEMBER_CONTENT);
+		Map<String, PropertyElement> propertyMap = new HashMap<String, PropertyElement>();
+
+		for (PropertyElement propertyElement : typeMembers)
+		{
+			propertyMap.put(propertyElement.getName(), propertyElement);
+		}
+
+		return propertyMap;
 	}
 
 	/*
@@ -140,7 +269,7 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 
 		monitor.done();
 	}
-	
+
 	/**
 	 * processScopes
 	 * 
@@ -150,103 +279,32 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 	 * @param location
 	 */
 	@SuppressWarnings("unchecked")
-	private List<PropertyElement> processLambdas(Index index, Scope<JSNode> globals, IParseNode ast, URI location)
+	private List<PropertyElement> processLambdas(Index index, JSScope globals, IParseNode ast, URI location)
 	{
 		List<PropertyElement> result = Collections.emptyList();
-		
+
 		try
 		{
 			XPath xpath = new ParseNodeXPath("invoke[position() = 1]/group/function|invoke[position() = 1]/function");
 			Object queryResult = xpath.evaluate(ast);
-			
+
 			if (queryResult != null)
 			{
 				List<JSFunctionNode> functions = (List<JSFunctionNode>) queryResult;
-				
+
 				for (JSFunctionNode function : functions)
 				{
-					Scope<JSNode> scope = globals.getScopeAtOffset(function.getBody().getStartingOffset());
-					
-					JSTypeWalker.getScopeProperties(scope, index, location);
-					
-					if (scope != null)
-					{
-						result = this.processWindowAssignments(index, scope);
-					}
-				}
-			}
-		}
-		catch (JaxenException e)
-		{
-			e.printStackTrace();
-		}
-		
-		return result;
-	}
-	
-	/**
-	 * processWindowAssignments
-	 * 
-	 * @param index
-	 * @param symbols
-	 */
-	@SuppressWarnings("unchecked")
-	private List<PropertyElement> processWindowAssignments(Index index, Scope<JSNode> symbols)
-	{
-		List<PropertyElement> properties = new ArrayList<PropertyElement>();
-		
-		try
-		{
-			XPath xpath = new ParseNodeXPath("get_property[position() = 1 and descendant-or-self::identifier[position() = 1 and string()='window']]");
-			Object result = xpath.evaluate(symbols.getAssignments());
-			
-			if (result != null)
-			{
-				List<IParseNode> leftHandSides = (List<IParseNode>) result;
-				
-				for (IParseNode lhs : leftHandSides)
-				{
-					IParseNode rhs = lhs.getNextSibling();
-					JSTypeWalker walker = new JSTypeWalker(symbols, index);
+					JSScope scope = globals.getScopeAtOffset(function.getBody().getStartingOffset());
 
-					((JSNode) rhs).accept(walker);
-					
-					List<String> types = walker.getTypes();
-					
-					boolean isFunction = true;
-					
-					for (String type : types)
-					{
-						if (type.startsWith("Function:") == false)
-						{
-							isFunction = false;
-							break;
-						}
-					}
-					
-					PropertyElement property = (isFunction) ? new FunctionElement() : new PropertyElement();
-					property.setName(lhs.getLastChild().getText());
-					
-					for (String type : walker.getTypes())
-					{
-						if (isFunction)
-						{
-							int i = type.indexOf(':');
-							
-							if (i != -1)
-							{
-								type = type.substring(i + 1);
-							}
-						}
-						
-						ReturnTypeElement returnType = new ReturnTypeElement();
-						
-						returnType.setType(type);
-						
-						property.addType(returnType);
-					}
-					
-					properties.add(property);
+					JSObject object = scope.getObject();
+
+					System.out.println(object.toSource());
+					// JSTypeWalker.getScopeProperties(scope, index, location);
+					//					
+					// if (scope != null)
+					// {
+					// result = this.processWindowAssignments(index, scope);
+					// }
 				}
 			}
 		}
@@ -254,8 +312,8 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 		{
 			e.printStackTrace();
 		}
-		
-		return properties;
+
+		return result;
 	}
 
 	/**
@@ -268,35 +326,165 @@ public class JSFileIndexingParticipant implements IFileStoreIndexingParticipant
 	private void processParseResults(Index index, IFileStore file, IParseNode ast)
 	{
 		URI location = file.toURI();
-		Scope<JSNode> globals = this.getGlobals(ast);
+		JSScope globals = this.getGlobals(ast);
 
 		if (globals != null)
 		{
+			// create new Window type for this file
 			TypeElement type = new TypeElement();
-
-			// set type
 			type.setName("Window"); //$NON-NLS-1$
 
 			// add declared variables and functions from the global scope
-			for (PropertyElement property : JSTypeWalker.getScopeProperties(globals, index, location))
+			for (PropertyElement property : this.getScopeProperties(index, globals))
 			{
 				type.addProperty(property);
 			}
-			
-			// include any assignments to Window
-			for (PropertyElement property : processWindowAssignments(index, globals))
-			{
-				type.addProperty(property);
-			}
-			
+
+			// // include any assignments to Window
+			// for (PropertyElement property : processWindowAssignments(index, globals))
+			// {
+			// type.addProperty(property);
+			// }
+
 			// process scopes (self-invoking functions)
 			for (PropertyElement property : processLambdas(index, globals, ast, location))
 			{
 				type.addProperty(property);
 			}
 			
+			// write generated types
+			if (this._generatedTypes != null)
+			{
+				for (TypeElement generatedType : this._generatedTypes)
+				{
+					this._indexWriter.writeType(index, generatedType, location);
+				}
+				
+				this._generatedTypes.clear();
+			}
+
 			// write new Window type to index
 			this._indexWriter.writeType(index, type, location);
 		}
+	}
+
+	/**
+	 * processSubProperties
+	 * 
+	 * @param index
+	 * @param globals
+	 * @param activeObject
+	 * @param types
+	 */
+	private void processSubProperties(Index index, JSScope globals, JSObject activeObject, List<String> types)
+	{
+		Map<String, PropertyElement> propertyMap = this.getTypePropertyMap(index, types);
+		List<String> additionalProperties = new ArrayList<String>();
+
+		// create a list of properties that are not in the ancestor chain of the
+		// types passed into this method
+		for (String name : activeObject.getPropertyNames())
+		{
+			// TODO: Treat as new property if types don't match?
+			if (propertyMap.containsKey(name) == false)
+			{
+				additionalProperties.add(name);
+			}
+		}
+
+		// if we have new properties, create a new sub-type and add the new
+		// properties to it
+		if (additionalProperties.isEmpty() == false)
+		{
+			// create new type
+			TypeElement subType = this.generateType();
+
+			// make the current list of types parent types of this type
+			for (String superType : types)
+			{
+				subType.addParentType(superType);
+			}
+
+			// reset the type list to this newly generated type
+			types.clear();
+			types.add(subType.getName());
+
+			// infer types of the
+			for (String pname : additionalProperties)
+			{
+				PropertyElement p = this.getSymbolPropertyElement(index, globals, activeObject, pname);
+
+				subType.addProperty(p);
+			}
+		}
+	}
+
+	/**
+	 * processWindowAssignments
+	 * 
+	 * @param index
+	 * @param symbols
+	 */
+	@SuppressWarnings("unchecked")
+	private List<PropertyElement> processWindowAssignments(Index index, JSScope symbols)
+	{
+		List<PropertyElement> properties = new ArrayList<PropertyElement>();
+
+		// XPath xpath = new
+		// ParseNodeXPath("get_property[position() = 1 and descendant-or-self::identifier[position() = 1 and string()='window']]");
+		// Object result = xpath.evaluate(symbols.getAssignments());
+		Object result = null;
+
+		if (result != null)
+		{
+			List<IParseNode> leftHandSides = (List<IParseNode>) result;
+
+			for (IParseNode lhs : leftHandSides)
+			{
+				IParseNode rhs = lhs.getNextSibling();
+				JSTypeInferrer walker = new JSTypeInferrer(symbols, index);
+
+				((JSNode) rhs).accept(walker);
+
+				List<String> types = walker.getTypes();
+
+				boolean isFunction = true;
+
+				for (String type : types)
+				{
+					if (type.startsWith("Function:") == false)
+					{
+						isFunction = false;
+						break;
+					}
+				}
+
+				PropertyElement property = (isFunction) ? new FunctionElement() : new PropertyElement();
+				property.setName(lhs.getLastChild().getText());
+
+				for (String type : walker.getTypes())
+				{
+					if (isFunction)
+					{
+						int i = type.indexOf(':');
+
+						if (i != -1)
+						{
+							type = type.substring(i + 1);
+						}
+					}
+
+					ReturnTypeElement returnType = new ReturnTypeElement();
+
+					returnType.setType(type);
+
+					property.addType(returnType);
+				}
+
+				properties.add(property);
+			}
+		}
+
+		return properties;
 	}
 }
