e5bace43583acb13d0612b32fbfafccd4d0c4d9e
Kevin Lindsey <klindsey@aptana.com>
2010-07-29 09:30:03 -0500

    Remove unnecessary overrides

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSRangeFinder.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSRangeFinder.java
@@ -1,16 +1,10 @@
 package com.aptana.editor.js.contentassist;
 
-import com.aptana.editor.js.parsing.ast.JSFalseNode;
 import com.aptana.editor.js.parsing.ast.JSIdentifierNode;
 import com.aptana.editor.js.parsing.ast.JSNode;
-import com.aptana.editor.js.parsing.ast.JSNullNode;
-import com.aptana.editor.js.parsing.ast.JSNumberNode;
 import com.aptana.editor.js.parsing.ast.JSParseRootNode;
-import com.aptana.editor.js.parsing.ast.JSRegexNode;
 import com.aptana.editor.js.parsing.ast.JSStringNode;
-import com.aptana.editor.js.parsing.ast.JSThisNode;
 import com.aptana.editor.js.parsing.ast.JSTreeWalker;
-import com.aptana.editor.js.parsing.ast.JSTrueNode;
 import com.aptana.parsing.ast.IParseNode;
 import com.aptana.parsing.lexer.IRange;
 import com.aptana.parsing.lexer.Range;
@@ -67,16 +61,6 @@ public class JSRangeFinder extends JSTreeWalker
 
 	/*
 	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSFalseNode)
-	 */
-	@Override
-	public void visit(JSFalseNode node)
-	{
-		// do nothing
-	}
-
-	/*
-	 * (non-Javadoc)
 	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSIdentifierNode)
 	 */
 	@Override
@@ -86,26 +70,6 @@ public class JSRangeFinder extends JSTreeWalker
 		{
 			this.setRange(node.getStart(), this._offset);
 		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSNullNode)
-	 */
-	@Override
-	public void visit(JSNullNode node)
-	{
-		// do nothing
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSNumberNode)
-	 */
-	@Override
-	public void visit(JSNumberNode node)
-	{
-		// do nothing
 	}
 
 	/*
@@ -130,16 +94,6 @@ public class JSRangeFinder extends JSTreeWalker
 
 	/*
 	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSRegexNode)
-	 */
-	@Override
-	public void visit(JSRegexNode node)
-	{
-		// do nothing
-	}
-
-	/*
-	 * (non-Javadoc)
 	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSStringNode)
 	 */
 	@Override
@@ -149,26 +103,6 @@ public class JSRangeFinder extends JSTreeWalker
 		{
 			this.setRange(node.getStart() + 1, this._offset - 1);
 		}
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSThisNode)
-	 */
-	@Override
-	public void visit(JSThisNode node)
-	{
-		// do nothing
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see com.aptana.editor.js.parsing.ast.JSTreeWalker#visit(com.aptana.editor.js.parsing.ast.JSTrueNode)
-	 */
-	@Override
-	public void visit(JSTrueNode node)
-	{
-		// do nothing
 	}
 
 	/*
