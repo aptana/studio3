ea9b39db06b8f91273c9dcce97245534017ff5a1
mxia <mxia@aptana.com>
2010-08-05 15:12:10 -0700

    Made Ctrl+Shift+U/D work in the editor window.

--- plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/handlers/SynchronizeProjectHandler.java
+++ plugins/com.aptana.syncing.ui/src/com/aptana/ide/syncing/ui/handlers/SynchronizeProjectHandler.java
@@ -36,20 +36,22 @@ package com.aptana.ide.syncing.ui.handlers;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.aptana.ide.syncing.ui.actions.SynchronizeProjectAction;
 
 public class SynchronizeProjectHandler extends BaseSyncHandler
 {
+
+	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException
 	{
 		SynchronizeProjectAction action = new SynchronizeProjectAction();
 		action.setActivePart(null, HandlerUtil.getActivePart(event));
-		action.setSelection(HandlerUtil.getCurrentSelection(event));
+		action.setSelection(new StructuredSelection(getSelectedResources()));
 		action.run(null);
 
 		return null;
 	}
-
 }
