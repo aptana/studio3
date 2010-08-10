cd92a2efb6aabb0bb2b93703482987e513799dca
Kevin Lindsey <klindsey@aptana.com>
2010-08-10 18:12:03 -0500

    Add case to not infer the type of object literals. We handle additional properties later in the code. If we didn't special case this, then JSNodeTypeInferrer would create a new type for objects literals with children. Later, JSSymbolTypeInferrer would create yet another type when handling new properties on the object.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/inferencing/JSSymbolTypeInferrer.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/inferencing/JSSymbolTypeInferrer.java
@@ -26,6 +26,7 @@ import com.aptana.editor.js.parsing.ast.JSFunctionNode;
 import com.aptana.editor.js.parsing.ast.JSIdentifierNode;
 import com.aptana.editor.js.parsing.ast.JSNode;
 import com.aptana.editor.js.parsing.ast.JSNodeTypes;
+import com.aptana.editor.js.parsing.ast.JSObjectNode;
 import com.aptana.editor.js.sdoc.model.DocumentationBlock;
 import com.aptana.index.core.Index;
 import com.aptana.parsing.ast.IParseNode;
@@ -482,7 +483,11 @@ public class JSSymbolTypeInferrer
 			{
 				JSNodeTypeInferrer inferrer = new JSNodeTypeInferrer(this._activeScope, this._index, this._location);
 
-				if (isFunction)
+				if (value instanceof JSObjectNode)
+				{
+					inferrer.addType(JSTypeConstants.OBJECT_TYPE);
+				}
+				else if (isFunction)
 				{
 					property.addType(JSTypeConstants.FUNCTION_TYPE);
 					inferrer.visit(value);
