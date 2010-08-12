ef1846f3f1389d9e29c4b64cb23c4f1f68dab54c
Kevin Lindsey <klindsey@aptana.com>
2010-08-12 17:23:05 -0500

    Minor commenting

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/IJSTokenScanner.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/IJSTokenScanner.java
@@ -2,6 +2,10 @@ package com.aptana.editor.js;
 
 public interface IJSTokenScanner
 {
-
+	/**
+	 * Determine if the scanner's position is valid for a regular expression
+	 * 
+	 * @return
+	 */
 	public boolean hasDivisionStart();
 }
