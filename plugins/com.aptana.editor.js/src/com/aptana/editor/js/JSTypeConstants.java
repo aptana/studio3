5f94b9859c7e8bafdeb49b09c29de8bc1cbc7bcb
Kevin Lindsey <klindsey@aptana.com>
2010-08-17 16:52:29 -0500

    Add a null type constant

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/JSTypeConstants.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/JSTypeConstants.java
@@ -18,6 +18,7 @@ public class JSTypeConstants
 
 	public static final String UNDEFINED_TYPE = "undefined"; //$NON-NLS-1$
 	public static final String VOID_TYPE = "void"; //$NON-NLS-1$
+	public static final String NULL_TYPE = "null"; //$NON-NLS-1$
 
 	public static final String WINDOW_PROPERTY = "window"; //$NON-NLS-1$
 
