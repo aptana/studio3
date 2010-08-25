3a56e4e6ee7016f56dfabdc3cbb0cf6a1e682385
Shalom Gibly <sgibly@aptana.com>
2010-08-25 13:21:09 -0700

    Attach the fields validator to the formatter preferences, fixing the display for the textual fields

--- plugins/com.aptana.formatter.epl/src/com/aptana/formatter/ui/internal/FormatterControlManager.java
+++ plugins/com.aptana.formatter.epl/src/com/aptana/formatter/ui/internal/FormatterControlManager.java
@@ -31,59 +31,55 @@ import com.aptana.ui.util.IStatusChangeListener;
 import com.aptana.ui.util.SWTFactory;
 import com.aptana.ui.util.Util;
 
-public class FormatterControlManager implements IFormatterControlManager,
-		IStatusChangeListener {
+public class FormatterControlManager implements IFormatterControlManager, IStatusChangeListener
+{
 
 	private final IPreferenceDelegate delegate;
 	private final ControlBindingManager bindingManager;
 	private final IStatusChangeListener listener;
 
-	public FormatterControlManager(IPreferenceDelegate delegate,
-			IStatusChangeListener listener) {
+	public FormatterControlManager(IPreferenceDelegate delegate, IStatusChangeListener listener)
+	{
 		this.delegate = delegate;
 		this.bindingManager = new ControlBindingManager(delegate, this);
 		this.listener = listener;
 	}
 
-	public Button createCheckbox(Composite parent, Object key, String text) {
+	public Button createCheckbox(Composite parent, Object key, String text)
+	{
 		return createCheckbox(parent, key, text, 1);
 	}
 
-	public Button createCheckbox(Composite parent, Object key, String text,
-			int hspan) {
-		Button button = SWTFactory.createCheckButton(parent, text, null, false,
-				hspan);
+	public Button createCheckbox(Composite parent, Object key, String text, int hspan)
+	{
+		Button button = SWTFactory.createCheckButton(parent, text, null, false, hspan);
 		bindingManager.bindControl(button, key, null);
 		return button;
 	}
 
-	public Combo createCombo(Composite parent, Object key, String label,
-			String[] items) {
+	public Combo createCombo(Composite parent, Object key, String label, String[] items)
+	{
 		final Label labelControl = SWTFactory.createLabel(parent, label, 1);
-		Combo combo = SWTFactory.createCombo(parent,
-				SWT.READ_ONLY | SWT.BORDER, 1, items);
+		Combo combo = SWTFactory.createCombo(parent, SWT.READ_ONLY | SWT.BORDER, 1, items);
 		bindingManager.bindControl(combo, key);
 		registerAssociatedLabel(combo, labelControl);
 		return combo;
 	}
 
-	public Combo createCombo(Composite parent, Object key, String label,
-			String[] itemValues, String[] itemLabels) {
+	public Combo createCombo(Composite parent, Object key, String label, String[] itemValues, String[] itemLabels)
+	{
 		final Label labelControl = SWTFactory.createLabel(parent, label, 1);
-		Combo combo = SWTFactory.createCombo(parent,
-				SWT.READ_ONLY | SWT.BORDER, 1, itemLabels);
+		Combo combo = SWTFactory.createCombo(parent, SWT.READ_ONLY | SWT.BORDER, 1, itemLabels);
 		bindingManager.bindControl(combo, key, itemValues);
 		registerAssociatedLabel(combo, labelControl);
 		return combo;
 	}
 
-	public Text createNumber(Composite parent, Object key, String label) {
+	public Text createNumber(Composite parent, Object key, String label)
+	{
 		final Label labelControl = SWTFactory.createLabel(parent, label, 1);
-		Text text = SWTFactory.createText(parent, SWT.BORDER, 1,
-				Util.EMPTY_STRING);
-		// TODO - Validate
-		// bindingManager.bindControl(text, key,
-		// FieldValidators.POSITIVE_NUMBER_VALIDATOR);
+		Text text = SWTFactory.createText(parent, SWT.BORDER, 1, Util.EMPTY_STRING);
+		bindingManager.bindControl(text, key, FieldValidators.POSITIVE_NUMBER_VALIDATOR);
 		registerAssociatedLabel(text, labelControl);
 		return text;
 	}
@@ -94,63 +90,79 @@ public class FormatterControlManager implements IFormatterControlManager,
 	 * @param control
 	 * @param label
 	 */
-	private void registerAssociatedLabel(Control control, Label label) {
+	private void registerAssociatedLabel(Control control, Label label)
+	{
 		labelAssociations.put(control, label);
 	}
 
-	public void enableControl(Control control, boolean enabled) {
+	public void enableControl(Control control, boolean enabled)
+	{
 		control.setEnabled(enabled);
 		final Label label = labelAssociations.get(control);
-		if (label != null) {
+		if (label != null)
+		{
 			label.setEnabled(enabled);
 		}
 	}
 
 	private final ListenerList initListeners = new ListenerList();
 
-	public void addInitializeListener(IInitializeListener listener) {
+	public void addInitializeListener(IInitializeListener listener)
+	{
 		initListeners.add(listener);
 	}
 
-	public void removeInitializeListener(IInitializeListener listener) {
+	public void removeInitializeListener(IInitializeListener listener)
+	{
 		initListeners.remove(listener);
 	}
 
 	private boolean initialization;
 
-	public void initialize() {
+	public void initialize()
+	{
 		initialization = true;
-		try {
+		try
+		{
 			bindingManager.initialize();
 			final Object[] listeners = initListeners.getListeners();
-			for (int i = 0; i < listeners.length; ++i) {
+			for (int i = 0; i < listeners.length; ++i)
+			{
 				((IInitializeListener) listeners[i]).initialize();
 			}
-		} finally {
+		}
+		finally
+		{
 			initialization = false;
 		}
 		listener.statusChanged(bindingManager.getStatus());
 	}
 
-	public void statusChanged(IStatus status) {
-		if (!initialization) {
+	public void statusChanged(IStatus status)
+	{
+		if (!initialization)
+		{
 			listener.statusChanged(status);
 		}
 	}
 
-	public boolean getBoolean(Object key) {
+	public boolean getBoolean(Object key)
+	{
 		return delegate.getBoolean(key);
 	}
 
-	public String getString(Object key) {
+	public String getString(Object key)
+	{
 		return delegate.getString(key);
 	}
 
-	public void setBoolean(Object key, boolean value) {
+	public void setBoolean(Object key, boolean value)
+	{
 		delegate.setBoolean(key, value);
 	}
 
-	public void setString(Object key, String value) {
+	public void setString(Object key, String value)
+	{
 		delegate.setString(key, value);
 	}
 
