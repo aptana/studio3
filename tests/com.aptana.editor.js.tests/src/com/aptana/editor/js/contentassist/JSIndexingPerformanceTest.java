5ee1b7c9950eb67c27e5b03881d13b3fba1a0526
Kevin Lindsey <klindsey@aptana.com>
2010-09-03 12:00:29 -0500

    Add another file for performance testing and comment out the unit test that walks a specified directory to process all descendant JS files.

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/JSIndexingPerformanceTest.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/contentassist/JSIndexingPerformanceTest.java
@@ -1,23 +1,15 @@
 package com.aptana.editor.js.contentassist;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Queue;
+
+import junit.framework.TestCase;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-
-import junit.framework.TestCase;
 
 import com.aptana.editor.js.Activator;
 import com.aptana.editor.js.contentassist.index.JSFileIndexingParticipant;
@@ -39,12 +31,156 @@ public class JSIndexingPerformanceTest extends TestCase
 		}
 	}
 
+	private JSParser fParser;
+
+	/**
+	 * getIndex
+	 * 
+	 * @return
+	 */
+	protected Index getIndex()
+	{
+		URI indexURI = this.getIndexURI();
+		Index result = null;
+
+		if (indexURI != null)
+		{
+			result = IndexManager.getInstance().getIndex(indexURI);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getIndexURI
+	 * 
+	 * @return
+	 */
+	protected URI getIndexURI()
+	{
+		return URI.create("inference.testing");
+	}
+
+	/**
+	 * getURI
+	 * 
+	 * @return
+	 */
+	protected URI getLocation()
+	{
+		return URI.create("inference_file.js");
+	}
+
+	/**
+	 * getSource
+	 * 
+	 * @param file
+	 * @return
+	 * @throws IOException
+	 *
+	private String getSource(File file) throws IOException
+	{
+		InputStream stream = new FileInputStream(file);
+
+		return getSource(stream);
+	}
+	*/
+
+	/**
+	 * getSource
+	 * 
+	 * @param stream
+	 * @return
+	 * @throws IOException
+	 */
+	private String getSource(InputStream stream) throws IOException
+	{
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		int read = -1;
+
+		while ((read = stream.read()) != -1)
+		{
+			out.write(read);
+		}
+
+		stream.close();
+
+		String src = new String(out.toByteArray());
+		return src;
+	}
+
+	/**
+	 * getSource
+	 * 
+	 * @param resourceName
+	 * @return
+	 * @throws IOException
+	 */
+	private String getSource(String resourceName) throws IOException
+	{
+		InputStream stream = FileLocator.openStream(Platform.getBundle(Activator.PLUGIN_ID), new Path(resourceName), false);
+
+		return getSource(stream);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+
+		fParser = new JSParser();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception
+	{
+		fParser = null;
+		
+		URI indexURI = this.getIndexURI();
+		
+		if (indexURI != null)
+		{
+			IndexManager.getInstance().removeIndex(indexURI);
+		}
+
+		super.tearDown();
+	}
+
+	/**
+	 * testDojo
+	 * 
+	 * @throws Exception
+	 */
+	public void testDojo() throws Exception
+	{
+		timeIndex("performance/dojo.js.uncompressed.js");
+	}
+
+	/**
+	 * testExt
+	 * 
+	 * @throws Exception
+	 */
+	public void testExt() throws Exception
+	{
+		timeIndex("performance/ext-core.js");
+	}
+
 	/**
 	 * main
 	 * 
 	 * @param args
 	 * @return
-	 */
+	 *
 	public void testFiles()
 	{
 		File file = new File("/Users/klindsey/Documents/Projects/Jaxer");
@@ -93,14 +229,20 @@ public class JSIndexingPerformanceTest extends TestCase
 				files.add(file);
 			}
 
-			long start = System.currentTimeMillis();
+			long start = System.nanoTime();
+			long characterCount = 0;
 
 			for (File f : files)
 			{
 				try
 				{
 					tester.setUp();
-					tester.timeIndex(f);
+					
+					String source = this.getSource(f);
+					
+					characterCount += source.length();
+					
+					tester.timeIndex(f.getAbsolutePath(), source);
 				}
 				catch (Throwable e)
 				{
@@ -117,153 +259,33 @@ public class JSIndexingPerformanceTest extends TestCase
 				}
 			}
 
-			long diff = System.currentTimeMillis() - start;
+			long diff = System.nanoTime() - start;
+			double seconds = diff / 1e9;
 
-			System.out.println("processed " + files.size() + " files in " + diff + " milliseconds");
+			String message = String.format("processed %d files (%d characters) in %f s", files.size(), characterCount, seconds);
+			System.out.println(message);
 		}
 	}
-
-	private JSParser fParser;
-
-	/**
-	 * getSource
-	 * 
-	 * @param file
-	 * @return
-	 * @throws IOException
-	 */
-	private String getSource(File file) throws IOException
-	{
-		InputStream stream = new FileInputStream(file);
-
-		return getSource(stream);
-	}
+	*/
 
 	/**
-	 * getSource
-	 * 
-	 * @param stream
-	 * @return
-	 * @throws IOException
-	 */
-	private String getSource(InputStream stream) throws IOException
-	{
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-		int read = -1;
-
-		while ((read = stream.read()) != -1)
-		{
-			out.write(read);
-		}
-
-		stream.close();
-
-		String src = new String(out.toByteArray());
-		return src;
-	}
-
-	/**
-	 * getSource
-	 * 
-	 * @param resourceName
-	 * @return
-	 * @throws IOException
-	 */
-	private String getSource(String resourceName) throws IOException
-	{
-		InputStream stream = FileLocator.openStream(Platform.getBundle(Activator.PLUGIN_ID), new Path(resourceName), false);
-
-		return getSource(stream);
-	}
-
-	/**
-	 * getURI
-	 * 
-	 * @return
-	 */
-	protected URI getLocation()
-	{
-		return URI.create("inference_file.js");
-	}
-
-	/**
-	 * getIndex
-	 * 
-	 * @return
-	 */
-	protected Index getIndex()
-	{
-		URI indexURI = this.getIndexURI();
-		Index result = null;
-
-		if (indexURI != null)
-		{
-			result = IndexManager.getInstance().getIndex(indexURI);
-		}
-
-		return result;
-	}
-
-	/**
-	 * getIndexURI
-	 * 
-	 * @return
-	 */
-	protected URI getIndexURI()
-	{
-		return URI.create("inference.testing");
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see junit.framework.TestCase#setUp()
-	 */
-	@Override
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-
-		fParser = new JSParser();
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	@Override
-	protected void tearDown() throws Exception
-	{
-		fParser = null;
-
-		URI indexURI = this.getIndexURI();
-
-		if (indexURI != null)
-		{
-			IndexManager.getInstance().removeIndex(indexURI);
-		}
-
-		super.tearDown();
-	}
-
-	/**
-	 * testDojo
+	 * testRegress155081_2
 	 * 
 	 * @throws Exception
 	 */
-	public void testDojo() throws Exception
+	public void testRegress155081_2() throws Exception
 	{
-		timeIndex("performance/dojo.js.uncompressed.js");
+		timeIndex("performance/regress-155081-2.js");
 	}
 
 	/**
-	 * testExt
+	 * testRegress98901
 	 * 
 	 * @throws Exception
 	 */
-	public void testExt() throws Exception
+	public void testRegress98901() throws Exception
 	{
-		timeIndex("performance/ext-core.js");
+		timeIndex("performance/regress-98901.js");
 	}
 
 	/**
@@ -275,7 +297,7 @@ public class JSIndexingPerformanceTest extends TestCase
 	{
 		timeIndex("performance/timobile.js");
 	}
-
+	
 	/**
 	 * testTinyMce
 	 * 
@@ -284,27 +306,6 @@ public class JSIndexingPerformanceTest extends TestCase
 	public void testTinyMce() throws Exception
 	{
 		timeIndex("performance/tiny_mce.js");
-	}
-
-	/**
-	 * testRegress
-	 * 
-	 * @throws Exception
-	 */
-	public void testRegress() throws Exception
-	{
-		timeIndex("performance/regress-155081-2.js");
-	}
-
-	/**
-	 * timeParse
-	 * 
-	 * @param file
-	 * @throws Exception
-	 */
-	protected void timeIndex(File file) throws Exception
-	{
-		this.timeIndex(file.getAbsolutePath(), getSource(file));
 	}
 
 	/**
@@ -326,6 +327,8 @@ public class JSIndexingPerformanceTest extends TestCase
 	 */
 	protected void timeIndex(String resourceName, String src) throws Exception
 	{
+		int numRuns = 5;
+		
 		// apply to parse state
 		IParseState parseState = new ParseState();
 		parseState.setEditState(src, src, 0, 0);
@@ -341,15 +344,26 @@ public class JSIndexingPerformanceTest extends TestCase
 				Indexer indexer = new Indexer();
 
 				// start timing
-				long start = System.currentTimeMillis();
+				long start = System.nanoTime();
 
-				indexer.indexTree(this.getIndex(), (JSParseRootNode) root, this.getLocation());
+				for (int i = 0; i < numRuns; i++)
+				{
+					URI indexURI = this.getIndexURI();
+					
+					if (indexURI != null)
+					{
+						IndexManager.getInstance().removeIndex(indexURI);
+					}
+					
+					indexer.indexTree(this.getIndex(), (JSParseRootNode) root, this.getLocation());
+				}
 
 				// get time difference
-				long diff = System.currentTimeMillis() - start;
+				long diff = System.nanoTime() - start;
+				double seconds = ((double) diff / numRuns) / 1e9;
 
 				// show results
-				System.out.println(String.format("index: %5dms: %s", diff, resourceName));
+				System.out.println(String.format("index: %12.9fs: %s", seconds, resourceName));
 			}
 			else
 			{
