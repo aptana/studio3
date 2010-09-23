c08587f3155af3eea7f9b34247e908cf729b1a23
sgtcoolguy <cwilliams@aptana.com>
2010-09-23 11:11:57 -0400

    Bring over "quaqua" binary plist reader, add it to a new com.aptana.plist plugin. Add unit test for it. Format the code, clean it up, and make it return back Maps and Lists and java primitives rather than an xml wrapper or BPLArray/BPLDict objects. Fix handling of dates.

---
+++ plugins/com.aptana.plist/src/com/aptana/plist/PListPlugin.java
@@ -0,0 +1,30 @@
+package com.aptana.plist;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class PListPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		PListPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		PListPlugin.context = null;
+	}
+
+}
