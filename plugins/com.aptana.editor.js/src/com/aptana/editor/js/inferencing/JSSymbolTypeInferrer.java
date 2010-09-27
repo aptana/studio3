436177d7691e4b3a708e3fdc9c04171e65ef94a2
Kevin Lindsey <klindsey@aptana.com>
2010-09-27 13:59:52 -0500

    Always include user-defined prototype properties in the list of new properties when type inferencing symbols and their properties.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/inferencing/JSSymbolTypeInferrer.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/inferencing/JSSymbolTypeInferrer.java
@@ -272,7 +272,7 @@ public class JSSymbolTypeInferrer
 		for (String name : activeObject.getPropertyNames())
 		{
 			// TODO: Treat as new property if names match but not their types?
-			if (propertyMap.containsKey(name) == false)
+			if (propertyMap.containsKey(name) == false || "prototype".equals(name))
 			{
 				additionalProperties.add(name);
 			}
