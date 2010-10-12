0b6052f9356f2905c3c6bbe2c59724dbb9ee071f
sgtcoolguy <cwilliams@aptana.com>
2010-10-12 16:13:49 -0400

    Store content of CSS comments in the node

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/parsing/ast/CSSCommentNode.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/parsing/ast/CSSCommentNode.java
@@ -37,8 +37,17 @@ package com.aptana.editor.css.parsing.ast;
 public class CSSCommentNode extends CSSNode
 {
 
-	public CSSCommentNode(int start, int end)
+	private String fText;
+
+	public CSSCommentNode(String text, int start, int end)
 	{
 		super(CSSNodeTypes.COMMENT, start, end);
+		fText = text;
+	}
+	
+	@Override
+	public String getText()
+	{
+		return fText;
 	}
 }
