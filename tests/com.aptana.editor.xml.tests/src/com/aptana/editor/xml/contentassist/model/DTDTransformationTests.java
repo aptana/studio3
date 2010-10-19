530027cf2c41ba169f0760a0ddc002354cf06289
Kevin Lindsey <klindsey@aptana.com>
2010-10-19 10:08:37 -0500

    Actually perform the DTD transform

--- tests/com.aptana.editor.xml.tests/src/com/aptana/editor/xml/contentassist/model/DTDTransformationTests.java
+++ tests/com.aptana.editor.xml.tests/src/com/aptana/editor/xml/contentassist/model/DTDTransformationTests.java
@@ -143,6 +143,8 @@ public class DTDTransformationTests extends TestCase
 	{
 		DTDTransformer transformer = new DTDTransformer();
 		
+		transformer.transform(source);
+		
 		// gather element names for easy lookup
 		Set<String> names = new HashSet<String>();
 		
