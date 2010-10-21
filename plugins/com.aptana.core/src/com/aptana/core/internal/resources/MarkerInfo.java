386de06adfc65ca7670fb71fc73f383e54826303
mxia <mxia@aptana.com>
2010-10-20 18:44:01 -0700

    Reverted one of the changes from previous check-in.

--- plugins/com.aptana.core/src/com/aptana/core/internal/resources/MarkerInfo.java
+++ plugins/com.aptana.core/src/com/aptana/core/internal/resources/MarkerInfo.java
@@ -42,7 +42,7 @@ import com.aptana.core.CorePlugin;
 /**
  *
  */
-@SuppressWarnings({"restriction", "rawtypes"})
+@SuppressWarnings({"restriction", "unchecked", "rawtypes"})
 public class MarkerInfo extends org.eclipse.core.internal.resources.MarkerInfo {
 
 	/** UNDEFINED_ID */
