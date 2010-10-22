cb90a9bff3d7e907be5236cda371609f9b6601ac
Max Stepanov <mstepanov@aptana.com>
2010-10-22 13:50:28 -0700

    [878-add-preference-to-change-default-working-directory-in-terminal]

--- plugins/com.aptana.terminal/src/com/aptana/terminal/preferences/IPreferenceConstants.java
+++ plugins/com.aptana.terminal/src/com/aptana/terminal/preferences/IPreferenceConstants.java
@@ -40,4 +40,5 @@ public interface IPreferenceConstants
 {
 	public static final String FIRST_RUN = Activator.PLUGIN_ID + ".firstRun"; //$NON-NLS-1$
 	public static final String CLOSE_VIEW_ON_EXIT = Activator.PLUGIN_ID + ".closeViewOnExit"; //$NON-NLS-1$
+	public static final String WORKING_DIRECTORY = Activator.PLUGIN_ID + ".workingDirectory"; //$NON-NLS-1$
 }
