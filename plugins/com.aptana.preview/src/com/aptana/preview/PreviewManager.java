90b1bbe1f1f1ceb018ab99b52b88bd1ffa4f1960
Max Stepanov <mstepanov@aptana.com>
2010-10-25 14:37:08 -0700

    [1134-add-link-with-editor-or-auto-refresh-on-save-to-preview-window] auto-refresh preview on linked editor save

--- plugins/com.aptana.preview/src/com/aptana/preview/PreviewManager.java
+++ plugins/com.aptana.preview/src/com/aptana/preview/PreviewManager.java
@@ -35,6 +35,8 @@
 
 package com.aptana.preview;
 
+import java.util.WeakHashMap;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -46,16 +48,20 @@ import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IPathEditorInput;
+import org.eclipse.ui.IPropertyListener;
+import org.eclipse.ui.IReusableEditor;
 import org.eclipse.ui.IStorageEditorInput;
 import org.eclipse.ui.IURIEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.ui.texteditor.IDocumentProvider;
 
 import com.aptana.preview.internal.DefaultPreviewHandler;
+import com.aptana.preview.internal.EditorUtils;
 import com.aptana.preview.internal.PreviewEditorInput;
 import com.aptana.preview.internal.PreviewEditorPart;
 import com.aptana.preview.internal.PreviewHandlers;
@@ -67,11 +73,26 @@ import com.aptana.preview.internal.PreviewHandlers;
 public final class PreviewManager {
 
 	private static PreviewManager instance;
+	private IPropertyListener editorPropertyListener;
+	private WeakHashMap<IEditorPart, PreviewEditorInput> trackedEditors = new WeakHashMap<IEditorPart, PreviewEditorInput>();
 
 	/**
 	 * 
 	 */
 	private PreviewManager() {
+		editorPropertyListener = new IPropertyListener() {
+			public void propertyChanged(Object source, int propId) {
+				if (source instanceof IEditorPart && EditorPart.PROP_DIRTY == propId
+						&& !((EditorPart) source).isDirty() && trackedEditors.containsKey(source)) {
+					IEditorPart editorPart = (IEditorPart) source;
+					try {
+						openPreview(editorPart, editorPart.getEditorInput(), null);
+					} catch (CoreException e) {
+						Activator.log(e);
+					}
+				}
+			}
+		};
 	}
 
 	public static PreviewManager getInstance() {
@@ -82,37 +103,50 @@ public final class PreviewManager {
 	}
 
 	public void openPreviewForEditor(IEditorPart editorPart) {
-		IEditorInput editorInput = editorPart.getEditorInput();
-		if (!editorPart.isDirty()) {
-			openPreviewForEditorInput(editorInput);
-		} else if (editorPart instanceof AbstractTextEditor) {
-			IDocumentProvider documentProvider = ((AbstractTextEditor) editorPart).getDocumentProvider();
-			if (documentProvider != null) {
-				if (documentProvider.canSaveDocument(editorInput)) {
-					IDocument document = documentProvider.getDocument(editorInput);
-					if (document != null) {
-						try {
-							openPreview(editorInput, document.get());
-						} catch (CoreException e) {
-							Activator.log(e);
+		try {
+			IEditorInput editorInput = editorPart.getEditorInput();
+			if (!editorPart.isDirty()) {
+				openPreview(editorPart, editorInput, null);
+			} else if (editorPart instanceof AbstractTextEditor) {
+				IDocumentProvider documentProvider = ((AbstractTextEditor) editorPart).getDocumentProvider();
+				if (documentProvider != null) {
+					if (documentProvider.canSaveDocument(editorInput)) {
+						IDocument document = documentProvider.getDocument(editorInput);
+						if (document != null) {
+							try {
+								openPreview(editorPart, editorInput, document.get());
+							} catch (CoreException e) {
+								Activator.log(e);
+							}
 						}
+					} else {
+						openPreview(editorPart, editorInput, null);
 					}
-				} else {
-					openPreviewForEditorInput(editorInput);
 				}
 			}
-		}
-	}
-
-	public void openPreviewForEditorInput(IEditorInput editorInput) {
-		try {
-			openPreview(editorInput, null);
 		} catch (CoreException e) {
 			Activator.log(e);
 		}
 	}
 
-	private void openPreview(IEditorInput editorInput, String content) throws CoreException {
+	public void openPreviewForEditorInput(IEditorInput editorInput) {
+		IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IWorkbenchPage workbenchPage = null;
+		if (workbenchWindow != null) {
+			workbenchPage = workbenchWindow.getActivePage();
+		}
+		IEditorPart editorPart = null;
+		if (workbenchPage != null) {
+			editorPart = workbenchPage.findEditor(editorInput);
+		}
+		try {
+			openPreview(editorPart, editorInput, null);
+		} catch (CoreException e) {
+			Activator.log(e);
+		}
+	}
+
+	private void openPreview(IEditorPart editorPart, IEditorInput editorInput, String content) throws CoreException {
 		String fileName = null;
 		IProject project = null;
 		IPath path = null;
@@ -149,13 +183,13 @@ public final class PreviewManager {
 			previewConfig = DefaultPreviewHandler.getInstance().handle(sourceConfig);
 		}
 		if (previewConfig != null) {
-			showEditor(sourceConfig, previewConfig);
+			showEditor(editorPart, sourceConfig, previewConfig);
 		} else {
 			// TODO: add some user notification
 		}
 	}
 
-	private void showEditor(SourceConfig sourceConfig, PreviewConfig previewConfig) throws CoreException {
+	private void showEditor(IEditorPart editorPart, SourceConfig sourceConfig, PreviewConfig previewConfig) throws CoreException {
 		IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		IWorkbenchPage workbenchPage = null;
 		if (workbenchWindow != null) {
@@ -164,9 +198,33 @@ public final class PreviewManager {
 		if (workbenchPage == null) {
 			throw new PartInitException("Cannot get Workbench page"); //$NON-NLS-1$
 		}
+		IEditorPart[] openedPreviewEditors = new IEditorPart[0];
+		if (editorPart != null) {
+			PreviewEditorInput previewEditorInput = trackedEditors.get(editorPart);
+			if (previewEditorInput != null) {
+				openedPreviewEditors = EditorUtils.findEditors(previewEditorInput, PreviewEditorPart.EDITOR_ID);
+			}
+		}
 		PreviewEditorInput input = new PreviewEditorInput(previewConfig.getURL(), sourceConfig.getEditorInput()
 				.getName(), sourceConfig.getEditorInput().getToolTipText());
-		workbenchPage.openEditor(input, PreviewEditorPart.EDITOR_ID, true, IWorkbenchPage.MATCH_INPUT);
+		if (openedPreviewEditors.length > 0) {
+			for (IEditorPart previewEditorPart : openedPreviewEditors) {
+				previewEditorPart.getSite().getPage().reuseEditor((IReusableEditor) previewEditorPart, input);
+			}
+		} else {
+			openedPreviewEditors = EditorUtils.findEditors(input, PreviewEditorPart.EDITOR_ID);
+			if (openedPreviewEditors.length > 0) {
+				for (IEditorPart previewEditorPart : openedPreviewEditors) {
+					previewEditorPart.getSite().getPage().reuseEditor((IReusableEditor) previewEditorPart, input);
+				}				
+			} else {
+				workbenchPage.openEditor(input, PreviewEditorPart.EDITOR_ID, true, IWorkbenchPage.MATCH_INPUT);
+			}
+		}
+		if (editorPart != null && !trackedEditors.containsKey(editorPart)) {
+			editorPart.addPropertyListener(editorPropertyListener);
+			trackedEditors.put(editorPart, input);
+		}
 	}
 
 }
