7654f35269c1b68fd3d5d30c4a375257523b283c
Max Stepanov <mstepanov@aptana.com>
2010-11-05 13:38:35 -0700

    update PortableGit to the latest version

--- plugins/com.aptana.portablegit.win32/src/com/aptana/portablegit/win32/Extractor.java
+++ plugins/com.aptana.portablegit.win32/src/com/aptana/portablegit/win32/Extractor.java
@@ -51,7 +51,7 @@ import org.eclipse.core.runtime.Path;
 /* package */ class Extractor {
 
 	private static final String ZIP_EXECUTABLE = "$os$/7za.exe"; //$NON-NLS-1$
-	private static final String ARCHIVE_PATH = "$os$/PortableGit-1.7.0.2-preview20100309.7z"; //$NON-NLS-1$
+	private static final String ARCHIVE_PATH = "$os$/PortableGit-1.7.3.1-preview20101002.7z"; //$NON-NLS-1$
 
 	/**
 	 * 
