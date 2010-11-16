717743226375119766b06e1e59b904635a36545f
Kevin Lindsey <klindsey@aptana.com>
2010-11-16 14:34:52 -0600

    Formatting

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/index/SDocMLFileIndexingParticipant.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/index/SDocMLFileIndexingParticipant.java
@@ -59,7 +59,8 @@ public class SDocMLFileIndexingParticipant implements IFileStoreIndexingParticip
 {
 	/*
 	 * (non-Javadoc)
-	 * @see com.aptana.index.core.IFileStoreIndexingParticipant#index(java.util.Set, com.aptana.index.core.Index, org.eclipse.core.runtime.IProgressMonitor)
+	 * @see com.aptana.index.core.IFileStoreIndexingParticipant#index(java.util.Set, com.aptana.index.core.Index,
+	 * org.eclipse.core.runtime.IProgressMonitor)
 	 */
 	public void index(Set<IFileStore> files, Index index, IProgressMonitor monitor) throws CoreException
 	{
@@ -71,9 +72,9 @@ public class SDocMLFileIndexingParticipant implements IFileStoreIndexingParticip
 			{
 				throw new CoreException(Status.CANCEL_STATUS);
 			}
-			
+
 			Thread.yield(); // be nice to other threads, let them get in before each file...
-			
+
 			this.indexFileStore(index, file, sub.newChild(100));
 		}
 
@@ -90,7 +91,7 @@ public class SDocMLFileIndexingParticipant implements IFileStoreIndexingParticip
 	private void indexFileStore(Index index, IFileStore file, IProgressMonitor monitor)
 	{
 		SubMonitor sub = SubMonitor.convert(monitor, 100);
-		
+
 		if (file == null)
 		{
 			return;
@@ -102,26 +103,26 @@ public class SDocMLFileIndexingParticipant implements IFileStoreIndexingParticip
 			try
 			{
 				JSMetadataReader reader = new JSMetadataReader();
-				
+
 				InputStream stream = file.openInputStream(EFS.NONE, sub.newChild(20));
-				
+
 				// parse
 				reader.loadXML(stream);
 				sub.worked(50);
 
 				// process results
 				JSIndexWriter indexer = new JSIndexWriter();
-				
+
 				// create new Window type for this file
 				JSIndexReader jsir = new JSIndexReader();
 				TypeElement window = jsir.getType(index, JSTypeConstants.WINDOW_TYPE, EnumSet.allOf(ContentSelector.class));
-				
+
 				if (window == null)
 				{
 					window = new TypeElement();
 					window.setName(JSTypeConstants.WINDOW_TYPE);
 				}
-				
+
 				URI location = file.toURI();
 
 				// write types and add properties to Window
@@ -129,31 +130,31 @@ public class SDocMLFileIndexingParticipant implements IFileStoreIndexingParticip
 				{
 					// apply user agents to type
 					JSTypeUtil.addAllUserAgents(type);
-					
+
 					// apply user agents to all properties
 					for (PropertyElement property : type.getProperties())
 					{
 						JSTypeUtil.addAllUserAgents(property);
 					}
-					
+
 					// write type
 					indexer.writeType(index, type, location);
-					
+
 					String typeName = type.getName();
-						
+
 					if (typeName.contains(".") == false && typeName.startsWith(JSTypeConstants.GENERIC_CLASS_OPEN) == false)
 					{
 						PropertyElement property = window.getProperty(typeName);
-						
+
 						if (property == null)
 						{
 							property = new PropertyElement();
-							
+
 							property.setName(typeName);
 							property.addType(typeName);
-							
+
 							JSTypeUtil.addAllUserAgents(property);
-							
+
 							window.addProperty(property);
 						}
 					}
