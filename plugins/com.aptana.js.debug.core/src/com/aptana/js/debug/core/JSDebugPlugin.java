a1f02edb5b33b3c38e2d3c068f39b30c6dce023e
Max Stepanov <mstepanov@aptana.com>
2010-12-08 00:59:53 -0800

    WIP: initial JS debug plugins

---
+++ plugins/com.aptana.js.debug.core/src/com/aptana/js/debug/core/JSDebugPlugin.java
@@ -0,0 +1,30 @@
+package com.aptana.js.debug.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class JSDebugPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		JSDebugPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		JSDebugPlugin.context = null;
+	}
+
+}
