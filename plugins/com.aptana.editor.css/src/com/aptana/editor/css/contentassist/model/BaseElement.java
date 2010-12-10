d96a4fd7a914819f4a09d4367bb562e0ae8667ad
Kevin Lindsey <klindsey@aptana.com>
2010-12-10 10:35:30 -0600

    Indicate that the base CSS CA model element implements the IndexDocument interface

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/model/BaseElement.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/contentassist/model/BaseElement.java
@@ -43,9 +43,10 @@ import org.mortbay.util.ajax.JSON.Output;
 
 import com.aptana.core.util.CollectionsUtil;
 import com.aptana.core.util.StringUtil;
+import com.aptana.index.core.IndexDocument;
 import com.aptana.index.core.IndexUtil;
 
-public abstract class BaseElement implements ICSSMetadataElement, Convertible
+public abstract class BaseElement implements ICSSMetadataElement, Convertible, IndexDocument
 {
 	private static final String USER_AGENTS_PROPERTY = "userAgents"; //$NON-NLS-1$
 	private static final String EXAMPLE_PROPERTY = "example"; //$NON-NLS-1$
