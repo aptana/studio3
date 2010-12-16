6bc759a1841fe9ce10a7c0149e44f8b6456664fd
sgtcoolguy <cwilliams@aptana.com>
2010-12-16 13:56:10 -0500

    add perf test for loading bundles with and without cache.

--- tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingPerformanceTest.java
+++ tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingPerformanceTest.java
@@ -4,11 +4,41 @@ import org.eclipse.test.performance.PerformanceTestCase;
 
 public class BundleLoadingPerformanceTest extends PerformanceTestCase
 {
-	public void testLoadingUserBundles() throws Exception
+	private BundleManager manager;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+
+		manager = BundleManager.getInstance();
+		System.setProperty("use.bundle.cache", Boolean.FALSE.toString());
+		manager.loadBundles();
+	}
+
+	@Override
+	protected void tearDown() throws Exception
+	{
+		System.getProperties().remove("use.bundle.cache");
+		super.tearDown();
+	}
+
+	public void testLoadingUserBundlesWithoutCache() throws Exception
 	{
 		System.setProperty("use.bundle.cache", Boolean.FALSE.toString());
-		BundleManager manager = BundleManager.getInstance();
-		
+		for (int i = 0; i < 25; i++)
+		{
+			startMeasuring();
+			manager.loadBundles();
+			stopMeasuring();
+		}
+		commitMeasurements();
+		assertPerformance();
+	}
+
+	public void testLoadingUserBundlesWithCache() throws Exception
+	{
+		System.setProperty("use.bundle.cache", Boolean.TRUE.toString());
 		for (int i = 0; i < 25; i++)
 		{
 			startMeasuring();
