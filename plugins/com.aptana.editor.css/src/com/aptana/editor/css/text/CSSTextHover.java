75fc51b2be9d46409d3755c41d0c9c923fda8a8a
Kevin Lindsey <klindsey@aptana.com>
2010-12-17 10:12:47 -0600

    Preliminary support for property info on hover

--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/text/CSSTextHover.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/text/CSSTextHover.java
@@ -35,9 +35,9 @@
 package com.aptana.editor.css.text;
 
 import java.text.MessageFormat;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IInformationControl;
 import org.eclipse.jface.text.IInformationControlCreator;
@@ -54,108 +54,80 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Shell;
 
 import com.aptana.editor.common.contentassist.InformationControl;
-import com.aptana.editor.css.CSSPlugin;
+import com.aptana.editor.common.contentassist.LexemeProvider;
 import com.aptana.editor.css.CSSColors;
+import com.aptana.editor.css.CSSScopeScanner;
+import com.aptana.editor.css.contentassist.CSSIndexQueryHelper;
+import com.aptana.editor.css.contentassist.model.PropertyElement;
+import com.aptana.editor.css.parsing.lexer.CSSTokenType;
+import com.aptana.parsing.lexer.Lexeme;
 import com.aptana.theme.ColorManager;
 import com.aptana.theme.Theme;
 import com.aptana.theme.ThemePlugin;
 
 public class CSSTextHover implements ITextHover, ITextHoverExtension, ITextHoverExtension2
 {
-
-	private static final Pattern RGB_PATTERN = Pattern.compile("#([a-fA-F0-9]{3}|[a-fA-F0-9]{6})"); //$NON-NLS-1$
-
-	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion)
+	private static class ThemedInformationControl extends InformationControl implements IInformationControlExtension2
 	{
-		// Not called
-		return null;
-	}
-
-	public IRegion getHoverRegion(ITextViewer textViewer, int offset)
-	{
-		// look for the current "word"
-		IDocument doc = textViewer.getDocument();
-		try
+		public ThemedInformationControl(Shell parent)
 		{
-			final int origOffset = offset;
-			int start = offset;
-			while (offset >= 0)
+			super(parent);
+
+			GridData gd = (GridData) getStyledTextWidget().getLayoutData();
+
+			gd.horizontalIndent = 0;
+			gd.verticalIndent = 0;
+		}
+
+		@Override
+		protected Color getBackground()
+		{
+			return getThemeBackground();
+		}
+
+		@Override
+		protected Color getBorderColor()
+		{
+			return getForeground();
+		}
+
+		protected ColorManager getColorManager()
+		{
+			return ThemePlugin.getDefault().getColorManager();
+		}
+
+		protected Theme getCurrentTheme()
+		{
+			return ThemePlugin.getDefault().getThemeManager().getCurrentTheme();
+		}
+
+		@Override
+		protected Color getForeground()
+		{
+			return getThemeForeground();
+		}
+
+		protected Color getThemeBackground()
+		{
+			return getColorManager().getColor(getCurrentTheme().getBackground());
+		}
+
+		protected Color getThemeForeground()
+		{
+			return getColorManager().getColor(getCurrentTheme().getForeground());
+		}
+
+		public void setInput(Object input)
+		{
+			if (input instanceof RGB)
 			{
-				char c = doc.getChar(offset--);
-				if (!isWordPart(c))
-				{
-					start = offset + 2;
-					break;
-				}
+				setBackgroundColor(getColorManager().getColor((RGB) input));
 			}
-			offset = origOffset;
-			int docLength = doc.getLength();
-			while (offset < docLength)
+			else if (input instanceof String)
 			{
-				char c = doc.getChar(offset++);
-				if (!isWordPart(c))
-				{
-					offset--;
-					break;
-				}
+				setInformation((String) input);
 			}
-			return new Region(start, offset - start);
 		}
-		catch (BadLocationException e)
-		{
-			CSSPlugin.logError(e.getMessage(), e);
-		}
-		return new Region(offset, 0);
-	}
-
-	private boolean isWordPart(char c)
-	{
-		return Character.isLetterOrDigit(c) || c == '-' || c == '_' || c == '.' || c == '#';
-	}
-
-	public Object getHoverInfo2(ITextViewer textViewer, IRegion hoverRegion)
-	{
-		try
-		{
-			String word = textViewer.getDocument().get(hoverRegion.getOffset(), hoverRegion.getLength());
-			if (CSSColors.namedColorExists(word))
-			{
-				word = CSSColors.to6CharHexWithLeadingHash(word);
-			}
-			else
-			{
-				// Match against a pattern to verify it's a color
-				if (!RGB_PATTERN.matcher(word).matches())
-				{
-					return null;
-				}
-				word = CSSColors.to6CharHexWithLeadingHash(word);
-			}
-			return parseHexRGB(word);
-		}
-		catch (BadLocationException e)
-		{
-			// ignores the exception; just assumes no hover info is available
-		}
-		return null;
-	}
-
-	private RGB parseHexRGB(String token)
-	{
-		if (token == null)
-			return new RGB(0, 0, 0);
-		if (token.length() != 7 && token.length() != 9)
-		{
-			ThemePlugin.logError(MessageFormat.format("Received RGB Hex value with invalid length: {0}", token), null); //$NON-NLS-1$
-			return new RGB(0, 0, 0);
-		}
-		String s = token.substring(1, 3);
-		int r = Integer.parseInt(s, 16);
-		s = token.substring(3, 5);
-		int g = Integer.parseInt(s, 16);
-		s = token.substring(5, 7);
-		int b = Integer.parseInt(s, 16);
-		return new RGB(r, g, b);
 	}
 
 	/*
@@ -172,60 +144,222 @@ public class CSSTextHover implements ITextHover, ITextHoverExtension, ITextHover
 		};
 	}
 
-	private static class ThemedInformationControl extends InformationControl implements IInformationControlExtension2
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.text.ITextHover#getHoverInfo(org.eclipse.jface.text.ITextViewer,
+	 * org.eclipse.jface.text.IRegion)
+	 */
+	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion)
 	{
-		public ThemedInformationControl(Shell parent)
+		// Not called
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.text.ITextHoverExtension2#getHoverInfo2(org.eclipse.jface.text.ITextViewer,
+	 * org.eclipse.jface.text.IRegion)
+	 */
+	public Object getHoverInfo2(ITextViewer textViewer, IRegion hoverRegion)
+	{
+		int offset = hoverRegion.getOffset();
+		LexemeProvider<CSSTokenType> lexemeProvider = getLexemeProvider(textViewer, offset);
+		Lexeme<CSSTokenType> lexeme = lexemeProvider.getLexemeFromOffset(offset);
+		Object result = null;
+
+		switch (lexeme.getType())
 		{
-			super(parent);
-			GridData gd = (GridData) getStyledTextWidget().getLayoutData();
-			gd.horizontalIndent = 0;
-			gd.verticalIndent = 0;
+			case COLOR:
+				result = parseHexRGB(CSSColors.to6CharHexWithLeadingHash(lexeme.getText()));
+				break;
+
+			case FUNCTION:
+				if ("rgb".equals(lexeme.getText()))
+				{
+					int start = lexeme.getEndingOffset();
+					List<Lexeme<CSSTokenType>> lexemes = this.getFunctionLexemes(lexemeProvider, start);
+
+					if (lexemes.size() == 8)
+					{
+						Lexeme<CSSTokenType> redLexeme = lexemes.get(2);
+						Lexeme<CSSTokenType> greenLexeme = lexemes.get(4);
+						Lexeme<CSSTokenType> blueLexeme = lexemes.get(6);
+
+						if (isNumber(redLexeme) && isNumber(greenLexeme) && isNumber(blueLexeme))
+						{
+							int red = Integer.parseInt(redLexeme.getText());
+							int green = Integer.parseInt(greenLexeme.getText());
+							int blue = Integer.parseInt(blueLexeme.getText());
+
+							result = new RGB(red, green, blue);
+						}
+					}
+				}
+
+			case PROPERTY:
+				CSSIndexQueryHelper queryHelper = new CSSIndexQueryHelper();
+				PropertyElement property = queryHelper.getProperty(lexeme.getText());
+
+				if (property != null)
+				{
+					result = property.getDescription();
+				}
+				break;
+
+			default:
+				System.out.println(lexeme.getType().name());
 		}
 
-		@Override
-		protected Color getBackground()
-		{
-			return getThemeBackground();
-		}
+		// try
+		// {
+		// String word = textViewer.getDocument().get(hoverRegion.getOffset(), hoverRegion.getLength());
+		//
+		// if (CSSColors.namedColorExists(word))
+		// {
+		// word = CSSColors.to6CharHexWithLeadingHash(word);
+		// }
+		// else
+		// {
+		// // Match against a pattern to verify it's a color
+		// if (!RGB_PATTERN.matcher(word).matches())
+		// {
+		// return null;
+		// }
+		// word = CSSColors.to6CharHexWithLeadingHash(word);
+		// }
+		//
+		// return parseHexRGB(word);
+		// }
+		// catch (BadLocationException e)
+		// {
+		// // ignores the exception; just assumes no hover info is available
+		// }
 
-		@Override
-		protected Color getForeground()
-		{
-			return getThemeForeground();
-		}
+		return result;
+	}
 
-		@Override
-		protected Color getBorderColor()
-		{
-			return getForeground();
-		}
+	private boolean isNumber(Lexeme<CSSTokenType> token)
+	{
+		return (token != null && token.getType() == CSSTokenType.NUMBER);
+	}
 
-		protected Color getThemeForeground()
-		{
-			return getColorManager().getColor(getCurrentTheme().getForeground());
-		}
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jface.text.ITextHover#getHoverRegion(org.eclipse.jface.text.ITextViewer, int)
+	 */
+	public IRegion getHoverRegion(ITextViewer textViewer, int offset)
+	{
+		// look for the current "word"
+		LexemeProvider<CSSTokenType> lexemeProvider = getLexemeProvider(textViewer, offset);
+		Lexeme<CSSTokenType> lexeme = lexemeProvider.getLexemeFromOffset(offset);
+		IRegion result = null;
 
-		protected Color getThemeBackground()
+		if (lexeme != null)
 		{
-			return getColorManager().getColor(getCurrentTheme().getBackground());
-		}
-
-		protected Theme getCurrentTheme()
-		{
-			return ThemePlugin.getDefault().getThemeManager().getCurrentTheme();
-		}
-
-		protected ColorManager getColorManager()
-		{
-			return ThemePlugin.getDefault().getColorManager();
-		}
-
-		public void setInput(Object input)
-		{
-			if (input instanceof RGB)
+			switch (lexeme.getType())
 			{
-				setBackgroundColor(getColorManager().getColor((RGB) input));
+				case COLOR:
+				case PROPERTY:
+					result = new Region(lexeme.getStartingOffset(), lexeme.getLength());
+					break;
+
+				case FUNCTION:
+					if ("rgb".equals(lexeme.getText()))
+					{
+						int start = lexeme.getStartingOffset();
+						List<Lexeme<CSSTokenType>> lexemes = this.getFunctionLexemes(lexemeProvider, start);
+						int end = lexemes.get(lexemes.size() - 1).getEndingOffset();
+
+						result = new Region(start, end - start + 1);
+					}
+					break;
 			}
 		}
+
+		if (result == null)
+		{
+			result = new Region(offset, 0);
+		}
+
+		return result;
+	}
+
+	/**
+	 * getFunctionEndingOffset
+	 * 
+	 * @param lexemeProvider
+	 * @param startingOffset
+	 * @return
+	 */
+	protected List<Lexeme<CSSTokenType>> getFunctionLexemes(LexemeProvider<CSSTokenType> lexemeProvider, int startingOffset)
+	{
+		List<Lexeme<CSSTokenType>> result = new ArrayList<Lexeme<CSSTokenType>>();
+		int index = lexemeProvider.getLexemeIndex(startingOffset);
+
+		for (int i = index; i < lexemeProvider.size(); i++)
+		{
+			Lexeme<CSSTokenType> candidate = lexemeProvider.getLexeme(i);
+
+			result.add(candidate);
+
+			if (candidate.getType() == CSSTokenType.RPAREN)
+			{
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * getLexemeProvider
+	 * 
+	 * @param textViewer
+	 * @param offset
+	 * @return
+	 */
+	protected LexemeProvider<CSSTokenType> getLexemeProvider(ITextViewer textViewer, int offset)
+	{
+		IDocument document = textViewer.getDocument();
+
+		LexemeProvider<CSSTokenType> lexemeProvider = new LexemeProvider<CSSTokenType>(document, offset, new CSSScopeScanner())
+		{
+			@Override
+			protected CSSTokenType getTypeFromData(Object data)
+			{
+				return (CSSTokenType) data;
+			}
+		};
+		return lexemeProvider;
+	}
+
+	/**
+	 * parseHexRGB
+	 * 
+	 * @param token
+	 * @return
+	 */
+	private RGB parseHexRGB(String token)
+	{
+		if (token == null)
+		{
+			return new RGB(0, 0, 0);
+		}
+
+		if (token.length() != 7 && token.length() != 9)
+		{
+			ThemePlugin.logError(MessageFormat.format("Received RGB Hex value with invalid length: {0}", token), null); //$NON-NLS-1$
+
+			return new RGB(0, 0, 0);
+		}
+
+		String s = token.substring(1, 3);
+		int r = Integer.parseInt(s, 16);
+		s = token.substring(3, 5);
+		int g = Integer.parseInt(s, 16);
+		s = token.substring(5, 7);
+		int b = Integer.parseInt(s, 16);
+
+		return new RGB(r, g, b);
 	}
 }
