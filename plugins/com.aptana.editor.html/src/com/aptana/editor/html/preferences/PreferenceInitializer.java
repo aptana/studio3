de178c1fe518ab608387677d65a5ec45408109c7
mxia <mxia@aptana.com>
2011-01-11 15:28:34 -0800

    Filtered out validation error messages on canvas, which is valid HTML5.

--- plugins/com.aptana.editor.html/src/com/aptana/editor/html/preferences/PreferenceInitializer.java
+++ plugins/com.aptana.editor.html/src/com/aptana/editor/html/preferences/PreferenceInitializer.java
@@ -38,11 +38,15 @@ import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
+import com.aptana.core.util.StringUtil;
+import com.aptana.editor.common.CommonEditorPlugin;
 import com.aptana.editor.html.HTMLPlugin;
+import com.aptana.editor.html.parsing.IHTMLParserConstants;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer
 {
 
+	@SuppressWarnings("nls")
 	@Override
 	public void initializeDefaultPreferences()
 	{
@@ -50,5 +54,11 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer
 
 		prefs.putBoolean(com.aptana.editor.common.preferences.IPreferenceConstants.LINK_OUTLINE_WITH_EDITOR, true);
 		prefs.putDouble(IPreferenceContants.HTML_INDEX_VERSION, 0);
+
+		prefs = new DefaultScope().getNode(CommonEditorPlugin.PLUGIN_ID);
+		String[] filtered = new String[] { ".*canvas.*" };
+		prefs.put(IHTMLParserConstants.LANGUAGE + ":"
+				+ com.aptana.editor.common.preferences.IPreferenceConstants.FILTER_EXPRESSIONS,
+				StringUtil.join("####", filtered));
 	}
 }
