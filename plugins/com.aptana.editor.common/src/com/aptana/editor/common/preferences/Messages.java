747bc3ea35a6bc348bdf6c61cff613d940c218ac
mxia <mxia@aptana.com>
2011-01-31 11:30:27 -0800

    Fixed Studio issue #1601: Added label 'Tab policy' to the front of dropdown for selecting tabs/spaces.

--- plugins/com.aptana.editor.common/src/com/aptana/editor/common/preferences/Messages.java
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/preferences/Messages.java
@@ -14,17 +14,12 @@ public class Messages extends NLS
 	private static final String BUNDLE_NAME = "com.aptana.editor.common.preferences.messages"; //$NON-NLS-1$
 
 	public static String CommonEditorPreferencePage_Default_Editor_Preference_Link;
-
 	public static String CommonEditorPreferencePage_Editor_Preferences;
-
+	public static String CommonEditorPreferencePage_LBL_TabPolicy;
 	public static String CommonEditorPreferencePage_Tab_Size_Label;
-
 	public static String CommonEditorPreferencePage_Text_Editing_Label;
-
 	public static String CommonEditorPreferencePage_UseDefaultOption;
-
 	public static String CommonEditorPreferencePage_UseSpacesOption;
-
 	public static String CommonEditorPreferencePage_UseTabOption;
 
 	public static String TasksPreferencePage_CaseSensitiveLabel;
@@ -42,25 +37,18 @@ public class Messages extends NLS
 	public static String UserAgentPreferencePage_Select_User_Agents;
 
 	public static String EditorsPreferencePage_Close_Matching_Character_Pairs;
-
 	public static String EditorsPreferencePage_Colorize_Matching_Character_Pairs;
-
 	public static String EditorsPreferencePage_Content_Assist;
-
 	public static String EditorsPreferencePage_Content_Assist_Auto_Display;
-
 	public static String EditorsPreferencePage_Content_Assist_Short_Delay;
-
 
 	/**
 	 * EditorsPreferencePage_Formatting
 	 */
 	public static String EditorsPreferencePage_Formatting;
-	
 	public static String EditorsPreferencePage_HomeEndBehavior;
-
 	public static String EditorsPreferencePage_JumpsStartEnd;
-	
+
 	/**
 	 * EditorsPreferencePage_MarkOccurrences
 	 */
@@ -70,17 +58,14 @@ public class Messages extends NLS
 	 * EditorsPreferencePage_PreferenceDescription
 	 */
 	public static String EditorsPreferencePage_PreferenceDescription;
-	
 	public static String EditorsPreferencePage_ToggleBetween;
-
 	public static String EditorsPreferencePage_Typing;
-	
-	
+
 	/**
 	 * EditorsPreferencePage_GeneralTextEditorPrefLink
 	 */
 	public static String EditorsPreferencePage_GeneralTextEditorPrefLink;
-	
+
 	public static String ValidationPreferencePage_ERR_EmptyExpression;
 	public static String ValidationPreferencePage_Filter_Description;
 	public static String ValidationPreferencePage_Ignore_Message;
