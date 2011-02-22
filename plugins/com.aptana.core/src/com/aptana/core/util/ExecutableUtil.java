e23e09c0aeae0f154c74c5c70d131ba6ad6cfa06
sgtcoolguy <cwilliams@aptana.com>
2011-02-22 12:09:27 -0500

    Add INFO level logging for ENV for a given working dir, as well as finding executable via which or at a common location. This should give some insight into problem seen at https://aptana.lighthouseapp.com/projects/35272/tickets/1840-studo3-debugger-segfault

--- plugins/com.aptana.core/src/com/aptana/core/util/ExecutableUtil.java
+++ plugins/com.aptana.core/src/com/aptana/core/util/ExecutableUtil.java
@@ -10,6 +10,7 @@ package com.aptana.core.util;
 import java.io.File;
 import java.io.FileFilter;
 import java.lang.reflect.Method;
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +20,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 
+import com.aptana.core.CorePlugin;
 import com.aptana.core.ShellExecutable;
 
 /**
@@ -27,7 +29,8 @@ import com.aptana.core.ShellExecutable;
 public final class ExecutableUtil
 {
 
-	private ExecutableUtil() {
+	private ExecutableUtil()
+	{
 	}
 
 	/**
@@ -44,7 +47,8 @@ public final class ExecutableUtil
 		return find(executableName, appendExtension, searchLocations, (FileFilter) null);
 	}
 
-	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations, IPath workingDirectory)
+	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations,
+			IPath workingDirectory)
 	{
 		return find(executableName, appendExtension, searchLocations, null, workingDirectory);
 	}
@@ -57,10 +61,11 @@ public final class ExecutableUtil
 	 * @param searchLocations
 	 *            Common locations to search.
 	 * @param filter
-	 * 			File filter
+	 *            File filter
 	 * @return
 	 */
-	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations, FileFilter filter)
+	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations,
+			FileFilter filter)
 	{
 		return find(executableName, appendExtension, searchLocations, filter, null);
 	}
@@ -73,11 +78,12 @@ public final class ExecutableUtil
 	 * @param searchLocations
 	 *            Common locations to search.
 	 * @param filter
-	 * 			File filter
+	 *            File filter
 	 * @param workingDirectory
 	 * @return
 	 */
-	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations, FileFilter filter, IPath workingDirectory)
+	public static IPath find(String executableName, boolean appendExtension, List<IPath> searchLocations,
+			FileFilter filter, IPath workingDirectory)
 	{
 		Map<String, String> env = ShellExecutable.getEnvironment(workingDirectory);
 		if (Platform.OS_WIN32.equals(Platform.getOS()))
@@ -85,12 +91,15 @@ public final class ExecutableUtil
 			String[] paths;
 			if (env != null && env.containsKey("PATH")) { //$NON-NLS-1$
 				paths = env.get("PATH").split(ShellExecutable.PATH_SEPARATOR); //$NON-NLS-1$
-				for( int i = 0; i < paths.length; ++i) {
+				for (int i = 0; i < paths.length; ++i)
+				{
 					if (paths[i].matches("^/(.)/.*")) { //$NON-NLS-1$
 						paths[i] = paths[i].replaceFirst("^/(.)/", "$1:/"); //$NON-NLS-1$ //$NON-NLS-2$
 					}
 				}
-			} else {
+			}
+			else
+			{
 				String pathENV = System.getenv("PATH"); //$NON-NLS-1$
 				paths = pathENV.split(File.pathSeparator);
 			}
@@ -113,7 +122,10 @@ public final class ExecutableUtil
 			{
 				IPath whichPath = Path.fromOSString(whichResult.trim());
 				if (isExecutable(whichPath) && (filter == null || filter.accept(whichPath.toFile())))
+				{
+					CorePlugin.logInfo(MessageFormat.format("Found executable via 'which': {0}", whichPath)); //$NON-NLS-1$
 					return whichPath;
+				}
 			}
 		}
 
@@ -124,7 +136,10 @@ public final class ExecutableUtil
 			{
 				IPath result = findExecutable(location.append(executableName), appendExtension);
 				if (result != null && (filter == null || filter.accept(result.toFile())))
+				{
+					CorePlugin.logInfo(MessageFormat.format("Found executable at common location: {0}", result)); //$NON-NLS-1$
 					return result;
+				}
 			}
 		}
 
@@ -166,7 +181,7 @@ public final class ExecutableUtil
 		{
 			return false;
 		}
-		
+
 		// OK, file exists
 		try
 		{
@@ -187,6 +202,6 @@ public final class ExecutableUtil
 			return true;
 		}
 		IFileStore fileStore = EFS.getLocalFileSystem().getStore(path);
-	    return fileStore.fetchInfo().getAttribute(EFS.ATTRIBUTE_EXECUTABLE);
+		return fileStore.fetchInfo().getAttribute(EFS.ATTRIBUTE_EXECUTABLE);
 	}
 }
