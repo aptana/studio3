89aeb80817be11658d3870468f1e0cc62ab1738b
sgtcoolguy <cwilliams@aptana.com>
2011-03-02 09:18:40 -0800

    Add preference to enable/disable auto-attaching our git support to a project imported/added that has an existing git repo

--- plugins/com.aptana.git.core/src/com/aptana/git/core/IPreferenceConstants.java
+++ plugins/com.aptana.git.core/src/com/aptana/git/core/IPreferenceConstants.java
@@ -14,9 +14,16 @@ public interface IPreferenceConstants
 	 * Preference key for location of git executable to use.
 	 */
 	String GIT_EXECUTABLE_PATH = "git_executable_path"; //$NON-NLS-1$
+
 	/**
 	 * Preference key for determining if we should perform actual fetches for pull indicator calculations.
 	 */
 	String GIT_CALCULATE_PULL_INDICATOR = "git_calculate_pull_indicator"; //$NON-NLS-1$
+
+	/**
+	 * Preference key for boolean pref determining if we auto-attach our git provider to projects with git repos when
+	 * the project gets added.
+	 */
+	String AUTO_ATTACH_REPOS = "auto_attach_repos"; //$NON-NLS-1$
 
 }
--- plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/preferences/GitExecutableLocationPage.java
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/preferences/GitExecutableLocationPage.java
@@ -27,6 +27,7 @@ public class GitExecutableLocationPage extends FieldEditorPreferencePage impleme
 
 	private FileFieldEditor fileEditor;
 	private BooleanFieldEditor pullIndicatorEditor;
+	private BooleanFieldEditor autoAttachEditor;
 
 	public GitExecutableLocationPage()
 	{
@@ -41,8 +42,9 @@ public class GitExecutableLocationPage extends FieldEditorPreferencePage impleme
 	protected void createFieldEditors()
 	{
 		// Git Executable location
-		fileEditor = new FileFieldEditor(IPreferenceConstants.GIT_EXECUTABLE_PATH, Messages.GitExecutableLocationPage_LocationLabel, true,
-				FileFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent())
+		fileEditor = new FileFieldEditor(IPreferenceConstants.GIT_EXECUTABLE_PATH,
+				Messages.GitExecutableLocationPage_LocationLabel, true, FileFieldEditor.VALIDATE_ON_KEY_STROKE,
+				getFieldEditorParent())
 		{
 			@Override
 			protected boolean checkState()
@@ -72,6 +74,11 @@ public class GitExecutableLocationPage extends FieldEditorPreferencePage impleme
 				Messages.GitExecutableLocationPage_CalculatePullIndicatorLabel, getFieldEditorParent());
 		addField(fileEditor);
 		addField(pullIndicatorEditor);
+
+		// Auto-attach to projects
+		autoAttachEditor = new BooleanFieldEditor(IPreferenceConstants.AUTO_ATTACH_REPOS,
+				Messages.GitExecutableLocationPage_AutoAttachProjectsLabel, getFieldEditorParent());
+		addField(autoAttachEditor);
 	}
 
 	@Override
