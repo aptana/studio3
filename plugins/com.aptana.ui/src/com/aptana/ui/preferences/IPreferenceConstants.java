8fa21c51bae6a05319d9d3b8e98d33c56971df70
mxia <mxia@aptana.com>
2011-03-04 14:32:47 -0800

    Prompt user for perspective reset when our perspective's default layout changes.

---
+++ plugins/com.aptana.ui/src/com/aptana/ui/preferences/IPreferenceConstants.java
@@ -0,0 +1,9 @@
+package com.aptana.ui.preferences;
+
+public class IPreferenceConstants
+{
+
+	public static final String IDE_HAS_LAUNCHED_BEFORE = "IDEHasLaunchedBefore"; //$NON-NLS-1$
+
+	public static final String PERSPECTIVE_VERSION = "PerspectiveVersion"; //$NON-NLS-1$
+}
