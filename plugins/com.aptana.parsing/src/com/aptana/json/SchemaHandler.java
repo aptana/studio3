67c9c1367baab8607b8babd29eaccee5dd30072b
Kevin Lindsey <klindsey@aptana.com>
2011-03-03 17:05:41 -0800

    Start implementation of building Schema when reading a schema

--- plugins/com.aptana.parsing/src/com/aptana/json/SchemaHandler.java
+++ plugins/com.aptana.parsing/src/com/aptana/json/SchemaHandler.java
@@ -7,11 +7,26 @@
  */
 package com.aptana.json;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 /**
- * @author klindsey
+ * SchemaHandler
  */
 public class SchemaHandler implements IContextHandler
 {
+	private Stack<Object> _stack;
+	private Schema _schema;
+
+	/**
+	 * SchemaHandler
+	 */
+	public SchemaHandler()
+	{
+		this._stack = new Stack<Object>();
+		this._schema = new Schema();
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.aptana.json.IContextAction#addElement(java.lang.String, com.aptana.json.IState)
@@ -28,6 +43,8 @@ public class SchemaHandler implements IContextHandler
 	public void createList(String elementTypeName, IState elementType)
 	{
 		System.out.println("create list of '" + elementTypeName + "'");
+		
+		this._stack.push(new ArrayList<Object>());
 	}
 
 	/*
@@ -37,14 +54,55 @@ public class SchemaHandler implements IContextHandler
 	public void createType(String typeName, IState type)
 	{
 		System.out.println("create type '" + typeName + "'");
+
+		Object instance = null;
+
+		if ("Schema".equals(typeName))
+		{
+			instance = this._schema.addType(typeName);
+		}
+		else if ("SchemaDocument".equals(typeName))
+		{
+			instance = this._schema.addType(typeName);
+		}
+		else if ("Type".equals(typeName))
+		{
+			instance = this._schema.addType(typeName);
+		}
+		else if ("Property".equals(typeName))
+		{
+			instance = this._schema.addType(typeName);
+		}
+		else if (typeName != null && typeName.startsWith("Array<"))
+		{
+			// do nothing as this will be handled by createList
+		}
+
+		if (instance == null)
+		{
+			// throw exception
+		}
+
+		this._stack.push(instance);
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see com.aptana.json.IContextAction#setProperty(java.lang.String, com.aptana.json.IState)
 	 */
-	public void setProperty(String propertyName, IState propertyType)
+	public void setProperty(String propertyName, String propertyTypeName, IState propertyType)
 	{
 		System.out.println("set property '" + propertyName + "'");
+
+		if (this._stack.isEmpty() == false)
+		{
+			/* Object value = */this._stack.pop();
+			Object top = this._stack.peek();
+
+			if (top instanceof SchemaObject)
+			{
+				((SchemaObject) top).addProperty(propertyName, propertyTypeName);
+			}
+		}
 	}
 }
