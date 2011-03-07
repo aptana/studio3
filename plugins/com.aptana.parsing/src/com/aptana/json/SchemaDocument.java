094e1b74eb6a59613ccc1fa13948cb3fa108c87e
Kevin Lindsey <klindsey@aptana.com>
2011-03-07 12:08:35 -0600

    Create top-level container for Schemas when generating schemas

---
+++ plugins/com.aptana.parsing/src/com/aptana/json/SchemaDocument.java
@@ -0,0 +1,62 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.json;
+
+/**
+ * @author klindsey
+ *
+ */
+public class SchemaDocument implements IState
+{
+
+	/* (non-Javadoc)
+	 * @see com.aptana.json.IState#create(java.lang.String, java.lang.Object)
+	 */
+	public Object create(String typeName, Object value)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.aptana.json.IState#enter()
+	 */
+	public void enter()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.aptana.json.IState#exit()
+	 */
+	public void exit()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.aptana.json.IState#isValidTransition(com.aptana.json.SchemaEventType, java.lang.Object)
+	 */
+	public boolean isValidTransition(SchemaEventType event, Object value)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.aptana.json.IState#transition(com.aptana.json.ISchemaContext, com.aptana.json.SchemaEventType, java.lang.Object)
+	 */
+	public void transition(ISchemaContext context, SchemaEventType event, Object value)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
