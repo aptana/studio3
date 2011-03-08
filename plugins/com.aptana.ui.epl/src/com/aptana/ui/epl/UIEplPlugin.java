062bd85fa42203874db51b06b179ac101cd43b1e
Allen Yeung <ayeung@aptana.com>
2011-03-07 23:57:30 -0800

    Added titaniumupdatepopup (used to alert users about titanium updates)

--- plugins/com.aptana.ui.epl/src/com/aptana/ui/epl/UIEplPlugin.java
+++ plugins/com.aptana.ui.epl/src/com/aptana/ui/epl/UIEplPlugin.java
@@ -7,10 +7,15 @@
  */
 package com.aptana.ui.epl;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
@@ -28,6 +33,10 @@ public class UIEplPlugin extends AbstractUIPlugin
 
 	// The shared instance
 	private static UIEplPlugin plugin;
+	
+	public final static String ICON_PATH = "$nl$/icons/"; //$NON-NLS-1$
+	public final static String IMG_TOOL_CLOSE = "close.gif"; //$NON-NLS-1$
+	public final static String IMG_TOOL_CLOSE_HOT = "close_hot.gif"; //$NON-NLS-1$
 
 	/**
 	 * The constructor
@@ -97,4 +106,20 @@ public class UIEplPlugin extends AbstractUIPlugin
 		}
 		return getDefault().getImageRegistry().get(string);
 	}
+	
+	protected void initializeImageRegistry(ImageRegistry reg) {
+		createImageDescriptor(IMG_TOOL_CLOSE, reg);
+		createImageDescriptor(IMG_TOOL_CLOSE_HOT, reg);
+
+	}
+
+	/**
+	 * Creates the specified image descriptor and registers it
+	 */
+	private void createImageDescriptor(String id, ImageRegistry reg) {
+		URL url = FileLocator.find(getBundle(), new Path(ICON_PATH).append(id), null);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		reg.put(id, desc);
+	}
+	
 }
