5b38406e4bac41349a3311e1494e89701a229f94
Max Stepanov <mstepanov@aptana.com>
2011-03-23 11:06:14 -0700

    add source configuration contributions extension point

---
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/hover/ITextHoverDelegate.java
@@ -0,0 +1,16 @@
+/**
+ * Appcelerator Titanium Studio
+ * Copyright (c) 2011 by Appcelerator, Inc. All Rights Reserved.
+ * Proprietary and Confidential - This source code is not for redistribution
+ */
+
+package com.aptana.editor.common.hover;
+
+
+/**
+ * @author Max Stepanov
+ *
+ */
+public interface ITextHoverDelegate {
+
+}
--- plugins/com.aptana.editor.css/src/com/aptana/editor/css/text/CSSTextHoverInformationControl.java
+++ plugins/com.aptana.editor.css/src/com/aptana/editor/css/text/CSSTextHoverInformationControl.java
@@ -1,7 +1,9 @@
 /**
- * Appcelerator Titanium Studio
- * Copyright (c) 2011 by Appcelerator, Inc. All Rights Reserved.
- * Proprietary and Confidential - This source code is not for redistribution
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
  */
 
 package com.aptana.editor.css.text;
