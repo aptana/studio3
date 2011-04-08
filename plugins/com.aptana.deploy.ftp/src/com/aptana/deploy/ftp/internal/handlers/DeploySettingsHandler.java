06db145e909bc3cb3fd1d12d99d78ec1505043c4
sgtcoolguy <cwilliams@aptana.com>
2011-04-08 15:47:53 -0400

    add FTP deploy wizard extension, move FTP settings command and handler to ftp deploy plugin

--- plugins/com.aptana.deploy.ftp/src/com/aptana/deploy/ftp/internal/handlers/DeploySettingsHandler.java
+++ plugins/com.aptana.deploy.ftp/src/com/aptana/deploy/ftp/internal/handlers/DeploySettingsHandler.java
@@ -5,7 +5,7 @@
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
  */
-package com.aptana.deploy.internal.handlers;
+package com.aptana.deploy.ftp.internal.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
