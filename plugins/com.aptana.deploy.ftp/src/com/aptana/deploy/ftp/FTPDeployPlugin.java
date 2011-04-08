5bf7fd70218cdadaa6654d256fb99a936a04fa25
sgtcoolguy <cwilliams@aptana.com>
2011-04-08 12:31:43 -0400

    Add separate plugins for each deployment option as a place where we're going to migrate the code

---
+++ plugins/com.aptana.deploy.ftp/src/com/aptana/deploy/ftp/FTPDeployPlugin.java
@@ -0,0 +1,30 @@
+package com.aptana.deploy.ftp;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class FTPDeployPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		FTPDeployPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		FTPDeployPlugin.context = null;
+	}
+
+}
---
+++ plugins/com.aptana.deploy.redhat/src/com/aptana/deploy/redhat/RedHatPlugin.java
@@ -0,0 +1,30 @@
+package com.aptana.deploy.redhat;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class RedHatPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		RedHatPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		RedHatPlugin.context = null;
+	}
+
+}
