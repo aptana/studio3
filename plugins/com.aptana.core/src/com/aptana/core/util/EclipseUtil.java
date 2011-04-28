a3b49f57bf9a1ffdf311afee855ac47692bd9b7f
Ingo Muschenetz <ingo@appcelerator.com>
2011-04-28 13:50:49 -0700

    Write out some debugging messages for UnifiedBuilder to help with debugging.

--- plugins/com.aptana.core/src/com/aptana/core/util/EclipseUtil.java
+++ plugins/com.aptana.core/src/com/aptana/core/util/EclipseUtil.java
@@ -214,11 +214,12 @@ public class EclipseUtil
 	 */
 	public static boolean showSystemJobs()
 	{
-		if (System.getProperty("osgi.debug") != null) //$NON-NLS-1$
+		if (Platform.inDebugMode())
 		{
 			return true;
 		}
 		return Platform.getPreferencesService().getBoolean(CorePlugin.PLUGIN_ID,
 				ICorePreferenceConstants.PREF_SHOW_SYSTEM_JOBS, false, null);
 	}
+
 }
