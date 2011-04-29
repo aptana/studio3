1ad4eb2abf3794a499bed5e5ef73013e329bb573
sgtcoolguy <cwilliams@aptana.com>
2011-04-29 15:57:40 -0400

    Implement #1580 - Add initial folding options for JSON editor

--- tests/com.aptana.editor.json.tests/src/com/aptana/editor/json/internal/text/JSONFoldingComputerTest.java
+++ tests/com.aptana.editor.json.tests/src/com/aptana/editor/json/internal/text/JSONFoldingComputerTest.java
@@ -13,12 +13,15 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 
 import com.aptana.editor.common.text.reconciler.IFoldingComputer;
+import com.aptana.editor.json.JSONPlugin;
 import com.aptana.editor.json.parsing.JSONParser;
+import com.aptana.editor.json.preferences.IPreferenceConstants;
 import com.aptana.parsing.IParseState;
 import com.aptana.parsing.ParseState;
 import com.aptana.parsing.ast.IParseNode;
@@ -62,16 +65,16 @@ public class JSONFoldingComputerTest extends TestCase
 		assertEquals(1, positions.size());
 		assertTrue(positions.contains(new Position(0, src.length())));
 	}
-	
+
 	public void testArrayFolding() throws Exception
 	{
-		String src = "{\n" + 
-		"    \"description\": [\n" +
-		"        \"event object\",\n" + 
-		"        \"name\",\n" +
-		"        \"event\"\n" +
-	    "    ]\n" + 
-	    "}";
+		String src = "{\n" + //
+				"    \"description\": [\n" + //
+				"        \"event object\",\n" + //
+				"        \"name\",\n" + //
+				"        \"event\"\n" + //
+				"    ]\n" + //
+				"}"; //
 		folder = new JSONFoldingComputer(null, new Document(src))
 		{
 			protected IParseNode getAST()
@@ -94,5 +97,90 @@ public class JSONFoldingComputerTest extends TestCase
 		assertEquals(2, positions.size());
 		assertTrue(positions.contains(new Position(0, src.length())));
 		assertTrue(positions.contains(new Position(21, 64)));
+	}
+
+	public void testObjectInitiallyFolded() throws Exception
+	{
+		String src = "{\n" + "    \"description\": \"event object\", \n" + "    \"name\": \"event\", \n"
+				+ "    \"type\": \"object\"\n" + "}";
+		folder = new JSONFoldingComputer(null, new Document(src))
+		{
+			protected IParseNode getAST()
+			{
+				IParseState parseState = new ParseState();
+				parseState.setEditState(getDocument().get(), null, 0, 0);
+				try
+				{
+					return new JSONParser().parse(parseState);
+				}
+				catch (Exception e)
+				{
+					fail(e.getMessage());
+				}
+				return null;
+			};
+		};
+
+		// Turn on initially folding objects
+		new InstanceScope().getNode(JSONPlugin.PLUGIN_ID).putBoolean(IPreferenceConstants.INITIALLY_FOLD_OBJECTS, true);
+
+		Map<ProjectionAnnotation, Position> annotations = folder.emitFoldingRegions(true, new NullProgressMonitor());
+		assertTrue(annotations.keySet().iterator().next().isCollapsed());
+
+		// After initial reconcile, don't mark any collapsed
+		annotations = folder.emitFoldingRegions(false, new NullProgressMonitor());
+		assertFalse(annotations.keySet().iterator().next().isCollapsed());
+	}
+
+	public void testArrayInitiallyFolded() throws Exception
+	{
+		String src = "{\n" + //
+				"    \"description\": [\n" + //
+				"        \"event object\",\n" + //
+				"        \"name\",\n" + //
+				"        \"event\"\n" + //
+				"    ]\n" + //
+				"}"; //
+		folder = new JSONFoldingComputer(null, new Document(src))
+		{
+			protected IParseNode getAST()
+			{
+				IParseState parseState = new ParseState();
+				parseState.setEditState(getDocument().get(), null, 0, 0);
+				try
+				{
+					return new JSONParser().parse(parseState);
+				}
+				catch (Exception e)
+				{
+					fail(e.getMessage());
+				}
+				return null;
+			};
+		};
+
+		// Turn on initially folding arrays
+		new InstanceScope().getNode(JSONPlugin.PLUGIN_ID).putBoolean(IPreferenceConstants.INITIALLY_FOLD_ARRAYS, true);
+
+		Map<ProjectionAnnotation, Position> annotations = folder.emitFoldingRegions(true, new NullProgressMonitor());
+		ProjectionAnnotation annotation = getByPosition(annotations, new Position(21, 64));
+		assertTrue(annotation.isCollapsed());
+
+		// After initial reconcile, don't mark any collapsed
+		annotations = folder.emitFoldingRegions(false, new NullProgressMonitor());
+		annotation = getByPosition(annotations, new Position(21, 64));
+		assertFalse(annotation.isCollapsed());
+	}
+
+	private ProjectionAnnotation getByPosition(Map<ProjectionAnnotation, Position> annotations, Position position)
+	{
+		for (Map.Entry<ProjectionAnnotation, Position> entry : annotations.entrySet())
+		{
+			if (entry.getValue().equals(position))
+			{
+				return entry.getKey();
+			}
+		}
+		return null;
 	}
 }
