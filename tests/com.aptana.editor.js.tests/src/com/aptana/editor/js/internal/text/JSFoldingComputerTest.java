d1461d5d366ac9515293f63bbc4b8d42cd17555a
sgtcoolguy <cwilliams@aptana.com>
2011-04-29 11:45:53 -0400

    Implement #1579 - Add initial folding options for JS Editor
    Prefs, impl, unit tests. Need to add more unit tests for folding objects and arrays.

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/internal/text/JSFoldingComputerTest.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/internal/text/JSFoldingComputerTest.java
@@ -13,12 +13,15 @@ import java.util.Map;
 import junit.framework.TestCase;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.projection.ProjectionAnnotation;
 
 import com.aptana.editor.common.text.reconciler.IFoldingComputer;
+import com.aptana.editor.js.JSPlugin;
 import com.aptana.editor.js.parsing.JSParser;
+import com.aptana.editor.js.preferences.IPreferenceConstants;
 import com.aptana.parsing.IParseState;
 import com.aptana.parsing.ParseState;
 import com.aptana.parsing.ast.IParseNode;
@@ -89,10 +92,15 @@ public class JSFoldingComputerTest extends TestCase
 
 	public void testJSFunctionFolding() throws Exception
 	{
-		String src = "function listItems(itemList) \n" + "{\n" + "   document.write(\"<UL>\\n\")\n"
-				+ "   for (i = 0;i < itemList.length;i++)\n" + "   {\n"
-				+ "      document.write(\"<LI>\" + itemList[i] + \"\\n\")\n" + "   }\n"
-				+ "   document.write(\"</UL>\\n\") \n" + "} ";
+		String src = "function listItems(itemList) \n" + //
+				"{\n" + //
+				"   document.write(\"<UL>\\n\")\n" + //
+				"   for (i = 0;i < itemList.length;i++)\n" + //
+				"   {\n" + //
+				"      document.write(\"<LI>\" + itemList[i] + \"\\n\")\n" + //
+				"   }\n" + //
+				"   document.write(\"</UL>\\n\") \n" + //
+				"} "; //
 		folder = new JSFoldingComputer(null, new Document(src))
 		{
 			protected IParseNode getAST()
@@ -115,5 +123,71 @@ public class JSFoldingComputerTest extends TestCase
 		assertEquals(2, positions.size()); // FIXME We're getting one too many here. Probably need to check if one already exists on this line!
 		assertTrue(positions.contains(new Position(0, src.length()))); // eats whole line at end
 		assertTrue(positions.contains(new Position(63, 96)));
+	}
+
+	public void testJSCommentInitiallyFolded() throws Exception
+	{
+		String src = "/*\n * This is a comment.\n */\n";
+		folder = new JSFoldingComputer(null, new Document(src))
+		{
+			protected IParseNode getAST()
+			{
+				IParseState parseState = new ParseState();
+				parseState.setEditState(getDocument().get(), null, 0, 0);
+				try
+				{
+					return new JSParser().parse(parseState);
+				}
+				catch (Exception e)
+				{
+					fail(e.getMessage());
+				}
+				return null;
+			};
+		};
+		// Turn on initially folding comments
+		new InstanceScope().getNode(JSPlugin.PLUGIN_ID).putBoolean(IPreferenceConstants.INITIALLY_FOLD_COMMENTS, true);
+
+		Map<ProjectionAnnotation, Position> annotations = folder.emitFoldingRegions(true, new NullProgressMonitor());
+		assertTrue(annotations.keySet().iterator().next().isCollapsed());
+
+		// After initial reconcile, don't mark any collapsed
+		annotations = folder.emitFoldingRegions(false, new NullProgressMonitor());
+		assertFalse(annotations.keySet().iterator().next().isCollapsed());
+	}
+
+	public void testJSFunctionInitiallyFolded() throws Exception
+	{
+		String src = "function listItems(itemList) \n" + //
+				"{\n" + //
+				"   document.write(\"<UL>\\n\")\n" + //
+				"   document.write(\"</UL>\\n\") \n" + //
+				"} "; //
+		folder = new JSFoldingComputer(null, new Document(src))
+		{
+			protected IParseNode getAST()
+			{
+				IParseState parseState = new ParseState();
+				parseState.setEditState(getDocument().get(), null, 0, 0);
+				try
+				{
+					return new JSParser().parse(parseState);
+				}
+				catch (Exception e)
+				{
+					fail(e.getMessage());
+				}
+				return null;
+			};
+		};
+		// Turn on initially folding functions
+		new InstanceScope().getNode(JSPlugin.PLUGIN_ID).putBoolean(IPreferenceConstants.INITIALLY_FOLD_FUNCTIONS, true);
+
+		Map<ProjectionAnnotation, Position> annotations = folder.emitFoldingRegions(true, new NullProgressMonitor());
+		assertTrue(annotations.keySet().iterator().next().isCollapsed());
+
+		// After initial reconcile, don't mark any collapsed
+		annotations = folder.emitFoldingRegions(false, new NullProgressMonitor());
+		assertFalse(annotations.keySet().iterator().next().isCollapsed());
 	}
 }
