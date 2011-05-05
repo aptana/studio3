6447ef033ebdcec8670c9b9002da5b4ad3d5c9cc
sgtcoolguy <cwilliams@aptana.com>
2011-05-05 11:55:20 -0400

    Implement #1508 - Create Dreamweaver color theme

--- plugins/com.aptana.theme/src/com/aptana/theme/TextmateImporter.java
+++ plugins/com.aptana.theme/src/com/aptana/theme/TextmateImporter.java
@@ -9,6 +9,7 @@ package com.aptana.theme;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -138,5 +139,25 @@ public class TextmateImporter
 	private static Map<String, Object> parse(File file) throws IOException
 	{
 		return PListParserFactory.parse(file);
+	}
+
+	public static void main(String[] args)
+	{
+		File input = new File(args[0]);
+		try
+		{
+			Properties p = new TextmateImporter().convertToProperties(input);
+			p.store(new FileOutputStream(new File(args[1])), "");
+		}
+		catch (FileNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
