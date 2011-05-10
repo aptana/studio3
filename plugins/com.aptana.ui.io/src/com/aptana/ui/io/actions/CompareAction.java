709384d8087fb9a364e6c11eec78e05e91a7ba20
mxia <mxia@aptana.com>
2011-05-10 15:56:02 -0700

    Fixed #753: added ability to compare two files with each other in local filesystem or a remote site or between them.

---
+++ plugins/com.aptana.ui.io/src/com/aptana/ui/io/actions/CompareAction.java
@@ -0,0 +1,74 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.ui.io.actions;
+
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.internal.CompareEditor;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+
+import com.aptana.ui.io.compare.FileStoreCompareEditorInput;
+
+@SuppressWarnings("restriction")
+public class CompareAction implements IObjectActionDelegate
+{
+
+	private IWorkbenchPage fWorkbenchPage;
+	private IStructuredSelection fSelection;
+	private FileStoreCompareEditorInput fEditorInput;
+
+	public CompareAction()
+	{
+	}
+
+	public void run(IAction action)
+	{
+		if (fSelection == null)
+		{
+			return;
+		}
+		Object[] elements = fSelection.toArray();
+		if (elements.length != 2)
+		{
+			return;
+		}
+		IFileStore left = (IFileStore) ((IAdaptable) elements[0]).getAdapter(IFileStore.class);
+		IFileStore right = (IFileStore) ((IAdaptable) elements[1]).getAdapter(IFileStore.class);
+
+		CompareConfiguration cc = new CompareConfiguration();
+		// buffered merge mode: don't ask for confirmation when switching between modified resources
+		cc.setProperty(CompareEditor.CONFIRM_SAVE_PROPERTY, new Boolean(false));
+
+		fEditorInput = new FileStoreCompareEditorInput(cc);
+		fEditorInput.setLeftFileStore(left);
+		fEditorInput.setRightFileStore(right);
+		fEditorInput.initializeCompareConfiguration();
+		CompareUI.openCompareEditorOnPage(fEditorInput, fWorkbenchPage);
+	}
+
+	public void selectionChanged(IAction action, ISelection selection)
+	{
+		fSelection = null;
+		if (selection instanceof IStructuredSelection)
+		{
+			fSelection = (IStructuredSelection) selection;
+		}
+	}
+
+	public void setActivePart(IAction action, IWorkbenchPart targetPart)
+	{
+		fWorkbenchPage = targetPart.getSite().getPage();
+	}
+}
