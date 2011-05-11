41bddadbef170011f3bcf005daaa2bbe658c3661
mxia <mxia@aptana.com>
2011-05-11 13:58:50 -0700

    Incorporated Chris' feedback to use the new menu extension point for Compare With->Each Other.

---
+++ plugins/com.aptana.ui.io/src/com/aptana/ui/io/compare/CompareWithEachOtherHandler.java
@@ -0,0 +1,54 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.ui.io.compare;
+
+import org.eclipse.compare.CompareConfiguration;
+import org.eclipse.compare.CompareUI;
+import org.eclipse.compare.internal.CompareEditor;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.aptana.ide.ui.io.Utils;
+
+@SuppressWarnings("restriction")
+public class CompareWithEachOtherHandler extends AbstractHandler
+{
+
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+		ISelection selection = HandlerUtil.getCurrentSelection(event);
+		if (selection.isEmpty() || !(selection instanceof IStructuredSelection))
+		{
+			return null;
+		}
+		Object[] elements = ((IStructuredSelection) selection).toArray();
+		if (elements.length != 2)
+		{
+			return null;
+		}
+		IFileStore left = Utils.getFileStore(elements[0]);
+		IFileStore right = Utils.getFileStore(elements[1]);
+
+		CompareConfiguration cc = new CompareConfiguration();
+		// buffered merge mode: don't ask for confirmation when switching between modified resources
+		cc.setProperty(CompareEditor.CONFIRM_SAVE_PROPERTY, new Boolean(false));
+
+		FileStoreCompareEditorInput editorInput = new FileStoreCompareEditorInput(cc);
+		editorInput.setLeftFileStore(left);
+		editorInput.setRightFileStore(right);
+		editorInput.initializeCompareConfiguration();
+		CompareUI.openCompareEditorOnPage(editorInput, HandlerUtil.getActiveWorkbenchWindow(event).getActivePage());
+
+		return null;
+	}
+}
