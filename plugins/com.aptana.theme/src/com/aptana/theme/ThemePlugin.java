d09de473035f0a0a6d8a669e211178ecce62eb49
sgtcoolguy <cwilliams@aptana.com>
2011-05-17 13:55:57 -0400

    Fix for #2334 - Background only for Editor View - Use the ControlThemer stuff to apply themes to the Samples or Bundles views. - Respect invasive theme preference in our control themers to not apply our colors if off

--- plugins/com.aptana.theme/src/com/aptana/theme/ThemePlugin.java
+++ plugins/com.aptana.theme/src/com/aptana/theme/ThemePlugin.java
@@ -8,13 +8,18 @@
 package com.aptana.theme;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 import com.aptana.theme.internal.ControlThemerFactory;
 import com.aptana.theme.internal.InvasiveThemeHijacker;
 import com.aptana.theme.internal.ThemeManager;
+import com.aptana.theme.preferences.IPreferenceConstants;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -33,6 +38,10 @@ public class ThemePlugin extends AbstractUIPlugin
 
 	private IControlThemerFactory fControlThemerFactory;
 
+	// Store latest value of whether invasive theme is on so we don't need to query platform prefs every time.
+	private Boolean fInvasiveThemesEnabled;
+	private IPreferenceChangeListener fThemeChangeListener;
+
 	/**
 	 * The constructor
 	 */
@@ -49,6 +58,22 @@ public class ThemePlugin extends AbstractUIPlugin
 		super.start(context);
 		plugin = this;
 
+		// Listen to when invasive themes is turned on or off and cache the value for perf sake
+		fThemeChangeListener = new IPreferenceChangeListener()
+		{
+			public void preferenceChange(PreferenceChangeEvent event)
+			{
+				if (event.getKey().equals(IPreferenceConstants.INVASIVE_THEMES))
+				{
+					fInvasiveThemesEnabled = Platform.getPreferencesService().getBoolean(ThemePlugin.PLUGIN_ID,
+							IPreferenceConstants.INVASIVE_THEMES, false, null);
+				}
+			}
+		};
+		new InstanceScope().getNode(ThemePlugin.PLUGIN_ID).addPreferenceChangeListener(fThemeChangeListener);
+		fInvasiveThemesEnabled = Platform.getPreferencesService().getBoolean(ThemePlugin.PLUGIN_ID,
+				IPreferenceConstants.INVASIVE_THEMES, false, null);
+
 		themeHijacker = new InvasiveThemeHijacker();
 		themeHijacker.apply();
 	}
@@ -61,6 +86,11 @@ public class ThemePlugin extends AbstractUIPlugin
 	{
 		try
 		{
+			if (fThemeChangeListener != null)
+			{
+				new InstanceScope().getNode(ThemePlugin.PLUGIN_ID).removePreferenceChangeListener(fThemeChangeListener);
+			}
+
 			if (themeHijacker != null)
 			{
 				themeHijacker.dispose();
@@ -138,5 +168,10 @@ public class ThemePlugin extends AbstractUIPlugin
 			fControlThemerFactory = new ControlThemerFactory();
 		}
 		return fControlThemerFactory;
+	}
+
+	public static synchronized boolean invasiveThemesEnabled()
+	{
+		return getDefault().fInvasiveThemesEnabled;
 	}
 }
