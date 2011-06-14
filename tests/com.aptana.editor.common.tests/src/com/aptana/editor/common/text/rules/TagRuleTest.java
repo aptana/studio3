efec0713a77121e20dbe597f3c3f85eeb1fa63a5
Max Stepanov <mstepanov@aptana.com>
2011-06-13 19:01:03 -0700

    [APSTUD-2280] remove test which makes no sense anymore

--- tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/text/rules/TagRuleTest.java
+++ tests/com.aptana.editor.common.tests/src/com/aptana/editor/common/text/rules/TagRuleTest.java
@@ -15,8 +15,6 @@ import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.RuleBasedScanner;
 import org.eclipse.jface.text.rules.Token;
 
-import com.aptana.editor.common.text.rules.TagRule;
-
 public class TagRuleTest extends TestCase
 {
 
@@ -122,18 +120,6 @@ public class TagRuleTest extends TestCase
 		TagRule rule = new TagRule("chris", successToken);
 		RuleBasedScanner scanner = new RuleBasedScanner();
 		IDocument document = new Document("<chris attr='>\nvalue\\'>'>");
-		scanner.setRange(document, 0, document.getLength());
-		assertEquals(successToken, rule.evaluate(scanner));
-		assertEquals(0, scanner.getTokenOffset());
-		assertEquals(document.getLength(), scanner.getTokenLength());
-	}
-
-	public void testTagWithEmbeddedTag()
-	{
-		IToken successToken = new Token("name");
-		TagRule rule = new TagRule("chris", successToken);
-		RuleBasedScanner scanner = new RuleBasedScanner();
-		IDocument document = new Document("<chris <? echo $class1;?>>");
 		scanner.setRange(document, 0, document.getLength());
 		assertEquals(successToken, rule.evaluate(scanner));
 		assertEquals(0, scanner.getTokenOffset());
