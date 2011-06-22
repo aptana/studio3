ec80d89e73d5af95666127153a19d41cbe16ed97
mxia <mxia@aptana.com>
2011-06-22 15:18:50 -0700

    Fixed APSTUD-2789: made the default name for new theme nicer.

--- plugins/com.aptana.theme/src/com/aptana/theme/preferences/Messages.java
+++ plugins/com.aptana.theme/src/com/aptana/theme/preferences/Messages.java
@@ -35,6 +35,7 @@ public class Messages extends NLS
 	public static String ThemePreferencePage_ItalicButtonLabel;
 	public static String ThemePreferencePage_LineHighlightLabel;
 	public static String ThemePreferencePage_NewThemeDefaultName;
+	public static String ThemePreferencePage_NewThemeDefaultName_2;
 	public static String ThemePreferencePage_NewThemeMsg;
 	public static String ThemePreferencePage_NewThemeTitle;
 	public static String ThemePreferencePage_RemoveTokenLabel;
