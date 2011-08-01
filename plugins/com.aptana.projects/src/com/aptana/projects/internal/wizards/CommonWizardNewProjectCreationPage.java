96a254a5ec14d231e24a6628e5dd0b998b6c4bc6
Allen Yeung <ayeung@aptana.com>
2011-08-01 11:51:06 -0700

    Squashed commit of the following:
        Code Review changes
    
        Fix APSTUD-2252: Warn user if they are creating project in an area with pre-existing files

--- plugins/com.aptana.projects/src/com/aptana/projects/internal/wizards/CommonWizardNewProjectCreationPage.java
+++ plugins/com.aptana.projects/src/com/aptana/projects/internal/wizards/CommonWizardNewProjectCreationPage.java
@@ -7,7 +7,20 @@
  */
 package com.aptana.projects.internal.wizards;
 
+import java.io.File;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+import com.aptana.core.util.StringUtil;
+import com.aptana.ui.util.SWTUtils;
+import com.aptana.ui.util.UIUtils;
 
 /**
  * @author Shalom Gibly <sgibly@appcelerator.com>
@@ -15,6 +28,8 @@ import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
 public class CommonWizardNewProjectCreationPage extends WizardNewProjectCreationPage implements
 		IWizardProjectCreationPage
 {
+
+	private Label warningLabel;
 
 	/**
 	 * Constructs a new common new project creation page.
@@ -43,4 +58,73 @@ public class CommonWizardNewProjectCreationPage extends WizardNewProjectCreation
 	{
 		return null;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.dialogs.WizardNewProjectCreationPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	@Override
+	public void createControl(Composite parent)
+	{
+		super.createControl(parent);
+		createWarningArea();
+	}
+
+	private void createWarningArea()
+	{
+		Composite control = (Composite) getControl();
+		final Font font = new Font(control.getDisplay(), SWTUtils.italicizedFont(getFont()));
+
+		warningLabel = new Label(control, SWT.WRAP);
+		warningLabel.setLayoutData(GridDataFactory.fillDefaults().align(SWT.FILL, SWT.BEGINNING).grab(true, false)
+				.create());
+		warningLabel.setFont(font);
+		warningLabel.setForeground(UIUtils.getDisplay().getSystemColor(SWT.COLOR_RED));
+
+		control.addDisposeListener(new DisposeListener()
+		{
+			public void widgetDisposed(DisposeEvent e)
+			{
+				if (font != null && !font.isDisposed())
+				{
+					font.dispose();
+				}
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.dialogs.WizardNewProjectCreationPage#validatePage()
+	 */
+	@Override
+	protected boolean validatePage()
+	{
+		// Check whether the given location already contains files
+		boolean isValid = super.validatePage();
+
+		if (warningLabel != null)
+		{
+			warningLabel.setText(StringUtil.EMPTY);
+		}
+
+		if (isValid)
+		{
+			File locationFile = getLocationPath().toFile();
+
+			if (!useDefaults() && locationFile.exists())
+			{
+				String[] files = locationFile.list();
+				if (files != null && files.length > 0)
+				{
+					warningLabel
+							.setText(Messages.CommonWizardNewProjectCreationPage_location_has_existing_content_warning);
+					warningLabel.getParent().layout(true);
+				}
+			}
+		}
+
+		return isValid;
+	}
+
 }
