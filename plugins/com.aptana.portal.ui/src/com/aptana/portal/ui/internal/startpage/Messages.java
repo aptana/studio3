9681e13157d9e1f85302092e9396def2916f85f8
Shalom Gibly <sgibly@aptana.com>
2011-08-01 11:12:40 -0700

    APSTUD-3134 - Add an Aptana Studio Start Page button to the toolbar.

--- plugins/com.aptana.portal.ui/src/com/aptana/portal/ui/internal/startpage/Messages.java
+++ plugins/com.aptana.portal.ui/src/com/aptana/portal/ui/internal/startpage/Messages.java
@@ -15,6 +15,8 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS
 {
 	private static final String BUNDLE_NAME = "com.aptana.portal.ui.internal.startpage.messages"; //$NON-NLS-1$
+	public static String StartPageBrowserEditor_startPageTitle;
+	public static String StartPageBrowserEditor_startPageTooltip;
 	public static String StartPageStartup_launchingStartPage;
 	static
 	{
