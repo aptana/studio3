b2676f8c0423e770de27cc60c7ceb9ee1a7fc7a4
Ingo Muschenetz <ingo@muschenetz.com>
2011-08-03 09:12:14 -0700

    fir for APSTUD-3191. We started sorting proposals with templates coming after regular items, which led to usability issues with some common templates. Re-jiggered sorting to move templates back up higher in the list.

--- plugins/com.aptana.editor.common/src/com/aptana/editor/common/CommonContentAssistProcessor.java
+++ plugins/com.aptana.editor.common/src/com/aptana/editor/common/CommonContentAssistProcessor.java
@@ -218,8 +218,7 @@ public class CommonContentAssistProcessor implements IContentAssistProcessor, IC
 	{
 		// Sort by relevance first, descending, and then alphabetically, ascending
 		Arrays.sort(proposals, CompletionProposalComparator.decending(CompletionProposalComparator.getComparator(
-				CompletionProposalComparator.RelevanceSort, CompletionProposalComparator.TemplateSort,
-				CompletionProposalComparator.NameSort)));
+				CompletionProposalComparator.RelevanceSort, CompletionProposalComparator.NameSort)));
 	}
 
 	/**
