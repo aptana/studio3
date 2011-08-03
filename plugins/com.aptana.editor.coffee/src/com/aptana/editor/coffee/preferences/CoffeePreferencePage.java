998185b1e1df37cde14d1645c36d8268fb06017b
Chris Williams <cwilliams@appcelerator.com>
2011-08-03 14:01:33 -0400

    Implement APSTUD-3186 - Make preferences for Coffeescript editor available

---
+++ plugins/com.aptana.editor.coffee/src/com/aptana/editor/coffee/preferences/CoffeePreferencePage.java
@@ -0,0 +1,42 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.editor.coffee.preferences;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import com.aptana.core.util.EclipseUtil;
+import com.aptana.editor.coffee.CoffeeScriptEditorPlugin;
+import com.aptana.editor.common.preferences.CommonEditorPreferencePage;
+
+public class CoffeePreferencePage extends CommonEditorPreferencePage
+{
+
+	/**
+	 * CoffeePreferencePage
+	 */
+	public CoffeePreferencePage()
+	{
+		super();
+		setDescription(Messages.CoffeePreferencePage_Coffee_Page_Title);
+		setPreferenceStore(CoffeeScriptEditorPlugin.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	protected IEclipsePreferences getPluginPreferenceStore()
+	{
+		return EclipseUtil.instanceScope().getNode(CoffeeScriptEditorPlugin.PLUGIN_ID);
+	}
+
+	@Override
+	protected IPreferenceStore getChainedEditorPreferenceStore()
+	{
+		return CoffeeScriptEditorPlugin.getChainedPreferenceStore();
+	}
+
+}
