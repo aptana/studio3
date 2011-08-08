b211a4308e280fbfb66274cdf843c16f87e249b0
Max Stepanov <mstepanov@aptana.com>
2011-08-08 11:00:15 -0700

    FirefoxUtilTest: cleanup created fiels/folders after a test completes

--- tests/com.aptana.core.tests/src/com/aptana/core/util/FirefoxUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/FirefoxUtilTest.java
@@ -35,64 +35,88 @@ public class FirefoxUtilTest extends TestCase {
 
 	public void testReadProfiles() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		File file = new File(dir, "profiles.ini");
-		assertTrue(file.createNewFile());
-		OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
-		w.write(PROFILES_INI);
-		w.close();
+		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			File file = new File(dir, "profiles.ini");
+			assertTrue(file.createNewFile());
+			OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
+			w.write(PROFILES_INI);
+			w.close();
 
-		File[] profiles = FirefoxUtil.readProfiles(dir);
-		assertNotNull(profiles);
-		assertEquals(2, profiles.length);
-		assertEquals(new File(dir, "Profiles/0sw283qs.default"), profiles[0]);
-		assertEquals(new File("/tmp/Profiles/0sw283qs.additional"), profiles[1]);
+			File[] profiles = FirefoxUtil.readProfiles(dir);
+			assertNotNull(profiles);
+			assertEquals(2, profiles.length);
+			assertEquals(new File(dir, "Profiles/0sw283qs.default"), profiles[0]);
+			assertEquals(new File("/tmp/Profiles/0sw283qs.additional"), profiles[1]);
+		}
+		finally {
+			FileUtil.deleteRecursively(dir);
+		}
+
 	}
 
 	public void testFindDefaultProfileLocation() throws IOException {
 		File profileDir = File.createTempFile(getClass().getSimpleName(), "profile");
-		assertTrue(profileDir.delete());
-		assertTrue(profileDir.mkdir());
-		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		File file = new File(dir, "profiles.ini");
-		assertTrue(file.createNewFile());
-		OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
-		w.write(PROFILES_INI + "[Profile2]\nName=real\nIsRelative=0\nPath=" + profileDir.getAbsolutePath() + "\n");
-		w.close();
+		try {
+			assertTrue(profileDir.delete());
+			assertTrue(profileDir.mkdir());
+			File dir = File.createTempFile(getClass().getSimpleName(), "temp");
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			File file = new File(dir, "profiles.ini");
+			assertTrue(file.createNewFile());
+			OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
+			w.write(PROFILES_INI + "[Profile2]\nName=real\nIsRelative=0\nPath=" + profileDir.getAbsolutePath() + "\n");
+			w.close();
 
-		assertNotNull(FirefoxUtil.findDefaultProfileLocation(new String[] { dir.getAbsolutePath() }));
+			assertNotNull(FirefoxUtil.findDefaultProfileLocation(new String[] { dir.getAbsolutePath() }));
+		}
+		finally {
+			FileUtil.deleteRecursively(profileDir);
+		}
+
 	}
 
 	public void testFindDefaultProfileLocationNoProfiles() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		File file = new File(dir, "profiles.ini");
-		assertTrue(file.createNewFile());
-		OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
-		w.write("\n");
-		w.close();
+		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			File file = new File(dir, "profiles.ini");
+			assertTrue(file.createNewFile());
+			OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
+			w.write("\n");
+			w.close();
 
-		assertNull(FirefoxUtil.findDefaultProfileLocation(new String[] { dir.getAbsolutePath() }));
+			assertNull(FirefoxUtil.findDefaultProfileLocation(new String[] { dir.getAbsolutePath() }));
+		}
+		finally {
+			FileUtil.deleteRecursively(dir);
+		}
+
 	}
 
 	public void testGetExtensionVersion() throws IOException {
 		File profileDir = File.createTempFile(getClass().getSimpleName(), "profile");
-		assertTrue(profileDir.delete());
-		assertTrue(profileDir.mkdir());
-		File file = new File(profileDir, "extensions");
-		assertTrue(file.mkdir());
-		file = new File(file, "mytest.ext");
-		assertTrue(file.mkdir());
-		file = new File(file, "install.rdf");
-		OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
-		w.write(INSTALL_RDF);
-		w.close();
-		assertEquals("1.7.3",
-				FirefoxUtil.getExtensionVersion("mytest.ext", Path.fromOSString(profileDir.getAbsolutePath())));
+		try {
+			assertTrue(profileDir.delete());
+			assertTrue(profileDir.mkdir());
+			File file = new File(profileDir, "extensions");
+			assertTrue(file.mkdir());
+			file = new File(file, "mytest.ext");
+			assertTrue(file.mkdir());
+			file = new File(file, "install.rdf");
+			OutputStreamWriter w = new OutputStreamWriter(new FileOutputStream(file));
+			w.write(INSTALL_RDF);
+			w.close();
+			assertEquals("1.7.3",
+					FirefoxUtil.getExtensionVersion("mytest.ext", Path.fromOSString(profileDir.getAbsolutePath())));
+		}
+		finally {
+			FileUtil.deleteRecursively(profileDir);
+		}
+
 	}
 
 	public void testGetExtensionVersionXPI() throws IOException {
@@ -104,11 +128,11 @@ public class FirefoxUtilTest extends TestCase {
 
 	public void testInstallLinkedExtension() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
-				Path.fromPortableString("/resources/extensions/test.xpi"), null);
 		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
+					Path.fromPortableString("/resources/extensions/test.xpi"), null);
 			assertTrue(FirefoxUtil.installLinkedExtension(url, "test", dir));
 			assertTrue(FirefoxUtil.installLinkedExtension(url, "test", dir));
 		}
@@ -119,11 +143,11 @@ public class FirefoxUtilTest extends TestCase {
 
 	public void testInstallExtensionNoFile() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
-				Path.fromPortableString("/resources/extensions/test.xpi"), null);
 		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
+					Path.fromPortableString("/resources/extensions/test.xpi"), null);
 			assertFalse(FirefoxUtil.installLinkedExtension(new URL(url, "test1.xpi"), "test", dir));
 		}
 		finally {
@@ -133,11 +157,11 @@ public class FirefoxUtilTest extends TestCase {
 
 	public void testInstallExtensionExistingFile() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
-				Path.fromPortableString("/resources/extensions/test.xpi"), null);
 		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
+					Path.fromPortableString("/resources/extensions/test.xpi"), null);
 			assertTrue(new File(dir, "test").createNewFile());
 			assertTrue(FirefoxUtil.installExtension(url, "test", dir));
 		}
@@ -148,11 +172,11 @@ public class FirefoxUtilTest extends TestCase {
 
 	public void testInstallExtensionCorruptedUnzip() throws IOException {
 		File dir = File.createTempFile(getClass().getSimpleName(), "temp");
-		assertTrue(dir.delete());
-		assertTrue(dir.mkdir());
-		URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
-				Path.fromPortableString("/resources/extensions/test_corrupted.xpi"), null);
 		try {
+			assertTrue(dir.delete());
+			assertTrue(dir.mkdir());
+			URL url = FileLocator.find(Platform.getBundle("com.aptana.core.tests"),
+					Path.fromPortableString("/resources/extensions/test_corrupted.xpi"), null);
 			assertFalse(FirefoxUtil.installExtension(url, "test", dir));
 		}
 		finally {
