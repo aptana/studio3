094da4f9d0fbc15a46e8047393b1389a0b65914b
Max Stepanov <mstepanov@aptana.com>
2011-08-08 14:44:19 -0700

    format

--- tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
@@ -20,19 +20,23 @@ import com.aptana.core.util.PlatformUtil.ProcessItem;
  * @author Max Stepanov
  */
 @SuppressWarnings("nls")
-public class PlatformUtilTest extends TestCase {
+public class PlatformUtilTest extends TestCase
+{
 
-	public void testGetRunningProcesses() {
+	public void testGetRunningProcesses()
+	{
 		ProcessItem[] processes = PlatformUtil.getRunningProcesses();
 		assertTrue(processes.length > 0);
-		for (ProcessItem i : processes) {
+		for (ProcessItem i : processes)
+		{
 			assertTrue(i.getPid() > 0);
 			assertTrue(i.getExecutableName().trim().length() > 0);
 			assertNotNull(i.toString());
 		}
 	}
 
-	public void testGetRunningChildProcesses() throws IOException {
+	public void testGetRunningChildProcesses() throws IOException
+	{
 		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : "sleep 5s";
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
@@ -41,15 +45,18 @@ public class PlatformUtilTest extends TestCase {
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		boolean passed = false;
-		for (ProcessItem i : processes) {
-			if (passed = i.getExecutableName().toLowerCase().endsWith(cmd)) {
+		for (ProcessItem i : processes)
+		{
+			if (passed = i.getExecutableName().toLowerCase().endsWith(cmd))
+			{
 				break;
 			}
 		}
 		assertTrue(passed);
 	}
 
-	public void testKillProcesses() throws IOException {
+	public void testKillProcesses() throws IOException
+	{
 		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : "sleep 5s";
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
@@ -58,8 +65,10 @@ public class PlatformUtilTest extends TestCase {
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		int pid = 0;
-		for (ProcessItem i : processes) {
-			if (i.getExecutableName().toLowerCase().endsWith(cmd)) {
+		for (ProcessItem i : processes)
+		{
+			if (i.getExecutableName().toLowerCase().endsWith(cmd))
+			{
 				pid = i.getPid();
 				break;
 			}
@@ -67,30 +76,35 @@ public class PlatformUtilTest extends TestCase {
 		assertTrue(pid > 0);
 		PlatformUtil.killProcess(pid);
 		processes = PlatformUtil.getRunningChildProcesses();
-		for (ProcessItem i : processes) {
+		for (ProcessItem i : processes)
+		{
 			assertNotSame(pid, i.getPid());
 		}
 	}
 
-	public void testExpandEnvironmentStrings() {
+	public void testExpandEnvironmentStrings()
+	{
 		assertEquals("abc/d", PlatformUtil.expandEnvironmentStrings("abc/d"));
 		expandAndCompareEnvironmentStrings("~", "/test");
 		expandAndCompareEnvironmentStrings(PlatformUtil.HOME_DIRECTORY, "/test");
-		if (Platform.OS_WIN32.equals(Platform.getOS())) {
+		if (Platform.OS_WIN32.equals(Platform.getOS()))
+		{
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.COMMON_APPDATA, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.LOCAL_APPDATA, "/test");
 		}
-		else if (Platform.OS_MACOSX.equals(Platform.getOS())) {
+		else if (Platform.OS_MACOSX.equals(Platform.getOS()))
+		{
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DOCUMENTS_DIRECTORY, "/test");
 		}
 	}
 
-	private void expandAndCompareEnvironmentStrings(String token, String path) {
-		String expanded = PlatformUtil.expandEnvironmentStrings(token+path);
-		assertNotSame(token+path, expanded);
+	private void expandAndCompareEnvironmentStrings(String token, String path)
+	{
+		String expanded = PlatformUtil.expandEnvironmentStrings(token + path);
+		assertNotSame(token + path, expanded);
 		assertTrue(expanded.endsWith(path));
 	}
 
--- tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
@@ -17,45 +17,55 @@ import junit.framework.TestCase;
 /**
  * @author Max Stepanov
  */
-public class SocketUtilTest extends TestCase {
+public class SocketUtilTest extends TestCase
+{
 
-	public void testFindFreePort() {
+	public void testFindFreePort()
+	{
 		assertTrue(SocketUtil.findFreePort(null) > 0);
 	}
 
-	public void testFindFreePortInRange() {
+	public void testFindFreePortInRange()
+	{
 		int port = SocketUtil.findFreePort(null, 8000, 8010);
 		assertTrue(port >= 8000 && port <= 8010);
 	}
 
-	public void testFindFreePortInInvalidRange() {
+	public void testFindFreePortInInvalidRange()
+	{
 		int port = SocketUtil.findFreePort(null, 1, 10);
 		assertEquals(-1, port);
 	}
 
-	public void testFindFreePortInTakenRange() throws IOException {
+	public void testFindFreePortInTakenRange() throws IOException
+	{
 		ServerSocket socket = new ServerSocket(9000);
 		int port = SocketUtil.findFreePort(null, 9000, 9000);
 		socket.close();
 		assertEquals(-1, port);
 	}
 
-	public void testGetLocalAddresses() {
+	public void testGetLocalAddresses()
+	{
 		InetAddress[] addresses = SocketUtil.getLocalAddresses();
 		assertTrue(addresses.length > 0);
 		boolean passed = false;
-		for (InetAddress i : addresses) {
-			if (passed = "127.0.0.1".equals(i.getHostAddress())) {
+		for (InetAddress i : addresses)
+		{
+			if (passed = "127.0.0.1".equals(i.getHostAddress()))
+			{
 				break;
 			}
 		}
 		assertTrue(passed);
 	}
 
-	public void testGetNonLoopbackLocalAdresses() {
+	public void testGetNonLoopbackLocalAdresses()
+	{
 		InetAddress[] addresses = SocketUtil.getNonLoopbackLocalAdresses();
 		assertTrue(addresses.length > 0);
-		for (InetAddress i : addresses) {
+		for (InetAddress i : addresses)
+		{
 			assertEquals(false, i.isLoopbackAddress());
 			assertNotSame("127.0.0.1", i.getHostAddress());
 		}
