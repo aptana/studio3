76238362ab2a9147739377f49cdbd75f31219736
Max Stepanov <mstepanov@aptana.com>
2011-08-09 15:41:02 -0700

    attempt to fix PlatformUtil test on linux

--- tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
@@ -20,24 +20,20 @@ import com.aptana.core.util.PlatformUtil.ProcessItem;
  * @author Max Stepanov
  */
 @SuppressWarnings("nls")
-public class PlatformUtilTest extends TestCase
-{
+public class PlatformUtilTest extends TestCase {
 
-	public void testGetRunningProcesses()
-	{
+	public void testGetRunningProcesses() {
 		ProcessItem[] processes = PlatformUtil.getRunningProcesses();
 		assertTrue(processes.length > 0);
-		for (ProcessItem i : processes)
-		{
+		for (ProcessItem i : processes) {
 			assertTrue(i.getPid() > 0);
 			assertTrue(i.getExecutableName().trim().length() > 0);
 			assertNotNull(i.toString());
 		}
 	}
 
-	public void testGetRunningChildProcesses() throws IOException
-	{
-		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : "sleep 5s";
+	public void testGetRunningChildProcesses() throws IOException {
+		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform.getOS()) ? "sleep 5s" : "sh -c sleep 5s");
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
 		ProcessBuilder processBuilder = new ProcessBuilder(command);
@@ -45,19 +41,16 @@ public class PlatformUtilTest extends TestCase
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		boolean passed = false;
-		for (ProcessItem i : processes)
-		{
-			if (passed = i.getExecutableName().toLowerCase().endsWith(cmd))
-			{
+		for (ProcessItem i : processes) {
+			if (passed = i.getExecutableName().toLowerCase().endsWith(cmd)) {
 				break;
 			}
 		}
 		assertTrue(passed);
 	}
 
-	public void testKillProcesses() throws IOException
-	{
-		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : "sleep 5s";
+	public void testKillProcesses() throws IOException {
+		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform.getOS()) ? "sleep 5s" : "sh -c sleep 5s");
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
 		ProcessBuilder processBuilder = new ProcessBuilder(command);
@@ -65,10 +58,8 @@ public class PlatformUtilTest extends TestCase
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		int pid = 0;
-		for (ProcessItem i : processes)
-		{
-			if (i.getExecutableName().toLowerCase().endsWith(cmd))
-			{
+		for (ProcessItem i : processes) {
+			if (i.getExecutableName().toLowerCase().endsWith(cmd)) {
 				pid = i.getPid();
 				break;
 			}
@@ -76,33 +67,27 @@ public class PlatformUtilTest extends TestCase
 		assertTrue(pid > 0);
 		PlatformUtil.killProcess(pid);
 		processes = PlatformUtil.getRunningChildProcesses();
-		for (ProcessItem i : processes)
-		{
+		for (ProcessItem i : processes) {
 			assertNotSame(pid, i.getPid());
 		}
 	}
 
-	public void testExpandEnvironmentStrings()
-	{
+	public void testExpandEnvironmentStrings() {
 		assertEquals("abc/d", PlatformUtil.expandEnvironmentStrings("abc/d"));
 		expandAndCompareEnvironmentStrings("~", "/test");
 		expandAndCompareEnvironmentStrings(PlatformUtil.HOME_DIRECTORY, "/test");
-		if (Platform.OS_WIN32.equals(Platform.getOS()))
-		{
+		if (Platform.OS_WIN32.equals(Platform.getOS())) {
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.COMMON_APPDATA, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.LOCAL_APPDATA, "/test");
-		}
-		else if (Platform.OS_MACOSX.equals(Platform.getOS()))
-		{
+		} else if (Platform.OS_MACOSX.equals(Platform.getOS())) {
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DOCUMENTS_DIRECTORY, "/test");
 		}
 	}
 
-	private void expandAndCompareEnvironmentStrings(String token, String path)
-	{
+	private void expandAndCompareEnvironmentStrings(String token, String path) {
 		String expanded = PlatformUtil.expandEnvironmentStrings(token + path);
 		assertNotSame(token + path, expanded);
 		assertTrue(expanded.endsWith(path));
