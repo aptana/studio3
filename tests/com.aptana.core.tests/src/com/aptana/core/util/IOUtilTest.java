4eae73a5b38a62c96e683af42bbdb2c0c2fda3e3
Allen Yeung <ayeung@aptana.com>
2011-08-09 14:43:29 -0700

    Added more IOUtil unittests

--- tests/com.aptana.core.tests/src/com/aptana/core/util/IOUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/IOUtilTest.java
@@ -11,14 +11,18 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -28,6 +32,31 @@ public class IOUtilTest extends TestCase
 {
 	private static final String BUNDLE_ID = "com.aptana.core.tests";
 	private static final String RESOURCE_DIR = "resources";
+	private static final String TEST_DIR = "copyTest";
+
+	private String tempDir;
+
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		tempDir = System.getProperty("java.io.tmpdir");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception
+	{
+		super.tearDown();
+		tempDir = null;
+	}
 
 	private Mockery context = new Mockery()
 	{
@@ -99,14 +128,144 @@ public class IOUtilTest extends TestCase
 		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
 		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
 
-		File source = new File(resourceFolder, "copyTest");
-		File dest = new File(resourceFolder, "tempdir");
+		File source = new File(resourceFolder, TEST_DIR);
+		File dest = new File(tempDir, "tempdir");
 
 		IOUtil.copyDirectory(source, dest);
-
 		assertTrue(compareDirectory(source, dest));
-
 		FileUtil.deleteRecursively(dest);
+	}
+
+	public void testCopyDirectorytoFile() throws IOException
+	{
+		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
+		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
+
+		File source = new File(resourceFolder, TEST_DIR);
+		File dest = new File(resourceFolder, "test.js");
+
+		IOUtil.copyDirectory(source, dest);
+		assertFalse(compareDirectory(source, dest));
+	}
+
+	public void testCopyFromReadOnlyDirectory() throws IOException
+	{
+		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
+		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
+
+		File source = new File(resourceFolder, TEST_DIR);
+		File dest = new File(tempDir, "tempdir");
+
+		source.setReadable(false);
+		IOUtil.copyDirectory(source, dest);
+		assertFalse(compareDirectory(source, dest));
+		FileUtil.deleteRecursively(dest);
+		source.setReadable(true);
+	}
+
+	public void testCopyToNotWriteableExistingDirectory() throws IOException
+	{
+		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
+		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
+
+		File source = new File(resourceFolder, TEST_DIR);
+		File dest = new File(tempDir, "testdir");
+
+		dest.mkdir();
+		dest.setReadOnly();
+		IOUtil.copyDirectory(source, dest);
+		assertFalse(compareDirectory(source, dest));
+		FileUtil.deleteRecursively(dest);
+	}
+
+	public void testCopyToNotWritableDirectory() throws IOException
+	{
+		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
+		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
+
+		File source = new File(resourceFolder, TEST_DIR);
+		File dest = new File(tempDir, "testdir");
+
+		dest.mkdir();
+		dest.setReadOnly();
+		IOUtil.copyDirectory(source, new File(dest, "testdir2"));
+		assertFalse(compareDirectory(source, dest));
+		FileUtil.deleteRecursively(dest);
+	}
+
+	public void testExtractFile() throws IOException
+	{
+		File dest = new File(tempDir, "testfile.txt");
+		IPath source = new Path("resources/test.js");
+
+		IOUtil.extractFile(BUNDLE_ID, source, dest);
+		assertTrue(compareFiles(source.toFile(), dest));
+		dest.delete();
+	}
+
+	public void testExtractFileWithInvalidPath() throws IOException
+	{
+		IPath source = new Path("invalid_file");
+		File dest = new File(tempDir, "testfile.txt");
+
+		IOUtil.extractFile(BUNDLE_ID, source, dest);
+
+		assertFalse(dest.exists());
+
+	}
+
+	public void testWrite() throws IOException
+	{
+		File dest = new File(tempDir, "test.txt");
+		dest.createNewFile();
+		String text = "This is a text for texting IOUtil.write()";
+
+		OutputStream output = new FileOutputStream(dest);
+		InputStream input = new FileInputStream(dest);
+
+		IOUtil.write(output, text);
+		assertTrue(IOUtil.read(input).equals(text));
+
+		input.close();
+		output.close();
+		dest.delete();
+	}
+
+	public void testWriteWithNullText() throws IOException
+	{
+		File dest = new File(tempDir, "test.txt");
+		dest.createNewFile();
+
+		OutputStream output = new FileOutputStream(dest);
+		InputStream input = new FileInputStream(dest);
+
+		IOUtil.write(output, null);
+		assertTrue(IOUtil.read(input).equals(StringUtil.EMPTY));
+
+		input.close();
+		output.close();
+		dest.delete();
+	}
+
+	public void testPipe() throws IOException
+	{
+		URL resourceURL = Platform.getBundle(BUNDLE_ID).getEntry(RESOURCE_DIR);
+		File resourceFolder = ResourceUtil.resourcePathToFile(resourceURL);
+
+		File source = new File(resourceFolder, "test.js");
+		File dest = new File(tempDir, "test.txt");
+
+		InputStream input = new FileInputStream(source);
+		OutputStream output = new FileOutputStream(dest);
+
+		IOUtil.pipe(input, output);
+
+		assertTrue(compareFiles(source, dest));
+
+		input.close();
+		output.close();
+		dest.delete();
+
 	}
 
 	private boolean compareDirectory(File directory1, File directory2)
@@ -162,6 +321,6 @@ public class IOUtilTest extends TestCase
 
 	private boolean compareFiles(File file1, File file2) throws FileNotFoundException
 	{
-		return IOUtil.read(new FileInputStream(file1)).equals(IOUtil.read(new FileInputStream(file2)));
+		return file1.length() == file2.length();
 	}
 }
