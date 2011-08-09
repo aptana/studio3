6d586716cb454eb646182c8cef68d91d52004dc8
Max Stepanov <mstepanov@aptana.com>
2011-08-09 14:18:02 -0700

    add assert messages to SocketUtil tests

--- tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
@@ -17,57 +17,59 @@ import junit.framework.TestCase;
 /**
  * @author Max Stepanov
  */
-public class SocketUtilTest extends TestCase
-{
+@SuppressWarnings("nls")
+public class SocketUtilTest extends TestCase {
 
-	public void testFindFreePort()
-	{
-		assertTrue(SocketUtil.findFreePort(null) > 0);
+	public void testFindFreePort() {
+		int port = SocketUtil.findFreePort(null);
+		assertTrue("Failed to find any free port", port > 0);
+		try {
+			new ServerSocket(port).close();
+		} catch(IOException e) {
+			fail("Found port is not free");
+		}
 	}
 
-	public void testFindFreePortInRange()
-	{
+	public void testFindFreePortInRange() {
 		int port = SocketUtil.findFreePort(null, 8000, 8010);
-		assertTrue(port >= 8000 && port <= 8010);
+		assertTrue("Failed to find free port in range", port >= 8000 && port <= 8010);
+		try {
+			new ServerSocket(port).close();
+		} catch(IOException e) {
+			fail("Found port is not free");
+		}
 	}
 
-	public void testFindFreePortInInvalidRange()
-	{
+	public void testFindFreePortInInvalidRange() {
 		int port = SocketUtil.findFreePort(null, 1, 10);
-		assertEquals(-1, port);
+		assertEquals("Found free port in invalid range", -1, port);
 	}
 
-	public void testFindFreePortInTakenRange() throws IOException
-	{
+	public void testFindFreePortInTakenRange() throws IOException {
 		ServerSocket socket = new ServerSocket(9000);
 		int port = SocketUtil.findFreePort(null, 9000, 9000);
 		socket.close();
-		assertEquals(-1, port);
+		assertEquals("Found free port in taken range", -1, port);
 	}
 
-	public void testGetLocalAddresses()
-	{
+	public void testGetLocalAddresses() {
 		InetAddress[] addresses = SocketUtil.getLocalAddresses();
 		assertTrue(addresses.length > 0);
 		boolean passed = false;
-		for (InetAddress i : addresses)
-		{
-			if (passed = "127.0.0.1".equals(i.getHostAddress()))
-			{
+		for (InetAddress i : addresses) {
+			if (passed = "127.0.0.1".equals(i.getHostAddress())) {
 				break;
 			}
 		}
-		assertTrue(passed);
+		assertTrue("Finding loopback 127.0.0.1 address failed", passed);
 	}
 
-	public void testGetNonLoopbackLocalAdresses()
-	{
+	public void testGetNonLoopbackLocalAdresses() {
 		InetAddress[] addresses = SocketUtil.getNonLoopbackLocalAdresses();
 		assertTrue(addresses.length > 0);
-		for (InetAddress i : addresses)
-		{
-			assertEquals(false, i.isLoopbackAddress());
-			assertNotSame("127.0.0.1", i.getHostAddress());
+		for (InetAddress i : addresses) {
+			assertEquals("Loopback address returned when non-loopback expected", false, i.isLoopbackAddress());
+			assertFalse("Loopback 127.0.0.1 address returned when non-loopback expected", "127.0.0.1".equals(i.getHostAddress()));
 		}
 	}
 
