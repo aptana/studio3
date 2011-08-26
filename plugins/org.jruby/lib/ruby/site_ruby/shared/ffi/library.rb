8386b3e769030a5fbee74cddc7597dc47efc18ce
Chris Williams <cwilliams@appcelerator.com>
2011-08-26 15:29:33 -0400

    Upgrade JRuby to 1.6.4

--- plugins/org.jruby/lib/ruby/site_ruby/shared/ffi/library.rb
+++ plugins/org.jruby/lib/ruby/site_ruby/shared/ffi/library.rb
@@ -1,3 +1,57 @@
+#
+# Copyright (C) 2008-2010 Wayne Meissner
+# Copyright (C) 2008 Luc Heinrich <luc@honk-honk.com>
+#
+# Version: CPL 1.0/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Common Public
+# License Version 1.0 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.eclipse.org/legal/cpl-v10.html
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either of the GNU General Public License Version 2 or later (the "GPL"),
+# or the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the CPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the CPL, the GPL or the LGPL.
+#
+#
+# This file contains code that was originally under the following license:
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the Evan Phoenix nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 module FFI
   CURRENT_PROCESS = USE_THIS_PROCESS_AS_LIBRARY = Object.new
 
@@ -172,7 +226,7 @@ module FFI
 
       options = Hash.new
       options[:convention] = defined?(@ffi_convention) ? @ffi_convention : :default
-      options[:enums] = @ffi_enums if defined?(@ffi_enums)
+      options[:enums] = defined?(@ffi_enum_map) ? @ffi_enum_map : nil
 
       cb = FFI::CallbackInfo.new(find_type(ret), params.map { |e| find_type(e) }, options)
 
@@ -198,14 +252,14 @@ module FFI
     def typedef(old, add, info=nil)
       @ffi_typedefs = Hash.new unless defined?(@ffi_typedefs)
 
-      @ffi_typedefs[add] = if old.kind_of?(FFI::Type)
+      @ffi_typedefs[add] = if old.kind_of?(Type)
         old
 
       elsif @ffi_typedefs.has_key?(old)
         @ffi_typedefs[old]
 
       elsif old.is_a?(DataConverter)
-        FFI::Type::Mapped.new(old)
+        Type::Mapped.new(old)
 
       elsif old == :enum
         if add.kind_of?(Array)
@@ -233,23 +287,29 @@ module FFI
       else
         [ nil, args ]
       end
-      @ffi_enums = FFI::Enums.new unless defined?(@ffi_enums)
-      @ffi_enums << (e = FFI::Enum.new(values, name))
+
+      e = Enum.new(values, name)
+      if name
+        @ffi_tagged_enums = Hash.new unless defined?(@ffi_tagged_enums)
+        @ffi_tagged_enums[name] = e
+        # If called as enum :foo, [ :zero, :one, :two ], add a typedef alias
+        typedef(e, name)
+      end
+
       @ffi_enum_map = Hash.new unless defined?(@ffi_enum_map)
+
       # append all the enum values to a global :name => value map
       @ffi_enum_map.merge!(e.symbol_map)
 
-      # If called as enum :foo, [ :zero, :one, :two ], add a typedef alias
-      typedef(e, name) if name
       e
     end
 
     def enum_type(name)
-      @ffi_enums.find(name) if defined?(@ffi_enums)
+      @ffi_tagged_enums[name] if defined?(@ffi_tagged_enums)
     end
 
     def enum_value(symbol)
-      @ffi_enums.__map_symbol(symbol)
+      @ffi_enum_map[symbol]
     end
 
     def find_type(t)
