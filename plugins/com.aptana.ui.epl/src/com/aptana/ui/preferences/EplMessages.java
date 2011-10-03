def3fcf79bebacb219b18934e2f6c8d2ca326e19
mxia <mxia@aptana.com>
2011-10-03 16:58:46 -0700

    Added the new messages classes.

---
+++ plugins/com.aptana.ui.epl/src/com/aptana/ui/preferences/EplMessages.java
@@ -0,0 +1,33 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Eclipse Public License (EPL).
+ * Please see the license-epl.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.ui.preferences;
+
+import org.eclipse.osgi.util.NLS;
+
+public class EplMessages extends NLS
+{
+	private static final String BUNDLE_NAME = "com.aptana.ui.preferences.eplmessages"; //$NON-NLS-1$
+
+	public static String GenericRootPage_genericPerferencesPageMessage;
+	public static String GenericRootPage_noAvailablePages;
+	public static String GenericRootPage_preferences;
+
+	public static String PropertyAndPreferencePage_configureProjectSettings;
+	public static String PropertyAndPreferencePage_configureWorkspaceSettings;
+	public static String PropertyAndPreferencePage_enableProjectSpecific;
+
+	static
+	{
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, EplMessages.class);
+	}
+
+	private EplMessages()
+	{
+	}
+}
---
+++ plugins/com.aptana.ui/src/com/aptana/ui/preferences/Messages.java
@@ -0,0 +1,45 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.ui.preferences;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS
+{
+
+	private static final String BUNDLE_NAME = "com.aptana.ui.preferences.messages"; //$NON-NLS-1$
+
+	public static String AptanaPreferencePage_Auto_Migrate_Projects;
+	public static String AptanaPreferencePage_Auto_Refresh_Projects;
+
+	public static String ProjectBuildPathPropertyPage_TableDescription;
+	public static String ProjectBuildPathPropertyPage_LibraryColumnLabel;
+	public static String ProjectBuildPathPropertyPage_PathColumnLabel;
+
+	public static String TroubleshootingPreferencePage_DebugSpecificComponents;
+	public static String TroubleshootingPreferencePage_LBL_DebuggingOutputLevel;
+	public static String TroubleshootingPreferencePage_LBL_Errors;
+	public static String TroubleshootingPreferencePage_LBL_All;
+	public static String TroubleshootingPreferencePage_LBL_UnknownLoggingLevel;
+	public static String TroubleshootingPreferencePage_LBL_AllDebuggingInformation;
+	public static String TroubleshootingPreferencePage_LBL_ErrorsAndImportant;
+	public static String TroubleshootingPreferencePage_LBL_OnlyError;
+	public static String TroubleshootingPreferencePage_LBL_NoDebuggingOutput;
+	public static String TroubleshootingPreferencePage_ShowHiddenProcesses;
+	public static String TroubleshootingPreferencePage_TroubleshootingPageHeader;
+
+	static
+	{
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages()
+	{
+	}
+}
