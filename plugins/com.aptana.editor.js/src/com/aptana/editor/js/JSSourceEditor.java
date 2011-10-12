b45be4c8f6559314a98abd3654645c5edf435bd1
Kevin Lindsey <klindsey@appcelerator.com>
2011-10-12 14:43:05 -0500

    Pass in a JSParseState into FileService when creating a JS editor

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/JSSourceEditor.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/JSSourceEditor.java
@@ -16,10 +16,12 @@ import org.eclipse.ui.texteditor.ChainedPreferenceStore;
 
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.common.CommonEditorPlugin;
+import com.aptana.editor.common.parsing.FileService;
 import com.aptana.editor.common.text.reconciler.IFoldingComputer;
 import com.aptana.editor.js.internal.text.JSFoldingComputer;
 import com.aptana.editor.js.outline.JSOutlineContentProvider;
 import com.aptana.editor.js.outline.JSOutlineLabelProvider;
+import com.aptana.editor.js.parsing.JSParseState;
 
 @SuppressWarnings("restriction")
 public class JSSourceEditor extends AbstractThemeableEditor
@@ -80,5 +82,15 @@ public class JSSourceEditor extends AbstractThemeableEditor
 	protected String getFileServiceContentTypeId()
 	{
 		return IJSConstants.CONTENT_TYPE_JS;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.editor.common.AbstractThemeableEditor#createFileService()
+	 */
+	@Override
+	protected FileService createFileService()
+	{
+		return new FileService(getFileServiceContentTypeId(), new JSParseState());
 	}
 }
