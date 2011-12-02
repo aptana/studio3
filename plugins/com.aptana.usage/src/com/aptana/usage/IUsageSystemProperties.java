b86bc1d2a284402d72f324288e1364f13432830d
mxia <mxia@aptana.com>
2011-12-02 15:17:36 -0800

    Implemented TISTUD-883.

---
+++ plugins/com.aptana.usage/src/com/aptana/usage/IUsageSystemProperties.java
@@ -0,0 +1,14 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.usage;
+
+public interface IUsageSystemProperties
+{
+
+	public static final String ANALYTICS_URL = "studio.analyticsURL"; //$NON-NLS-1$
+}
