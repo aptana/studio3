66409ae7ee795fdccb3ec92f853439ea07086575
Shalom Gibly <sgibly@aptana.com>
2011-12-19 18:50:29 -0800

    Add a build-utility class

---
+++ plugins/com.aptana.core/src/com/aptana/core/util/BuildUtil.java
@@ -0,0 +1,63 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.core.util;
+
+import java.util.Map;
+
+import org.eclipse.core.internal.events.BuildManager;
+import org.eclipse.core.internal.resources.Workspace;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+
+/**
+ * Build utilities class. <br>
+ * The class allows synchronous and asynchronous calls for specific builders.
+ * 
+ * @author Shalom Gibly <sgibly@appcelerator.com>
+ */
+@SuppressWarnings({ "rawtypes", "restriction" })
+public class BuildUtil
+{
+	/**
+	 * Synchronous run of a builder with the given name on the given project.
+	 * 
+	 * @param project
+	 * @param kind
+	 *            See {@link IncrementalProjectBuilder} for the available types.
+	 * @param builderName
+	 * @param args
+	 * @param monitor
+	 * @return A status indicating if the build succeeded or failed
+	 */
+	public static IStatus syncBuild(IProject project, int kind, String builderName, Map args, IProgressMonitor monitor)
+	{
+		BuildManager buildManager = ((Workspace) ResourcesPlugin.getWorkspace()).getBuildManager();
+		return buildManager.build(project, kind, builderName, args, monitor);
+	}
+
+	/**
+	 * Asynchronous run of a builder with the given name on the given project.
+	 * 
+	 * @param project
+	 * @param kind
+	 *            See {@link IncrementalProjectBuilder} for the available types.
+	 * @param builderName
+	 * @param args
+	 * @param monitor
+	 * @throws CoreException
+	 */
+	public static void asyncBuild(IProject project, int kind, String builderName, Map args, IProgressMonitor monitor)
+			throws CoreException
+	{
+		project.build(kind, builderName, args, monitor);
+	}
+}
