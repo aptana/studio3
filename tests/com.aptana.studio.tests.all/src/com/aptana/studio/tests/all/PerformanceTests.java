21bd5bac7c2f2abb032cd718b452c611e9fc118d
Chris Williams <cwilliams@appcelerator.com>
2012-02-22 09:43:17 -0500

    Add performance tests for XML

--- tests/com.aptana.studio.tests.all/src/com/aptana/studio/tests/all/PerformanceTests.java
+++ tests/com.aptana.studio.tests.all/src/com/aptana/studio/tests/all/PerformanceTests.java
@@ -40,6 +40,7 @@ public class PerformanceTests
 		suite.addTest(com.aptana.editor.html.tests.PerformanceTests.suite());
 		suite.addTest(com.aptana.editor.js.tests.PerformanceTests.suite());
 		suite.addTest(com.aptana.editor.json.tests.PerformanceTests.suite());
+		suite.addTest(com.aptana.editor.xml.tests.PerformanceTests.suite());
 		suite.addTest(com.aptana.git.core.tests.PerformanceTests.suite());
 		// $JUnit-END$
 		return suite;
