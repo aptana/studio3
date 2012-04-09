a1d5d73a17e412ba16dcd1b459ed42fa28844b34
mxia <mxia@appcelerator.com>
2012-04-09 14:56:09 -0700

    Fixed APSTUD-3722: added a no-op parser for our text editor.

---
+++ plugins/com.aptana.editor.text/src/com/aptana/editor/text/parsing/GenericTextParser.java
@@ -0,0 +1,25 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.editor.text.parsing;
+
+import com.aptana.parsing.IParseState;
+import com.aptana.parsing.IParser;
+import com.aptana.parsing.ast.IParseRootNode;
+
+public class GenericTextParser implements IParser
+{
+
+	public GenericTextParser()
+	{
+	}
+
+	public IParseRootNode parse(IParseState parseState) throws Exception
+	{
+		return null;
+	}
+}
