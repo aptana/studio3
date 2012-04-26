c83c8a3421515f28d97e5c29ce512b003aeb2041
Shalom Gibly <sgibly@appcelerator.com>
2012-04-25 18:19:19 -0700

    APSTUD-4680 - Formatter messages update for a fatal syntax error.

--- plugins/com.aptana.formatter.epl/src/com/aptana/formatter/ui/FormatterMessages.java
+++ plugins/com.aptana.formatter.epl/src/com/aptana/formatter/ui/FormatterMessages.java
@@ -85,6 +85,7 @@ public class FormatterMessages extends NLS
 	public static String RubyFormatterIndentationTabPage_declarationWithinModuleBody;
 	public static String FormatterModifyTabPage_generalSettings;
 	public static String Formatter_contentErrorMessage;
+	public static String PHPFormatter_fatalSyntaxErrors;
 	public static String RubyFormatterIndentationTabPage_indentDefinitionsGroupTitle;
 	public static String RubyFormatterIndentationTabPage_indentWithinBlocks;
 	public static String RubyFormatterIndentationTabPage_statementWithinBlocksBody;
