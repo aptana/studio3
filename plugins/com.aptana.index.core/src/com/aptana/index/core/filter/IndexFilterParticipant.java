8804350508a6e3012cd3b06da8e4126c1a43b6e9
Chris Williams <cwilliams@appcelerator.com>
2012-05-16 17:03:38 -0400

    Move non-UI index filtering code into index.core plugin from index.core.ui plugin

--- plugins/com.aptana.index.core/src/com/aptana/index/core/filter/IndexFilterParticipant.java
+++ plugins/com.aptana.index.core/src/com/aptana/index/core/filter/IndexFilterParticipant.java
@@ -1,17 +1,16 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
  */
-package com.aptana.index.core.ui;
+package com.aptana.index.core.filter;
 
 import java.util.Set;
 
 import org.eclipse.core.filesystem.IFileStore;
 
-import com.aptana.index.core.IIndexFilterParticipant;
 
 public class IndexFilterParticipant implements IIndexFilterParticipant
 {
