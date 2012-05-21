127064eedcb37c29b4315c370cdfe6c4a100d959
Shalom Gibly <sgibly@appcelerator.com>
2012-05-21 05:23:19 -0700

    TISTUD-1597 - Add an IPv4 verification utility method.

--- tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/SocketUtilTest.java
@@ -18,59 +18,83 @@ import junit.framework.TestCase;
  * @author Max Stepanov
  */
 @SuppressWarnings("nls")
-public class SocketUtilTest extends TestCase {
+public class SocketUtilTest extends TestCase
+{
 
-	public void testFindFreePort() {
+	public void testFindFreePort()
+	{
 		int port = SocketUtil.findFreePort(null);
 		assertTrue("Failed to find any free port", port > 0);
-		try {
+		try
+		{
 			new ServerSocket(port).close();
-		} catch(IOException e) {
+		}
+		catch (IOException e)
+		{
 			fail("Found port is not free");
 		}
 	}
 
-	public void testFindFreePortInRange() {
+	public void testFindFreePortInRange()
+	{
 		int port = SocketUtil.findFreePort(null, 8000, 8010);
 		assertTrue("Failed to find free port in range", port >= 8000 && port <= 8010);
-		try {
+		try
+		{
 			new ServerSocket(port).close();
-		} catch(IOException e) {
+		}
+		catch (IOException e)
+		{
 			fail("Found port is not free");
 		}
 	}
 
-	public void testFindFreePortInInvalidRange() {
+	public void testFindFreePortInInvalidRange()
+	{
 		int port = SocketUtil.findFreePort(null, 1, 10);
 		assertEquals("Found free port in invalid range", -1, port);
 	}
 
-	public void testFindFreePortInTakenRange() throws IOException {
+	public void testFindFreePortInTakenRange() throws IOException
+	{
 		ServerSocket socket = new ServerSocket(9000);
 		int port = SocketUtil.findFreePort(null, 9000, 9000);
 		socket.close();
 		assertEquals("Found free port in taken range", -1, port);
 	}
 
-	public void testGetLocalAddresses() {
+	public void testGetLocalAddresses()
+	{
 		InetAddress[] addresses = SocketUtil.getLocalAddresses();
 		assertTrue(addresses.length > 0);
 		boolean passed = false;
-		for (InetAddress i : addresses) {
-			if (passed = "127.0.0.1".equals(i.getHostAddress())) {
+		for (InetAddress i : addresses)
+		{
+			if (passed = "127.0.0.1".equals(i.getHostAddress()))
+			{
 				break;
 			}
 		}
 		assertTrue("Finding loopback 127.0.0.1 address failed", passed);
 	}
 
-	public void testGetNonLoopbackLocalAdresses() {
+	public void testGetNonLoopbackLocalAdresses()
+	{
 		InetAddress[] addresses = SocketUtil.getNonLoopbackLocalAdresses();
 		assertTrue(addresses.length > 0);
-		for (InetAddress i : addresses) {
+		for (InetAddress i : addresses)
+		{
 			assertEquals("Loopback address returned when non-loopback expected", false, i.isLoopbackAddress());
-			assertFalse("Loopback 127.0.0.1 address returned when non-loopback expected", "127.0.0.1".equals(i.getHostAddress()));
+			assertFalse("Loopback 127.0.0.1 address returned when non-loopback expected",
+					"127.0.0.1".equals(i.getHostAddress()));
 		}
 	}
 
+	public void testIPv4() throws Exception
+	{
+		assertTrue("Expected a valid IPv4 address for '127.0.0.1'", SocketUtil.isValidIPv4("127.0.0.1"));
+		assertFalse("Expected an invalid IPv4 address for '127.0.0.a'", SocketUtil.isValidIPv4("127.0.0.a"));
+		assertFalse("Expected an invalid IPv4 address for '127.0.0.256'", SocketUtil.isValidIPv4("127.0.0.256"));
+		assertFalse("Expected an invalid IPv4 address for '127.0.0'", SocketUtil.isValidIPv4("127.0.0"));
+	}
 }
