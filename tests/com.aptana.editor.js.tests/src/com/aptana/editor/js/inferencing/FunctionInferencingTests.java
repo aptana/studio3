9e4e850206d6f985fdfc968291c5c0b5cd521019
Chris Williams <cwilliams@appcelerator.com>
2012-05-24 09:59:06 -0400

    Fix APSTUD-4836 Unable to construct JS instances from built-in types using the "new" operator     - Update metadata for prototype properties for classes Array/RegExp/Error/Function/etc. to return their own type and not "Object"     - Add unit tests for these cases     - Add toString() impls for some metadata classes in JS just for dev purposes     - Make use of some CollectionsUtil methods in JSSymbolTypeInferrer

--- tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/inferencing/FunctionInferencingTests.java
+++ tests/com.aptana.editor.js.tests/src/com/aptana/editor/js/inferencing/FunctionInferencingTests.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -16,6 +16,7 @@ import org.eclipse.core.runtime.Path;
 import com.aptana.core.IMap;
 import com.aptana.core.util.CollectionsUtil;
 import com.aptana.editor.common.tests.util.TestProject;
+import com.aptana.editor.epl.tests.EditorTestHelper;
 import com.aptana.editor.js.contentassist.JSIndexQueryHelper;
 import com.aptana.editor.js.contentassist.index.JSFileIndexingParticipant;
 import com.aptana.editor.js.contentassist.model.PropertyElement;
@@ -26,74 +27,48 @@ import com.aptana.index.core.build.BuildContext;
 
 public class FunctionInferencingTests extends InferencingTestsBase
 {
-	/**
-	 * testReturnsBoolean
-	 */
+
 	public void testReturnsBoolean()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-boolean.js"), "Boolean");
 	}
 
-	/**
-	 * testReturnsFunction
-	 */
 	public void testReturnsFunction()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-function.js"), "Function");
 	}
 
-	/**
-	 * testReturnsNumber
-	 */
 	public void testReturnsNumber()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-number.js"), "Number");
 	}
 
-	/**
-	 * testReturnsRegExp
-	 */
 	public void testReturnsRegExp()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-regexp.js"), "RegExp");
 	}
 
-	/**
-	 * testReturnsString
-	 */
 	public void testReturnsString()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-string.js"), "String");
 	}
 
-	/**
-	 * testReturnsArray
-	 */
 	public void testReturnsArray()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-array.js"), "Array");
 	}
 
-	/**
-	 * testReturnsArrayOfNumbers
-	 */
 	public void testReturnsArrayOfNumbers()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-array-of-numbers.js"),
 				"Array<Number>");
 	}
 
-	/**
-	 * testReturnsObject
-	 */
 	public void testReturnsObject()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-object.js"), "Object");
 	}
 
-	/**
-	 * testReturnsUserObject
-	 */
 	public void testReturnsUserObject()
 	{
 		this.lastStatementTypeTests(Path.fromPortableString("inferencing/function-returns-user-object.js"), "foo");
@@ -145,5 +120,47 @@ public class FunctionInferencingTests extends InferencingTestsBase
 				project.delete();
 			}
 		}
+	}
+
+	public void testConstructedArray() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-array.js"), "Array");
+	}
+
+	public void testConstructedBoolean() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-boolean.js"), "Boolean");
+	}
+
+	public void testConstructedDate() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-date.js"), "Date");
+	}
+
+	public void testConstructedError() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-error.js"), "Error");
+	}
+
+	public void testConstructedFunction() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-function.js"), "Function");
+	}
+
+	public void testConstructedNumber() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-number.js"), "Number");
+	}
+
+	public void testConstructedRegExp() throws Exception
+	{
+		EditorTestHelper.joinBackgroundActivities();
+		this.lastStatementTypeTests(Path.fromPortableString("inferencing/constructed-regexp.js"), "RegExp");
 	}
 }
