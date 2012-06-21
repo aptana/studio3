0c1e6e4c6ebc9bc7ff8ed38366bb2b529a17de4b
Chris Williams <cwilliams@appcelerator.com>
2012-06-21 15:01:53 -0400

    Fix TISTUD-1787 Titanium Studio: JavaScript Style Validator: prototype.js library produces thousands of warnings     - Fix option default values     - reverse some option value checks     - Write some tests for false positives we're generating
    Fix APSTUD-4941 NumberFormatException in JSStyleValidator.enterNumber()
        - lower logging to INFO level

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/validator/Messages.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/validator/Messages.java
@@ -14,62 +14,56 @@ public class Messages extends NLS
 
 	private static final String BUNDLE_NAME = "com.aptana.editor.js.validator.messages"; //$NON-NLS-1$
 
-	public static String JSLintReplacementValidator_AdsafeA;
-	public static String JSLintReplacementValidator_ALabel;
-	public static String JSLintReplacementValidator_AlreadyDefined;
-
-	public static String JSLintReplacementValidator_And;
-	public static String JSLintReplacementValidator_AScope;
-	public static String JSLintReplacementValidator_AssignException;
-	public static String JSLintReplacementValidator_AvoidA;
-	public static String JSLintReplacementValidator_BadAssignment;
-	public static String JSLintReplacementValidator_BadNew;
-
-	public static String JSLintReplacementValidator_BadNumber;
-
-	public static String JSLintReplacementValidator_BadWrap;
-	public static String JSLintReplacementValidator_ConditionalAssignment;
-
-	public static String JSLintReplacementValidator_ConstructorNameA;
-	public static String JSLintReplacementValidator_DangerousComment;
-
-	public static String JSLintReplacementValidator_DanglingA;
-	public static String JSLintReplacementValidator_EmptyBlock;
-
-	public static String JSLintReplacementValidator_EmptyClass;
-	public static String JSLintReplacementValidator_Evil;
-	public static String JSLintReplacementValidator_ExpectedNumberA;
-	public static String JSLintReplacementValidator_FunctionEval;
-	public static String JSLintReplacementValidator_ImpliedEvil;
-	public static String JSLintReplacementValidator_LeadingDecimalA;
-	public static String JSLintReplacementValidator_NotAConstructor;
-	public static String JSLintReplacementValidator_NotGreater;
-	public static String JSLintReplacementValidator_ParameterArgumentsA;
-	public static String JSLintReplacementValidator_Radix;
-	public static String JSLintReplacementValidator_ReservedA;
-	public static String JSLintReplacementValidator_StrangeLoop;
-
-	public static String JSLintReplacementValidator_Subscript;
-	public static String JSLintReplacementValidator_Sync;
-	public static String JSLintReplacementValidator_TrailingDecimalA;
-	public static String JSLintReplacementValidator_UnexpectedA;
-	public static String JSLintReplacementValidator_UnnecessaryInitialize;
-
-	public static String JSLintReplacementValidator_UnreachableAB;
-	public static String JSLintReplacementValidator_UseArray;
-	public static String JSLintReplacementValidator_UseBraces;
-	public static String JSLintReplacementValidator_UsedBeforeA;
-	public static String JSLintReplacementValidator_UseObject;
-	public static String JSLintReplacementValidator_UseParam;
-	public static String JSLintReplacementValidator_VarANot;
-	public static String JSLintReplacementValidator_WeirdNew;
-
-	public static String JSLintReplacementValidator_WeirdProgram;
-
-	public static String JSLintReplacementValidator_WrapImmediate;
-
-	public static String JSLintReplacementValidator_WriteIsWrong;
 	public static String JSLintValidator_ERR_FailToGetJSLint;
+
+	public static String JSStyleValidator_AdsafeA;
+	public static String JSStyleValidator_ALabel;
+	public static String JSStyleValidator_AlreadyDefined;
+	public static String JSStyleValidator_And;
+	public static String JSStyleValidator_AScope;
+	public static String JSStyleValidator_AssignException;
+	public static String JSStyleValidator_AvoidA;
+	public static String JSStyleValidator_BadAssignment;
+	public static String JSStyleValidator_BadNew;
+	public static String JSStyleValidator_BadNumber;
+	public static String JSStyleValidator_BadWrap;
+	public static String JSStyleValidator_CombineVar;
+	public static String JSStyleValidator_ConditionalAssignment;
+	public static String JSStyleValidator_ConstructorNameA;
+	public static String JSStyleValidator_DangerousComment;
+	public static String JSStyleValidator_DanglingA;
+	public static String JSStyleValidator_EmptyBlock;
+	public static String JSStyleValidator_EmptyClass;
+	public static String JSStyleValidator_Evil;
+	public static String JSStyleValidator_ExpectedNumberA;
+	public static String JSStyleValidator_FunctionEval;
+	public static String JSStyleValidator_ImpliedEvil;
+	public static String JSStyleValidator_InsecureA;
+	public static String JSStyleValidator_LeadingDecimalA;
+	public static String JSStyleValidator_MoveInvocation;
+	public static String JSStyleValidator_NotAConstructor;
+	public static String JSStyleValidator_NotGreater;
+	public static String JSStyleValidator_ParameterArgumentsA;
+	public static String JSStyleValidator_Radix;
+	public static String JSStyleValidator_ReservedA;
+	public static String JSStyleValidator_StrangeLoop;
+	public static String JSStyleValidator_Subscript;
+	public static String JSStyleValidator_Sync;
+	public static String JSStyleValidator_TrailingDecimalA;
+	public static String JSStyleValidator_UnexpectedA;
+	public static String JSStyleValidator_UnnecessaryInitialize;
+	public static String JSStyleValidator_UnreachableAB;
+	public static String JSStyleValidator_UseArray;
+	public static String JSStyleValidator_UseBraces;
+	public static String JSStyleValidator_UsedBeforeA;
+	public static String JSStyleValidator_UseObject;
+	public static String JSStyleValidator_UseParam;
+	public static String JSStyleValidator_VarANot;
+	public static String JSStyleValidator_WeirdNew;
+	public static String JSStyleValidator_WeirdProgram;
+	public static String JSStyleValidator_WrapImmediate;
+	public static String JSStyleValidator_WrapRegexp;
+	public static String JSStyleValidator_WriteIsWrong;
 
 	static
 	{
