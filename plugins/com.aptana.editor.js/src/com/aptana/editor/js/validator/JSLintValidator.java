0ad1668196fe9b5368abd4440432af4647827ee5
Chris Williams <cwilliams@appcelerator.com>
2012-06-27 11:00:55 -0400

    Fix APSTUD-4944 JSLint inside Aptana runs far slower than on the command line   - upgrade Rhino to 1.7R4 (our old 1.7R2 version was the major culprit behind teh performance issues)   - Move jslint into com.aptana.editor.js where it's used so the rhino plugin is just rhino   - Update to use lint invocation similar to jslint4java

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/validator/JSLintValidator.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/validator/JSLintValidator.java
@@ -16,29 +16,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
-import org.mozilla.javascript.CompilerEnvirons;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContextAction;
+import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.ErrorReporter;
 import org.mozilla.javascript.EvaluatorException;
 import org.mozilla.javascript.Function;
 import org.mozilla.javascript.NativeArray;
 import org.mozilla.javascript.NativeObject;
-import org.mozilla.javascript.Parser;
 import org.mozilla.javascript.Script;
-import org.mozilla.javascript.ScriptOrFnNode;
 import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.optimizer.Codegen;
+import org.mozilla.javascript.ScriptableObject;
 
+import com.aptana.core.IFilter;
 import com.aptana.core.build.AbstractBuildParticipant;
 import com.aptana.core.build.IProblem;
 import com.aptana.core.logging.IdeLog;
 import com.aptana.core.util.ArrayUtil;
+import com.aptana.core.util.CollectionsUtil;
 import com.aptana.core.util.StreamUtil;
 import com.aptana.editor.js.IJSConstants;
 import com.aptana.editor.js.JSPlugin;
@@ -56,13 +58,10 @@ public class JSLintValidator extends AbstractBuildParticipant
 	 */
 	public static final String ID = "com.aptana.editor.js.validator.JSLintValidator"; //$NON-NLS-1$
 
-	/**
-	 * The bundle id containing JSLint script.
-	 */
-	private static final String ORG_MOZILLA_RHINO = "org.mozilla.rhino"; //$NON-NLS-1$
-
 	private static final String JSLINT_FILENAME = "fulljslint.js"; //$NON-NLS-1$
-	private static Script JS_LINT_SCRIPT;
+	private static JSLint JS_LINT_SCRIPT;
+
+	private static ContextFactory contextFactory = new ContextFactory();
 
 	private static Map<String, Object> DEFAULT_OPTION = new HashMap<String, Object>();
 	static
@@ -99,18 +98,7 @@ public class JSLintValidator extends AbstractBuildParticipant
 		String sourcePath = context.getURI().toString();
 		try
 		{
-
-			Context cContext = Context.enter();
-			try
-			{
-				DefaultErrorReporter reporter = new DefaultErrorReporter();
-				cContext.setErrorReporter(reporter);
-				problems = parseWithLint(cContext, context.getContents(), sourcePath);
-			}
-			finally
-			{
-				Context.exit();
-			}
+			problems = parseWithLint(context.getContents(), sourcePath);
 		}
 		catch (Exception e)
 		{
@@ -132,135 +120,26 @@ public class JSLintValidator extends AbstractBuildParticipant
 		context.removeProblems(IJSConstants.JSLINT_PROBLEM_MARKER_TYPE);
 	}
 
-	private List<IProblem> parseWithLint(Context context, String source, String path)
+	private List<IProblem> parseWithLint(String source, String path)
 	{
-		Scriptable scope = context.initStandardObjects();
-		Script script = getJSLintScript();
+		JSLint script = getJSLintScript();
 		if (script == null)
 		{
 			return Collections.emptyList();
 		}
 
-		script.exec(context, scope);
+		script.runLint(source, this.options);
 
-		Object functionObj = scope.get("JSLINT", scope); //$NON-NLS-1$
-		if (!(functionObj instanceof Function))
+		List<IProblem> collected = script.getProblems(source, path);
+		final List<String> filters = getFilters();
+		return CollectionsUtil.filter(collected, new IFilter<IProblem>()
 		{
-			return Collections.emptyList();
-		}
 
-		Function function = (Function) functionObj;
-		NativeObject optionsObj = new NativeObject();
-		// Set our overriding options.
-		if (this.options != null)
-		{
-			for (Map.Entry<String, Object> entry : this.options.entrySet())
+			public boolean include(IProblem item)
 			{
-				scope.put(entry.getKey(), optionsObj, entry.getValue());
+				return !isIgnored(item.getMessage(), filters);
 			}
-		}
-
-		Object[] args = { source, optionsObj };
-		// PC: we ignore the result, because i have found that with some versions, there might
-		// be errors but this function returned true (false == errors)
-		function.call(context, scope, scope, args);
-
-		Object errorObject = function.get("errors", scope); //$NON-NLS-1$
-		if (!(errorObject instanceof NativeArray))
-		{
-			return Collections.emptyList();
-		}
-
-		NativeArray errorArray = (NativeArray) errorObject;
-		Object[] ids = errorArray.getIds();
-		if (ArrayUtil.isEmpty(ids))
-		{
-			return Collections.emptyList();
-		}
-
-		boolean lastIsError = false;
-		NativeObject last = (NativeObject) errorArray.get(Integer.parseInt(ids[ids.length - 1].toString()), scope);
-		if (last == null)
-		{
-			lastIsError = true;
-		}
-
-		List<String> filters = getFilters();
-		IDocument doc = null; // Lazily init document object to query about lines/offsets
-		List<IProblem> items = new ArrayList<IProblem>(ids.length);
-		for (int i = 0; i < ids.length; ++i)
-		{
-			// Grab the warning/error
-			NativeObject object = (NativeObject) errorArray.get(Integer.parseInt(ids[i].toString()), scope);
-			if (object == null)
-			{
-				continue;
-			}
-
-			// Grab the line of the error. Skip if we already recorded an error on this line (why?)
-			int line = (int) Double.parseDouble(object.get("line", scope).toString()); //$NON-NLS-1$
-
-			// Grab the details of the error. If user has set up filters to ignore it, move on
-			String reason = object.get("reason", scope).toString().trim(); //$NON-NLS-1$
-			if (isIgnored(reason, filters))
-			{
-				continue;
-			}
-
-			// lazy init of document to query for offsets/line info
-			if (doc == null)
-			{
-				doc = new Document(source);
-			}
-
-			// Translate the column reported into the absolute offset from start of doc
-			int character = (int) Double.parseDouble(object.get("character", scope).toString()); //$NON-NLS-1$
-			try
-			{
-				// JSLint reports the offset as column on the given line, and counts tab characters as 4 columns
-				// We account for that by adding the offset of the line start, and reducing the column count on
-				// tabs
-				IRegion lineInfo = doc.getLineInformation(line - 1);
-				int realOffset = lineInfo.getOffset();
-				String rawLine = doc.get(realOffset, lineInfo.getLength());
-				int actual = character - 1;
-				for (int x = 0; x < actual; x++)
-				{
-					char c = rawLine.charAt(x);
-					if (c == '\t')
-					{
-						actual -= 3;
-					}
-					realOffset++;
-				}
-				character = realOffset;
-			}
-			catch (BadLocationException e)
-			{
-				// ignore
-			}
-
-			// Now record the error
-			if (i == ids.length - 2 && lastIsError)
-			{
-				// If this starts with "Stopping", convert the last warning to an error and skip this.
-				if (reason.startsWith("Stopping")) //$NON-NLS-1$
-				{
-					IProblem lastWarning = items.remove(items.size() - 1);
-					items.add(createError(lastWarning.getMessage(), lastWarning.getLineNumber(),
-							lastWarning.getOffset(), 1, path));
-				}
-				else
-				{
-					items.add(createError(reason, line, character, 1, path));
-				}
-			}
-			else
-			{
-				items.add(createWarning(reason, line, character, 1, path));
-			}
-		}
-		return items;
+		});
 	}
 
 	/**
@@ -268,25 +147,25 @@ public class JSLintValidator extends AbstractBuildParticipant
 	 * 
 	 * @return
 	 */
-	private static synchronized Script getJSLintScript()
+	private synchronized JSLint getJSLintScript()
 	{
 		if (JS_LINT_SCRIPT == null)
 		{
-			URL url = Platform.getBundle(ORG_MOZILLA_RHINO).getEntry("/" + JSLINT_FILENAME); //$NON-NLS-1$
+			URL url = FileLocator.find(JSPlugin.getDefault().getBundle(), Path.fromPortableString(JSLINT_FILENAME),
+					null);
 			if (url != null)
 			{
-				String source = null;
 				try
 				{
-					source = StreamUtil.readContent(url.openStream());
+					String source = StreamUtil.readContent(url.openStream());
+					if (source != null)
+					{
+						JS_LINT_SCRIPT = getJSLintScript(source);
+					}
 				}
 				catch (IOException e)
 				{
 					IdeLog.logError(JSPlugin.getDefault(), Messages.JSLintValidator_ERR_FailToGetJSLint, e);
-				}
-				if (source != null)
-				{
-					JS_LINT_SCRIPT = getJSLintScript(source);
 				}
 			}
 		}
@@ -299,32 +178,19 @@ public class JSLintValidator extends AbstractBuildParticipant
 	 * @param source
 	 * @return
 	 */
-	private static Script getJSLintScript(String source)
+	private JSLint getJSLintScript(String source)
 	{
-		Context context = Context.enter();
 		try
 		{
-			CompilerEnvirons compilerEnv = new CompilerEnvirons();
-			compilerEnv.initFromContext(context);
-			Parser p = new Parser(compilerEnv, context.getErrorReporter());
-
-			ScriptOrFnNode tree = p.parse(source, JSLINT_FILENAME, 1);
-			String encodedSource = p.getEncodedSource();
-
-			Codegen compiler = new Codegen();
-			Object bytecode = compiler.compile(compilerEnv, tree, encodedSource, false);
-
-			return compiler.createScriptObject(bytecode, null);
-		}
-		catch (Exception e)
-		{
-			IdeLog.logError(JSPlugin.getDefault(), Messages.JSLintValidator_ERR_FailToGetJSLint, e);
+			Context cx = contextFactory.enterContext();
+			ScriptableObject scope = cx.initStandardObjects();
+			cx.evaluateString(scope, source, JSLINT_FILENAME, 1, null);
+			return new JSLint(contextFactory, scope);
 		}
 		finally
 		{
 			Context.exit();
 		}
-		return null;
 	}
 
 	public void setOption(String propertyName, Object value)
@@ -339,6 +205,172 @@ public class JSLintValidator extends AbstractBuildParticipant
 		{
 			this.options = new HashMap<String, Object>();
 			this.options.putAll(DEFAULT_OPTION);
+		}
+	}
+
+	class JSLint
+	{
+		private ContextFactory contextFactory;
+		private ScriptableObject scope;
+
+		JSLint(ContextFactory contextFactory, ScriptableObject scope)
+		{
+			this.contextFactory = contextFactory;
+			this.scope = scope;
+			this.scope.sealObject();
+		}
+
+		public List<IProblem> getProblems(final String source, final String path)
+		{
+			final List<IProblem> items = new ArrayList<IProblem>();
+
+			contextFactory.call(new ContextAction()
+			{
+				public Object run(Context cx)
+				{
+					Function lintFunc = (Function) scope.get("JSLINT", scope); //$NON-NLS-1$
+
+					Object errorObject = lintFunc.get("errors", scope); //$NON-NLS-1$
+					if (!(errorObject instanceof NativeArray))
+					{
+						return null;
+					}
+
+					NativeArray errorArray = (NativeArray) errorObject;
+					Object[] ids = errorArray.getIds();
+					if (ArrayUtil.isEmpty(ids))
+					{
+						return null;
+					}
+
+					boolean lastIsError = false;
+					NativeObject last = (NativeObject) errorArray.get(Integer.parseInt(ids[ids.length - 1].toString()),
+							scope);
+					if (last == null)
+					{
+						lastIsError = true;
+					}
+
+					IDocument doc = null; // Lazily init document object to query about lines/offsets
+					for (int i = 0; i < ids.length; ++i)
+					{
+						// Grab the warning/error
+						NativeObject object = (NativeObject) errorArray.get(Integer.parseInt(ids[i].toString()), scope);
+						if (object == null)
+						{
+							continue;
+						}
+
+						// Grab the line of the error. Skip if we already recorded an error on this line (why?)
+						int line = (int) Double.parseDouble(object.get("line", scope).toString()); //$NON-NLS-1$
+
+						// Grab the details of the error. If user has set up filters to ignore it, move on
+						String reason = object.get("reason", scope).toString().trim(); //$NON-NLS-1$
+
+						// lazy init of document to query for offsets/line info
+						if (doc == null)
+						{
+							doc = new Document(source);
+						}
+
+						// Translate the column reported into the absolute offset from start of doc
+						int character = (int) Double.parseDouble(object.get("character", scope).toString()); //$NON-NLS-1$
+						try
+						{
+							// JSLint reports the offset as column on the given line, and counts tab characters as 4
+							// columns
+							// We account for that by adding the offset of the line start, and reducing the column count
+							// on
+							// tabs
+							IRegion lineInfo = doc.getLineInformation(line - 1);
+							int realOffset = lineInfo.getOffset();
+							String rawLine = doc.get(realOffset, lineInfo.getLength());
+							int actual = character - 1;
+							for (int x = 0; x < actual; x++)
+							{
+								char c = rawLine.charAt(x);
+								if (c == '\t')
+								{
+									actual -= 3;
+								}
+								realOffset++;
+							}
+							character = realOffset;
+						}
+						catch (BadLocationException e)
+						{
+							// ignore
+						}
+
+						// Now record the error
+						if (i == ids.length - 2 && lastIsError)
+						{
+							// If this starts with "Stopping", convert the last warning to an error and skip this.
+							if (reason.startsWith("Stopping")) //$NON-NLS-1$
+							{
+								IProblem lastWarning = items.remove(items.size() - 1);
+								items.add(createError(lastWarning.getMessage(), lastWarning.getLineNumber(),
+										lastWarning.getOffset(), 1, path));
+							}
+							else
+							{
+								items.add(createError(reason, line, character, 1, path));
+							}
+						}
+						else
+						{
+							items.add(createWarning(reason, line, character, 1, path));
+						}
+					}
+					return null;
+				}
+			});
+			return items;
+		}
+
+		void runLint(final String source, final Map<String, Object> options)
+		{
+			contextFactory.call(new ContextAction()
+			{
+				public Object run(Context cx)
+				{
+					Object[] args = new Object[] { source, optionsAsJavaScriptObject(options) };
+					Function lintFunc = (Function) scope.get("JSLINT", scope); //$NON-NLS-1$
+					// PC: we ignore the result, because i have found that with some versions, there might
+					// be errors but this function returned true (false == errors)
+					lintFunc.call(cx, scope, scope, args);
+					return null;
+				}
+			});
+		}
+
+		private Scriptable optionsAsJavaScriptObject(final Map<String, Object> options)
+		{
+			return (Scriptable) contextFactory.call(new ContextAction()
+			{
+				public Object run(Context cx)
+				{
+					Scriptable opts = cx.newObject(scope);
+					for (Map.Entry<String, Object> entry : options.entrySet())
+					{
+						String key = entry.getKey();
+						Object value = javaToJS(entry.getValue(), opts);
+						opts.put(key, opts, value);
+					}
+					return opts;
+				}
+			});
+		}
+
+		Object javaToJS(Object o, Scriptable scope)
+		{
+			Class<?> cls = o.getClass();
+			if (cls.isArray())
+			{
+				return new NativeArray((Object[]) o);
+			}
+
+			return Context.javaToJS(o, scope);
 		}
 	}
 
