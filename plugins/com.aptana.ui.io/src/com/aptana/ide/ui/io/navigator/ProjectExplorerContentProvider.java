0039863185f38981f0824050a010ada0e3609314
mxia <mxia@appcelerator.com>
2012-07-03 14:56:06 -0700

    Fixed APSTUD-796: Project Explorer was showing duplicate files and folders.

--- plugins/com.aptana.ui.io/src/com/aptana/ide/ui/io/navigator/ProjectExplorerContentProvider.java
+++ plugins/com.aptana.ui.io/src/com/aptana/ide/ui/io/navigator/ProjectExplorerContentProvider.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -10,14 +10,26 @@ package com.aptana.ide.ui.io.navigator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 
+import com.aptana.core.util.ArrayUtil;
 import com.aptana.ide.core.io.CoreIOPlugin;
 
 public class ProjectExplorerContentProvider extends FileTreeContentProvider
 {
 
 	private static final String LOCAL_SHORTCUTS_ID = "com.aptana.ide.core.io.localShortcuts"; //$NON-NLS-1$
+
+	@Override
+	public Object[] getChildren(Object parentElement)
+	{
+		if (parentElement instanceof IResource)
+		{
+			return ArrayUtil.NO_OBJECTS;
+		}
+		return super.getChildren(parentElement);
+	}
 
 	@Override
 	public Object[] getElements(Object inputElement)
