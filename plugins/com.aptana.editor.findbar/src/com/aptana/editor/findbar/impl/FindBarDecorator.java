19c9b0f2bb17d79373dd7b35f41413b5c09eb11f
Michael Xia <mxia@appcelerator.com>
2012-07-26 16:52:17 -0700

    Fixed TISTUD-605: update 'show match count' text dynamically when the search text changes.

--- plugins/com.aptana.editor.findbar/src/com/aptana/editor/findbar/impl/FindBarDecorator.java
+++ plugins/com.aptana.editor.findbar/src/com/aptana/editor/findbar/impl/FindBarDecorator.java
@@ -259,7 +259,7 @@ public class FindBarDecorator implements IFindBarDecorator, SelectionListener
 		public void keyPressed(KeyEvent e)
 		{
 			boolean isCommandCtrl = e.stateMask == SWT.MOD1;
-			boolean isCKey = e.character == 'A' || e.character == 'a'; //$NON-NLS-1$ //$NON-NLS-2$
+			boolean isCKey = e.character == 'A' || e.character == 'a';
 
 			if (isCommandCtrl && isCKey)
 			{
@@ -480,9 +480,7 @@ public class FindBarDecorator implements IFindBarDecorator, SelectionListener
 				findButton.setEnabled(!isTextFindEmpty());
 				if (countMatches.getSelection())
 				{
-					FindBarDecorator.findBarConfiguration.toggle(countMatchesOption.preferencesKey);
-					countMatches.setText(StringUtil.EMPTY);
-
+					showCountTotal();
 					countMatches.getParent().getParent().layout(new Control[] { countMatches.getParent() });
 				}
 			}
@@ -1667,7 +1665,7 @@ public class FindBarDecorator implements IFindBarDecorator, SelectionListener
 			if (isWholeWord && !isRegEx && isWord(convertedText))
 			{
 				isRegEx = true;
-				convertedText = REGEX_WORD_BOUNDARY + convertedText + REGEX_WORD_BOUNDARY; //$NON-NLS-1$ //$NON-NLS-2$
+				convertedText = REGEX_WORD_BOUNDARY + convertedText + REGEX_WORD_BOUNDARY;
 			}
 
 			IStatusLineManager statusLineManager = (IStatusLineManager) textEditor.getAdapter(IStatusLineManager.class);
