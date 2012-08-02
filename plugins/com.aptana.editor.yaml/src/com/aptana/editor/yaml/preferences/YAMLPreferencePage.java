3a211beeb41d485b7c6020b26a64f490a5b76c20
Michael Xia <mxia@appcelerator.com>
2012-08-02 16:40:07 -0700

    Fixed APSTUD-7244: YAML spaces setting wasn't being saved when uses global default.

--- plugins/com.aptana.editor.yaml/src/com/aptana/editor/yaml/preferences/YAMLPreferencePage.java
+++ plugins/com.aptana.editor.yaml/src/com/aptana/editor/yaml/preferences/YAMLPreferencePage.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -41,4 +41,21 @@ public class YAMLPreferencePage extends CommonEditorPreferencePage
 		return YAMLEditor.getChainedPreferenceStore();
 	}
 
+	@Override
+	protected IEclipsePreferences getDefaultPluginPreferenceStore()
+	{
+		return EclipseUtil.defaultScope().getNode(YAMLPlugin.PLUGIN_ID);
+	}
+
+	@Override
+	protected boolean getDefaultSpacesForTabs()
+	{
+		return IYAMLPreferenceConstants.DEFAULT_YAML_SPACES_FOR_TABS;
+	}
+
+	@Override
+	protected int getDefaultTabWidth()
+	{
+		return IYAMLPreferenceConstants.DEFAULT_YAML_TAB_WIDTH;
+	}
 }
