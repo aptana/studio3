ca7b7c4d0e38fd108e9ae97f4d0a6c85aa664dd8
Michael Xia <mxia@appcelerator.com>
2012-08-02 16:40:07 -0700

    Fixed APSTUD-7244: YAML spaces setting wasn't being saved when uses global default.

---
+++ plugins/com.aptana.editor.yaml/src/com/aptana/editor/yaml/preferences/IYAMLPreferenceConstants.java
@@ -0,0 +1,25 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2005-20112 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.editor.yaml.preferences;
+
+/**
+ * IYAMLPreferenceConstants
+ */
+
+public interface IYAMLPreferenceConstants
+{
+	/**
+	 * Default value of tab width for yaml editor
+	 */
+	int DEFAULT_YAML_TAB_WIDTH = 2;
+
+	/**
+	 * Default value of spaces for tabs for yaml editor
+	 */
+	boolean DEFAULT_YAML_SPACES_FOR_TABS = true;
+}
--- plugins/com.aptana.editor.yaml/src/com/aptana/editor/yaml/preferences/YAMLPreferenceInitializer.java
+++ plugins/com.aptana.editor.yaml/src/com/aptana/editor/yaml/preferences/YAMLPreferenceInitializer.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -24,11 +24,18 @@ public class YAMLPreferenceInitializer extends AbstractPreferenceInitializer
 		IEclipsePreferences prefs = EclipseUtil.defaultScope().getNode(YAMLPlugin.PLUGIN_ID);
 		prefs.putBoolean(IPreferenceConstants.EDITOR_AUTO_INDENT, true);
 		prefs.putBoolean(IPreferenceConstants.EDITOR_ENABLE_FOLDING, true);
-		prefs.putBoolean(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_SPACES_FOR_TABS, true);
-		prefs.putInt(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_TAB_WIDTH, 2);
 
 		// mark occurrences
 		// prefs.putBoolean(com.aptana.editor.common.preferences.IPreferenceConstants.EDITOR_MARK_OCCURRENCES, true);
-	}
 
+		// Check if we previously set preference to use global defaults
+		IEclipsePreferences instanceScopePref = EclipseUtil.instanceScope().getNode(YAMLPlugin.PLUGIN_ID);
+		if (!instanceScopePref.getBoolean(IPreferenceConstants.USE_GLOBAL_DEFAULTS, false))
+		{
+			prefs.putInt(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_TAB_WIDTH,
+					IYAMLPreferenceConstants.DEFAULT_YAML_TAB_WIDTH);
+			prefs.putBoolean(AbstractDecoratedTextEditorPreferenceConstants.EDITOR_SPACES_FOR_TABS,
+					IYAMLPreferenceConstants.DEFAULT_YAML_SPACES_FOR_TABS);
+		}
+	}
 }
