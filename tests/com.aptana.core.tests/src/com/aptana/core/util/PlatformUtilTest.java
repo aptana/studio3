638c1c9d74c4abe1d13afe099441dde5e6178461
Christopher Williams <cwilliams@appcelerator.com>
2012-08-30 08:52:33 -0400

    try to fix intermittent test failures on linux build machine due to process showing as [sh]

--- tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
+++ tests/com.aptana.core.tests/src/com/aptana/core/util/PlatformUtilTest.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -9,6 +9,7 @@
 package com.aptana.core.util;
 
 import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.Arrays;
 
 import junit.framework.TestCase;
@@ -21,20 +22,25 @@ import com.aptana.core.util.PlatformUtil.ProcessItem;
  * @author Max Stepanov
  */
 @SuppressWarnings("nls")
-public class PlatformUtilTest extends TestCase {
+public class PlatformUtilTest extends TestCase
+{
 
-	public void testGetRunningProcesses() {
+	public void testGetRunningProcesses()
+	{
 		ProcessItem[] processes = PlatformUtil.getRunningProcesses();
 		assertTrue(processes.length > 0);
-		for (ProcessItem i : processes) {
+		for (ProcessItem i : processes)
+		{
 			assertTrue(i.getPid() > 0);
 			assertTrue(i.getExecutableName().trim().length() > 0);
 			assertNotNull(i.toString());
 		}
 	}
 
-	public void testGetRunningChildProcesses() throws IOException {
-		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform.getOS()) ? "sleep 5s" : "sh");
+	public void testGetRunningChildProcesses() throws IOException
+	{
+		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform
+				.getOS()) ? "sleep 5s" : "sh");
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
 		ProcessBuilder processBuilder = new ProcessBuilder(command);
@@ -42,16 +48,29 @@ public class PlatformUtilTest extends TestCase {
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		boolean passed = false;
-		for (ProcessItem i : processes) {
-			if (passed = i.getExecutableName().toLowerCase().endsWith(cmd)) {
+		for (ProcessItem i : processes)
+		{
+			String lowerCaseProcessName = i.getExecutableName().toLowerCase();
+			if (passed = lowerCaseProcessName.endsWith(cmd))
+			{
 				break;
 			}
+			// Sometimes linux shows "[sh]" as process name
+			if ("sh".equals(cmd))
+			{
+				if (passed = lowerCaseProcessName.endsWith("[sh]"))
+				{
+					break;
+				}
+			}
 		}
-		assertTrue("Expected child process \""+cmd+"\" not found in "+Arrays.asList(processes).toString(), passed);
+		assertTrue("Expected child process \"" + cmd + "\" not found in " + Arrays.asList(processes).toString(), passed);
 	}
 
-	public void testKillProcesses() throws IOException {
-		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform.getOS()) ? "sleep 5s" : "sh");
+	public void testKillProcesses() throws IOException
+	{
+		String cmd = Platform.OS_WIN32.equals(Platform.getOS()) ? "cmd.exe" : (Platform.OS_MACOSX.equals(Platform
+				.getOS()) ? "sleep 5s" : "sh");
 		String[] command = Platform.OS_WIN32.equals(Platform.getOS()) ? new String[] { "cmd.exe", "/C", "sleep 5s" }
 				: new String[] { "sh", "-c", "sleep 5s" };
 		ProcessBuilder processBuilder = new ProcessBuilder(command);
@@ -59,36 +78,54 @@ public class PlatformUtilTest extends TestCase {
 		ProcessItem[] processes = PlatformUtil.getRunningChildProcesses();
 		assertTrue(processes.length > 0);
 		int pid = 0;
-		for (ProcessItem i : processes) {
-			if (i.getExecutableName().toLowerCase().endsWith(cmd)) {
+
+		for (ProcessItem i : processes)
+		{
+			String lowerCaseProcessName = i.getExecutableName().toLowerCase();
+			if (lowerCaseProcessName.endsWith(cmd))
+			{
+				pid = i.getPid();
+				break;
+			}
+			// Sometimes linux shows "[sh]" as process name
+			if ("sh".equals(cmd) && lowerCaseProcessName.endsWith("[sh]"))
+			{
 				pid = i.getPid();
 				break;
 			}
 		}
-		assertTrue("Expected child process \""+cmd+"\" not found in "+Arrays.asList(processes).toString(), pid > 0);
+		assertTrue("Expected child process \"" + cmd + "\" not found in " + Arrays.asList(processes).toString(),
+				pid > 0);
 		PlatformUtil.killProcess(pid);
 		processes = PlatformUtil.getRunningChildProcesses();
-		for (ProcessItem i : processes) {
-			assertNotSame("Expected process did not terminate on kill command", pid, i.getPid());
+		for (ProcessItem i : processes)
+		{
+			assertNotSame(MessageFormat.format("Expected process {0} did not terminate on kill command", cmd), pid,
+					i.getPid());
 		}
 	}
 
-	public void testExpandEnvironmentStrings() {
+	public void testExpandEnvironmentStrings()
+	{
 		assertEquals("abc/d", PlatformUtil.expandEnvironmentStrings("abc/d"));
 		expandAndCompareEnvironmentStrings("~", "/test");
 		expandAndCompareEnvironmentStrings(PlatformUtil.HOME_DIRECTORY, "/test");
-		if (Platform.OS_WIN32.equals(Platform.getOS())) {
+		if (Platform.OS_WIN32.equals(Platform.getOS()))
+		{
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.COMMON_APPDATA, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.LOCAL_APPDATA, "/test");
-		} else if (Platform.OS_MACOSX.equals(Platform.getOS())) {
+		}
+		else if (Platform.OS_MACOSX.equals(Platform.getOS()))
+		{
 			expandAndCompareEnvironmentStrings(PlatformUtil.DESKTOP_DIRECTORY, "/test");
 			expandAndCompareEnvironmentStrings(PlatformUtil.DOCUMENTS_DIRECTORY, "/test");
 		}
 	}
 
-	private void expandAndCompareEnvironmentStrings(String token, String path) {
+	private void expandAndCompareEnvironmentStrings(String token, String path)
+	{
 		String expanded = PlatformUtil.expandEnvironmentStrings(token + path);
 		assertNotSame(token + path, expanded);
 		assertTrue(expanded.endsWith(path));
