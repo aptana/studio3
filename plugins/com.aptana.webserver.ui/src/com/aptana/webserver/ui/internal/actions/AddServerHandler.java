7d64d5053dc341f51e375d1edaa8c4f09f694c01
Michael Xia <mxia@appcelerator.com>
2012-09-05 15:41:50 -0700

    Fixed the exception when clicking on the + icon in Servers veiw to add a server.

--- plugins/com.aptana.webserver.ui/src/com/aptana/webserver/ui/internal/actions/AddServerHandler.java
+++ plugins/com.aptana.webserver.ui/src/com/aptana/webserver/ui/internal/actions/AddServerHandler.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -12,6 +12,7 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.window.Window;
@@ -50,11 +51,17 @@ public class AddServerHandler extends AbstractHandler
 				{
 					Identifiable identifiable = (Identifiable) element;
 					String id = identifiable.getId();
+					ImageRegistry imageRegistry = WebServerUIPlugin.getDefault().getImageRegistry();
+					Image image = imageRegistry.get(id);
+					if (image != null)
+					{
+						return image;
+					}
 					ImageDescriptor desc = ImageAssociations.getInstance().getImageDescriptor(id);
 					if (desc != null)
 					{
-						WebServerUIPlugin.getDefault().getImageRegistry().put(id, desc);
-						return WebServerUIPlugin.getDefault().getImageRegistry().get(id);
+						imageRegistry.put(id, desc);
+						return imageRegistry.get(id);
 					}
 				}
 				return WebServerUIPlugin.getImage(WebServerUIPlugin.SERVER_ICON);
