da419da822d0ac85afb280016d44f01bc1915946
Shalom Gibly <sgibly@appcelerator.com>
2012-09-19 15:08:16 -0700

    TISTUD-2301 - Add functionality to get the machine's IPs.

--- plugins/com.aptana.debug.core/src/com/aptana/debug/core/util/DebugUtil.java
+++ plugins/com.aptana.debug.core/src/com/aptana/debug/core/util/DebugUtil.java
@@ -8,9 +8,16 @@
 package com.aptana.debug.core.util;
 
 import java.io.IOException;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.IAdaptable;
@@ -18,9 +25,12 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IProcess;
 
+import com.aptana.core.logging.IdeLog;
 import com.aptana.core.resources.IUniformResource;
+import com.aptana.core.util.CollectionsUtil;
 import com.aptana.core.util.EclipseUtil;
 import com.aptana.core.util.SocketUtil;
+import com.aptana.debug.core.DebugCorePlugin;
 import com.aptana.debug.core.IDebugScopes;
 
 /**
@@ -28,9 +38,8 @@ import com.aptana.debug.core.IDebugScopes;
  */
 public final class DebugUtil
 {
-
+	private static final Set<String> LOCAL_HOSTS = CollectionsUtil.newSet("lo", "lo0", "localhost", "127.0.0.1"); //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$ //$NON-NLS-4$
 	private static final int DEFAULT_PORT = 8999;
-
 	private static final int SOCKET_TIMEOUT = 30000;
 
 	private DebugUtil()
@@ -144,5 +153,47 @@ public final class DebugUtil
 			socket.setSoTimeout(SOCKET_TIMEOUT);
 		}
 		return socket;
+	}
+
+	/**
+	 * Resolve and return the machine's IPs. The returned list does not hold the localhost.
+	 * 
+	 * @return The resolved IPs (all IPv4)
+	 */
+	public static List<String> getHostIPs()
+	{
+		List<String> hosts = new ArrayList<String>(5);
+		try
+		{
+			Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+			while (interfaces.hasMoreElements())
+			{
+				NetworkInterface nextInterface = interfaces.nextElement();
+				String interfaceName = nextInterface.getName();
+				if (!LOCAL_HOSTS.contains(interfaceName.toLowerCase()))
+				{
+					Enumeration<InetAddress> inetAddresses = nextInterface.getInetAddresses();
+					while (inetAddresses.hasMoreElements())
+					{
+						InetAddress address = inetAddresses.nextElement();
+						if (!(address instanceof Inet6Address))
+						{
+							// We only collect IPv4 addressed.
+							String addressString = address.toString();
+							if (addressString.startsWith("/")) //$NON-NLS-1$
+							{
+								addressString = addressString.substring(1);
+							}
+							hosts.add(addressString);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			IdeLog.logError(DebugCorePlugin.getDefault(), "Error resolving hosts", e); //$NON-NLS-1$
+		}
+		return hosts;
 	}
 }
