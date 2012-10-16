a26e8b7f15f78447ebfa577b34380bc83e6dc16c
Christopher Williams <cwilliams@appcelerator.com>
2012-10-16 10:03:38 -0400

    Force collecting and attaching comments for JS File indexing and JS Content assist   - This will have a performance impact   - This is necessary for file indexing, since we use type info gained from docs   - This fixes a failing unit test on the build machine by enabling comments in JS CA, but it's possible there may be other ways around it (i.e. looking up info from index to supplement what we get from parse and use more accurate - though possibly out-of-date type info from index?)

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSContentAssistProcessor.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSContentAssistProcessor.java
@@ -828,7 +828,7 @@ public class JSContentAssistProcessor extends CommonContentAssistProcessor
 			int startingOffset = (activeRange != null) ? activeRange.getStartingOffset() : 0;
 
 			// create parse state and turn off all processing of comments
-			JSParseState parseState = new JSParseState(source, startingOffset, false, false);
+			JSParseState parseState = new JSParseState(source, startingOffset, true, true);
 
 			// parse and grab resulting AST
 			IParseNode ast = ParserPoolFactory.parse(IJSConstants.CONTENT_TYPE_JS, parseState).getRootNode();
