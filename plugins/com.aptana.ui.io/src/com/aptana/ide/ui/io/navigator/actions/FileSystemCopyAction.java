65c90ac42e25f34e92cbf34fa642cdd4765d6521
Michael Xia <mxia@appcelerator.com>
2012-10-17 16:46:23 -0700

    Fixed APSTUD-7509: cannot upload file via ftp.

--- plugins/com.aptana.ui.io/src/com/aptana/ide/ui/io/navigator/actions/FileSystemCopyAction.java
+++ plugins/com.aptana.ui.io/src/com/aptana/ide/ui/io/navigator/actions/FileSystemCopyAction.java
@@ -10,7 +10,11 @@ package com.aptana.ide.ui.io.navigator.actions;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.util.LocalSelectionTransfer;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -25,133 +29,174 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.BaseSelectionListenerAction;
 
+import com.aptana.core.io.efs.EFSUtils;
+import com.aptana.ide.ui.io.FileSystemUtils;
 import com.aptana.ide.ui.io.IOUIPlugin;
-import com.aptana.ide.ui.io.Utils;
 
 /**
  * @author Michael Xia (mxia@aptana.com)
  */
-public class FileSystemCopyAction extends BaseSelectionListenerAction {
+public class FileSystemCopyAction extends BaseSelectionListenerAction
+{
 
-    /**
-     * The id of this action
-     */
-    public static final String ID = IOUIPlugin.PLUGIN_ID + ".CopyAction"; //$NON-NLS-1$
+	/**
+	 * The id of this action
+	 */
+	public static final String ID = IOUIPlugin.PLUGIN_ID + ".CopyAction"; //$NON-NLS-1$
 
-    /**
-     * The shell in which to show any dialogs
-     */
-    private Shell fShell;
+	/**
+	 * The shell in which to show any dialogs
+	 */
+	private Shell fShell;
 
-    /**
-     * System clipboard
-     */
-    private Clipboard fClipboard;
+	/**
+	 * System clipboard
+	 */
+	private Clipboard fClipboard;
 
-    private List<IFileStore> fFileStores;
+	private List<IFileStore> fFileStores;
 
-    /**
-     * Associated paste action. May be <code>null</code>
-     */
-    private FileSystemPasteAction fPasteAction;
+	/**
+	 * Associated paste action. May be <code>null</code>
+	 */
+	private FileSystemPasteAction fPasteAction;
 
-    public FileSystemCopyAction(Shell shell, Clipboard clipboard) {
-        super(Messages.FileSystemCopyAction_TXT);
-        fShell = shell;
-        fClipboard = clipboard;
-        fFileStores = new ArrayList<IFileStore>();
+	public FileSystemCopyAction(Shell shell, Clipboard clipboard)
+	{
+		super(Messages.FileSystemCopyAction_TXT);
+		fShell = shell;
+		fClipboard = clipboard;
+		fFileStores = new ArrayList<IFileStore>();
 
-        setToolTipText(Messages.FileSystemCopyAction_TTP);
-        setId(ID);
-        PlatformUI.getWorkbench().getHelpSystem().setHelp(this, "CopyHelpId"); //$NON-NLS-1$
-    }
+		setToolTipText(Messages.FileSystemCopyAction_TTP);
+		setId(ID);
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(this, "CopyHelpId"); //$NON-NLS-1$
+	}
 
-    public FileSystemCopyAction(Shell shell, Clipboard clipboard, FileSystemPasteAction pasteAction) {
-        this(shell, clipboard);
-        fPasteAction = pasteAction;
-    }
+	public FileSystemCopyAction(Shell shell, Clipboard clipboard, FileSystemPasteAction pasteAction)
+	{
+		this(shell, clipboard);
+		fPasteAction = pasteAction;
+	}
 
-    @Override
-    public void run() {
-        // Get the file names and a string representation
-        IFileStore[] fileStores = fFileStores.toArray(new IFileStore[fFileStores.size()]);
-        String[] fileNames = new String[fileStores.length];
-        StringBuilder buf = new StringBuilder();
-        for (int i = 0; i < fileStores.length; ++i) {
-            fileNames[i] = fileStores[i].toString();
-            if (i > 0) {
-                buf.append('\n'); // $codepro.audit.disable platformSpecificLineSeparator
-            }
-            buf.append(fileStores[i].getName());
-        }
-        setClipboard(fileStores, fileNames, buf.toString());
+	@Override
+	public void run()
+	{
+		// Get the file names and a string representation
+		IFileStore[] fileStores = fFileStores.toArray(new IFileStore[fFileStores.size()]);
+		String[] fileNames = new String[fileStores.length];
+		StringBuilder buf = new StringBuilder();
+		for (int i = 0; i < fileStores.length; ++i)
+		{
+			fileNames[i] = fileStores[i].toString();
+			if (i > 0)
+			{
+				buf.append('\n'); // $codepro.audit.disable platformSpecificLineSeparator
+			}
+			buf.append(fileStores[i].getName());
+		}
+		setClipboard(fileStores, fileNames, buf.toString());
 
-        // update the enablement of the paste action
-        // workaround since the clipboard does not support callbacks
-        if (fPasteAction != null && fPasteAction.getStructuredSelection() != null) {
-            fPasteAction.selectionChanged(fPasteAction.getStructuredSelection());
-        }
-    }
+		// update the enablement of the paste action
+		// workaround since the clipboard does not support callbacks
+		if (fPasteAction != null && fPasteAction.getStructuredSelection() != null)
+		{
+			fPasteAction.selectionChanged(fPasteAction.getStructuredSelection());
+		}
+	}
 
-    @Override
-    protected boolean updateSelection(IStructuredSelection selection) {
-        fFileStores.clear();
-        if (!super.updateSelection(selection)) {
-            return false;
-        }
+	@Override
+	protected boolean updateSelection(IStructuredSelection selection)
+	{
+		fFileStores.clear();
+		if (!super.updateSelection(selection))
+		{
+			return false;
+		}
 
-        if (selection == null || selection.isEmpty()) {
-            return false;
-        }
-        Object[] elements = selection.toArray();
+		if (selection == null || selection.isEmpty())
+		{
+			return false;
+		}
+		Object[] elements = selection.toArray();
 
-        IFileStore fileStore;
-        for (Object element : elements) {
-            fileStore = Utils.getFileStore(element);
-            if (fileStore != null) {
-                fFileStores.add(fileStore);
-            }
-        }
-        if (fFileStores.size() == 0) {
-            return false;
-        }
+		IFileStore fileStore;
+		for (Object element : elements)
+		{
+			fileStore = getFileStore(element);
+			if (fileStore != null)
+			{
+				fFileStores.add(fileStore);
+			}
+		}
+		if (fFileStores.size() == 0)
+		{
+			return false;
+		}
 
-        // must have a common parent
-        IFileStore firstParent = fFileStores.get(0).getParent();
-        if (firstParent == null) {
-            return false;
-        }
+		// must have a common parent
+		IFileStore firstParent = fFileStores.get(0).getParent();
+		if (firstParent == null)
+		{
+			return false;
+		}
 
-        for (IFileStore store : fFileStores) {
-            if (!store.getParent().equals(firstParent)) {
-                return false;
-            }
-        }
+		for (IFileStore store : fFileStores)
+		{
+			if (!store.getParent().equals(firstParent))
+			{
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private void setClipboard(IFileStore[] fileStores, String[] fileNames, String names) {
-        try {
-            // set the clipboard contents
-            LocalSelectionTransfer selectionTransfer = LocalSelectionTransfer.getTransfer();
-            selectionTransfer.setSelection(new StructuredSelection(fileStores));
-            if (fileNames.length > 0) {
-                fClipboard.setContents(new Object[] { fileStores, fileNames, names },
-                        new Transfer[] { LocalSelectionTransfer.getTransfer(),
-                                FileTransfer.getInstance(), TextTransfer.getInstance() });
-            } else {
-                fClipboard.setContents(new Object[] { fileStores, names }, new Transfer[] {
-                        LocalSelectionTransfer.getTransfer(), TextTransfer.getInstance() });
-            }
-        } catch (SWTError e) {
-            if (e.code != DND.ERROR_CANNOT_SET_CLIPBOARD) {
-                throw e; // $codepro.audit.disable thrownExceptions
-            }
-            if (MessageDialog.openQuestion(fShell, "Problem with copy title", //$NON-NLS-1$
-                    "Problem with copy.")) { //$NON-NLS-1$
-                setClipboard(fileStores, fileNames, names);
-            }
-        }
-    }
+	private void setClipboard(IFileStore[] fileStores, String[] fileNames, String names)
+	{
+		try
+		{
+			// set the clipboard contents
+			LocalSelectionTransfer selectionTransfer = LocalSelectionTransfer.getTransfer();
+			selectionTransfer.setSelection(new StructuredSelection(fileStores));
+			if (fileNames.length > 0)
+			{
+				fClipboard.setContents(new Object[] { fileStores, fileNames, names }, new Transfer[] {
+						LocalSelectionTransfer.getTransfer(), FileTransfer.getInstance(), TextTransfer.getInstance() });
+			}
+			else
+			{
+				fClipboard.setContents(new Object[] { fileStores, names },
+						new Transfer[] { LocalSelectionTransfer.getTransfer(), TextTransfer.getInstance() });
+			}
+		}
+		catch (SWTError e)
+		{
+			if (e.code != DND.ERROR_CANNOT_SET_CLIPBOARD)
+			{
+				throw e; // $codepro.audit.disable thrownExceptions
+			}
+			if (MessageDialog.openQuestion(fShell, "Problem with copy title", //$NON-NLS-1$
+					"Problem with copy.")) { //$NON-NLS-1$
+				setClipboard(fileStores, fileNames, names);
+			}
+		}
+	}
+
+	private static IFileStore getFileStore(Object adaptable)
+	{
+		if (adaptable instanceof IAdaptable)
+		{
+			IResource resource = (IResource) ((IAdaptable) adaptable).getAdapter(IResource.class);
+			try
+			{
+				return EFS.getStore(resource.getLocationURI());
+			}
+			catch (CoreException e)
+			{
+				return EFSUtils.getFileStore(resource);
+			}
+		}
+		return FileSystemUtils.getFileStore(adaptable);
+	}
 }
