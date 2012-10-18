f0a1fa148796af557a1c9efe3d5d536ba8bb4f9f
pinnamur <pinnamur@gmail.com>
2012-10-17 18:12:24 -0700

    TISTUD-2550	Issue install using NPM to install within Titanium Studio in Windows 7

--- plugins/com.aptana.ui/src/com/aptana/ui/dialogs/PasswordPromptDialog.java
+++ plugins/com.aptana.ui/src/com/aptana/ui/dialogs/PasswordPromptDialog.java
@@ -1,6 +1,6 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2011 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
@@ -27,6 +27,9 @@ import com.aptana.core.util.StringUtil;
 import com.aptana.ui.UIPlugin;
 
 /**
+ * Note: Moved from com.aptana.ide.ui.io.auth package to re-use it
+ * in different plugins.
+ * 
  * @author Max Stepanov
  */
 public class PasswordPromptDialog extends TitleAreaDialog
