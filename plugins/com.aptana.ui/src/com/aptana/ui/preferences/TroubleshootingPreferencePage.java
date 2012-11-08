4ba72e60981f4f9084ee15e2b0042b62ce4eac93
Christopher Williams <cwilliams@appcelerator.com>
2012-11-08 13:17:04 -0500

    Fix style of buttons for TISTUD-2577 Troubleshooting: add select and deselect all to debug specific component

--- plugins/com.aptana.ui/src/com/aptana/ui/preferences/TroubleshootingPreferencePage.java
+++ plugins/com.aptana.ui/src/com/aptana/ui/preferences/TroubleshootingPreferencePage.java
@@ -155,7 +155,7 @@ public class TroubleshootingPreferencePage extends FieldEditorPreferencePage imp
 		buttonComp.setLayout(GridLayoutFactory.fillDefaults().numColumns(2).create());
 		buttonComp.setLayoutData(GridDataFactory.fillDefaults().create());
 
-		Button selectAllButton = new Button(buttonComp, SWT.BORDER);
+		Button selectAllButton = new Button(buttonComp, SWT.PUSH);
 		selectAllButton.setText(Messages.TroubleshootingPreferencePage_SelectAll);
 		selectAllButton.addSelectionListener(new SelectionAdapter()
 		{
@@ -166,7 +166,7 @@ public class TroubleshootingPreferencePage extends FieldEditorPreferencePage imp
 			}
 		});
 
-		Button selectNoneButton = new Button(buttonComp, SWT.BORDER);
+		Button selectNoneButton = new Button(buttonComp, SWT.PUSH);
 		selectNoneButton.setText(Messages.TroubleshootingPreferencePage_SelectNone);
 		selectNoneButton.addSelectionListener(new SelectionAdapter()
 		{
