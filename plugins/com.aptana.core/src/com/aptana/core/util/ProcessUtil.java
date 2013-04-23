e15d030eefdd3c1f894f10ef81033816fdd55890
pinnamur <pinnamur@gmail.com>
2013-04-23 12:30:51 -0700

    TISTUD-4335 Node.ACS studio plugin : Parsing error in extracting published url on windows

--- plugins/com.aptana.core/src/com/aptana/core/util/ProcessUtil.java
+++ plugins/com.aptana.core/src/com/aptana/core/util/ProcessUtil.java
@@ -1,13 +1,18 @@
 /**
  * Aptana Studio
- * Copyright (c) 2005-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2005-2013 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
  * Please see the license.html included with this distribution for details.
  * Any modifications to this file must keep this entire header intact.
  */
 package com.aptana.core.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -130,22 +135,73 @@ public class ProcessUtil
 		return processData(process, null);
 	}
 
+	private static IStatus processData(Process process, File outputFile, File errorFile)
+	{
+		FileInputStream inputStream = null, errorStream = null;
+		try
+		{
+			// Wait until the process exits to start reading from the redirected output file.
+			inputStream = new FileInputStream(outputFile);
+			errorStream = new FileInputStream(errorFile);
+			return processData(inputStream, errorStream, null, null, process, true);
+		}
+		catch (FileNotFoundException e)
+		{
+			IdeLog.logError(CorePlugin.getDefault(), e);
+		}
+		finally
+		{
+			try
+			{
+				if (inputStream != null)
+				{
+					inputStream.close();
+				}
+			}
+			catch (IOException e)
+			{
+			}
+			try
+			{
+				if (errorStream != null)
+				{
+					errorStream.close();
+				}
+			}
+			catch (IOException e)
+			{
+			}
+		}
+		return null;
+	}
+
 	private static IStatus processData(Process process, String input)
+	{
+		return processData(process.getInputStream(), process.getErrorStream(), process.getOutputStream(), input,
+				process, false);
+	}
+
+	private static IStatus processData(InputStream inputStream, InputStream errorStream, OutputStream outputStream,
+			String input, Process process, boolean earlyWait)
 	{
 		String lineSeparator = ResourceUtil.getLineSeparatorValue(null);
 		try
 		{
+			int exitValue = 0;
+			if (earlyWait)
+			{
+				exitValue = process.waitFor();
+			}
 			// Read and write in threads to avoid from choking the process streams
 			OutputStreamThread writerThread = null;
-			if (input != null)
+			if (!StringUtil.isEmpty(input))
 			{
 				// TODO - Use EditorUtils.getEncoding once we have an IFile reference.
 				// Using the UTF-8 will not work for all cases.
-				writerThread = new OutputStreamThread(process.getOutputStream(), input, IOUtil.UTF_8);
+				writerThread = new OutputStreamThread(outputStream, input, IOUtil.UTF_8);
 			}
-			InputStreamGobbler readerGobbler = new InputStreamGobbler(process.getInputStream(), lineSeparator,
-					IOUtil.UTF_8);
-			InputStreamGobbler errorGobbler = new InputStreamGobbler(process.getErrorStream(), lineSeparator, null);
+			InputStreamGobbler readerGobbler = new InputStreamGobbler(inputStream, lineSeparator, IOUtil.UTF_8);
+			InputStreamGobbler errorGobbler = new InputStreamGobbler(errorStream, lineSeparator, null);
 
 			// Start the threads
 			if (writerThread != null)
@@ -154,8 +210,11 @@ public class ProcessUtil
 			}
 			readerGobbler.start();
 			errorGobbler.start();
-			// This will wait till the process is done.
-			int exitValue = process.waitFor();
+			if (!earlyWait)
+			{
+				// This will wait till the process is done.
+				exitValue = process.waitFor();
+			}
 			if (writerThread != null)
 			{
 				writerThread.interrupt();
@@ -190,17 +249,33 @@ public class ProcessUtil
 	 *            The working directory to use for the process.
 	 * @param environment
 	 *            Environment variable map to use for the process.
+	 * @param redirect
+	 *            Whether the output is redirected to a temporary file and read from that.
 	 * @param arguments
 	 *            A List of String arguments to the command.
 	 * @return
 	 */
 	public static IStatus runInBackground(String command, IPath workingDirectory, String input,
-			Map<String, String> environment, String... arguments)
+			Map<String, String> environment, boolean redirect, String... arguments)
 	{
+		File outFile = null, errFile = null;
 		try
 		{
-			Process p = run(command, workingDirectory, environment, arguments);
-			return processData(p, input);
+			List<String> argsList = CollectionsUtil.newList(arguments);
+			if (redirect)
+			{
+				outFile = File.createTempFile("studio", ".out"); //$NON-NLS-1$ //$NON-NLS-2$
+				errFile = File.createTempFile("studio", ".err"); //$NON-NLS-1$ //$NON-NLS-2$
+				CollectionsUtil.addToList(argsList,
+						new String[] { ">", outFile.getAbsolutePath(), "2>", errFile.getAbsolutePath() }); //$NON-NLS-1$ //$NON-NLS-2$
+				Process p = run(command, workingDirectory, environment, argsList.toArray(new String[argsList.size()]));
+				return processData(p, outFile, errFile);
+			}
+			else
+			{
+				Process p = run(command, workingDirectory, environment, arguments);
+				return processData(p, input);
+			}
 		}
 		catch (IOException e)
 		{
@@ -210,6 +285,40 @@ public class ProcessUtil
 		{
 			return e.getStatus();
 		}
+		finally
+		{
+			if (outFile != null)
+			{
+				outFile.delete();
+			}
+			if (errFile != null)
+			{
+				errFile.delete();
+			}
+		}
+	}
+
+	/**
+	 * Launches the process, pipes input to STDIN and returns an IStatus representing the result of execution. Exit code
+	 * of the process is stored in the IStatuse.getCode(). Output is stored in IStatus.getMessage(). A non-zero exit
+	 * code makes it an IStatus with ERROR severity. Otherwise it uses OK severity.
+	 * 
+	 * @param command
+	 *            The executable/script to run
+	 * @param input
+	 *            String input to pipe to STDIN after launching the process.
+	 * @param workingDirectory
+	 *            The working directory to use for the process.
+	 * @param environment
+	 *            Environment variable map to use for the process.
+	 * @param arguments
+	 *            A List of String arguments to the command.
+	 * @return
+	 */
+	public static IStatus runInBackground(String command, IPath workingDirectory, String input,
+			Map<String, String> environment, String... arguments)
+	{
+		return runInBackground(command, workingDirectory, input, environment, false, arguments);
 	}
 
 	/**
