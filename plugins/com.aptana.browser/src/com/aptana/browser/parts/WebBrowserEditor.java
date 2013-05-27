7d011586df2aa90807ca716ea1ca794f45ffb08d
Michael Xia <mxia@appcelerator.com>
2013-05-26 22:15:58 -0700

    Fixed TISTUD-4775: code processor could not show result on Linux.

--- plugins/com.aptana.browser/src/com/aptana/browser/parts/WebBrowserEditor.java
+++ plugins/com.aptana.browser/src/com/aptana/browser/parts/WebBrowserEditor.java
@@ -12,22 +12,24 @@ package com.aptana.browser.parts;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.ProgressEvent;
 import org.eclipse.swt.browser.ProgressListener;
 import org.eclipse.swt.browser.TitleEvent;
 import org.eclipse.swt.browser.TitleListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.internal.browser.BrowserViewer;
 import org.eclipse.ui.internal.browser.WebBrowserEditorInput;
 import org.eclipse.ui.part.EditorPart;
 
 import com.aptana.browser.WebBrowserViewer;
 import com.aptana.core.util.StringUtil;
-import com.aptana.swt.webkitbrowser.WebKitBrowser;
 
 /**
  * @author Max Stepanov
@@ -38,7 +40,7 @@ public class WebBrowserEditor extends EditorPart {
 
 	public static final String EDITOR_ID = "com.aptana.browser.editors.webbrowser"; //$NON-NLS-1$
 	
-	protected WebBrowserViewer webBrowser;
+	protected Browser webBrowser;
 	private int progressWorked;
 	private String initialURL;
 	private Image image;
@@ -71,7 +73,7 @@ public class WebBrowserEditor extends EditorPart {
 			if (wbei.getURL() != null)
 				initialURL = wbei.getURL().toExternalForm();
 			if (webBrowser != null) {
-				webBrowser.setURL(initialURL);
+				webBrowser.setUrl(initialURL);
 				site.getWorkbenchWindow().getActivePage().activate(this);
 			}
 	
@@ -133,9 +135,8 @@ public class WebBrowserEditor extends EditorPart {
 		if (input == null || input.isToolbarLocal()) {
 			style |= WebBrowserViewer.NAVIGATION_BAR;
 		}
-		webBrowser = new WebBrowserViewer(parent, style);
-		WebKitBrowser browser = (WebKitBrowser) webBrowser.getBrowser();
-		browser.addProgressListener(new ProgressListener() {
+		webBrowser = createBrowser(parent, style);
+		webBrowser.addProgressListener(new ProgressListener() {
 			public void changed(ProgressEvent event) {
 				if (event.total == 0) {
 					return;
@@ -156,12 +157,12 @@ public class WebBrowserEditor extends EditorPart {
 				getStatusBarProgressMonitor().done();
 			}
 		});
-		browser.addTitleListener(new TitleListener() {
+		webBrowser.addTitleListener(new TitleListener() {
 			public void changed(TitleEvent event) {
 				setTitleToolTip(event.title);
 			}
 		});
-		webBrowser.setURL(initialURL);
+		webBrowser.setUrl(initialURL);
 	}
 
 	/* (non-Javadoc)
@@ -172,6 +173,16 @@ public class WebBrowserEditor extends EditorPart {
 		if (webBrowser != null) {
 			webBrowser.setFocus();
 		}
+	}
+
+	private Browser createBrowser(Composite parent, int style)
+	{
+		Control browser = new WebBrowserViewer(parent, style).getBrowser();
+		if (browser instanceof Browser)
+		{
+			return (Browser) browser;
+		}
+		return new BrowserViewer(parent, style).getBrowser();
 	}
 
 	private IProgressMonitor getStatusBarProgressMonitor() {
