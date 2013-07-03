98d97bd10a9ad45b1e7f9ff5f6a16bdf3f4a4f42
Christopher Williams <cwilliams@appcelerator.com>
2013-07-03 15:00:42 -0400

    TISTUD-5060 Add support to create github pull requests for current branch   - first cut at getting something working.  ready to run in dev mode under debugger

---
+++ plugins/com.aptana.git.core/src/com/aptana/git/internal/core/github/GithubRepository.java
@@ -0,0 +1,182 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.git.internal.core.github;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.json.simple.JSONObject;
+
+import com.aptana.git.core.GitPlugin;
+import com.aptana.git.core.github.IGithubManager;
+import com.aptana.git.core.github.IGithubRepository;
+import com.aptana.git.core.model.GitRepository;
+
+/**
+ * @author cwilliams
+ */
+public class GithubRepository implements IGithubRepository
+{
+
+	// @formatter:off
+//  {
+//    "id": 1296269,
+//    "owner": {
+//      "login": "octocat",
+//      "id": 1,
+//      "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+//      "gravatar_id": "somehexcode",
+//      "url": "https://api.github.com/users/octocat"
+//    },
+//    "name": "Hello-World",
+//    "full_name": "octocat/Hello-World",
+//    "description": "This your first repo!",
+//    "private": false,
+//    "fork": false,
+//    "url": "https://api.github.com/repos/octocat/Hello-World",
+//    "html_url": "https://github.com/octocat/Hello-World",
+//    "clone_url": "https://github.com/octocat/Hello-World.git",
+//    "git_url": "git://github.com/octocat/Hello-World.git",
+//    "ssh_url": "git@github.com:octocat/Hello-World.git",
+//    "svn_url": "https://svn.github.com/octocat/Hello-World",
+//    "mirror_url": "git://git.example.com/octocat/Hello-World",
+//    "homepage": "https://github.com",
+//    "language": null,
+//    "forks": 9,
+//    "forks_count": 9,
+//    "watchers": 80,
+//    "watchers_count": 80,
+//    "size": 108,
+//    "master_branch": "master",
+//    "open_issues": 0,
+//    "pushed_at": "2011-01-26T19:06:43Z",
+//    "created_at": "2011-01-26T19:01:12Z",
+//    "updated_at": "2011-01-26T19:14:43Z"
+//  }
+//  @formatter:on
+	private static final String ATTR_SSH_URL = "ssh_url"; //$NON-NLS-1$
+	private JSONObject json;
+	private boolean hasDetailed;
+
+	public GithubRepository(JSONObject repo)
+	{
+		this.json = repo;
+		this.hasDetailed = false; // TODO Do a spot check of JSOn keys and check for one we only get in detailed
+									// response?
+	}
+
+	public int getID()
+	{
+		return (Integer) json.get("id");
+	}
+
+	public String getName()
+	{
+		return (String) json.get("name");
+	}
+
+	public boolean isPrivate()
+	{
+		return (Boolean) json.get("private");
+	}
+
+	public boolean isFork()
+	{
+		return (Boolean) json.get("fork");
+	}
+
+	public String getSSHURL()
+	{
+		return (String) json.get(ATTR_SSH_URL);
+	}
+
+	public String getDefaultBranch()
+	{
+		return (String) json.get("default_branch");
+	}
+
+	public String getOwner()
+	{
+		JSONObject owner = (JSONObject) json.get("owner");
+		return (String) owner.get("login");
+	}
+
+	public IGithubRepository getParent() throws CoreException
+	{
+		if (!isFork())
+		{
+			return null;
+		}
+
+		if (!hasDetailedJSON())
+		{
+			// Replace json with this new object and set a flag indicating we have the more detailed data now!
+			this.json = (JSONObject) getAPI().get(getAPIURL());
+			this.hasDetailed = true;
+		}
+		// TODO Keep a cache of the repos inside the manager or something?
+		return new GithubRepository((JSONObject) json.get("parent"));
+	}
+
+	/**
+	 * The base URL for operations on this repo in Github's API
+	 * 
+	 * @return
+	 */
+	protected String getAPIURL()
+	{
+		return "/repos/" + getOwner() + "/" + getName();
+	}
+
+	protected GithubAPI getAPI()
+	{
+		return new GithubAPI(getGithubManager().getUser());
+	}
+
+	private boolean hasDetailedJSON()
+	{
+		return this.hasDetailed;
+	}
+
+	public IStatus createPullRequest(String title, String body, GitRepository repo)
+	{
+		String branch = repo.currentBranch();
+
+		// push current branch to origin first!
+		IStatus pushStatus = repo.push("origin", branch);
+		if (!pushStatus.isOK())
+		{
+			return pushStatus;
+		}
+
+		try
+		{
+			// FIXME Allow user to choose the branch from parent to merge against. Default to the parent's default
+			// branch
+			String base = getParent().getDefaultBranch();
+			// TODO Allow user to choose branch on the fork to use as contents for PR?
+			String head = getGithubManager().getUser().getUsername() + ":" + branch; // Where the change
+																						// is
+			String prJSON = "{\"title\": \"" + title + "\", \"body\": \"" + body + "\", \"head\": \"" + head
+					+ "\", \"base\": \"" + base + "\"}";
+
+			// TODO Do something with the response?
+			getAPI().post(getAPIURL() + "/pulls", prJSON);
+		}
+		catch (CoreException e)
+		{
+			return e.getStatus();
+		}
+		return Status.OK_STATUS;
+	}
+
+	protected IGithubManager getGithubManager()
+	{
+		return GitPlugin.getDefault().getGithubManager();
+	}
+}
---
+++ plugins/com.aptana.git.ui/src/com/aptana/git/ui/internal/actions/CreatePullRequestHandler.java
@@ -0,0 +1,167 @@
+/**
+ * Aptana Studio
+ * Copyright (c) 2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the GNU Public License (GPL) v3 (with exceptions).
+ * Please see the license.html included with this distribution for details.
+ * Any modifications to this file must keep this entire header intact.
+ */
+package com.aptana.git.ui.internal.actions;
+
+import java.text.MessageFormat;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+import com.aptana.git.core.GitPlugin;
+import com.aptana.git.core.github.IGithubManager;
+import com.aptana.git.core.github.IGithubRepository;
+import com.aptana.git.core.github.IGithubUser;
+import com.aptana.git.core.model.GitRepository;
+import com.aptana.git.ui.internal.preferences.GithubAccountPageProvider;
+import com.aptana.ui.util.UIUtils;
+
+/**
+ * @author cwilliams
+ */
+public class CreatePullRequestHandler extends AbstractGitHandler
+{
+	@Override
+	protected Object doExecute(ExecutionEvent event) throws ExecutionException
+	{
+		final GitRepository repo = getSelectedRepository();
+		if (repo == null)
+		{
+			throw new ExecutionException("No git repository selected");
+		}
+		String ghRepoName = getGithubRepoName();
+
+		// Make sure user is logged in!
+		IGithubManager ghManager = GitPlugin.getDefault().getGithubManager();
+		IGithubUser user = ghManager.getUser();
+		if (user == null)
+		{
+			// prompt for login!
+			final GithubAccountPageProvider userInfoProvider = new GithubAccountPageProvider();
+			Dialog dialog = new Dialog(UIUtils.getActiveShell())
+			{
+				@Override
+				protected Control createDialogArea(Composite parent)
+				{
+					Composite composite = (Composite) super.createDialogArea(parent);
+					Control userInfoControl = userInfoProvider.createContents(composite);
+					userInfoControl.setLayoutData(GridDataFactory.fillDefaults().grab(true, false).span(3, 1).create());
+					return composite;
+				}
+
+				@Override
+				protected void okPressed()
+				{
+					if (!userInfoProvider.performOk())
+					{
+						// TODO Show an error message that we were unable to login?
+						return;
+					}
+					super.okPressed();
+				}
+			};
+			if (dialog.open() == Window.CANCEL)
+			{
+				return null; // User cancelled
+			}
+
+			user = ghManager.getUser();
+			if (user == null)
+			{
+				throw new ExecutionException("User is not logged into their Github account");
+			}
+		}
+
+		// Ok, we have the name of the repo on github, the user has logged into github (or we know their credentials and
+		// they work), now lets' generate a pull request!
+		IGithubRepository ghRepo;
+		try
+		{
+			ghRepo = user.getRepo(ghRepoName);
+		}
+		catch (CoreException e)
+		{
+			throw new ExecutionException("Unable to get repository details from github API", e);
+		}
+		// Prompt for title and body!
+		// FIXME Create a much nicer PR title/body dialog!
+		InputDialog id = new InputDialog(UIUtils.getActiveShell(), "Create Pull Request",
+				"Please enter the title of your pull request", "Example PR Title", null);
+		if (id.open() == Window.CANCEL)
+		{
+			return null;
+		}
+
+		String title = id.getValue();
+		String body = "contents";
+		IStatus status = ghRepo.createPullRequest(title, body, repo);
+		// TODO Use toast to display result?
+		if (!status.isOK())
+		{
+			throw new ExecutionException(status.getMessage());
+		}
+		return null;
+	}
+
+	private String getGithubRepoName() throws ExecutionException
+	{
+		final GitRepository repo = getSelectedRepository();
+		if (repo == null)
+		{
+			throw new ExecutionException("No git repository selected");
+		}
+
+		String remoteURL;
+		try
+		{
+			Map<String, String> pairs = repo.remotePairs();
+			remoteURL = pairs.get(GitRepository.ORIGIN);
+			if (remoteURL == null)
+			{
+				// FIXME Loop through and find correct remote? If we find multiple do we need to prompt?
+				throw new ExecutionException("Remote 'origin' not set up");
+			}
+		}
+		catch (CoreException e)
+		{
+			throw new ExecutionException("Unable to get remotes for repository", e);
+		}
+
+		Pattern p = Pattern.compile(".+?github\\.com:[^/]+?/([\\w\\-_]+)\\.git");
+		Matcher m = p.matcher(remoteURL);
+		if (!m.find())
+		{
+			throw new ExecutionException(MessageFormat.format(
+					"Unable to extract repo name from 'origin' remote url: {0}", remoteURL));
+		}
+		return m.group(1);
+	}
+
+	@Override
+	protected boolean calculateEnabled()
+	{
+		try
+		{
+			return getGithubRepoName() != null;
+		}
+		catch (ExecutionException e)
+		{
+			return false;
+		}
+	}
+}
