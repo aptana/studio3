8fadf8363be1ba6bdda6623c7aad324f34a528fd
pinnamur <pinnamur@gmail.com>
2013-09-19 13:35:16 -0700

    TISTUD-5295 Add diagnostic tests for troubleshooting npm configuration issues

--- plugins/com.aptana.js.core/src/com/aptana/js/internal/core/JSDiagnosticLog.java
+++ plugins/com.aptana.js.core/src/com/aptana/js/internal/core/JSDiagnosticLog.java
@@ -7,10 +7,12 @@
  */
 package com.aptana.js.internal.core;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 
 import com.aptana.core.ShellExecutable;
 import com.aptana.core.diagnostic.IDiagnosticLog;
+import com.aptana.core.util.FileUtil;
 import com.aptana.core.util.ProcessUtil;
 import com.aptana.js.core.JSCorePlugin;
 import com.aptana.js.core.node.INodeJSService;
@@ -31,7 +33,7 @@ public class JSDiagnosticLog implements IDiagnosticLog
 		{
 			version = "Not installed"; //$NON-NLS-1$
 		}
-		buf.append("Node.JS Version: ").append(version).append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
+		buf.append("Node.JS Version: ").append(version).append(FileUtil.NEW_LINE); //$NON-NLS-1$ //$NON-NLS-2$
 
 		INodePackageManager npm = JSCorePlugin.getDefault().getNodePackageManager();
 		IPath npmPath = npm.findNPM();
@@ -40,20 +42,42 @@ public class JSDiagnosticLog implements IDiagnosticLog
 		{
 			pathString = npmPath.toOSString();
 		}
-		buf.append("NPM Path: ").append(pathString).append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
+		buf.append("NPM Path: ").append(pathString).append(FileUtil.NEW_LINE); //$NON-NLS-1$ //$NON-NLS-2$
 		if (npmPath != null)
 		{
 			String npmVersion = ProcessUtil.outputForCommand(npmPath.toOSString(), null,
 					ShellExecutable.getEnvironment(), "-v"); //$NON-NLS-1$
-			buf.append("NPM Version: ").append(npmVersion).append('\n'); //$NON-NLS-1$
+			buf.append("NPM Version: ").append(npmVersion).append(FileUtil.NEW_LINE); //$NON-NLS-1$
 
 			String highLevelPackages = getInstalledNpmPackages(npmPath);
 			buf.append(highLevelPackages);
-			buf.append('\n');
+			buf.append(FileUtil.NEW_LINE);
 
+			// Step1 : Get the installed version of a npm package - try to get titanium
+			String npmVersionOutput = ProcessUtil.outputForCommand(npmPath.toOSString(), null,
+					ShellExecutable.getEnvironment(), INodePackageManager.GLOBAL_ARG, "ls", "titanium"); //$NON-NLS-1$ //$NON-NLS-2$
+			buf.append("npm -g ls titanium: ").append(npmVersionOutput).append(FileUtil.NEW_LINE); //$NON-NLS-1$ //$NON-NLS-2$
+
+			// Step2: If Step1 fails for any reason, find the installed version of a package from the detailed list of
+			// all npm packages.
+			// The original command (npm -g ls -p) is slightly different than this one, but the content and intent is
+			// the same.
 			String listOutput = ProcessUtil.outputForCommand(npmPath.toOSString(), null,
 					ShellExecutable.getEnvironment(), INodePackageManager.GLOBAL_ARG, "list"); //$NON-NLS-1$
-			buf.append("Packages: ").append(listOutput).append("\n"); //$NON-NLS-1$ //$NON-NLS-2$
+			buf.append("Packages: ").append(listOutput).append(FileUtil.NEW_LINE); //$NON-NLS-1$ //$NON-NLS-2$
+
+			// NPM config prefix values.
+			buf.append("NPM_CONFIG_PREFIX env value: " + ShellExecutable.getEnvironment().get("NPM_CONFIG_PREFIX")) //$NON-NLS-1$ //$NON-NLS-2$
+					.append(FileUtil.NEW_LINE);
+			try
+			{
+				String configPrefixValue = JSCorePlugin.getDefault().getNodePackageManager().getConfigValue("prefix"); //$NON-NLS-1$
+				buf.append("Npm config prefix value : ").append(configPrefixValue).append(FileUtil.NEW_LINE); //$NON-NLS-1$
+			}
+			catch (CoreException ignore)
+			{
+			}
+
 		}
 		return buf.toString();
 	}
