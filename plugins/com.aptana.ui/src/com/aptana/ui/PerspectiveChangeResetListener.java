fad54510437d4dda500928637016b3302214322e
Christopher Williams <cwilliams@appcelerator.com>
2013-10-18 11:01:16 -0400

    Fix possible NPE

--- plugins/com.aptana.ui/src/com/aptana/ui/PerspectiveChangeResetListener.java
+++ plugins/com.aptana.ui/src/com/aptana/ui/PerspectiveChangeResetListener.java
@@ -49,7 +49,7 @@ public class PerspectiveChangeResetListener extends PerspectiveAdapter
 
 	public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective)
 	{
-		if (perspectiveId.equals(perspective.getId()))
+		if (perspectiveId != null && perspective != null && perspectiveId.equals(perspective.getId()))
 		{
 			int version = Platform.getPreferencesService().getInt(pluginId, preferenceId, 0, null);
 			if (perspectiveVersion > version)
