f293798221fb93688ea869e3377248066bae6cb4
Christopher Williams <cwilliams@appcelerator.com>
2013-10-18 13:45:19 -0400

    Fix NPE I'm seeing locally.

--- plugins/com.aptana.ui/src/com/aptana/ui/PerspectiveChangeResetListener.java
+++ plugins/com.aptana.ui/src/com/aptana/ui/PerspectiveChangeResetListener.java
@@ -68,16 +68,19 @@ public class PerspectiveChangeResetListener extends PerspectiveAdapter
 				}
 			}
 		}
-		String perspectiveId = perspective.getId();
-		if (!StringUtil.isEmpty(perspectiveId))
+		if (perspective != null)
 		{
-			int index = perspectiveId.lastIndexOf("."); //$NON-NLS-1$
-			if (index > -1)
+			String perspectiveId = perspective.getId();
+			if (!StringUtil.isEmpty(perspectiveId))
 			{
-				perspectiveId = perspectiveId.substring(index + 1);
+				int index = perspectiveId.lastIndexOf("."); //$NON-NLS-1$
+				if (index > -1)
+				{
+					perspectiveId = perspectiveId.substring(index + 1);
+				}
+				StudioAnalytics.getInstance().sendEvent(
+						new FeatureEvent(MessageFormat.format(PERSPECTIVE_ACTIVATE_EVENT, perspectiveId), null));
 			}
-			StudioAnalytics.getInstance().sendEvent(
-					new FeatureEvent(MessageFormat.format(PERSPECTIVE_ACTIVATE_EVENT, perspectiveId), null));
 		}
 	}
 
