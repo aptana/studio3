ef553c3b4448410f722948c08f273ae79c3e15ae
Christopher Williams <cwilliams@appcelerator.com>
2014-01-03 11:23:19 -0500

    Add JS tests back in

--- tests/com.aptana.studio.tests.all/src/com/aptana/studio/tests/all/UITests.java
+++ tests/com.aptana.studio.tests.all/src/com/aptana/studio/tests/all/UITests.java
@@ -22,7 +22,7 @@ import org.junit.runners.Suite;
 	com.aptana.editor.dtd.AllTests.class,
 //	com.aptana.editor.html.tests.AllTests.class,
 //	com.aptana.editor.html.formatter.tests.AllTests.class,
-//	com.aptana.editor.js.tests.AllTests.class,
+	com.aptana.editor.js.tests.AllTests.class,
 //	com.aptana.editor.js.formatter.tests.AllTests.class,
 //	com.aptana.editor.json.tests.AllTests.class,
 //	com.aptana.editor.xml.tests.AllTests.class,
