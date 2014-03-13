10c86181ad4a7c5af0d12494c3d52edbf42d9071
Praveen Innamuri <pinnamuri@appcelerator.com>
2014-03-13 15:46:15 -0700

    Adding diagnostic log into the test to investigate the failure on the unit test machine

--- tests/com.aptana.editor.xml.tests/src/com/aptana/editor/xml/contentassist/XMLSourceQuickFixProcessorTest.java
+++ tests/com.aptana.editor.xml.tests/src/com/aptana/editor/xml/contentassist/XMLSourceQuickFixProcessorTest.java
@@ -8,6 +8,8 @@ package com.aptana.editor.xml.contentassist;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Map;
+
 import org.eclipse.core.internal.registry.RegistryProviderFactory;
 import org.eclipse.core.internal.registry.osgi.RegistryProviderOSGI;
 import org.eclipse.core.runtime.CoreException;
@@ -15,6 +17,7 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.RegistryFactory;
 import org.eclipse.core.runtime.spi.IRegistryProvider;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
@@ -22,14 +25,21 @@ import org.eclipse.jface.text.quickassist.IQuickAssistAssistant;
 import org.eclipse.jface.text.quickassist.IQuickAssistInvocationContext;
 import org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
 import org.eclipse.jface.text.source.Annotation;
+import org.hamcrest.Description;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.api.Action;
+import org.jmock.api.Invocation;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.aptana.core.logging.IdeLog;
+import com.aptana.core.logging.IdeLog.StatusLevel;
+import com.aptana.core.util.EclipseUtil;
 import com.aptana.editor.common.AbstractThemeableEditor;
 import com.aptana.editor.common.CommonEditorPlugin;
 import com.aptana.editor.xml.XMLSourceViewerConfiguration;
@@ -40,6 +50,18 @@ public class XMLSourceQuickFixProcessorTest
 	private static final String ERROR_MESSAGE = "error_message";
 	private static final String CONTENT_TYPE = "org.eclipse.core.runtime.xml";
 	private Mockery context;
+
+	@BeforeClass
+	public static void turnUpLogging()
+	{
+		System.err.println("Turning logging to INFO");
+		IdeLog.setCurrentSeverity(StatusLevel.INFO);
+		System.err.println("Turning on all debug options");
+		Map<String, String> currentOptions = EclipseUtil.getTraceableItems();
+		EclipseUtil.setBundleDebugOptions(currentOptions.keySet().toArray(new String[currentOptions.size()]), true);
+		System.err.println("Turning on platform debugging flag");
+		EclipseUtil.setPlatformDebugging(true);
+	}
 
 	@Before
 	public void setUp() throws CoreException
@@ -89,31 +111,47 @@ public class XMLSourceQuickFixProcessorTest
 		final IConfigurationElement element = context.mock(IConfigurationElement.class);
 		final IQuickAssistProcessor quickFixProcessor = createProcessor();
 
+		final Action printLogAction = new Action()
+		{
+			public Object invoke(Invocation invocation) throws Throwable
+			{
+				System.err.println("Invoking:: " + invocation);
+				return null;
+			}
+
+			public void describeTo(Description description)
+			{
+			}
+		};
+
 		final AbstractThemeableEditor editor = context.mock(AbstractThemeableEditor.class);
 
 		context.checking(new Expectations()
 		{
 			{
 				allowing(registryProvider).getRegistry();
-				will(returnValue(extensionRegistry));
+				will(doAll(printLogAction, returnValue(extensionRegistry)));
 
 				oneOf(extensionRegistry).getExtensionPoint(CommonEditorPlugin.PLUGIN_ID, "quickFixProcessors");
-				will(returnValue(extensionPoint));
+				will(doAll(printLogAction, returnValue(extensionPoint)));
 
 				oneOf(extensionPoint).getExtensions();
-				will(returnValue(new IExtension[] { extension }));
+				will(doAll(printLogAction, returnValue(new IExtension[] { extension })));
 
 				oneOf(extension).getConfigurationElements();
-				will(returnValue(new IConfigurationElement[] { element }));
+				will(doAll(printLogAction, returnValue(new IConfigurationElement[] { element })));
 
 				allowing(element).getName();
-				will(returnValue("processor"));
+				will(doAll(printLogAction, returnValue("processor")));
 
 				oneOf(element).getAttribute("contentType");
-				will(returnValue(CONTENT_TYPE));
+				will(doAll(printLogAction, returnValue(CONTENT_TYPE)));
+
+				oneOf(element).getAttributeNames();
+				will(returnValue(new String[] {}));
 
 				oneOf(element).createExecutableExtension("class");
-				will(returnValue(quickFixProcessor));
+				will(doAll(printLogAction, returnValue(quickFixProcessor)));
 
 				allowing(editor).getContentType();
 				will(returnValue(CONTENT_TYPE));
@@ -124,7 +162,7 @@ public class XMLSourceQuickFixProcessorTest
 
 			RegistryProviderFactory.releaseDefault();
 			RegistryFactory.setDefaultRegistryProvider(registryProvider);
-
+			Assert.assertEquals(extensionRegistry, Platform.getExtensionRegistry());
 			XMLSourceViewerConfiguration viewerConfiguration = new XMLSourceViewerConfiguration(null, editor);
 			IQuickAssistAssistant quickAssistant = viewerConfiguration.getQuickAssistAssistant(null);
 			IQuickAssistProcessor assistProcessor = quickAssistant.getQuickAssistProcessor();
