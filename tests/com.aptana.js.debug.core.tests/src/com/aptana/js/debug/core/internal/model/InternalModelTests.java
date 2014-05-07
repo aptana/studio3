e729836120a76616041181b53810b43689cd47fb
Christopher Williams <cwilliams@appcelerator.com>
2014-05-07 14:56:42 -0400

    Add unit tests for JSDebugTarget

--- tests/com.aptana.js.debug.core.tests/src/com/aptana/js/debug/core/internal/model/InternalModelTests.java
+++ tests/com.aptana.js.debug.core.tests/src/com/aptana/js/debug/core/internal/model/InternalModelTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ JSDebugThreadTest.class })
+@SuiteClasses({ JSDebugThreadTest.class, JSDebugTargetTest.class })
 public class InternalModelTests
 {
 
