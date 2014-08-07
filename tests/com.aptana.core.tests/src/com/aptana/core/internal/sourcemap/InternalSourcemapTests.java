018ca1a9d21f5f5d98fd8fdf4a25680c89d2da9a
sgtcoolguy <chris.a.williams@gmail.com>
2014-08-07 09:22:06 -0400

    Add unit test for SourceMapRegistry class

---
+++ tests/com.aptana.core.tests/src/com/aptana/core/internal/sourcemap/InternalSourcemapTests.java
@@ -0,0 +1,12 @@
+package com.aptana.core.internal.sourcemap;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SourceMapRegistryTest.class })
+public class InternalSourcemapTests
+{
+
+}
