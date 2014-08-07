54dfa89213c88708c6d52934bbe1957b0e1fd873
sgtcoolguy <chris.a.williams@gmail.com>
2014-08-07 10:36:26 -0400

    - Add unit tests for AuthenticationManager. - Hide impl in internal package, make plugin manage lifecycle of singleton instance

--- plugins/com.aptana.core.io/src/com/aptana/ide/core/io/internal/auth/AuthenticationManager.java
+++ plugins/com.aptana.core.io/src/com/aptana/ide/core/io/internal/auth/AuthenticationManager.java
@@ -7,12 +7,13 @@
  */
 // $codepro.audit.disable staticFieldNamingConvention
 
-package com.aptana.ide.core.io.auth;
+package com.aptana.ide.core.io.internal.auth;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IAdapterManager;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.equinox.security.storage.ISecurePreferences;
 import org.eclipse.equinox.security.storage.SecurePreferencesFactory;
@@ -20,118 +21,157 @@ import org.eclipse.equinox.security.storage.StorageException;
 
 import com.aptana.core.logging.IdeLog;
 import com.aptana.ide.core.io.CoreIOPlugin;
+import com.aptana.ide.core.io.auth.IAuthenticationManager;
+import com.aptana.ide.core.io.auth.IAuthenticationPrompt;
 
 /**
  * @author Max Stepanov
- *
  */
-public final class AuthenticationManager implements IAuthenticationManager {
+public class AuthenticationManager implements IAuthenticationManager
+{
 
 	private static final String SECURITY_NODE = "com.aptana.core.io.auth"; //$NON-NLS-1$
 	private static final String PROP_PASSWORD = "password"; //$NON-NLS-1$
-	
-	private static AuthenticationManager instance;
-	
-	private transient Map<String, char[]> sessionPasswords = new HashMap<String, char[]>();
-		
+
+	protected transient Map<String, char[]> sessionPasswords;
+
 	/**
 	 * 
 	 */
-	private AuthenticationManager() {
+	public AuthenticationManager()
+	{
+		sessionPasswords = new HashMap<String, char[]>();
 	}
 
-	public static AuthenticationManager getInstance() {
-		if (instance == null) {
-			instance = new AuthenticationManager();
-		}
-		return instance;
-	}
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#hasPersistent(java.lang.String)
 	 */
-	public boolean hasPersistent(String authId) {
+	public boolean hasPersistent(String authId)
+	{
 		ISecurePreferences preferences = getSecurePreferences();
-		if (preferences.nodeExists(authId)) {
+		if (preferences.nodeExists(authId))
+		{
 			ISecurePreferences node = preferences.node(authId);
 			return Arrays.asList(node.keys()).contains(PROP_PASSWORD);
 		}
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#getPassword(java.lang.String)
 	 */
-	public char[] getPassword(String authId) {
+	public char[] getPassword(String authId)
+	{
 		ISecurePreferences preferences = getSecurePreferences();
-		if (preferences.nodeExists(authId)) {
-			try {
+		if (preferences.nodeExists(authId))
+		{
+			try
+			{
 				ISecurePreferences node = preferences.node(authId);
 				String password = node.get(PROP_PASSWORD, null);
-				if (password != null) {
+				if (password != null)
+				{
 					return password.toCharArray();
 				}
-			} catch (StorageException e) {
-				IdeLog.logWarning(CoreIOPlugin.getDefault(), Messages.AuthenticationManager_FailedGetSecurePreference, e);
+			}
+			catch (StorageException e)
+			{
+				IdeLog.logWarning(CoreIOPlugin.getDefault(), Messages.AuthenticationManager_FailedGetSecurePreference,
+						e);
 			}
 		}
-		if (sessionPasswords.containsKey(authId)) {
+		if (sessionPasswords.containsKey(authId))
+		{
 			return sessionPasswords.get(authId);
 		}
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#promptPassword(java.lang.String, java.lang.String, java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#promptPassword(java.lang.String, java.lang.String,
+	 * java.lang.String, java.lang.String)
 	 */
-	public char[] promptPassword(String authId, String login, String title, String message) {
-		IAuthenticationPrompt authPrompt = (IAuthenticationPrompt) Platform.getAdapterManager()
-					.getAdapter(this, IAuthenticationPrompt.class);
-		if (authPrompt == null && Platform.getAdapterManager().hasAdapter(this, IAuthenticationPrompt.class.getName())) {
-			authPrompt = (IAuthenticationPrompt) Platform.getAdapterManager().loadAdapter(this, IAuthenticationPrompt.class.getName());
-		}
-		if (authPrompt != null) {
-			if (authPrompt.promptPassword(this, authId, login, title, message)) {
+	public char[] promptPassword(String authId, String login, String title, String message)
+	{
+		IAuthenticationPrompt authPrompt = getAuthPrompt();
+		if (authPrompt != null)
+		{
+			if (authPrompt.promptPassword(this, authId, login, title, message))
+			{
 				return sessionPasswords.get(authId);
 			}
 		}
 		return null;
 	}
 
-	/* (non-Javadoc)
+	protected IAuthenticationPrompt getAuthPrompt()
+	{
+		IAdapterManager adaptManager = getAdapterManager();
+		IAuthenticationPrompt authPrompt = (IAuthenticationPrompt) adaptManager.getAdapter(this,
+				IAuthenticationPrompt.class);
+		if (authPrompt == null && adaptManager.hasAdapter(this, IAuthenticationPrompt.class.getName()))
+		{
+			authPrompt = (IAuthenticationPrompt) adaptManager.loadAdapter(this, IAuthenticationPrompt.class.getName());
+		}
+		return authPrompt;
+	}
+
+	protected IAdapterManager getAdapterManager()
+	{
+		return Platform.getAdapterManager();
+	}
+
+	/*
+	 * (non-Javadoc)
 	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#resetPassword(java.lang.String)
 	 */
-	public void resetPassword(String authId) {
+	public void resetPassword(String authId)
+	{
 		ISecurePreferences preferences = getSecurePreferences();
-		if (preferences.nodeExists(authId)) {
+		if (preferences.nodeExists(authId))
+		{
 			ISecurePreferences node = preferences.node(authId);
 			node.removeNode();
 		}
 		sessionPasswords.remove(authId);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
 	 * @see com.aptana.ide.core.io.auth.IAuthenticationManager#setAuth(java.lang.String, char[], boolean)
 	 */
-	public void setPassword(String authId, char[] password, boolean persistent) {
-		ISecurePreferences node = getSecurePreferences().node(authId);
-		try {
+	public void setPassword(String authId, char[] password, boolean persistent)
+	{
+		ISecurePreferences root = getSecurePreferences();
+		ISecurePreferences node = root.node(authId);
+		try
+		{
 			sessionPasswords.remove(authId);
-			if (password != null) {
+			if (password != null)
+			{
 				sessionPasswords.put(authId, password);
-				if (persistent) {
+				if (persistent)
+				{
 					node.put(PROP_PASSWORD, String.copyValueOf(password), true);
-				} else {
+				}
+				else
+				{
 					node.removeNode();
 				}
 			}
-			getSecurePreferences().flush();
-		} catch (Exception e) {
+			root.flush();
+		}
+		catch (Exception e)
+		{
 			IdeLog.logWarning(CoreIOPlugin.getDefault(), Messages.AuthenticationManager_FailedSaveSecurePreference, e);
 		}
 	}
 
-	private ISecurePreferences getSecurePreferences() {
+	protected ISecurePreferences getSecurePreferences()
+	{
 		return SecurePreferencesFactory.getDefault().node(SECURITY_NODE);
 	}
 }
