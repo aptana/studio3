2c801bfd3f4d0f5277ce11b16221a4a5678bed8e
sgtcoolguy <chris.a.williams@gmail.com>
2014-08-19 10:22:23 -0400

    More hacks to fix tests. Assume that an identifier with no type found by same name is an instance. If we do find a type, if it starts with uppercase assume it's a type. If not, assume it's an instance (basically defined by an object literal). This is a very simple heurisitic which won't always be right. We should do more intensive tracking of the identifier back to it's last assignment to determine if we're referring to a type or an instance.

--- plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSContentAssistProcessor.java
+++ plugins/com.aptana.editor.js/src/com/aptana/editor/js/contentassist/JSContentAssistProcessor.java
@@ -50,6 +50,7 @@ import com.aptana.editor.js.text.JSFlexLexemeProvider;
 import com.aptana.index.core.Index;
 import com.aptana.js.core.IJSConstants;
 import com.aptana.js.core.JSLanguageConstants;
+import com.aptana.js.core.JSTypeConstants;
 import com.aptana.js.core.index.IJSIndexConstants;
 import com.aptana.js.core.index.JSIndexQueryHelper;
 import com.aptana.js.core.inferencing.JSNodeTypeInferrer;
@@ -340,7 +341,6 @@ public class JSContentAssistProcessor extends CommonContentAssistProcessor
 			if (left instanceof JSIdentifierNode)
 			{
 				// FIXME Track back to last assignment to determine better
-				// cheat and assume identifiers beginning with upper case letter are types.
 				JSIdentifierNode ident = (JSIdentifierNode) left;
 				String name = ident.getNameNode().getName();
 				if ("$".equals(name) || "Ti".equals(name) || "jQuery".equals(name)) // HACK for jQuery
@@ -351,7 +351,15 @@ public class JSContentAssistProcessor extends CommonContentAssistProcessor
 					return false;
 				}
 				Collection<TypeElement> types = getQueryHelper().getTypes(name, false);
-				return CollectionsUtil.isEmpty(types); // if there are no types by this name, assume it's an instance
+				// FIXME If the type was defined using an object literal, the actual reference is an instance! How the
+				// hell do I handle that?
+				if (CollectionsUtil.isEmpty(types))
+				{
+					// if there are no types by this name, assume it's an instance
+					return true;
+				}
+				// cheat and assume identifiers beginning with upper case letter are types?
+				return !Character.isUpperCase(name.charAt(0));
 			}
 			return true;
 		}
