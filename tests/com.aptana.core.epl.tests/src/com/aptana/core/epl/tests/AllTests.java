73e67fc5466cfb27425b62f2b6a0fbacc1422e19
Christopher Williams <chris.a.williams@gmail.com>
2017-02-14 12:55:57 -0500

    Add more tests

--- tests/com.aptana.core.epl.tests/src/com/aptana/core/epl/tests/AllTests.java
+++ tests/com.aptana.core.epl.tests/src/com/aptana/core/epl/tests/AllTests.java
@@ -11,14 +11,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.aptana.core.epl.downloader.FileReaderTest;
-import com.aptana.core.epl.downloader.ProgressStatisticsTest;
+import com.aptana.core.epl.downloader.EPLDownloaderTests;
 import com.aptana.core.epl.util.LRUCacheTest;
 import com.aptana.core.epl.util.LRUCacheWithSoftPrunedValuesTest;
 import com.aptana.core.epl.util.SoftHashMapTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ LRUCacheTest.class, LRUCacheWithSoftPrunedValuesTest.class, SoftHashMapTest.class, FileReaderTest.class, ProgressStatisticsTest.class })
+@SuiteClasses({ LRUCacheTest.class, LRUCacheWithSoftPrunedValuesTest.class, SoftHashMapTest.class,
+		EPLDownloaderTests.class })
 public class AllTests
 {
 
