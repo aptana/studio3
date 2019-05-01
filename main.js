371cb3468e46872586e9a4170a122013d1f8ee9e
Chris Williams <chris.a.williams@gmail.com>
2019-05-01 16:16:11 -0400

    chore: add node script to download and install the target platform dependencies into eclipse

---
+++ main.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+const eclipsePath = '/Applications/Eclipse.app';
+
+async function main() {
+	const targetPath = path.join(__dirname, 'releng/com.aptana.studio.target/com.aptana.studio.target.target');
+	const parser = require('xml2json');
+	const xml = fs.readFileSync(targetPath, 'utf8');
+	const json = parser.toJson(xml, { object: true });
+	const locations = json.target.locations.location;
+	// MUST RUN SERIALLY!
+	for (const l of locations) {
+		await installUnits(l);
+	}
+}
+
+async function installUnits(location) {
+	return new Promise((resolve, reject) => {
+		const units = Array.isArray(location.unit) ? location.unit : [ location.unit ]; // each has 'id', 'version'
+		const p2RepoURL = location.repository.location;
+		const ius = units.map(unit => `${unit.id}/${unit.version}`).join(',');
+		const p = spawn(`${eclipsePath}/Contents/MacOS/eclipse`, [
+			'-application', 'org.eclipse.equinox.p2.director',
+			'-repository', p2RepoURL,
+			'-installIU', ius,
+			'-nosplash'
+		]);
+		p.stderr.on('data', data => console.error(data.toString().trim()));
+		p.stdout.on('data', data => console.log(data.toString().trim()));
+		p.on('close', code => {
+			if (code !== 0) {
+				reject(code);
+			} else {
+				resolve(code);
+			}
+		});
+		p.on('error', err => reject(err));
+	});
+}
+
+main().then(() => {
+	console.log('Installed target plugins/features into Eclipse');
+	process.exit(0);
+}).catch(err => {
+	console.error(err);
+	process.exit(1);
+});
