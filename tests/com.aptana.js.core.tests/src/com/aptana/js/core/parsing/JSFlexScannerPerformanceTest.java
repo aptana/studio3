d39bdbd2eedb250b415dbfffe84431dc104101e2
Chris Williams <chris.a.williams@gmail.com>
2019-05-02 13:44:24 -0400

    test: re-arrange test plugins/fragments/classes to try and fix up build errors in Eclipse

--- tests/com.aptana.js.core.tests/src/com/aptana/js/core/parsing/JSFlexScannerPerformanceTest.java
+++ tests/com.aptana.js.core.tests/src/com/aptana/js/core/parsing/JSFlexScannerPerformanceTest.java
@@ -62,7 +62,7 @@ public class JSFlexScannerPerformanceTest extends GlobalTimePerformanceTestCase
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	@Override
-	protected void setUp() throws Exception
+	public void setUp() throws Exception
 	{
 		super.setUp();
 
--- tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingPerformanceTest.java
+++ tests/com.aptana.scripting.tests/src/com/aptana/scripting/model/BundleLoadingPerformanceTest.java
@@ -10,7 +10,7 @@ public class BundleLoadingPerformanceTest extends GlobalTimePerformanceTestCase
 	private BundleManager manager;
 
 	@Override
-	protected void setUp() throws Exception
+	public void setUp() throws Exception
 	{
 		super.setUp();
 
