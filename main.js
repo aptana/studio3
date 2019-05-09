c5ed57efa5ea71ffae27350f64e36d557dd54585
Chris Williams <chris.a.williams@gmail.com>
2019-05-09 16:11:34 -0400

    refactor: update node script for installing dependencies to support windows

--- main.js
+++ main.js
@@ -1,37 +1,49 @@
 const fs = require('fs-extra');
 const path = require('path');
 const spawn = require('child_process').spawn;
+const util = require('util');
+const os = require('os');
 
-const eclipsePath = '/Applications/Eclipse.app';
+const eclipsePath = os.platform() === 'win32' ? 'C:\\eclipse\\eclipsec.exe' : '/Applications/Eclipse.app/Contents/MacOS/eclipse';
 
 async function installEclipseDependencies() {
 	const targetPath = path.join(__dirname, 'releng/com.aptana.studio.target/com.aptana.studio.target.target');
-	const parser = require('xml2json');
+	const parser = new require('xml2js').Parser();
 	const xml = fs.readFileSync(targetPath, 'utf8');
-	const json = parser.toJson(xml, { object: true });
-	const locations = json.target.locations.location;
+	const json = await util.promisify(parser.parseString)(xml);
+	const locations = json.target.locations[0].location;
 	// Add plugins/features useful for Eclipse development
 	locations.unshift({
-		repository: {
-			location: 'http://repo1.maven.org/maven2/.m2e/connectors/m2eclipse-tycho/0.8.1/N/0.8.1.201704211436/'
-		},
+		repository: [
+			{
+				'$': {
+					location: 'http://repo1.maven.org/maven2/.m2e/connectors/m2eclipse-tycho/0.8.1/N/0.8.1.201704211436/'
+				}
+			}
+		],
 		unit: {
-			id: 'org.sonatype.tycho.m2e.feature.feature.group',
-			version: '0.8.1.201704211436'
+			'$': {
+				id: 'org.sonatype.tycho.m2e.feature.feature.group',
+				version: '0.8.1.201704211436'
+			}
 		}
 	});
 	// MUST RUN SERIALLY!
 	for (const l of locations) {
-		await installUnits(l);
+		try {
+			await installUnits(l);
+		} catch (e) {
+			console.error(e);
+		}
 	}
 }
 
 async function installUnits(location) {
 	return new Promise((resolve, reject) => {
 		const units = Array.isArray(location.unit) ? location.unit : [ location.unit ]; // each has 'id', 'version'
-		const p2RepoURL = location.repository.location;
-		const ius = units.map(unit => `${unit.id}/${unit.version}`).join(',');
-		const p = spawn(`${eclipsePath}/Contents/MacOS/eclipse`, [
+		const p2RepoURL = location.repository[0]['$'].location;
+		const ius = units.map(unit => `${unit['$'].id}/${unit['$'].version}`).join(',');
+		const p = spawn(eclipsePath, [
 			'-application', 'org.eclipse.equinox.p2.director',
 			'-repository', p2RepoURL,
 			'-installIU', ius,
