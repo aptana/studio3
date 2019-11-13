b867d3ca9db1d417bc918e0e54acf470ed8d4223
Chris Williams <chris.a.williams@gmail.com>
2019-11-13 14:03:49 -0500

    fix: make Node.JS 10.13 the new minimum version, 10.17 the version we attempt to install

--- bundles/com.aptana.js.core/src/com/aptana/js/internal/core/node/NodeJSService.java
+++ bundles/com.aptana.js.core/src/com/aptana/js/internal/core/node/NodeJSService.java
@@ -67,8 +67,8 @@ public class NodeJSService implements INodeJSService
 	private static final String NODE_EXE = "node.exe"; //$NON-NLS-1$
 	private static final String NPM_EXE = "npm.cmd"; //$NON-NLS-1$
 
-	private static final String MAC_NODE_URL = "http://go.appcelerator.com/installer_nodejs_osx_8_16_0"; //$NON-NLS-1$
-	private static final String WIN_NODE_URL = "http://go.appcelerator.com/installer_nodejs_windows_8_16_0.msi"; //$NON-NLS-1$
+	private static final String MAC_NODE_URL = "http://go.appcelerator.com/installer_nodejs_osx_10_17_0"; //$NON-NLS-1$
+	private static final String WIN_NODE_URL = "http://go.appcelerator.com/installer_nodejs_windows_x64_10_17_0.msi"; //$NON-NLS-1$
 	private static final String MAC_EXTENSION = ".pkg"; //$NON-NLS-1$
 	private static final String WIN_EXTENSION = ".msi"; //$NON-NLS-1$
 
